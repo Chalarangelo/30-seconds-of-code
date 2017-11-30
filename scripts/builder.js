0a0e993d8a57408e284bcf2581f0ad227fa203e4
Angelos Chalaris <chalarangelo@gmail.com>
2017-11-30 17:40:06 +0200

    First snippet, builder working

---
+++ scripts/builder.js
@@ -0,0 +1,38 @@
+var fs = require('fs-extra');
+var path = require('path');
+
+var snippetsPath = './snippets';
+var staticPartsPath = './static-parts';
+
+var snippets = {}, startPart = '', endPart = '', output = '';
+
+try {
+  for(var snippet of fs.readdirSync(snippetsPath)){
+    snippets[snippet] = fs.readFileSync(path.join(snippetsPath,snippet),'utf8');
+  }
+}
+catch (err){
+  console.log('Error during snippet loading: '+err);
+  process.exit(1);
+}
+
+try {
+  startPart = fs.readFileSync(path.join(staticPartsPath,'README-start.md'),'utf8');
+  endPart = fs.readFileSync(path.join(staticPartsPath,'README-end.md'),'utf8');
+}
+catch (err){
+  console.log('Error during static part loading: '+err);
+  process.exit(1);
+}
+
+try {
+  output += `${startPart+'\n'}`;
+  for(var snippet of Object.entries(snippets))
+    output += `${snippet[1]+'\n'}`;
+  output += `${endPart+'\n'}`;
+  fs.writeFileSync('README.md', output);
+}
+catch (err){
+  console.log('Error during README generation: '+err);
+  process.exit(1);
+}
