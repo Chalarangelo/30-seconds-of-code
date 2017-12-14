f8c1141b8e03b465dc395832ca846c34c0e370a0
Leandro Franciscato <leandro@ingaflex.com.br>
2017-12-14 09:38:10 -0200

    Testing auto deploy

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "leandro_franciscato@hotmail.com"
+  git config --global user.name "Leandro Franciscato"
+}
+
+commit_website_files() {  
+  git add .
+  git commit --message "Travis build"
+}
+
+upload_files() {
+  git remote add master https://${token}@github.com/LeandroFranciscato/30-seconds-of-code.git > /dev/null 2>&1
+  git push --quiet
+}
+
+setup_git
+commit_website_files
+upload_files
+
