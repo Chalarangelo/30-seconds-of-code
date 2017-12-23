0721ebd37517d3341b06df3265497bed10d3585d
Stefan Feješ <ns.fejes.stefan@gmail.com>
2017-12-23 16:00:07 +0100

    add copy to clipboard functionality

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,11 +1,20 @@
-var s = document.querySelectorAll("pre");
-s.forEach(element => {
-    var button = document.createElement("button");
+var pres = document.querySelectorAll("pre");
+pres.forEach(element => {
+    const button = document.createElement("button");
     button.innerHTML = "Copy to clipboard";
     element.parentElement.appendChild(button);
 
     button.addEventListener ("click", function() {
-        var text = element.textContent.replace(/\/\*(.|[\r\n])*?\*\//g, '').replace(/\/\/.*/gm, '');
-        console.log(text);
+        //The following regex removes all the comments from the snippet
+        const text = element.textContent.replace(/\/\*(.|[\r\n])*?\*\//g, '').replace(/\/\/.*/gm, '');
+        // Apparently you can copy variable to clipboard so you need to create text input element,
+        // give it a value, copy it and then remove it
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("Copy");
+        document.body.removeChild(textArea);
+        console.log(`copied: ${text}`);
     });
 });
