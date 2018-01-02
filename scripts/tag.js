afff058e0c76fd06a5740d4d9e4db984689b8936
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-02 21:23:56 +0200

    Fixed scripts

--- scripts/tag.js
+++ scripts/tag.js
@@ -8,7 +8,7 @@ const fs = require('fs-extra'),
   chalk = require('chalk');
 // Load helper functions (these are from existing snippets in 30 seconds of code!)
 const isTravisCI = () => 'TRAVIS' in process.env && 'CI' in process.env;
-if(isTravisCI() && (/^Travis build: \d+/g.test(process.env['TRAVIS_COMMIT_MESSAGE'])) {
+if(isTravisCI() && /^Travis build: \d+/g.test(process.env['TRAVIS_COMMIT_MESSAGE'])) {
   console.log(`${chalk.green('NOBUILD')} Tagging terminated, parent commit is a Travis build!`);
   process.exit(0);
 }
