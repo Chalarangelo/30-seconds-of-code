e0ad58d40bf93cf356c3c715f0823f204e69acaa
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-03 16:21:08 +0200

    Added travis checking

--- scripts/tdd.js
+++ scripts/tdd.js
@@ -5,7 +5,13 @@
 
 // Load modules
 const fs = require('fs-extra');
-
+const chalk = require('chalk');
+// Load helper functions (these are from existing snippets in 30 seconds of code!)
+const isTravisCI = () => 'TRAVIS' in process.env && 'CI' in process.env;
+if(isTravisCI() && process.env['TRAVIS_EVENT_TYPE'] !== 'cron' && process.env['TRAVIS_EVENT_TYPE'] !== 'api') {
+  console.log(`${chalk.green('NOBUILD')} Testing terminated, not a cron job or a custom build!`);
+  process.exit(0);
+}
 // Declare paths
 const SNIPPETS_PATH = './snippets';
 const TEST_PATH = './test';
