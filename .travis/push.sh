38545fbf3360acde500f5eecd47173f31938c14c
Rohit Tanwar <31792358+kriadmin@users.noreply.github.com>
2018-01-09 05:03:11 +0530

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,18 +1,32 @@
-#!/bin/sh
-
 setup_git() {
   git config --global user.email "mst10041967@gmail.com"
   git config --global user.name "Rohit Tanwar"
 }
 
 commit_website_files() {
-  git add -A
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
+    if [ $TRAVIS_BRANCH == "master" ]; then
+      echo "Committing to master branch..."
+      git checkout master
+      git add *
+      if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [cron]"
+      elif [ $TRAVIS_EVENT_TYPE == "api" ]; then
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [custom]"
+      else
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+      fi
+    fi
+  fi
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/kriadmin/30-seconds-of-python-code.git
-  git push --quiet --set-upstream origin master
+  if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
+    if [ $TRAVIS_BRANCH == "master" ]; then
+      echo "Pushing to master branch..."
+      git push --force --quiet "https://${GH_TOKEN}@github.com/kriadmin/30-seconds-of-python-code.git" master > /dev/null 2>&1
+    fi
+  fi
 }
 
 setup_git
