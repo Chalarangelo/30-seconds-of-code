5207132ff5d809f9e8133edbfcbd744947c37681
King <kingdavidmartins@users.noreply.github.com>
2018-01-09 06:19:22 -0500

    add test for average -> average(1, 9) equals 5

--- test/average/average.test.js
+++ test/average/average.test.js
@@ -4,7 +4,8 @@ const average = require('./average.js');
 test('Testing average', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
-	t.true(typeof average === 'function', 'average is a Function');
+  t.true(typeof average === 'function', 'average is a Function');
+  t.equal(average(9, 1), 5, 'The average of 1 & 9 is 5');
 	//t.deepEqual(average(args..), 'Expected');
 	//t.equal(average(args..), 'Expected');
 	//t.false(average(args..), 'Expected');
