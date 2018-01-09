9645d4212b3f2562a0b547560be093d92c1d7f5e
Rohit Tanwar <31792358+kriadmin@users.noreply.github.com>
2018-01-09 10:48:49 +0530

    Update readme.py

--- scripts/readme.py
+++ scripts/readme.py
@@ -1,5 +1,6 @@
 import os
 files = os.listdir('snippets')
+print(os.listdir())
 start = open("static-parts/readme-start.md").read() + '\n\n'
 end = open("static-parts/readme-end.md").read()
 toAppend = ''
