51b6ff4b9c70657efd91465e224d6a6c005c36f9
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:36:16 +0100

    add 2 tests, differenceWith & digitize

--- test/differenceWith/differenceWith.test.js
+++ test/differenceWith/differenceWith.test.js
@@ -5,6 +5,7 @@ test('Testing differenceWith', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof differenceWith === 'function', 'differenceWith is a Function');
+	t.deepEqual(differenceWith([1, 1.2, 1.5, 3, 0], [1.9, 3, 0], (a, b) => Math.round(a) === Math.round(b)), [1, 1.2], "Filters out all values from an array");
 	//t.deepEqual(differenceWith(args..), 'Expected');
 	//t.equal(differenceWith(args..), 'Expected');
 	//t.false(differenceWith(args..), 'Expected');
