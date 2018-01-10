195b96fb377407d376867bb720fe9646814a7987
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 13:12:17 +0100

    add 2 tests, sortCharactersInString & sortedIndex

--- test/sortedIndex/sortedIndex.test.js
+++ test/sortedIndex/sortedIndex.test.js
@@ -5,6 +5,8 @@ test('Testing sortedIndex', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof sortedIndex === 'function', 'sortedIndex is a Function');
+	t.equal(sortedIndex([5, 3, 2, 1], 4), 1, "Returns the lowest index at which value should be inserted into array in order to maintain its sort order.");
+	t.equal(sortedIndex([30, 50], 40), 1, "Returns the lowest index at which value should be inserted into array in order to maintain its sort order.");
 	//t.deepEqual(sortedIndex(args..), 'Expected');
 	//t.equal(sortedIndex(args..), 'Expected');
 	//t.false(sortedIndex(args..), 'Expected');
