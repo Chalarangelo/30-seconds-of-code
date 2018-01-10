d96a6567d8fa07faaa0e736ebf52fb485a7750cf
King <kingdavidmartins@users.noreply.github.com>
2018-01-10 17:25:04 -0500

    add 2 basic test for pullAtIndex

--- test/pullAtIndex/pullAtIndex.test.js
+++ test/pullAtIndex/pullAtIndex.test.js
@@ -5,6 +5,16 @@ test('Testing pullAtIndex', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof pullAtIndex === 'function', 'pullAtIndex is a Function');
+
+	let myArray1 = ['a', 'b', 'c', 'd'];
+	pullAtIndex(myArray1, [1, 3]);
+	t.deepEqual(myArray1, [ 'a', 'c' ], 'b & d was pulled from myArray1');
+
+	let myArray2 = ['a', 'b', 'c', 'd'];
+	pullAtIndex(myArray2, [0, 2]);
+	t.deepEqual(myArray2, [ 'b', 'd' ], 'a & c was pulled from myArray2');
+
+	//t.deepEqual(pullAtIndex(args..), 'Expected');
 	//t.deepEqual(pullAtIndex(args..), 'Expected');
 	//t.equal(pullAtIndex(args..), 'Expected');
 	//t.false(pullAtIndex(args..), 'Expected');
