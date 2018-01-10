0c98c5476eaaddecad8aec4669da33afd80c4c89
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 14:59:07 +0100

    add 2 tests, lcm & longestItem

--- test/lcm/lcm.test.js
+++ test/lcm/lcm.test.js
@@ -5,6 +5,9 @@ test('Testing lcm', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof lcm === 'function', 'lcm is a Function');
+	t.equal(lcm(12, 7), 84, "Returns the least common multiple of two or more numbers.");
+	t.equal(lcm(...[1, 3, 4, 5]), 60, "Returns the least common multiple of two or more numbers.");
+
 	//t.deepEqual(lcm(args..), 'Expected');
 	//t.equal(lcm(args..), 'Expected');
 	//t.false(lcm(args..), 'Expected');
