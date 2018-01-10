4262a8c8ed3b467b2b30667542fcbfcfe6e71488
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 12:58:10 +0100

    add 2 tests for sum and sumPower

--- test/sum/sum.test.js
+++ test/sum/sum.test.js
@@ -5,6 +5,7 @@ test('Testing sum', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof sum === 'function', 'sum is a Function');
+	t.equal(sum(...[1, 2, 3, 4]), 10, "Returns the sum of two or more numbers/arrays.");
 	//t.deepEqual(sum(args..), 'Expected');
 	//t.equal(sum(args..), 'Expected');
 	//t.false(sum(args..), 'Expected');
