276557cae5b035b7225889be6f49123ac5b48e63
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:20:26 +0100

    add 2 tests for everyNth and extendHex
    typo factorial test

--- test/extendHex/extendHex.test.js
+++ test/extendHex/extendHex.test.js
@@ -5,6 +5,8 @@ test('Testing extendHex', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof extendHex === 'function', 'extendHex is a Function');
+	t.equal(extendHex('#03f'), '#0033ff', "Extends a 3-digit color code to a 6-digit color code");
+	t.equal(extendHex('05a'), '#0055aa', "Extends a 3-digit color code to a 6-digit color code");
 	//t.deepEqual(extendHex(args..), 'Expected');
 	//t.equal(extendHex(args..), 'Expected');
 	//t.false(extendHex(args..), 'Expected');
