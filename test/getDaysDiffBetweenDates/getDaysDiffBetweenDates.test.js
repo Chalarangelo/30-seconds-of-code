ae140c308d67ea010559f5e38e472de15dfb8124
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 16:31:49 +0100

    add 2 tests, getDaysDiffBetweenDates, getType

--- test/getDaysDiffBetweenDates/getDaysDiffBetweenDates.test.js
+++ test/getDaysDiffBetweenDates/getDaysDiffBetweenDates.test.js
@@ -5,6 +5,7 @@ test('Testing getDaysDiffBetweenDates', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof getDaysDiffBetweenDates === 'function', 'getDaysDiffBetweenDates is a Function');
+	t.equal(getDaysDiffBetweenDates(new Date('2017-12-13'), new Date('2017-12-22')), 9, "Returns the difference in days between two dates");
 	//t.deepEqual(getDaysDiffBetweenDates(args..), 'Expected');
 	//t.equal(getDaysDiffBetweenDates(args..), 'Expected');
 	//t.false(getDaysDiffBetweenDates(args..), 'Expected');
