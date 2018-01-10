4cfa5b7636404fa911f583b2253691ede51cbc8f
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:30:31 +0100

    add 2 tests for elo & dropRight

--- test/dropRight/dropRight.test.js
+++ test/dropRight/dropRight.test.js
@@ -5,6 +5,9 @@ test('Testing dropRight', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof dropRight === 'function', 'dropRight is a Function');
+	t.deepEqual(dropRight([1, 2, 3]), [1,2], "Returns a new array with n elements removed from the right");
+	t.deepEqual(dropRight([1, 2, 3], 2), [1], "Returns a new array with n elements removed from the right");
+	t.deepEqual(dropRight([1, 2, 3], 42), [], "Returns a new array with n elements removed from the right");
 	//t.deepEqual(dropRight(args..), 'Expected');
 	//t.equal(dropRight(args..), 'Expected');
 	//t.false(dropRight(args..), 'Expected');
