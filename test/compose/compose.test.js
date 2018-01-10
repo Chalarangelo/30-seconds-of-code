f4b3a062db612db53534768cfd8bb73c627cb1f1
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:44:26 +0100

    add 2 tests, compose and countOccurences

--- test/compose/compose.test.js
+++ test/compose/compose.test.js
@@ -5,6 +5,10 @@ test('Testing compose', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof compose === 'function', 'compose is a Function');
+	const add5 = x => x + 5;
+	const multiply = (x, y) => x * y;
+	const multiplyAndAdd5 = compose(add5, multiply);
+	t.equal(multiplyAndAdd5(5, 2), 15, "Performs right-to-left function composition");
 	//t.deepEqual(compose(args..), 'Expected');
 	//t.equal(compose(args..), 'Expected');
 	//t.false(compose(args..), 'Expected');
