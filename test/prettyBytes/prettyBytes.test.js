4a9ed1bf1e68835b6e10d54b64d16381b46b66dd
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 14:01:35 +0100

    add 3 tests,  prettyBytes, primes & reducedFilter
    updated prettyBytes example to output string

--- test/prettyBytes/prettyBytes.test.js
+++ test/prettyBytes/prettyBytes.test.js
@@ -5,6 +5,10 @@ test('Testing prettyBytes', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof prettyBytes === 'function', 'prettyBytes is a Function');
+	t.equal(prettyBytes(1000), '1 KB', "Converts a number in bytes to a human-readable string.");
+	t.equal(prettyBytes(-27145424323.5821, 5), '-27.145 GB', "Converts a number in bytes to a human-readable string.");
+	t.equal(prettyBytes(123456789, 3, false), '123MB', "Converts a number in bytes to a human-readable string.");
+
 	//t.deepEqual(prettyBytes(args..), 'Expected');
 	//t.equal(prettyBytes(args..), 'Expected');
 	//t.false(prettyBytes(args..), 'Expected');
