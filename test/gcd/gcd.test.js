1c791e42fcc084ba938f502cfce36643f7a9287d
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 16:55:01 +0100

    fix gcd and add test for fromCamelCase

--- test/gcd/gcd.test.js
+++ test/gcd/gcd.test.js
@@ -6,7 +6,8 @@ test('Testing gcd', (t) => {
 	//Please go to https://github.com/substack/tape
 	t.true(typeof gcd === 'function', 'gcd is a Function');
 	t.equal(gcd(8, 36), 4, "Calculates the greatest common divisor between two or more numbers/arrays");
-	t.equal(gcd(...[12, 8, 32]), 4, "Calculates the greatest common divisor between two or more numbers/arrays");	//t.deepEqual(gcd(args..), 'Expected');
+	t.deepEqual(gcd(...[12, 8, 32]), 4, "Calculates the greatest common divisor between two or more numbers/arrays");
+	//t.deepEqual(gcd(args..), 'Expected');
 	//t.equal(gcd(args..), 'Expected');
 	//t.false(gcd(args..), 'Expected');
 	//t.throws(gcd(args..), 'Expected');
