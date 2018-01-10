f5bd5e717ed7d60c570424e2f0f8ea7bc469c721
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 08:28:58 +0100

    add test for isDivisible

--- test/isDivisible/isDivisible.test.js
+++ test/isDivisible/isDivisible.test.js
@@ -5,8 +5,8 @@ test('Testing isDivisible', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof isDivisible === 'function', 'isDivisible is a Function');
+	t.equal(isDivisible(6, 3), true, 'The number 6 is divisible by 3');
 	//t.deepEqual(isDivisible(args..), 'Expected');
-	//t.equal(isDivisible(args..), 'Expected');
 	//t.false(isDivisible(args..), 'Expected');
 	//t.throws(isDivisible(args..), 'Expected');
 	t.end();
