bfc43327b8d4790c9e6266a39ce50cff919c8fe1
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 15:19:09 +0100

    update description for isPrimitive, add 2 tests, isPrimitive & isPrime

--- test/isPrimitive/isPrimitive.test.js
+++ test/isPrimitive/isPrimitive.test.js
@@ -5,6 +5,14 @@ test('Testing isPrimitive', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof isPrimitive === 'function', 'isPrimitive is a Function');
+	t.equal(isPrimitive(null), true, "passed value is primitive");
+	t.equal(isPrimitive(50), true, "passed value is primitive");
+	t.equal(isPrimitive('Hello'), true, "passed value is primitive");
+	t.equal(isPrimitive(false), true, "passed value is primitive");
+	t.equal(isPrimitive(Symbol()), true, "passed value is primitive");
+	t.equal(isPrimitive([]), false, "passed value is primitive");
+
+
 	//t.deepEqual(isPrimitive(args..), 'Expected');
 	//t.equal(isPrimitive(args..), 'Expected');
 	//t.false(isPrimitive(args..), 'Expected');
