6434e7dcd93b27b4918789201b2fb0b8017dc3d0
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 15:30:16 +0100

    add 2 tests, isNull & isNumber

--- test/isNull/isNull.test.js
+++ test/isNull/isNull.test.js
@@ -5,6 +5,8 @@ test('Testing isNull', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof isNull === 'function', 'isNull is a Function');
+	t.equal(isNull(null), true, "passed argument is a null");
+	t.equal(isNull(NaN), false, "passed argument is a null");
 	//t.deepEqual(isNull(args..), 'Expected');
 	//t.equal(isNull(args..), 'Expected');
 	//t.false(isNull(args..), 'Expected');
