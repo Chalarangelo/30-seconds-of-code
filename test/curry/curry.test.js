f9ac0526af6ff0cce8fe8f347860e7bec03ecc51
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:41:36 +0100

    added 2 test, 2 broken snippets due to recursive issue curry & deepFlatten

--- test/curry/curry.test.js
+++ test/curry/curry.test.js
@@ -5,6 +5,8 @@ test('Testing curry', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof curry === 'function', 'curry is a Function');
+	//BROKEN
+	// t.equal(curry(Math.pow)(2)(10), 1024, "Curries a function");
 	//t.deepEqual(curry(args..), 'Expected');
 	//t.equal(curry(args..), 'Expected');
 	//t.false(curry(args..), 'Expected');
