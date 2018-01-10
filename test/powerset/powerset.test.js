d26813003760b12f52916b1efe5e60bb9fe2d412
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 14:05:00 +0100

    add 2 tests, pick & powerset

--- test/powerset/powerset.test.js
+++ test/powerset/powerset.test.js
@@ -5,6 +5,7 @@ test('Testing powerset', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof powerset === 'function', 'powerset is a Function');
+	t.deepEqual(powerset([1, 2]), [[], [1], [2], [2,1]], "Returns the powerset of a given array of numbers.");
 	//t.deepEqual(powerset(args..), 'Expected');
 	//t.equal(powerset(args..), 'Expected');
 	//t.false(powerset(args..), 'Expected');
