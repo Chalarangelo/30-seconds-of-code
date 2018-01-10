a234ca519e4ee3505fdc14fd404a1c540dbfb789
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 18:02:22 +0100

    add 2 tests, byteSize & capitalize

--- test/byteSize/byteSize.test.js
+++ test/byteSize/byteSize.test.js
@@ -5,6 +5,8 @@ test('Testing byteSize', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof byteSize === 'function', 'byteSize is a Function');
+	// Works only in browser
+	// t.equal(byteSize('Hello World'), 11, "Returns the length of a string in bytes");
 	//t.deepEqual(byteSize(args..), 'Expected');
 	//t.equal(byteSize(args..), 'Expected');
 	//t.false(byteSize(args..), 'Expected');
