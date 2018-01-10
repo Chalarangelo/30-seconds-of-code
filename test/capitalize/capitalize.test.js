e69f44140456f82723c4ef51b42b87efcfce84ae
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 18:02:22 +0100

    add 2 tests, byteSize & capitalize

--- test/capitalize/capitalize.test.js
+++ test/capitalize/capitalize.test.js
@@ -5,6 +5,8 @@ test('Testing capitalize', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof capitalize === 'function', 'capitalize is a Function');
+	t.equal(capitalize('fooBar'), 'FooBar', "Capitalizes the first letter of a string");
+	t.equal(capitalize('fooBar', true), 'Foobar', "Capitalizes the first letter of a string");
 	//t.deepEqual(capitalize(args..), 'Expected');
 	//t.equal(capitalize(args..), 'Expected');
 	//t.false(capitalize(args..), 'Expected');
