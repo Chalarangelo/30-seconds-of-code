06b7fcc0083d0f8a21018951739e7b521967a353
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:38:30 +0100

    add 2 tests, deepFlatten & difference

--- test/deepFlatten/deepFlatten.test.js
+++ test/deepFlatten/deepFlatten.test.js
@@ -5,6 +5,7 @@ test('Testing deepFlatten', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof deepFlatten === 'function', 'deepFlatten is a Function');
+	t.deepEqual(deepFlatten([1, [2], [[3], 4], 5]), [1, 2, 3, 4, 5], "Deep flattens an array");
 	//t.deepEqual(deepFlatten(args..), 'Expected');
 	//t.equal(deepFlatten(args..), 'Expected');
 	//t.false(deepFlatten(args..), 'Expected');
