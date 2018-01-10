9edfbcd690d7f383941a5b565306cbd6c258bce1
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 15:37:38 +0100

    add 2 tests, isArray & isBoolean

--- test/isArray/isArray.test.js
+++ test/isArray/isArray.test.js
@@ -5,7 +5,8 @@ test('Testing isArray', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof isArray === 'function', 'isArray is a Function');
-	//t.deepEqual(isArray(args..), 'Expected');
+	t.equal(isArray([1]), true, "passed value is an array");
+	t.equal(isArray('array'), false, "passed value is not an array");
 	//t.equal(isArray(args..), 'Expected');
 	//t.false(isArray(args..), 'Expected');
 	//t.throws(isArray(args..), 'Expected');
