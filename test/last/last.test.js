936c71478bc59239079a9973ad2b5a1404d65313
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 15:04:00 +0100

    add 2 tests for join and last

--- test/last/last.test.js
+++ test/last/last.test.js
@@ -5,6 +5,7 @@ test('Testing last', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof last === 'function', 'last is a Function');
+	t.equal(last([1, 2, 3]), 3, "Returns the last element in an array");
 	//t.deepEqual(last(args..), 'Expected');
 	//t.equal(last(args..), 'Expected');
 	//t.false(last(args..), 'Expected');
