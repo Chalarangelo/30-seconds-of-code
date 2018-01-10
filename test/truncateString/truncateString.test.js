5c24a029583a24a154df0707fb4c0f0ffcb35e69
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 11:13:32 +0100

    add test for truncateString

--- test/truncateString/truncateString.test.js
+++ test/truncateString/truncateString.test.js
@@ -5,7 +5,7 @@ test('Testing truncateString', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof truncateString === 'function', 'truncateString is a Function');
-	//t.deepEqual(truncateString(args..), 'Expected');
+	t.equal(truncateString('boomerang', 7), 'boom...', 'Truncates a "boomerang" up to a specified length.');
 	//t.equal(truncateString(args..), 'Expected');
 	//t.false(truncateString(args..), 'Expected');
 	//t.throws(truncateString(args..), 'Expected');
