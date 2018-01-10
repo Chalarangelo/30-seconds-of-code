40b0a1fa68b1f41ca4701f5d8e705aaa4393bb73
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 16:19:30 +0100

    fix initial test, add 2 tests, hextoRGB & indexOfAll

--- test/initial/initial.test.js
+++ test/initial/initial.test.js
@@ -5,7 +5,7 @@ test('Testing initial', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof initial === 'function', 'initial is a Function');
-	t.deepEqual(initial([1, 2, 3]), [2, 3], "Returns all the elements of an array except the last one");
+	t.deepEqual(initial([1, 2, 3]), [1, 2], "Returns all the elements of an array except the last one");
 	//t.deepEqual(initial(args..), 'Expected');
 	//t.equal(initial(args..), 'Expected');
 	//t.false(initial(args..), 'Expected');
