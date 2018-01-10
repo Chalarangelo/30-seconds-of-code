6ded0be9ff48691fdd7eeafed6b9cb0f1f342549
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 14:30:17 +0100

    add 2 tests, nthElement & objectFromPairs

--- test/nthElement/nthElement.test.js
+++ test/nthElement/nthElement.test.js
@@ -5,6 +5,8 @@ test('Testing nthElement', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof nthElement === 'function', 'nthElement is a Function');
+	t.equal(nthElement(['a', 'b', 'c'], 1), 'b', "Returns the nth element of an array.");
+	t.equal(nthElement(['a', 'b', 'c'], -3), 'a', "Returns the nth element of an array.");
 	//t.deepEqual(nthElement(args..), 'Expected');
 	//t.equal(nthElement(args..), 'Expected');
 	//t.false(nthElement(args..), 'Expected');
