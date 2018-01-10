9f74333559e340ce32d5fdd3f63b544f6fada418
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 15:41:24 +0100

    add 2 tests, invertKeyValues

--- test/isAbsoluteURL/isAbsoluteURL.test.js
+++ test/isAbsoluteURL/isAbsoluteURL.test.js
@@ -5,6 +5,9 @@ test('Testing isAbsoluteURL', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof isAbsoluteURL === 'function', 'isAbsoluteURL is a Function');
+	t.equal(isAbsoluteURL('https://google.com'), true, "Given string is an absolute URL");
+	t.equal(isAbsoluteURL('ftp://www.myserver.net'), true, "Given string is an absolute URL");
+	t.equal(isAbsoluteURL('/foo/bar'), false, "Given string is not an absolute URL");
 	//t.deepEqual(isAbsoluteURL(args..), 'Expected');
 	//t.equal(isAbsoluteURL(args..), 'Expected');
 	//t.false(isAbsoluteURL(args..), 'Expected');
