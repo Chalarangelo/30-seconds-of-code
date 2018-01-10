e86c1d89be2b8dfd227892d28ba729f67b730e7a
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 13:46:19 +0100

    add 2 tests for remove and reverseString

--- test/remove/remove.test.js
+++ test/remove/remove.test.js
@@ -5,6 +5,7 @@ test('Testing remove', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof remove === 'function', 'remove is a Function');
+	t.deepEqual(remove([1, 2, 3, 4], n => n % 2 == 0), [2, 4], "Removes elements from an array for which the given function returns false");
 	//t.deepEqual(remove(args..), 'Expected');
 	//t.equal(remove(args..), 'Expected');
 	//t.false(remove(args..), 'Expected');
