a5d14af485c845d50ead13e8e0d64c2a5717b9e6
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:17:57 +0100

    add 2 tests, factorial and fibonacci
    NOTE: factorial doesn't work, see the issues tab for more info

--- test/fibonacci/fibonacci.test.js
+++ test/fibonacci/fibonacci.test.js
@@ -5,6 +5,7 @@ test('Testing fibonacci', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof fibonacci === 'function', 'fibonacci is a Function');
+	t.deepEqual(fibonacci(6), [0, 1, 1, 2, 3, 5], "Generates an array, containing the Fibonacci sequence");
 	//t.deepEqual(fibonacci(args..), 'Expected');
 	//t.equal(fibonacci(args..), 'Expected');
 	//t.false(fibonacci(args..), 'Expected');
