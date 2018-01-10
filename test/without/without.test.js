b5be6b42f39270f9d9df9999d692a8a0992d4b80
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 12:07:03 +0100

    add 2 tests for union & without

--- test/without/without.test.js
+++ test/without/without.test.js
@@ -5,6 +5,7 @@ test('Testing without', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof without === 'function', 'without is a Function');
+	t.deepEqual(without([2, 1, 2, 3], 1, 2), [3], "Filters out the elements of an array, that have one of the specified values.");
 	//t.deepEqual(without(args..), 'Expected');
 	//t.equal(without(args..), 'Expected');
 	//t.false(without(args..), 'Expected');
