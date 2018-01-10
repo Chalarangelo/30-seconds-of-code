442717c7ed866e22669c28d65ad5a0a338b233f0
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 16:31:49 +0100

    add 2 tests, getDaysDiffBetweenDates, getType

--- test/getType/getType.test.js
+++ test/getType/getType.test.js
@@ -5,6 +5,7 @@ test('Testing getType', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof getType === 'function', 'getType is a Function');
+	t.equal(getType(new Set([1, 2, 3])), 'set', "Returns the native type of a value");
 	//t.deepEqual(getType(args..), 'Expected');
 	//t.equal(getType(args..), 'Expected');
 	//t.false(getType(args..), 'Expected');
