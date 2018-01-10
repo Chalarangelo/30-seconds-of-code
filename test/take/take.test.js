19223d7b55d693705071c5e0a156f4cc20c3898f
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 12:47:57 +0100

    add tests for take and takeRight

--- test/take/take.test.js
+++ test/take/take.test.js
@@ -5,6 +5,8 @@ test('Testing take', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof take === 'function', 'take is a Function');
+	t.deepEqual(take([1, 2, 3], 5), [1, 2, 3], "Returns an array with n elements removed from the beginning.")
+	t.deepEqual(take([1, 2, 3], 0), [], "Returns an array with n elements removed from the beginning.")
 	//t.deepEqual(take(args..), 'Expected');
 	//t.equal(take(args..), 'Expected');
 	//t.false(take(args..), 'Expected');
