2a3cf8895058a59c6a00c4c3d36b4920873f448c
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:57:21 +0100

    add 2 tests, capitalizeEveryWord & chunk

--- test/chunk/chunk.test.js
+++ test/chunk/chunk.test.js
@@ -5,6 +5,7 @@ test('Testing chunk', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof chunk === 'function', 'chunk is a Function');
+	t.deepEqual(chunk([1, 2, 3, 4, 5], 2), [[1,2],[3,4],[5]], "Chunks an array into smaller arrays of a specified size");
 	//t.deepEqual(chunk(args..), 'Expected');
 	//t.equal(chunk(args..), 'Expected');
 	//t.false(chunk(args..), 'Expected');
