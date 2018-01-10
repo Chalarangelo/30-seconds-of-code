64a1d2612fbc2415fa1ea41d18ca6349c43e5c12
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 15:41:24 +0100

    add 2 tests, invertKeyValues

--- test/invertKeyValues/invertKeyValues.test.js
+++ test/invertKeyValues/invertKeyValues.test.js
@@ -5,6 +5,7 @@ test('Testing invertKeyValues', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof invertKeyValues === 'function', 'invertKeyValues is a Function');
+	t.deepEqual(invertKeyValues({ name: 'John', age: 20 }), { 20: 'age', John: 'name' }, "Inverts the key-value pairs of an object");
 	//t.deepEqual(invertKeyValues(args..), 'Expected');
 	//t.equal(invertKeyValues(args..), 'Expected');
 	//t.false(invertKeyValues(args..), 'Expected');
