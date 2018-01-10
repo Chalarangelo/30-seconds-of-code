d9ea128fe574bca00268cbc8933eeff0d1bf6758
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 14:33:52 +0100

    add 2 tests, minN & negate

--- test/minN/minN.test.js
+++ test/minN/minN.test.js
@@ -5,7 +5,9 @@ test('Testing minN', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof minN === 'function', 'minN is a Function');
-	//t.deepEqual(minN(args..), 'Expected');
+	t.deepEqual(minN([1, 2, 3]), [1], "Returns the n minimum elements from the provided array");
+	t.deepEqual(minN([1, 2, 3], 2), [1, 2], "Returns the n minimum elements from the provided array");
+
 	//t.equal(minN(args..), 'Expected');
 	//t.false(minN(args..), 'Expected');
 	//t.throws(minN(args..), 'Expected');
