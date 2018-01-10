9d7a1d674c65b3578541d18c1303f69bee24246d
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 11:34:15 +0100

    add 2 tests, toOrdinalSuffix, toSafeInteger

--- test/toOrdinalSuffix/toOrdinalSuffix.test.js
+++ test/toOrdinalSuffix/toOrdinalSuffix.test.js
@@ -5,6 +5,11 @@ test('Testing toOrdinalSuffix', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof toOrdinalSuffix === 'function', 'toOrdinalSuffix is a Function');
+	t.equal(toOrdinalSuffix('123'), '123rd', 'Adds an ordinal suffix to a number');
+	t.equal(toOrdinalSuffix(5), '5th', 'Adds an ordinal suffix to a number');
+	t.equal(toOrdinalSuffix(1), '1st', 'Adds an ordinal suffix to a number');
+	t.equal(toOrdinalSuffix(0), '0th', 'Adds an ordinal suffix to a number');
+	// t.equal(toOrdinalSuffix(), ,);
 	//t.deepEqual(toOrdinalSuffix(args..), 'Expected');
 	//t.equal(toOrdinalSuffix(args..), 'Expected');
 	//t.false(toOrdinalSuffix(args..), 'Expected');
