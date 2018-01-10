e59c66676c41d535dc89ca44571edd900d60652e
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 16:45:55 +0100

    add 2 tests, gcd & geometricProgression

--- test/gcd/gcd.test.js
+++ test/gcd/gcd.test.js
@@ -5,7 +5,8 @@ test('Testing gcd', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof gcd === 'function', 'gcd is a Function');
-	//t.deepEqual(gcd(args..), 'Expected');
+	t.equal(gcd(8, 36), 4, "Calculates the greatest common divisor between two or more numbers/arrays");
+	t.equal(gcd(...[12, 8, 32]), 4, "Calculates the greatest common divisor between two or more numbers/arrays");	//t.deepEqual(gcd(args..), 'Expected');
 	//t.equal(gcd(args..), 'Expected');
 	//t.false(gcd(args..), 'Expected');
 	//t.throws(gcd(args..), 'Expected');
