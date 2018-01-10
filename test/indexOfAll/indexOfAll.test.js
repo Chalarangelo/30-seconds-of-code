1ed62476fbc66da151e615ac706826f1007978d3
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 16:19:30 +0100

    fix initial test, add 2 tests, hextoRGB & indexOfAll

--- test/indexOfAll/indexOfAll.test.js
+++ test/indexOfAll/indexOfAll.test.js
@@ -5,6 +5,8 @@ test('Testing indexOfAll', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof indexOfAll === 'function', 'indexOfAll is a Function');
+	t.deepEqual(indexOfAll([1, 2, 3, 1, 2, 3], 1), [0,3], "Returns all indices of val in an array");
+	t.deepEqual(indexOfAll([1, 2, 3], 4), [], "Returns all indices of val in an array");
 	//t.deepEqual(indexOfAll(args..), 'Expected');
 	//t.equal(indexOfAll(args..), 'Expected');
 	//t.false(indexOfAll(args..), 'Expected');
