adeec058c98375451bfab53361c12d19002a15fc
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 14:24:45 +0100

    add 2 tests orderBy and palindrome

--- test/palindrome/palindrome.test.js
+++ test/palindrome/palindrome.test.js
@@ -5,6 +5,8 @@ test('Testing palindrome', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof palindrome === 'function', 'palindrome is a Function');
+	t.equal(palindrome('taco cat'), true, "Given string is a palindrome");
+	t.equal(palindrome('foobar'), false, "Given string is not a palindrome");
 	//t.deepEqual(palindrome(args..), 'Expected');
 	//t.equal(palindrome(args..), 'Expected');
 	//t.false(palindrome(args..), 'Expected');
