ef151f3ff7f3fe988f61fd1c3cc4643260304424
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-10 17:44:26 +0100

    add 2 tests, compose and countOccurences

--- test/countOccurrences/countOccurrences.test.js
+++ test/countOccurrences/countOccurrences.test.js
@@ -5,6 +5,7 @@ test('Testing countOccurrences', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof countOccurrences === 'function', 'countOccurrences is a Function');
+	t.deepEqual(countOccurrences([1, 1, 2, 1, 2, 3], 1), 3, "Counts the occurrences of a value in an array");
 	//t.deepEqual(countOccurrences(args..), 'Expected');
 	//t.equal(countOccurrences(args..), 'Expected');
 	//t.false(countOccurrences(args..), 'Expected');
