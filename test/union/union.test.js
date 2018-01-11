457e3cc53d4e80de734603042295e183fcf4dd0e
King <kingdavidmartins@users.noreply.github.com>
2018-01-11 04:52:28 -0500

    add 2 type test union

--- test/union/union.test.js
+++ test/union/union.test.js
@@ -6,7 +6,11 @@ test('Testing union', (t) => {
 	//Please go to https://github.com/substack/tape
 	t.true(typeof union === 'function', 'union is a Function');
 	t.deepEqual(union([1, 2, 3], [4, 3, 2]), [1, 2, 3, 4], "Returns every element that exists in any of the two arrays once");
-	//t.deepEqual(union(args..), 'Expected');
+
+	t.true(Array.isArray(union([1, 2, 3], [4, 3, 2])), "union returns an array");
+
+	const isObjectHard = (obj) => obj === Object(obj) && Object.prototype.toString.call(obj) !== '[object Array]';
+  t.true(!isObjectHard(union([1, 2, 3], [4, 3, 2])), 'union always returns an array & never a hard object ~ {}');
 	//t.equal(union(args..), 'Expected');
 	//t.false(union(args..), 'Expected');
 	//t.throws(union(args..), 'Expected');
