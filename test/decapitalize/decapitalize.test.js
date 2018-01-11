7c805e941a57a7eeb2bdd07974dfb18dd1469207
King <kingdavidmartins@users.noreply.github.com>
2018-01-11 05:27:49 -0500

    ran npm tdd

---
+++ test/decapitalize/decapitalize.test.js
@@ -0,0 +1,13 @@
+const test = require('tape');
+const decapitalize = require('./decapitalize.js');
+
+test('Testing decapitalize', (t) => {
+	//For more information on all the methods supported by tape
+	//Please go to https://github.com/substack/tape
+	t.true(typeof decapitalize === 'function', 'decapitalize is a Function');
+	//t.deepEqual(decapitalize(args..), 'Expected');
+	//t.equal(decapitalize(args..), 'Expected');
+	//t.false(decapitalize(args..), 'Expected');
+	//t.throws(decapitalize(args..), 'Expected');
+	t.end();
+});
