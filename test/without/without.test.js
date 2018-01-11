e7362f29c8bcd4e48d0d02a9967b0b255fe07554
King <kingdavidmartins@users.noreply.github.com>
2018-01-11 04:40:02 -0500

    2 type test check type is Array & always return array & not hard objext ~ {}

--- test/without/without.test.js
+++ test/without/without.test.js
@@ -6,6 +6,11 @@ test('Testing without', (t) => {
 	//Please go to https://github.com/substack/tape
 	t.true(typeof without === 'function', 'without is a Function');
 	t.deepEqual(without([2, 1, 2, 3], 1, 2), [3], "Filters out the elements of an array, that have one of the specified values.");
+	t.true(Array.isArray(without([2, 1, 2, 3], 1, 2)), 'without return an array');
+
+	const isObjectHard = (obj) => obj === Object(obj) && Object.prototype.toString.call(obj) !== '[object Array]';
+	t.true(!isObjectHard(without([2, 1, 2, 3], 1, 2)), 'without always returns an array & never a hard object ~ {}');
+
 	//t.deepEqual(without(args..), 'Expected');
 	//t.equal(without(args..), 'Expected');
 	//t.false(without(args..), 'Expected');
