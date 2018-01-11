5c7b7d4a98ebcade2d0ff7cebe9026fa9c88a006
King <kingdavidmartins@users.noreply.github.com>
2018-01-11 02:50:14 -0500

    add 2 basic for pullAtValue

--- test/pullAtValue/pullAtValue.test.js
+++ test/pullAtValue/pullAtValue.test.js
@@ -5,8 +5,15 @@ test('Testing pullAtValue', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof pullAtValue === 'function', 'pullAtValue is a Function');
-	//t.deepEqual(pullAtValue(args..), 'Expected');
-	//t.equal(pullAtValue(args..), 'Expected');
+
+	let myArray1 = ['a', 'b', 'c', 'd'];
+	pullAtValue(myArray1, ['b', 'd']);
+	t.deepEqual(myArray1, [ 'a', 'c' ], 'b & d was pulled from myArray1');
+
+	let myArray2 = ['a', 'b', 'c', 'd'];
+	pullAtValue(myArray2, ['a', 'c']);
+	t.deepEqual(myArray2, [ 'b', 'd' ], 'a & c was pulled from myArray2');
+
 	//t.false(pullAtValue(args..), 'Expected');
 	//t.throws(pullAtValue(args..), 'Expected');
 	t.end();
