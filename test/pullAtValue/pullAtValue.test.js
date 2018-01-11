eb25439d160909bc6cdf57795530926543ec219b
King <kingdavidmartins@users.noreply.github.com>
2018-01-11 02:52:14 -0500

    add 2 type test for pullAtValue

--- test/pullAtValue/pullAtValue.test.js
+++ test/pullAtValue/pullAtValue.test.js
@@ -9,10 +9,12 @@ test('Testing pullAtValue', (t) => {
 	let myArray1 = ['a', 'b', 'c', 'd'];
 	pullAtValue(myArray1, ['b', 'd']);
 	t.deepEqual(myArray1, [ 'a', 'c' ], 'b & d was pulled from myArray1');
+	t.true(Array.isArray(myArray1), 'myArray1 is still an Array');
 
 	let myArray2 = ['a', 'b', 'c', 'd'];
 	pullAtValue(myArray2, ['a', 'c']);
 	t.deepEqual(myArray2, [ 'b', 'd' ], 'a & c was pulled from myArray2');
+	t.true(Array.isArray(myArray2), 'myArray2 is still an Array');
 
 	//t.false(pullAtValue(args..), 'Expected');
 	//t.throws(pullAtValue(args..), 'Expected');
