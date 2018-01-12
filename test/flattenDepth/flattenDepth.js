779e18c08eba6bf7aaf17beea4689baad4e864f6
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-12 21:21:00 +0100

    remove flattenDepth
    seems that tdd script doesn't remove old removed snippets and their tests

--- test/flattenDepth/flattenDepth.js
+++
@@ -1,4 +0,0 @@
-module.exports = flattenDepth = (arr, depth = 1) =>
-depth != 1
-? arr.reduce((a, v) => a.concat(Array.isArray(v) ? flattenDepth(v, depth - 1) : v), [])
-: arr.reduce((a, v) => a.concat(v), []);
