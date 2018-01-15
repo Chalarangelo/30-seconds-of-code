e791e679ffd9193ebafbb5ae4fa31b8e9aec75fe
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-01-15 21:03:32 +0100

    add test for cleanObj

--- test/cleanObj/cleanObj.test.js
+++ test/cleanObj/cleanObj.test.js
@@ -5,6 +5,8 @@ test('Testing cleanObj', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof cleanObj === 'function', 'cleanObj is a Function');
+	const testObj = { a: 1, b: 2, children: { a: 1, b: 2 } };
+	t.deepEqual(cleanObj(testObj, ['a'], 'children'), { a: 1, children : { a: 1}}, "Removes any properties except the ones specified from a JSON object");
 	//t.deepEqual(cleanObj(args..), 'Expected');
 	//t.equal(cleanObj(args..), 'Expected');
 	//t.false(cleanObj(args..), 'Expected');
