53699c07acce57ffe8176698a54369f3f4e15952
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-16 16:13:13 +0200

    Fixed builder

--- scripts/build.js
+++ scripts/build.js
@@ -44,7 +44,7 @@ These snippets, while useful and interesting, didn\'t quite make it into the rep
 ## Table of Contents
 
 `
-    for(const snippet of Object.entries(snippets).filter(s[0] !=== 'README.md'))
+    for(const snippet of Object.entries(snippets).filter(s[0] !== 'README.md'))
       output += `* [\`${snippet[0].slice(0,-3)}\`](#${snippet[0].toLowerCase().slice(0,-3)})\n`;
     output += '\n---\n';
     for(const snippet of Object.entries(snippets)){
