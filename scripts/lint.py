1bc227cb1f3bfa23f7dc22ef296cbb5e2a8f8ca6
Rohit Tanwar <mst10041967@gmail.com>
2018-01-16 18:38:25 +0530

    add lint

---
+++ scripts/lint.py
@@ -0,0 +1,15 @@
+import autopep8
+import re
+import os
+files = os.listdir('snippets')
+codeRe = "```\s*python([\s\S]*?)```"
+for file in files:
+    someFile = open("snippets/" + file)
+    fileData = someFile.read() 
+    someFile.close()
+    originalCode = re.search(codeRe,fileData).group(0)
+    formatedCode = autopep8.fix_code(re.split(codeRe,fileData)[1])
+    fileToSave = fileData.replace(originalCode,('```python \n'+formatedCode+'\n```'))
+    someFile = open("snippets/"+file,'w')
+    someFile.write(fileToSave)
+    someFile.close()
