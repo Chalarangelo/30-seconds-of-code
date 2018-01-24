3e3982a0e4ea28d08813dc28d1700862b8cc31e9
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-24 17:49:09 +0200

    More tests

--- test/cloneRegExp/cloneRegExp.test.js
+++ test/cloneRegExp/cloneRegExp.test.js
@@ -5,6 +5,8 @@ test('Testing cloneRegExp', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof cloneRegExp === 'function', 'cloneRegExp is a Function');
+  const rgTest = /./g;
+  t.notEqual(cloneRegExp(rgTest), rgTest, 'Clones regular expressions properly');
 	//t.deepEqual(cloneRegExp(args..), 'Expected');
 	//t.equal(cloneRegExp(args..), 'Expected');
 	//t.false(cloneRegExp(args..), 'Expected');
