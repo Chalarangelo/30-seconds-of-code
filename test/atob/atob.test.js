17ff813fc3cb1852d71ee44284cdea0f66d67eb8
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-24 17:33:38 +0200

    Tests for atob, btoa

--- test/atob/atob.test.js
+++ test/atob/atob.test.js
@@ -5,6 +5,8 @@ test('Testing atob', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof atob === 'function', 'atob is a Function');
+  t.equals(atob('Zm9vYmFy'), 'foobar', 'atob("Zm9vYmFy") equals "foobar"');
+  t.equals(atob('Z'), '', 'atob("Z") returns ""');
 	//t.deepEqual(atob(args..), 'Expected');
 	//t.equal(atob(args..), 'Expected');
 	//t.false(atob(args..), 'Expected');
--- test/btoa/btoa.test.js
+++ test/btoa/btoa.test.js
@@ -5,6 +5,7 @@ test('Testing btoa', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof btoa === 'function', 'btoa is a Function');
+  t.equals(btoa('foobar'), 'Zm9vYmFy', 'btoa("foobar") equals "Zm9vYmFy"');
 	//t.deepEqual(btoa(args..), 'Expected');
 	//t.equal(btoa(args..), 'Expected');
 	//t.false(btoa(args..), 'Expected');
