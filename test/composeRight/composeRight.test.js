55816dccc35ebff78084a4dc60a8b3321c6c71eb
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-24 17:49:09 +0200

    More tests

--- test/composeRight/composeRight.test.js
+++ test/composeRight/composeRight.test.js
@@ -5,6 +5,10 @@ test('Testing composeRight', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof composeRight === 'function', 'composeRight is a Function');
+  const add = (x, y) => x + y;
+  const square = x => x * x;
+  const addAndSquare = composeRight(add, square);
+  t.equal(addAndSquare(1, 2), 9, "Performs left-to-right function composition");
 	//t.deepEqual(composeRight(args..), 'Expected');
 	//t.equal(composeRight(args..), 'Expected');
 	//t.false(composeRight(args..), 'Expected');
--- test/deepClone/deepClone.test.js
+++ test/deepClone/deepClone.test.js
@@ -5,6 +5,10 @@ test('Testing deepClone', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof deepClone === 'function', 'deepClone is a Function');
+  const a = { foo: 'bar', obj: { a: 1, b: 2 } };
+  const b = deepClone(a);
+  t.notEqual(a, b, 'Shallow cloning works');
+  t.notEqual(a.obj, b.obj, 'Deep cloning works');
 	//t.deepEqual(deepClone(args..), 'Expected');
 	//t.equal(deepClone(args..), 'Expected');
 	//t.false(deepClone(args..), 'Expected');
