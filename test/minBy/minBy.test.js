79a70885adbd80cb00464561a8cd1aa1313357f1
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-24 17:38:08 +0200

    Tests for averageBy, maxBy, minBy
    Fixed examples in minBy

--- test/minBy/minBy.test.js
+++ test/minBy/minBy.test.js
@@ -5,6 +5,8 @@ test('Testing minBy', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof minBy === 'function', 'minBy is a Function');
+  t.equals(minBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], o => o.n), 2, 'Produces the right result with a function');
+  t.equals(minBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'), 2, 'Produces the right result with a property name');
 	//t.deepEqual(minBy(args..), 'Expected');
 	//t.equal(minBy(args..), 'Expected');
 	//t.false(minBy(args..), 'Expected');
