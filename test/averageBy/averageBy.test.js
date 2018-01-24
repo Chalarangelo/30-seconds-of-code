1a863678ff65f1651d76c864e46db60e1963b660
Angelos Chalaris <chalarangelo@gmail.com>
2018-01-24 17:38:08 +0200

    Tests for averageBy, maxBy, minBy
    Fixed examples in minBy

--- test/averageBy/averageBy.test.js
+++ test/averageBy/averageBy.test.js
@@ -5,6 +5,8 @@ test('Testing averageBy', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof averageBy === 'function', 'averageBy is a Function');
+  t.equals(averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], o => o.n), 5, 'Produces the right result with a function');
+  t.equals(averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'), 5, 'Produces the right result with a property name');
 	//t.deepEqual(averageBy(args..), 'Expected');
 	//t.equal(averageBy(args..), 'Expected');
 	//t.false(averageBy(args..), 'Expected');
--- test/maxBy/maxBy.test.js
+++ test/maxBy/maxBy.test.js
@@ -5,6 +5,8 @@ test('Testing maxBy', (t) => {
 	//For more information on all the methods supported by tape
 	//Please go to https://github.com/substack/tape
 	t.true(typeof maxBy === 'function', 'maxBy is a Function');
+  t.equals(maxBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], o => o.n), 8, 'Produces the right result with a function');
+  t.equals(maxBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'), 8, 'Produces the right result with a property name');
 	//t.deepEqual(maxBy(args..), 'Expected');
 	//t.equal(maxBy(args..), 'Expected');
 	//t.false(maxBy(args..), 'Expected');
