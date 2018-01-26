97c219efcd392c80620037352dd4df292e2b7997
King <kingdavidmartins@users.noreply.github.com>
2018-01-26 17:09:04 -0500

    fix testing message for toKebabCase

--- test/toKebabCase/toKebabCase.test.js
+++ test/toKebabCase/toKebabCase.test.js
@@ -10,9 +10,9 @@ test('Testing toKebabCase', (t) => {
   t.equal(toKebabCase('some-mixed-string With spaces-underscores-and-hyphens'), 'some-mixed-string-with-spaces-underscores-and-hyphens', "toKebabCase('some-mixed-string With spaces-underscores-and-hyphens') returns some-mixed-string-with-spaces-underscores-and-hyphens");
   t.equal(toKebabCase('IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML'), 'i-am-listening-to-fm-while-loading-different-url-on-my-browser-and-also-editing-some-xml-and-html', "toKebabCase('IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML') returns i-am-listening-to-fm-while-loading-different-url-on-my-browser-and-also-editing-some-xml-and-html");
   t.equal(toKebabCase(), undefined, 'toKebabCase() return undefined');
-  t.throws(() => toKebabCase([]), 'Expected');
-  t.throws(() => toKebabCase({}), 'Expected');
-  t.throws(() => toKebabCase(123), 'Expected');
+  t.throws(() => toKebabCase([]), 'toKebabCase([]) throws an error');
+  t.throws(() => toKebabCase({}), 'toKebabCase({}) throws an error');
+  t.throws(() => toKebabCase(123), 'toKebabCase(123) throws an error');
 
   let start = new Date().getTime();
   toKebabCase('IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML')
