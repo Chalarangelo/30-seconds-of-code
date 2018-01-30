a6dcb375ade84a4810a4bf38c0df65cb83cce85e
Oscar Shrimpton <oscar.shrimpton.personal@gmail.com>
2018-01-30 19:01:54 +0000

    Test for binarySearch()

--- test/binarySearch/binarySearch.test.js
+++ test/binarySearch/binarySearch.test.js
@@ -6,7 +6,8 @@ test('Testing binarySearch', (t) => {
   //Please go to https://github.com/substack/tape
   t.true(typeof binarySearch === 'function', 'binarySearch is a Function');
   //t.deepEqual(binarySearch(args..), 'Expected');
-  //t.equal(binarySearch(args..), 'Expected');
+  t.equal(binarySearch([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 6), 2);
+  t.equal(binarySearch([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 21), -1);
   //t.false(binarySearch(args..), 'Expected');
   //t.throws(binarySearch(args..), 'Expected');
   t.end();
