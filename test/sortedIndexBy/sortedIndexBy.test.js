9001f983c686714bfdd320cd89102dcc21e313e0
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:26:01 +0200

    Tests for sortedLastIndex, sortedLastIndexBy, sortedIndexBy

--- test/sortedIndexBy/sortedIndexBy.test.js
+++ test/sortedIndexBy/sortedIndexBy.test.js
@@ -5,6 +5,7 @@ test('Testing sortedIndexBy', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof sortedIndexBy === 'function', 'sortedIndexBy is a Function');
+  t.equal(sortedIndexBy([{ x: 4 }, { x: 5 }], { x: 4 }, o => o.x), 0, 'Returns the lowest index to insert the element without messing up the list order');
   //t.deepEqual(sortedIndexBy(args..), 'Expected');
   //t.equal(sortedIndexBy(args..), 'Expected');
   //t.false(sortedIndexBy(args..), 'Expected');
--- test/sortedLastIndex/sortedLastIndex.test.js
+++ test/sortedLastIndex/sortedLastIndex.test.js
@@ -5,6 +5,7 @@ test('Testing sortedLastIndex', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof sortedLastIndex === 'function', 'sortedLastIndex is a Function');
+  t.equal(sortedLastIndex([10, 20, 30, 30, 40], 30), 3, 'Returns the highest index to insert the element without messing up the list order');
   //t.deepEqual(sortedLastIndex(args..), 'Expected');
   //t.equal(sortedLastIndex(args..), 'Expected');
   //t.false(sortedLastIndex(args..), 'Expected');
