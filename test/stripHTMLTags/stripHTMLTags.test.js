d79f5a582243255f78b59f5d380e8ce2b2337836
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:22:08 +0200

    Tests for stripHTMLTags

--- test/stripHTMLTags/stripHTMLTags.test.js
+++ test/stripHTMLTags/stripHTMLTags.test.js
@@ -5,6 +5,7 @@ test('Testing stripHTMLTags', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof stripHTMLTags === 'function', 'stripHTMLTags is a Function');
+  t.equals(stripHTMLTags('<p><em>lorem</em> <strong>ipsum</strong></p><img /><br>'), 'lorem ipsum', 'Removes HTML tags');
   //t.deepEqual(stripHTMLTags(args..), 'Expected');
   //t.equal(stripHTMLTags(args..), 'Expected');
   //t.false(stripHTMLTags(args..), 'Expected');
