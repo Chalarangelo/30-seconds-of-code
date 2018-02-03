e69b900c5f3fbadee2502285083cdd37d01d2e66
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:10:42 +0200

    Tests for transform

--- test/transform/transform.test.js
+++ test/transform/transform.test.js
@@ -5,6 +5,14 @@ test('Testing transform', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof transform === 'function', 'transform is a Function');
+  t.deepEqual(transform(
+  { a: 1, b: 2, c: 1 },
+  (r, v, k) => {
+    (r[v] || (r[v] = [])).push(k);
+    return r;
+  },
+  {}
+), { '1': ['a', 'c'], '2': ['b'] }, 'Transforms an object');
   //t.deepEqual(transform(args..), 'Expected');
   //t.equal(transform(args..), 'Expected');
   //t.false(transform(args..), 'Expected');
