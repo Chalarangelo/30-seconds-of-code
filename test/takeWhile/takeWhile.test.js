7d8614be0f4f61fb8d1090492563cc906d958999
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:18:04 +0200

    Tests for takeWhile, takeRightWhile, times

--- test/takeWhile/takeWhile.test.js
+++ test/takeWhile/takeWhile.test.js
@@ -5,6 +5,7 @@ test('Testing takeWhile', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof takeWhile === 'function', 'takeWhile is a Function');
+  t.deepEqual(takeWhile([1, 2, 3, 4], n => n >= 3), [1, 2], 'Removes elements until the function returns true');
   //t.deepEqual(takeWhile(args..), 'Expected');
   //t.equal(takeWhile(args..), 'Expected');
   //t.false(takeWhile(args..), 'Expected');
