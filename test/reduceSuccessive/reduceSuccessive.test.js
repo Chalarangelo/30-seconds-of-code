e8a5490516c1210827003d7e6f38f1950a86ebc9
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:52:14 +0200

    Tests for reduceWhich, reduceSuccessive, removeNonASCII

--- test/reduceSuccessive/reduceSuccessive.test.js
+++ test/reduceSuccessive/reduceSuccessive.test.js
@@ -5,6 +5,7 @@ test('Testing reduceSuccessive', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof reduceSuccessive === 'function', 'reduceSuccessive is a Function');
+  t.deepEqual(reduceSuccessive([1, 2, 3, 4, 5, 6], (acc, val) => acc + val, 0), [0, 1, 3, 6, 10, 15, 21], 'Returns the array of successively reduced values');
   //t.deepEqual(reduceSuccessive(args..), 'Expected');
   //t.equal(reduceSuccessive(args..), 'Expected');
   //t.false(reduceSuccessive(args..), 'Expected');
--- test/removeNonASCII/removeNonASCII.test.js
+++ test/removeNonASCII/removeNonASCII.test.js
@@ -5,6 +5,7 @@ test('Testing removeNonASCII', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof removeNonASCII === 'function', 'removeNonASCII is a Function');
+  t.equal(removeNonASCII('äÄçÇéÉêlorem-ipsumöÖÐþúÚ'), 'lorem-ipsum', 'Removes non-ASCII characters');
   //t.deepEqual(removeNonASCII(args..), 'Expected');
   //t.equal(removeNonASCII(args..), 'Expected');
   //t.false(removeNonASCII(args..), 'Expected');
