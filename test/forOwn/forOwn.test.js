bdac20fb7f81daf7320afbf4ddd14aa507cbacc7
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:59:41 +0200

    Tests for forEachRight, forOwn, forOwnRight

--- test/forOwn/forOwn.test.js
+++ test/forOwn/forOwn.test.js
@@ -5,6 +5,9 @@ test('Testing forOwn', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof forOwn === 'function', 'forOwn is a Function');
+  let output = [];
+  forOwn({ foo: 'bar', a: 1 }, v => output.push(v)); // 'bar', 1
+  t.deepEqual(output, ['bar', 1], 'Iterates over an element\'s key-value pairs');
   //t.deepEqual(forOwn(args..), 'Expected');
   //t.equal(forOwn(args..), 'Expected');
   //t.false(forOwn(args..), 'Expected');
