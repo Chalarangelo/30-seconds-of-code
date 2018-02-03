b8e4a406b29332a961f38692f1f3f71d443b5a78
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:55:36 +0200

    Tests for findKey, findLastKey, findLast, findLastIndex

--- test/findLastIndex/findLastIndex.test.js
+++ test/findLastIndex/findLastIndex.test.js
@@ -5,6 +5,7 @@ test('Testing findLastIndex', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof findLastIndex === 'function', 'findLastIndex is a Function');
+  t.equal(findLastIndex([1, 2, 3, 4], n => n % 2 === 1), 2, 'Finds last index for which the given function returns true');
   //t.deepEqual(findLastIndex(args..), 'Expected');
   //t.equal(findLastIndex(args..), 'Expected');
   //t.false(findLastIndex(args..), 'Expected');
