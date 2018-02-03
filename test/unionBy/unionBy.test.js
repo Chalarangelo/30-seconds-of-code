bb50dae43b3ce51f407ecb6b293f1d6e77c329dd
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:01:14 +0200

    Tests for unionBy, unionWith

--- test/unionBy/unionBy.test.js
+++ test/unionBy/unionBy.test.js
@@ -5,6 +5,7 @@ test('Testing unionBy', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof unionBy === 'function', 'unionBy is a Function');
+  t.deepEqual(unionBy([2.1], [1.2, 2.3], Math.floor), [2.1, 1.2], 'Produces the appropriate results');
   //t.deepEqual(unionBy(args..), 'Expected');
   //t.equal(unionBy(args..), 'Expected');
   //t.false(unionBy(args..), 'Expected');
