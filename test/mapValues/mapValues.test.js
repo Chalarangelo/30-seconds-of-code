e06afdb114aac4516955bee8daa5f324b44fb4d3
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 13:33:09 +0200

    Tests for matches, matchesWith, mapValues

--- test/mapValues/mapValues.test.js
+++ test/mapValues/mapValues.test.js
@@ -5,6 +5,11 @@ test('Testing mapValues', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof mapValues === 'function', 'mapValues is a Function');
+  const users = {
+    fred: { user: 'fred', age: 40 },
+    pebbles: { user: 'pebbles', age: 1 }
+  };
+  t.deepEqual(mapValues(users, u => u.age), { fred: 40, pebbles: 1 }, 'Maps values');
   //t.deepEqual(mapValues(args..), 'Expected');
   //t.equal(mapValues(args..), 'Expected');
   //t.false(mapValues(args..), 'Expected');
