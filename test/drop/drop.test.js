82b4583de233f1a70f2c0d648c5ec89c2edb65e1
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 11:58:24 +0200

    Tests for drop, distance and differenceBy

--- test/drop/drop.test.js
+++ test/drop/drop.test.js
@@ -5,6 +5,9 @@ test('Testing drop', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof drop === 'function', 'drop is a Function');
+  t.deepEqual(drop([1, 2, 3]), [2,3], 'Works without the last argument');
+  t.deepEqual(drop([1, 2, 3], 2), [3], 'Removes appropriate element count as specified');
+  t.deepEqual(drop([1, 2, 3], 42), [], 'Empties array given a count greater than length');
   //t.deepEqual(drop(args..), 'Expected');
   //t.equal(drop(args..), 'Expected');
   //t.false(drop(args..), 'Expected');
