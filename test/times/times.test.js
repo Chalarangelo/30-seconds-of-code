185fef6ec28fc2db81ffdd154f3e199680a74f2a
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:18:04 +0200

    Tests for takeWhile, takeRightWhile, times

--- test/times/times.test.js
+++ test/times/times.test.js
@@ -5,6 +5,9 @@ test('Testing times', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof times === 'function', 'times is a Function');
+  var output = '';
+  times(5, i => (output += i));
+  t.equals(output, '01234', 'Runs a function the specified amount of times');
   //t.deepEqual(times(args..), 'Expected');
   //t.equal(times(args..), 'Expected');
   //t.false(times(args..), 'Expected');
