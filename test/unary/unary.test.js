a7c0a73ff99284ec71df6fc3153f2fe92dda75cd
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:04:15 +0200

    Tests for unfold, unary

--- test/unary/unary.test.js
+++ test/unary/unary.test.js
@@ -5,6 +5,7 @@ test('Testing unary', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof unary === 'function', 'unary is a Function');
+  t.deepEqual(['6', '8', '10'].map(unary(parseInt)), [6, 8, 10], 'Discards arguments after the first one');
   //t.deepEqual(unary(args..), 'Expected');
   //t.equal(unary(args..), 'Expected');
   //t.false(unary(args..), 'Expected');
