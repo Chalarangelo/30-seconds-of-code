b81d7a575990d4f28f610fbc1be04d5cdea129fd
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:59:41 +0200

    Tests for forEachRight, forOwn, forOwnRight

--- test/forEachRight/forEachRight.test.js
+++ test/forEachRight/forEachRight.test.js
@@ -5,6 +5,9 @@ test('Testing forEachRight', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof forEachRight === 'function', 'forEachRight is a Function');
+  let output = '';
+  forEachRight([1, 2, 3, 4], val => output+=val);
+  t.equal(output, '4321', 'Iterates over the array in reverse');
   //t.deepEqual(forEachRight(args..), 'Expected');
   //t.equal(forEachRight(args..), 'Expected');
   //t.false(forEachRight(args..), 'Expected');
