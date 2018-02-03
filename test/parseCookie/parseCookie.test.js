ab38a73c2b84385566688631df3c5fc62a5bb6ab
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:32:46 +0200

    Tests for parseCookie, serializeCookie, shuffle

--- test/parseCookie/parseCookie.test.js
+++ test/parseCookie/parseCookie.test.js
@@ -5,6 +5,7 @@ test('Testing parseCookie', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof parseCookie === 'function', 'parseCookie is a Function');
+  t.deepEqual(parseCookie('foo=bar; equation=E%3Dmc%5E2'), { foo: 'bar', equation: 'E=mc^2' }, 'Parses the cookie');
   //t.deepEqual(parseCookie(args..), 'Expected');
   //t.equal(parseCookie(args..), 'Expected');
   //t.false(parseCookie(args..), 'Expected');
--- test/shuffle/shuffle.test.js
+++ test/shuffle/shuffle.test.js
@@ -5,6 +5,8 @@ test('Testing shuffle', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof shuffle === 'function', 'shuffle is a Function');
+  const arr = [1,2,3,4,5,6];
+  t.notDeepEqual(shuffle(arr), arr, 'Shuffles the array');
   //t.deepEqual(shuffle(args..), 'Expected');
   //t.equal(shuffle(args..), 'Expected');
   //t.false(shuffle(args..), 'Expected');
