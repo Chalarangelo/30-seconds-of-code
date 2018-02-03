61c49fbda05231fb0415501005ab967f64417c8e
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 13:28:50 +0200

    Tests for mapKeys, lowercaseKeys

--- test/lowercaseKeys/lowercaseKeys.test.js
+++ test/lowercaseKeys/lowercaseKeys.test.js
@@ -5,6 +5,10 @@ test('Testing lowercaseKeys', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof lowercaseKeys === 'function', 'lowercaseKeys is a Function');
+  const myObj = { Name: 'Adam', sUrnAME: 'Smith' };
+  const myObjLower = lowercaseKeys(myObj);
+  t.deepEqual(myObjLower, {name: 'Adam', surname: 'Smith'}, 'Lowercases object keys');
+  t.deepEqual(myObj, { Name: 'Adam', sUrnAME: 'Smith' }, 'Does not mutate original object');
   //t.deepEqual(lowercaseKeys(args..), 'Expected');
   //t.equal(lowercaseKeys(args..), 'Expected');
   //t.false(lowercaseKeys(args..), 'Expected');
