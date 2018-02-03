f2ff2b33ea36233e1ff0ed9055f9cc32c924a1e1
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:26:01 +0200

    Tests for sortedLastIndex, sortedLastIndexBy, sortedIndexBy

--- test/sortedLastIndexBy/sortedLastIndexBy.test.js
+++ test/sortedLastIndexBy/sortedLastIndexBy.test.js
@@ -5,6 +5,7 @@ test('Testing sortedLastIndexBy', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof sortedLastIndexBy === 'function', 'sortedLastIndexBy is a Function');
+  t.equal(sortedLastIndexBy([{ x: 4 }, { x: 5 }], { x: 4 }, o => o.x), 1, 'Returns the highest index to insert the element without messing up the list order');
   //t.deepEqual(sortedLastIndexBy(args..), 'Expected');
   //t.equal(sortedLastIndexBy(args..), 'Expected');
   //t.false(sortedLastIndexBy(args..), 'Expected');
