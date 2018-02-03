9a1e39117621de2c1a5f9c93b807f62b20f83793
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 11:58:24 +0200

    Tests for drop, distance and differenceBy

--- test/distance/distance.test.js
+++ test/distance/distance.test.js
@@ -5,6 +5,7 @@ test('Testing distance', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof distance === 'function', 'distance is a Function');
+  t.equals(distance(1, 1, 2, 3), 2.23606797749979, 'Calculates the distance between two points');
   //t.deepEqual(distance(args..), 'Expected');
   //t.equal(distance(args..), 'Expected');
   //t.false(distance(args..), 'Expected');
