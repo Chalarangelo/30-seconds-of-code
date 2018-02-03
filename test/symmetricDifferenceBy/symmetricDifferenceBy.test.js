ddffff8059b2c6e20dfd9fde10732df580ae251a
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 12:20:15 +0200

    Tests for symmetricDifferenceBy, symmetricDifferenceWith

--- test/symmetricDifferenceBy/symmetricDifferenceBy.test.js
+++ test/symmetricDifferenceBy/symmetricDifferenceBy.test.js
@@ -5,6 +5,7 @@ test('Testing symmetricDifferenceBy', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof symmetricDifferenceBy === 'function', 'symmetricDifferenceBy is a Function');
+  t.deepEqual(symmetricDifferenceBy([2.1, 1.2], [2.3, 3.4], Math.floor), [ 1.2, 3.4 ], 'Returns the symmetric difference between two arrays, after applying the provided function to each array element of both');
   //t.deepEqual(symmetricDifferenceBy(args..), 'Expected');
   //t.equal(symmetricDifferenceBy(args..), 'Expected');
   //t.false(symmetricDifferenceBy(args..), 'Expected');
