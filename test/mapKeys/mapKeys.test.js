0036e83530e0ebfd42d51d30d865a0e29ddb8410
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-03 13:28:50 +0200

    Tests for mapKeys, lowercaseKeys

--- test/mapKeys/mapKeys.test.js
+++ test/mapKeys/mapKeys.test.js
@@ -5,6 +5,7 @@ test('Testing mapKeys', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof mapKeys === 'function', 'mapKeys is a Function');
+  t.deepEqual(mapKeys({ a: 1, b: 2 }, (val, key) => key + val), { a1: 1, b2: 2 }, 'Maps keys');
   //t.deepEqual(mapKeys(args..), 'Expected');
   //t.equal(mapKeys(args..), 'Expected');
   //t.false(mapKeys(args..), 'Expected');
