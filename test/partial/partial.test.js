c0735d56c0b11d5020ed5c7f36f74c9944481cf2
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 16:58:59 +0200

    Tests for partial, partialRight

--- test/partial/partial.test.js
+++ test/partial/partial.test.js
@@ -5,6 +5,11 @@ test('Testing partial', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof partial === 'function', 'partial is a Function');
+  function greet(greeting, name) {
+    return greeting + ' ' + name + '!';
+  }
+  const greetHello = partial(greet, 'Hello');
+  t.equal(greetHello('John'), 'Hello John!', 'Prepends arguments');
   //t.deepEqual(partial(args..), 'Expected');
   //t.equal(partial(args..), 'Expected');
   //t.false(partial(args..), 'Expected');
