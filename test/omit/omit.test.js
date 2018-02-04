1364fbd1223e70bf055f91a091437eedef378349
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 20:53:25 +0200

    Tests for omit, omitBy

--- test/omit/omit.test.js
+++ test/omit/omit.test.js
@@ -5,6 +5,7 @@ test('Testing omit', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof omit === 'function', 'omit is a Function');
+  t.deepEqual(omit({ a: 1, b: '2', c: 3 }, ['b']), { 'a': 1, 'c': 3 }, 'Omits the key-value pairs corresponding to the given keys from an object');
   //t.deepEqual(omit(args..), 'Expected');
   //t.equal(omit(args..), 'Expected');
   //t.false(omit(args..), 'Expected');
