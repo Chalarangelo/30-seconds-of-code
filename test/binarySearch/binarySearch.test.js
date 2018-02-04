b44dd7d905abd0de872a613874803a0e74543db8
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-02-04 21:37:37 +0100

    add test for equal

--- test/binarySearch/binarySearch.test.js
+++ test/binarySearch/binarySearch.test.js
@@ -8,6 +8,7 @@ test('Testing binarySearch', (t) => {
   //t.deepEqual(binarySearch(args..), 'Expected');
   t.equal(binarySearch([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 6), 2, 'Finds item in array');
   t.equal(binarySearch([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 21), -1, 'Returns -1 when not found');
+  t.equal(binarySearch([], 21), -1, 'Works with empty arrays');
   //t.false(binarySearch(args..), 'Expected');
   //t.throws(binarySearch(args..), 'Expected');
   t.end();
