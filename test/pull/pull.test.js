e5576e74ac1f044d12f76c99bc188a244d91c703
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 21:02:32 +0200

    Tests for pull, pullBy, pullAtIndex, pullAtValue

--- test/pull/pull.test.js
+++ test/pull/pull.test.js
@@ -5,6 +5,9 @@ test('Testing pull', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof pull === 'function', 'pull is a Function');
+  let myArray = ['a', 'b', 'c', 'a', 'b', 'c'];
+  pull(myArray, 'a', 'c');
+  t.deepEqual(myArray, ['b','b'], 'Pulls the specified values');
   //t.deepEqual(pull(args..), 'Expected');
   //t.equal(pull(args..), 'Expected');
   //t.false(pull(args..), 'Expected');
--- test/pullAtValue/pullAtValue.test.js
+++ test/pullAtValue/pullAtValue.test.js
@@ -5,6 +5,10 @@ test('Testing pullAtValue', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof pullAtValue === 'function', 'pullAtValue is a Function');
+  let myArray = ['a', 'b', 'c', 'd'];
+  let pulled = pullAtValue(myArray, ['b', 'd']);
+  t.deepEqual(myArray, [ 'a', 'c' ], 'Pulls the specified values');
+  t.deepEqual(pulled, [ 'b', 'd' ], 'Pulls the specified values');
   //t.deepEqual(pullAtValue(args..), 'Expected');
   //t.equal(pullAtValue(args..), 'Expected');
   //t.false(pullAtValue(args..), 'Expected');
