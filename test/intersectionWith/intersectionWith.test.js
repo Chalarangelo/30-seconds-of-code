c918d616a8813e538fb054a7fce6b6f690a75464
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 20:48:12 +0200

    Tests for intersectionBy, intersectionWith

--- test/intersectionWith/intersectionWith.test.js
+++ test/intersectionWith/intersectionWith.test.js
@@ -5,6 +5,7 @@ test('Testing intersectionWith', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof intersectionWith === 'function', 'intersectionWith is a Function');
+  t.deepEqual(intersectionWith([1, 1.2, 1.5, 3, 0], [1.9, 3, 0, 3.9], (a, b) => Math.round(a) === Math.round(b)), [1.5, 3, 0], 'Returns a list of elements that exist in both arrays, using a provided comparator function');
   //t.deepEqual(intersectionWith(args..), 'Expected');
   //t.equal(intersectionWith(args..), 'Expected');
   //t.false(intersectionWith(args..), 'Expected');
