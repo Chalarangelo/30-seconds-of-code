3144573716b3efb57946ff3134d497b0d62ac459
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 20:46:30 +0200

    Test and fix for hashNode

--- test/hashNode/hashNode.test.js
+++ test/hashNode/hashNode.test.js
@@ -5,6 +5,7 @@ test('Testing hashNode', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof hashNode === 'function', 'hashNode is a Function');
+  hashNode(JSON.stringify({ a: 'a', b: [1, 2, 3, 4], foo: { c: 'bar' } })).then(v => t.equal(v, '04aa106279f5977f59f9067fa9712afc4aedc6f5862a8defc34552d8c7206393', 'Produces the appropriate hash'));
   //t.deepEqual(hashNode(args..), 'Expected');
   //t.equal(hashNode(args..), 'Expected');
   //t.false(hashNode(args..), 'Expected');
