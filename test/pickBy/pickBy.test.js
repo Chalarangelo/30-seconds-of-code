5b72ef9b8105390467563d6327cb1fd140aee8b4
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 16:57:07 +0200

    Tests for rearg, pickBy

--- test/pickBy/pickBy.test.js
+++ test/pickBy/pickBy.test.js
@@ -5,6 +5,7 @@ test('Testing pickBy', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof pickBy === 'function', 'pickBy is a Function');
+  t.deepEqual(pickBy({ a: 1, b: '2', c: 3 }, x => typeof x === 'number'), { 'a': 1, 'c': 3 }, 'Creates an object composed of the properties the given function returns truthy for.');
   //t.deepEqual(pickBy(args..), 'Expected');
   //t.equal(pickBy(args..), 'Expected');
   //t.false(pickBy(args..), 'Expected');
--- test/rearg/rearg.test.js
+++ test/rearg/rearg.test.js
@@ -5,6 +5,13 @@ test('Testing rearg', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof rearg === 'function', 'rearg is a Function');
+  var rearged = rearg(
+    function(a, b, c) {
+      return [a, b, c];
+    },
+    [2, 0, 1]
+  );
+  t.deepEqual(rearged('b', 'c', 'a'), ['a', 'b', 'c'], 'Reorders arguments in invoked function');
   //t.deepEqual(rearg(args..), 'Expected');
   //t.equal(rearg(args..), 'Expected');
   //t.false(rearg(args..), 'Expected');
