48c112d92b16da3f83adc3526f366e112d18a189
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-04 20:53:25 +0200

    Tests for omit, omitBy

--- test/omitBy/omitBy.test.js
+++ test/omitBy/omitBy.test.js
@@ -5,6 +5,7 @@ test('Testing omitBy', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof omitBy === 'function', 'omitBy is a Function');
+  t.deepEqual(omitBy({ a: 1, b: '2', c: 3 }, x => typeof x === 'number'), { b: '2' }, 'Creates an object composed of the properties the given function returns falsey for');
   //t.deepEqual(omitBy(args..), 'Expected');
   //t.equal(omitBy(args..), 'Expected');
   //t.false(omitBy(args..), 'Expected');
