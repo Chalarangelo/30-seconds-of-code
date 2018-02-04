6f80eaddb684f54c37fd6352f9d9ee6a7721c363
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-02-04 21:42:36 +0100

    add test for capitalize

--- test/capitalize/capitalize.test.js
+++ test/capitalize/capitalize.test.js
@@ -7,6 +7,7 @@ test('Testing capitalize', (t) => {
   t.true(typeof capitalize === 'function', 'capitalize is a Function');
   t.equal(capitalize('fooBar'), 'FooBar', "Capitalizes the first letter of a string");
   t.equal(capitalize('fooBar', true), 'Foobar', "Capitalizes the first letter of a string");
+  t.equal(capitalize('#!#', true), '#!#', "Works with characters");
   //t.deepEqual(capitalize(args..), 'Expected');
   //t.equal(capitalize(args..), 'Expected');
   //t.false(capitalize(args..), 'Expected');
