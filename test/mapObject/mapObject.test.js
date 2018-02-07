a0016ed76539c167e911651e0c813c7e9c41ced8
King <kingdavidmartins@users.noreply.github.com>
2018-02-07 11:05:46 -0500

    update mapObject basic test

--- test/mapObject/mapObject.test.js
+++ test/mapObject/mapObject.test.js
@@ -5,9 +5,10 @@ test('Testing mapObject', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof mapObject === 'function', 'mapObject is a Function');
-  const squareIt = arr => mapObject(arr, a => a * a);
-  t.deepEqual(squareIt([1, 2, 3]), { 1: 1, 2: 4, 3: 9 }, "Maps the values of an array to an object using a function");
-  //t.deepEqual(mapObject(args..), 'Expected');
+  t.deepEqual(mapObject([1, 2, 3], a => a * a), { 1: 1, 2: 4, 3: 9 }, "mapObject([1, 2, 3], a => a * a) returns { 1: 1, 2: 4, 3: 9 }");
+  t.deepEqual(mapObject([1, 2, 3, 4], (a, b) => b - a), { 1: -1, 2: -1, 3: -1, 4: -1 }, 'mapObject([1, 2, 3, 4], (a, b) => b - a) returns { 1: -1, 2: -1, 3: -1, 4: -1 }');
+  t.deepEqual(mapObject([1, 2, 3, 4], (a, b) => a - b), { 1: 1, 2: 1, 3: 1, 4: 1 }, 'mapObject([1, 2, 3, 4], (a, b) => a - b) returns { 1: 1, 2: 1, 3: 1, 4: 1 }');
+
   //t.equal(mapObject(args..), 'Expected');
   //t.false(mapObject(args..), 'Expected');
   //t.throws(mapObject(args..), 'Expected');
