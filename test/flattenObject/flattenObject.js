d14b19406ed100b247031841d2f83fb1ce3bcb7b
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-07 11:36:01 +0200

    Updated flattenObject tests

--- test/flattenObject/flattenObject.js
+++ test/flattenObject/flattenObject.js
@@ -1,11 +1,7 @@
 const flattenObject = (obj, prefix = '') =>
 Object.keys(obj).reduce((acc, k) => {
-const pre = prefix.length ? (prefix + '.') : '';
-if (typeof obj[k] === 'object')
-Object.assign(
-acc,
-flattenObject(obj[k], pre + k)
-);
+const pre = prefix.length ? prefix + '.' : '';
+if (typeof obj[k] === 'object') Object.assign(acc, flattenObject(obj[k], pre + k));
 else acc[pre + k] = obj[k];
 return acc;
 }, {});
