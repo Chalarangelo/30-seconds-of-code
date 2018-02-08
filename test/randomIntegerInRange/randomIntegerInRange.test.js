b990231acc9e0ec89009092e087716bf7a9236aa
Rohit Tanwar <31792358+kriadmin@users.noreply.github.com>
2018-02-08 19:57:18 +0530

    Update randomIntegerInRange.test.js

--- test/randomIntegerInRange/randomIntegerInRange.test.js
+++ test/randomIntegerInRange/randomIntegerInRange.test.js
@@ -5,6 +5,11 @@ test('Testing randomIntegerInRange', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof randomIntegerInRange === 'function', 'randomIntegerInRange is a Function');
+  const lowerLimit = Math.floor(Math.random() * 20);
+  const upperLimit = Math.floor(lowerLimit + Math.random() * 10);
+  t.true(Number.isInteger(randomIntegerInRange(lowerLimit,upperLimit)),'The returned value is a Integer');
+  const numberForTest = randomIntegerInRange(lowerLimit,upperLimit);
+  t.true((numberForTest >= lowerLimit) && (numberForTest <= upperLimit)),'The returned value lies between provied lowerLimit and upperLimit (both inclusive).');  
   //t.deepEqual(randomIntegerInRange(args..), 'Expected');
   //t.equal(randomIntegerInRange(args..), 'Expected');
   //t.false(randomIntegerInRange(args..), 'Expected');
