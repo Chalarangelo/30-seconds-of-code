7cc54a7a2e00cd09d3a2b291df3190e1880c1b9f
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-08 16:13:19 +0200

    Tests for isTravisCI

--- test/isTravisCI/isTravisCI.test.js
+++ test/isTravisCI/isTravisCI.test.js
@@ -5,6 +5,10 @@ test('Testing isTravisCI', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof isTravisCI === 'function', 'isTravisCI is a Function');
+  if(isTravisCI())
+    t.true(isTravisCI(), 'Running on Travis, correctly evaluates');
+  else
+    t.false(isTravisCI(), 'Not running on Travis, correctly evaluates');
   //t.deepEqual(isTravisCI(args..), 'Expected');
   //t.equal(isTravisCI(args..), 'Expected');
   //t.false(isTravisCI(args..), 'Expected');
