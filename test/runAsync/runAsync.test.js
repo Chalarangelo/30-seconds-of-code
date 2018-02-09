5976ec6535388469a31a8a4f17a4a37c13584981
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-09 13:40:08 +0200

    Tests for fileLines, runAsync, promisesInSeries

--- test/runAsync/runAsync.test.js
+++ test/runAsync/runAsync.test.js
@@ -5,6 +5,7 @@ test('Testing runAsync', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof runAsync === 'function', 'runAsync is a Function');
+  t.pass('Tested on 09/02/2018 by @chalarangelo');
   //t.deepEqual(runAsync(args..), 'Expected');
   //t.equal(runAsync(args..), 'Expected');
   //t.false(runAsync(args..), 'Expected');
