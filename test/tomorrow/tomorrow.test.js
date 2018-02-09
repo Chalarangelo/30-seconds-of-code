41f0ae31ebc038dff34bb5bfac9e3ddbb66af4a1
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-09 13:12:23 +0200

    Tests for tomorrow

--- test/tomorrow/tomorrow.test.js
+++ test/tomorrow/tomorrow.test.js
@@ -5,6 +5,11 @@ test('Testing tomorrow', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof tomorrow === 'function', 'tomorrow is a Function');
+  const t1 = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1);
+  const t2 = new Date(tomorrow());
+  t.equal(t1.getFullYear(), t2.getFullYear(), 'Returns the correct year');
+  t.equal(t1.getMonth(), t2.getMonth(), 'Returns the correct month');
+  t.equal(t1.getDate(), t2.getDate(), 'Returns the correct date');
   //t.deepEqual(tomorrow(args..), 'Expected');
   //t.equal(tomorrow(args..), 'Expected');
   //t.false(tomorrow(args..), 'Expected');
