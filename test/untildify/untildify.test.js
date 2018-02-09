2ac475ef646b9e6e30c280df6c7fec8fafd9cb58
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-09 13:23:46 +0200

    Tests for untildify

--- test/untildify/untildify.test.js
+++ test/untildify/untildify.test.js
@@ -5,6 +5,9 @@ test('Testing untildify', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof untildify === 'function', 'untildify is a Function');
+  t.true(untildify('~/test/dir/file.f').indexOf('~') === -1, 'Contains no tildes');
+  t.equal(untildify('~/test/dir/file.f').slice(-16), '/test/dir/file.f', 'Does not alter the rest of the path');
+  t.equal(untildify('test/dir/file.f'), 'test/dir/file.f', 'Does not alter paths without tildes');
   //t.deepEqual(untildify(args..), 'Expected');
   //t.equal(untildify(args..), 'Expected');
   //t.false(untildify(args..), 'Expected');
