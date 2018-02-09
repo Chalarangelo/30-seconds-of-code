f926ebeff7f0e0b9f891496798072519ac76e74b
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-09 13:35:43 +0200

    Tests for promisify

--- test/promisify/promisify.test.js
+++ test/promisify/promisify.test.js
@@ -5,6 +5,10 @@ test('Testing promisify', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof promisify === 'function', 'promisify is a Function');
+  const x = promisify(Math.max);
+  t.true(x() instanceof Promise, 'Returns a promise');
+  const delay = promisify((d, cb) => setTimeout(cb, d));
+  delay(200).then(() => t.pass('Runs the function provided'));
   //t.deepEqual(promisify(args..), 'Expected');
   //t.equal(promisify(args..), 'Expected');
   //t.false(promisify(args..), 'Expected');
