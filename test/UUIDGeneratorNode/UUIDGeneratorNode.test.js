4f93027b3564479d928442a10f834b179cd3fef1
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-09 13:21:06 +0200

    Tests for UUID generators

--- test/UUIDGeneratorNode/UUIDGeneratorNode.test.js
+++ test/UUIDGeneratorNode/UUIDGeneratorNode.test.js
@@ -5,6 +5,9 @@ test('Testing UUIDGeneratorNode', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof UUIDGeneratorNode === 'function', 'UUIDGeneratorNode is a Function');
+  const uuid = UUIDGeneratorNode();
+  t.deepEqual([uuid[8], uuid[13], uuid[18], uuid[23]], ['-', '-', '-', '-'], 'Contains dashes in the proper places');
+  t.true(/^[0-9A-Fa-f-]+$/.test(uuid), 'Only contains hexadecimal digits');
   //t.deepEqual(UUIDGeneratorNode(args..), 'Expected');
   //t.equal(UUIDGeneratorNode(args..), 'Expected');
   //t.false(UUIDGeneratorNode(args..), 'Expected');
