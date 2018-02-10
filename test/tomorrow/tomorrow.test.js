cfba80cfa5a32f1a569cd0d16647bec6c1b40d03
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-10 17:11:48 +0200

    Revert changes from #602
    The changes in #602 would break the test, as it was comparing today to tomorrow. The original test worked just fine and the cron job that run on Travis proves that the `t1` assignment was valid and working.

--- test/tomorrow/tomorrow.test.js
+++ test/tomorrow/tomorrow.test.js
@@ -5,7 +5,7 @@ test('Testing tomorrow', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof tomorrow === 'function', 'tomorrow is a Function');
-  const t1 = new Date();
+  const t1 = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1);
   const t2 = new Date(tomorrow());
   t.equal(t1.getFullYear(), t2.getFullYear(), 'Returns the correct year');
   t.equal(t1.getMonth(), t2.getMonth(), 'Returns the correct month');
