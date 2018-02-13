b472974b42dd6b15752e56bbc35c4a4b3ce7a6f1
King <kingdavidmartins@users.noreply.github.com>
2018-02-12 19:22:11 -0500

    ran npm run tester generate js test files and refactored permuteAll

--- test/permuteAll/permuteAll.js
+++ test/permuteAll/permuteAll.js
@@ -2,20 +2,18 @@ const permuteAll = (input) => {
 const result = [];
 let inputState = input;
 
-if (typeof input === 'string') inputState = input.split('')
+if (typeof input === 'string') inputState = input.split('');
 if (typeof input === 'number') inputState = (input).toString().split('');
 
 const permute = (arr, m = []) => {
-if (arr.length === 0) {
-result.push(m)
-} else {
-for (let i = 0; i < arr.length; i++) {
+(arr.length === 0)
+? result.push(m)
+: arr.forEach((_, i) => {
 let curr = arr.slice();
 let next = curr.splice(i, 1);
-permute(curr.slice(), m.concat(next))
-}
-}
-}
+permute(curr.slice(), m.concat(next));
+});
+};
 
 permute(inputState);
 
