23fbd750dec59df54cdded1fd542f090f1ca9be8
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-14 11:58:12 +0200

    Resolves Codacy quality test issue

--- test/noneBy/noneBy.test.js
+++ test/noneBy/noneBy.test.js
@@ -6,7 +6,7 @@ test('Testing noneBy', (t) => {
   //Please go to https://github.com/substack/tape
   t.true(typeof noneBy === 'function', 'noneBy is a Function');
   t.true(noneBy([4,1,0,3], x => x < 0), 'Returns true with a predicate function');
-  t.false(noneBy([0,1,2], x => x == 1), 'Returns false with predicate function');
+  t.false(noneBy([0,1,2], x => x === 1), 'Returns false with predicate function');
   //t.deepEqual(noneBy(args..), 'Expected');
   //t.equal(noneBy(args..), 'Expected');
   //t.false(noneBy(args..), 'Expected');
