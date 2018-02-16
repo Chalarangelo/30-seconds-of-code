efdb54dac044adc007875e87a3f0af5b21f60c81
Angelos Chalaris <chalarangelo@gmail.com>
2018-02-16 14:00:23 +0200

    Added remaining tests

--- test/throttle/throttle.test.js
+++ test/throttle/throttle.test.js
@@ -5,6 +5,7 @@ test('Testing throttle', (t) => {
   //For more information on all the methods supported by tape
   //Please go to https://github.com/substack/tape
   t.true(typeof throttle === 'function', 'throttle is a Function');
+  t.pass('Tested by @chalarangelo on 16/02/2018');
   //t.deepEqual(throttle(args..), 'Expected');
   //t.equal(throttle(args..), 'Expected');
   //t.false(throttle(args..), 'Expected');
