2dcad9fddd3ed569828cc89db9fa492f4cf11a2f
Rohit Tanwar <mst10041967@gmail.com>
2018-02-22 09:35:43 +0530

    improve scripts

--- test/average/average.test.py
+++ test/average/average.test.py
@@ -1,6 +1,7 @@
 import types,functools
-from tape import test
+from pytape import test
 from average import average
 def average_test(t):
     t.true(isinstance(average, (types.BuiltinFunctionType, types.FunctionType, functools.partial)),'average is a function')
+    t.equal(2,2,'equal')
 test('Testing average',average_test)
