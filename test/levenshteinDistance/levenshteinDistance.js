9e1f449b1d56516d5f1e0f5fe4da9d37209e3fff
Rohit Tanwar <mst10041967@gmail.com>
2018-02-26 15:01:26 +0530

    add snippet to archive

--- test/levenshteinDistance/levenshteinDistance.js
+++ test/levenshteinDistance/levenshteinDistance.js
@@ -3,8 +3,8 @@ if(string1.length === 0) return string2.length;
 if(string2.length === 0) return string1.length;
 let matrix = Array(string2.length+ 1).fill(0).map((x,i) => [i]);
 matrix[0] = Array(string1.length + 1).fill(0).map((x,i) => i);
-for(i = 1;i <= string2.length;i++){
-for(j = 1;j<=string1.length; j++){
+for(let i = 1;i <= string2.length;i++){
+for(let j = 1;j<=string1.length; j++){
 if(string2[i-1] === string1[j-1]){
 matrix[i][j] = matrix[i-1][j-1];
 }
