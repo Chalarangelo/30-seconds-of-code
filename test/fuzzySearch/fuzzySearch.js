818525e87e68bf1d880348d284bbaf19f88ed898
Rohit Tanwar <mst10041967@gmail.com>
2018-02-26 15:08:42 +0530

    remove test files

--- test/fuzzySearch/fuzzySearch.js
+++
@@ -1,5 +0,0 @@
-const fuzzySearch = (pattern, str) =>
-  [...str].reduce(
-    (matchIndex, char) => char.toLowerCase() === (pattern[matchIndex]  || '').toLowerCase() ? matchIndex + 1 : matchIndex, 0
-  ) === pattern.length ? true : false;
-module.exports = fuzzySearch;
