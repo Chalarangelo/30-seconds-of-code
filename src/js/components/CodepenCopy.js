6ef9c2d53adfcacbdc40b7badcacbf711306d816
Angelos Chalaris <chalarangelo@gmail.com>
2018-03-03 13:38:08 +0200

    Fixed quote issue

--- src/js/components/CodepenCopy.js
+++ src/js/components/CodepenCopy.js
@@ -15,10 +15,13 @@ snippets.forEach(snippet => {
   var css = snippet.querySelector('pre code.lang-css');
   var html = snippet.querySelector('pre code.lang-html');
   var js = snippet.querySelector('pre code.lang-js');
-  css = css ? css.textContent  : '';
-  html = html ? html.textContent  : '';
-  js = js ? js.textContent  : '';
-  codepenInput.value = '{"title":"'+snippet.querySelector('h3 > span').textContent+'", "html":"'+html+'" , "css":"'+css+'" , "js":"'+js+'" }';
+  console.log(css);
+  codepenInput.value = `{
+    "title": "${snippet.querySelector('h3 > span').textContent}",
+    "css": "${css.textContent}"
+    ${html ? `,"html" : "${html.textContent.replace(/"/g, "&apos;")}"` : ''}
+    ${js ? `,"js" : "${js.textContent.replace(/"/g, "&apos;")}"` : ''}
+  }`.replace(/"/g, "&quot;");//, html: ${html}, css: ${css}, js: ${js}}`;
   codepenForm.appendChild(codepenInput);
   codepenForm.appendChild(codepenButton);
   snippet.appendChild(codepenForm);
