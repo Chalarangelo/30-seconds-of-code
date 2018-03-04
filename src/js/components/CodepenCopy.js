023ba5842ec2d0eb379577438abec003e6cb3031
atomiks <cc.glows@gmail.com>
2018-03-04 14:24:22 +1000

    rebuild docs

--- src/js/components/CodepenCopy.js
+++ src/js/components/CodepenCopy.js
@@ -2,27 +2,27 @@ import { selectAll } from '../deps/utils'
 
 const snippets = selectAll('.snippet')
 snippets.forEach(snippet => {
-  var codepenForm = document.createElement('form');
-  codepenForm.action = 'https://codepen.io/pen/define';
-  codepenForm.method = 'POST';
-  codepenForm.target = '_blank';
-  var codepenInput = document.createElement('input');
-  codepenInput.type = 'hidden';
-  codepenInput.name = 'data';
-  var codepenButton = document.createElement('button');
-  codepenButton.classList = 'btn is-large codepen-btn';
-  codepenButton.innerHTML = '<i data-feather="eye"></i>Edit on Codepen';
-  var css = snippet.querySelector('pre code.lang-css');
-  var html = snippet.querySelector('pre code.lang-html');
-  var js = snippet.querySelector('pre code.lang-js');
+  var codepenForm = document.createElement('form')
+  codepenForm.action = 'https://codepen.io/pen/define'
+  codepenForm.method = 'POST'
+  codepenForm.target = '_blank'
+  var codepenInput = document.createElement('input')
+  codepenInput.type = 'hidden'
+  codepenInput.name = 'data'
+  var codepenButton = document.createElement('button')
+  codepenButton.classList = 'btn is-large codepen-btn'
+  codepenButton.innerHTML = '<i data-feather="eye"></i>Edit on Codepen'
+  var css = snippet.querySelector('pre code.lang-css')
+  var html = snippet.querySelector('pre code.lang-html')
+  var js = snippet.querySelector('pre code.lang-js')
   var data = {
-    css : css.textContent,
+    css: css.textContent,
     title: snippet.querySelector('h3 > span').textContent,
     html: html ? html.textContent : '',
     js: js ? js.textContent : ''
   }
-  codepenInput.value = JSON.stringify(data);
-  codepenForm.appendChild(codepenInput);
-  codepenForm.appendChild(codepenButton);
-  snippet.insertBefore(codepenForm, snippet.querySelector('.snippet-demo').nextSibling);
-});
+  codepenInput.value = JSON.stringify(data)
+  codepenForm.appendChild(codepenInput)
+  codepenForm.appendChild(codepenButton)
+  snippet.insertBefore(codepenForm, snippet.querySelector('.snippet-demo').nextSibling)
+})
