991999e520cdcc39fa7acbfaf80535a84a9e1cc9
Rohit Tanwar <31792358+kriadmin@users.noreply.github.com>
2018-03-07 17:07:26 +0530

    Update Tag.js

--- src/js/components/Tag.js
+++ src/js/components/Tag.js
@@ -1,14 +1,27 @@
 import { select, selectAll, on } from '../deps/utils'
 
 const tagButtons = selectAll('button.tags__tag')
-
+let isShiftSelected = false;
 const onClick = function() {
+  if(isShiftSelected){
+    document.querySelector.classList.remove('is-active')
+  }
+  else{
   tagButtons.forEach(button => button.classList.remove('is-active'))
+  }
   this.classList.add('is-active')
 
   EventHub.emit('Tag.click', {
     type: this.dataset.type
   })
 }
+onkeydown = e => (
+  if(e.shiftKey){
+    isShiftSelected = true;
+});
 
+onkeyup = e => (
+  if(e.shiftKey){
+    isShiftSelected = false;
+});
 tagButtons.forEach(button => on(button, 'click', onClick))
