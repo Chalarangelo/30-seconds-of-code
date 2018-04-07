11e17a706d894739c5f8f0f6cc2050114874b215
Angelos Chalaris <chalarangelo@gmail.com>
2018-04-07 10:43:42 +0300

    Individual pages for snippet tags
    Mostyl complete

--- scripts/web.js
+++ scripts/web.js
@@ -54,6 +54,7 @@ let snippets = {},
   startPart = '',
   endPart = '',
   output = '',
+  pagesOutput = [];
   tagDbData = {};
 // Start the timer of the script
 console.time('Webber');
@@ -61,8 +62,8 @@ console.time('Webber');
 snippets = util.readSnippets(snippetsPath);
 // Load static parts for the index.html file
 try {
-  startPart = fs.readFileSync(path.join(staticPartsPath, 'index-start.html'), 'utf8');
-  endPart = fs.readFileSync(path.join(staticPartsPath, 'index-end.html'), 'utf8');
+  startPart = fs.readFileSync(path.join(staticPartsPath, 'page-start.html'), 'utf8');
+  endPart = fs.readFileSync(path.join(staticPartsPath, 'page-end.html'), 'utf8');
 } catch (err) {
   // Handle errors (hopefully not!)
   console.log(`${chalk.red('ERROR!')} During static part loading: ${err}`);
@@ -78,8 +79,7 @@ try {
   for (let tag of [...new Set(Object.entries(tagDbData).map(t => t[1][0]))]
     .filter(v => v)
     .sort((a, b) => util.capitalize(a, true) === 'Uncategorized' ? 1 : util.capitalize(b, true) === 'Uncategorized' ? -1 : a.localeCompare(b))) {
-      output +=
-        `<h3>` +
+        output += `<h3>` +
         md
           .render(`${util.capitalize(tag, true)}\n`)
           .replace(/<p>/g, '')
@@ -87,7 +87,7 @@ try {
         `</h3>`;
       for (let taggedSnippet of Object.entries(tagDbData).filter(v => v[1][0] === tag))
         output += md
-          .render(`[${taggedSnippet[0]}](#${taggedSnippet[0].toLowerCase()})\n`)
+          .render(`[${taggedSnippet[0]}](./${tag}.html#${taggedSnippet[0].toLowerCase()})\n`)
           .replace(/<p>/g, '')
           .replace(/<\/p>/g, '')
           .replace(/<a/g, `<a class="sublink-1" tags="${taggedSnippet[1].join(',')}"`);
@@ -99,11 +99,12 @@ try {
   for (let tag of [...new Set(Object.entries(tagDbData).map(t => t[1][0]))]
     .filter(v => v)
     .sort((a, b) => util.capitalize(a, true) === 'Uncategorized' ? 1 : util.capitalize(b, true) === 'Uncategorized' ? -1 : a.localeCompare(b))) {
-      output += md
+      let localOutput = output.replace(/\$tag/g, util.capitalize(tag)).replace(new RegExp(`./${tag}.html#`,'g'),'#');
+      localOutput += md
         .render(`## ${util.capitalize(tag, true)}\n`)
         .replace(/<h2>/g, '<h2 style="text-align:center;">');
       for (let taggedSnippet of Object.entries(tagDbData).filter(v => v[1][0] === tag))
-        output +=
+        localOutput +=
           '<div class="card fluid">' +
           md
             .render(`\n${snippets[taggedSnippet[0] + '.md']}`)
@@ -114,66 +115,43 @@ try {
             .replace(/<\/pre>\s+<pre/g, '</pre><label class="collapse">Show examples</label><pre') +
           '<button class="primary clipboard-copy">&#128203;&nbsp;Copy to clipboard</button>' +
           '</div></div>';
+          // Add the ending static part
+          localOutput += `\n${endPart + '\n'}`;
+          localOutput = util.optimizeNodes(localOutput, /<span class="token punctuation">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token punctuation">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token punctuation">${p1}${p2}${p3}</span>`);
+          // Optimize operator nodes
+          localOutput = util.optimizeNodes(localOutput, /<span class="token operator">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token operator">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token operator">${p1}${p2}${p3}</span>`);
+          // Optimize keyword nodes
+          localOutput = util.optimizeNodes(localOutput, /<span class="token keyword">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token keyword">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token keyword">${p1}${p2}${p3}</span>`);
+          pagesOutput.push({'tag': tag,'content': localOutput});
   }
-  // Add the ending static part
-  output += `\n${endPart + '\n'}`;
-  // Optimize punctuation nodes
-  output = util.optimizeNodes(output, /<span class="token punctuation">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token punctuation">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token punctuation">${p1}${p2}${p3}</span>`);
-  // Optimize operator nodes
-  output = util.optimizeNodes(output, /<span class="token operator">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token operator">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token operator">${p1}${p2}${p3}</span>`);
-  // Optimize keyword nodes
-  output = util.optimizeNodes(output, /<span class="token keyword">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token keyword">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token keyword">${p1}${p2}${p3}</span>`);
-  // do {
-  //   const punctuationRegex = /<span class="token punctuation">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token punctuation">([^\0]*?)<\/span>/gm;
-  //   output = output.replace(punctuationRegex,
-  //     (match, p1, p2, p3)  => `<span class="token punctuation">${p1}${p2}${p3}</span>`
-  //   );
-  //   count = 0;
-  //   while (punctuationRegex.exec(output) !== null) {
-  //     ++count;
-  //   }
-  // } while (count > 0);
+  // // Optimize punctuation nodes
+  // output = util.optimizeNodes(output, /<span class="token punctuation">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token punctuation">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token punctuation">${p1}${p2}${p3}</span>`);
   // // Optimize operator nodes
-  // do {
-  //   const operatorRegex = /<span class="token operator">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token operator">([^\0]*?)<\/span>/gm;
-  //   output = output.replace(operatorRegex,
-  //     (match, p1, p2, p3)  => `<span class="token operator">${p1}${p2}${p3}</span>`
-  //   );
-  //   count = 0;
-  //   while (operatorRegex.exec(output) !== null) {
-  //     ++count;
-  //   }
-  // } while (count > 0);
+  // output = util.optimizeNodes(output, /<span class="token operator">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token operator">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token operator">${p1}${p2}${p3}</span>`);
   // // Optimize keyword nodes
-  // do {
-  //   const keyWordRegex = /<span class="token keyword">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token keyword">([^\0]*?)<\/span>/gm;
-  //   output = output.replace(keyWordRegex,
-  //     (match, p1, p2, p3)  => `<span class="token keyword">${p1}${p2}${p3}</span>`
-  //   );
-  //   count = 0;
-  //   while (keyWordRegex.exec(output) !== null) {
-  //     ++count;
-  //   }
-  // } while (count > 0);
+  // output = util.optimizeNodes(output, /<span class="token keyword">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token keyword">([^\0]*?)<\/span>/gm, (match, p1, p2, p3)  => `<span class="token keyword">${p1}${p2}${p3}</span>`);
   // Minify output
-  output = minify(output, {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    decodeEntities: false,
-    minifyCSS: true,
-    minifyJS: true,
-    keepClosingSlash: true,
-    processConditionalComments: true,
-    removeAttributeQuotes: false,
-    removeComments: true,
-    removeEmptyAttributes: false,
-    removeOptionalTags: false,
-    removeScriptTypeAttributes: false,
-    removeStyleLinkTypeAttributes: false,
-    trimCustomFragments: true
-  });
+  pagesOutput.forEach(page => {
+    page.content = minify(page.content, {
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      decodeEntities: false,
+      minifyCSS: true,
+      minifyJS: true,
+      keepClosingSlash: true,
+      processConditionalComments: true,
+      removeAttributeQuotes: false,
+      removeComments: true,
+      removeEmptyAttributes: false,
+      removeOptionalTags: false,
+      removeScriptTypeAttributes: false,
+      removeStyleLinkTypeAttributes: false,
+      trimCustomFragments: true
+    });
+    fs.writeFileSync(path.join(docsPath, page.tag+'.html'), page.content);
+  })
   // Write to the index.html file
-  fs.writeFileSync(path.join(docsPath, 'index.html'), output);
+  //fs.writeFileSync(path.join(docsPath, 'index.html'), output);
 } catch (err) {
   // Handle errors (hopefully not!)
   console.log(`${chalk.red('ERROR!')} During index.html generation: ${err}`);
