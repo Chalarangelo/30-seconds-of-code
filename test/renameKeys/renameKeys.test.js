c24f9d9695b26121b036ad45809d470c567d3339
yazeedb <ybzadough@gmail.com>
2018-04-10 15:46:48 -0400

    use spaces

--- test/renameKeys/renameKeys.test.js
+++ test/renameKeys/renameKeys.test.js
@@ -6,10 +6,10 @@ test('Testing renameKeys', (t) => {
   //Please go to https://github.com/substack/tape
   t.true(typeof renameKeys === 'function', 'renameKeys is a Function');
 
-	const obj = { name: 'Bobo', job: 'Front-End Master', shoeSize: 100 };
-	const renamedObj = renameKeys({ name: 'firstName', job: 'passion' }, obj);
+  const obj = { name: 'Bobo', job: 'Front-End Master', shoeSize: 100 };
+  const renamedObj = renameKeys({ name: 'firstName', job: 'passion' }, obj);
 
-	t.deepEqual(renamedObj, { firstName: 'Bobo', passion: 'Front-End Master', shoeSize: 100 });
+  t.deepEqual(renamedObj, { firstName: 'Bobo', passion: 'Front-End Master', shoeSize: 100 });
 
   t.end();
 });
