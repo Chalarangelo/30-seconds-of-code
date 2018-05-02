02162a9fd50406d0c7b3fa8c8b2834068e7ba60c
30secondsofcode <30secondsofcode@gmail.com>
2018-05-02 21:04:11 +0000

    Travis build: 2024 [cron]

--- test/join/join.js
+++ test/join/join.js
@@ -3,7 +3,9 @@ arr.reduce(
 (acc, val, i) =>
 i === arr.length - 2
 ? acc + val + end
-: i === arr.length - 1 ? acc + val : acc + val + separator,
+: i === arr.length - 1
+? acc + val
+: acc + val + separator,
 ''
 );
 module.exports = join;
