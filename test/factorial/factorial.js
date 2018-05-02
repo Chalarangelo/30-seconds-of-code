d91c9da37c37b054d0f8f79861a4402a9357e02f
30secondsofcode <30secondsofcode@gmail.com>
2018-05-02 17:27:32 +0000

    Travis build: 2022

--- test/factorial/factorial.js
+++ test/factorial/factorial.js
@@ -3,5 +3,7 @@ n < 0
 ? (() => {
 throw new TypeError('Negative numbers are not allowed!');
 })()
-: n <= 1 ? 1 : n * factorial(n - 1);
+: n <= 1
+? 1
+: n * factorial(n - 1);
 module.exports = factorial;
