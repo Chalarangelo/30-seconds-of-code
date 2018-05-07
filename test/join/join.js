629c113bd522412870acb02291a1a776cf01b858
Vinod Pahuja <vinodpahuja@users.noreply.github.com>
2018-05-07 11:06:31 +0530

    Revert "Travis build: 2024 [cron]"
    This reverts commit abf99f857653719c9c68cffbb6b2d661033e59c9.

--- test/join/join.js
+++ test/join/join.js
@@ -3,9 +3,7 @@ arr.reduce(
 (acc, val, i) =>
 i === arr.length - 2
 ? acc + val + end
-: i === arr.length - 1
-? acc + val
-: acc + val + separator,
+: i === arr.length - 1 ? acc + val : acc + val + separator,
 ''
 );
 module.exports = join;
