78e3890e982358d965a21ecbd6fc2a860898d10e
oh <oscar.shrimpton.personal@gmail.com>
2018-05-09 20:01:55 +0100

    fix(deepClone): Fixed problems with array values
    Would previously create an object with indices as keys. Now properly clones as array.
    
    #658

--- test/deepClone/deepClone.test.js
+++ test/deepClone/deepClone.test.js
@@ -7,8 +7,12 @@ test('Testing deepClone', (t) => {
   t.true(typeof deepClone === 'function', 'deepClone is a Function');
   const a = { foo: 'bar', obj: { a: 1, b: 2 } };
   const b = deepClone(a);
+  const c = [{foo: 'bar'}];
+  const d = deepClone(c);
   t.notEqual(a, b, 'Shallow cloning works');
   t.notEqual(a.obj, b.obj, 'Deep cloning works');
+  t.notEqual(c, d, 'Array shallow cloning works');
+  t.notEqual(c[0], d[0], 'Array deep cloning works');
   //t.deepEqual(deepClone(args..), 'Expected');
   //t.equal(deepClone(args..), 'Expected');
   //t.false(deepClone(args..), 'Expected');
