efe98121f7e27918f21040dddad0c17866637db1
Siarhei <siarhei.zharnasek@gmail.com>
2018-05-14 16:51:56 +0400

    Simplify findLastIndex

--- test/findLastIndex/findLastIndex.js
+++ test/findLastIndex/findLastIndex.js
@@ -1,6 +1,6 @@
 const findLastIndex = (arr, fn) =>
 arr
 .map((val, i) => [i, val])
-.filter(val => fn(val[1], val[0], arr))
-.slice(-1)[0][0];
+.filter(([i, val]) => fn(val, i, arr))
+.pop()[0];
 module.exports = findLastIndex;
