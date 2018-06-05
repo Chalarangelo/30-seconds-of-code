2d16ed2da62024de4fe187d3f6cbc5047c981c3f
30secondsofcode <30secondsofcode@gmail.com>
2018-06-05 15:11:04 +0000

    Travis build: 2127

--- test/countOccurrences/countOccurrences.js
+++ test/countOccurrences/countOccurrences.js
@@ -1,2 +1,2 @@
-const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a + 0), 0);
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
 module.exports = countOccurrences;
