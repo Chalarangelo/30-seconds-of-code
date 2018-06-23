ec5c61a9d7a000dac1cad7bc055f43f392eff550
30secondsofcode <30secondsofcode@gmail.com>
2018-06-23 19:31:04 +0000

    Travis build: 18 [cron]

--- test/equals/equals.js
+++ test/equals/equals.js
@@ -1,7 +1,7 @@
 const equals = (a, b) => {
 if (a === b) return true;
 if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
-if (!a || !b || (typeof a != 'object' && typeof b !== 'object')) return a === b;
+if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) return a === b;
 if (a === null || a === undefined || b === null || b === undefined) return false;
 if (a.prototype !== b.prototype) return false;
 let keys = Object.keys(a);
