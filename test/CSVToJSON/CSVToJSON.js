03b9b97f3a5c74a9cc96ea8974fbe4c318b2c385
30secondsofcode <30secondsofcode@gmail.com>
2018-06-27 19:32:51 +0000

    Travis build: 33 [cron]

--- test/CSVToJSON/CSVToJSON.js
+++ test/CSVToJSON/CSVToJSON.js
@@ -1,8 +1,11 @@
 const CSVToJSON = (data, delimiter = ',') => {
-const titles = data.slice(0,data.indexOf('\n')).split(delimiter);
-return data.slice(data.indexOf('\n')+1).split('\n').map(v => {
+const titles = data.slice(0, data.indexOf('\n')).split(delimiter);
+return data
+.slice(data.indexOf('\n') + 1)
+.split('\n')
+.map(v => {
 const values = v.split(delimiter);
 return titles.reduce((obj, title, index) => ((obj[title] = values[index]), obj), {});
 });
-}
+};
 module.exports = CSVToJSON;
