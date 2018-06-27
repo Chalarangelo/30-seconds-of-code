5ef18a9a551ec5ce1b10926c7ef3bddcdb98246c
Angelos Chalaris <chalarangelo@gmail.com>
2018-06-27 21:14:24 +0300

    Add CSVToJSON

---
+++ test/CSVToJSON/CSVToJSON.js
@@ -0,0 +1,8 @@
+const CSVToJSON = (data, delimiter = ',') => {
+const titles = data.slice(0,data.indexOf('\n')).split(delimiter);
+return data.slice(data.indexOf('\n')+1).split('\n').map(v => {
+const values = v.split(delimiter);
+return titles.reduce((obj, title, index) => ((obj[title] = values[index]), obj), {});
+});
+}
+module.exports = CSVToJSON;
