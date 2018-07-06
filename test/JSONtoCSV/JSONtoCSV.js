5e0b1e113b9d5320e87981bb0a5661dbd6a4a979
Angelos Chalaris <chalarangelo@gmail.com>
2018-07-06 20:25:46 +0300

    Add JSONtoCSV
    Resolve #686

---
+++ test/JSONtoCSV/JSONtoCSV.js
@@ -0,0 +1,11 @@
+const JSONtoCSV = (arr, columns, delimiter = ',') =>
+[
+columns.join(delimiter),
+...arr.map(obj =>
+columns.reduce(
+(acc, key) => `${acc}${!acc.length ? '' : delimiter}"${!obj[key] ? '' : obj[key]}"`,
+''
+)
+)
+].join('\n');
+module.exports = JSONtoCSV;
---
+++ test/JSONtoCSV/JSONtoCSV.test.js
@@ -0,0 +1,12 @@
+const expect = require('expect');
+const JSONtoCSV = require('./JSONtoCSV.js');
+
+test('JSONtoCSV is a Function', () => {
+  expect(JSONtoCSV).toBeInstanceOf(Function);
+});
+test('JSONtoCSV works with default delimiter', () => {
+  expect(JSONtoCSV([{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }], ['a', 'b'])).toBe('a,b\n"1","2"\n"3","4"\n"6",""\n"","7"');
+});
+test('JSONtoCSV works with custom delimiter', () => {
+  expect(JSONtoCSV([{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }], ['a', 'b'], ';')).toBe('a;b\n"1";"2"\n"3";"4"\n"6";""\n"";"7"');
+});
