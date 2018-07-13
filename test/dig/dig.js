345b4a5a311f1336a41e4bd440e39966cc4a6ca4
Henry Szeto <henryszeto@gmail.com>
2018-07-13 11:02:11 -0700

    fix issues reported by Codacy

--- test/dig/dig.js
+++ test/dig/dig.js
@@ -4,8 +4,12 @@ const dig = (obj, target) =>
     : Object
         .values(obj)
         .reduce((acc, val) => {
-          if (acc !== undefined) return acc;
-          if (typeof val === 'object') return dig(val, target);
+          if (acc !== undefined) {
+            return acc
+          };
+          if (typeof val === "object") {
+            return dig(val, target)
+          };
         }, undefined);
 
 module.exports = dig;
