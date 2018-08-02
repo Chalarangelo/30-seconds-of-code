43af1264ee04f84ed3e80b82e507165339ca345f
30secondsofcode <30secondsofcode@gmail.com>
2018-08-02 19:51:06 +0000

    Travis build: 176 [cron]

--- test/indexOfAll/indexOfAll.js
+++ test/indexOfAll/indexOfAll.js
@@ -1,2 +1,3 @@
-const indexOfAll = (arr, val) => arr.reduce((acc, el, i) => el === val ? [...acc, i] : acc, []);
+const indexOfAll = (arr, val) => (arr, val) =>
+  arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), []);
 module.exports = indexOfAll;
