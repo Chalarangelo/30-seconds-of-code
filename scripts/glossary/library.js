5630ae1d98f5d5a3c900c11582942b2d040ef62f
Angelos Chalaris <chalarangelo@gmail.com>
2018-08-02 11:33:11 +0300

    Codacy style update for library.js

--- scripts/glossary/library.js
+++ scripts/glossary/library.js
@@ -11,46 +11,46 @@ const glossaryFiles = util.getFilesInDir('./glossary', true, ['keyword_database'
 const fileTitles = [];
 
 const getGlossaryTermMarkdownBlock = (fileName) => {
-	let fileContent = fs.readFileSync(fileName, 'utf8');
+  let fileContent = fs.readFileSync(fileName, 'utf8');
 	
-	let title = fileContent.match(/###[^\n]*/)[0].replace('### ', '').trim();
-	// let description = fileContent.replace(title, '').trim();
-	fileTitles.push(title);
+  let title = fileContent.match(/###[^\n]*/)[0].replace('### ', '').trim();
+  // let description = fileContent.replace(title, '').trim();
+  fileTitles.push(title);
 
-	return fileContent.trim() + "\n";
+  return fileContent.trim() + "\n";
 };
 
 const glossaryFilesContentReducer = (accumulator, currentFilename) => {
-	// handle first array item
-	if (accumulator === glossaryFiles[0]) {
-		return getGlossaryTermMarkdownBlock(accumulator) + "\n" + getGlossaryTermMarkdownBlock(currentFilename);
-	}
-	return accumulator + "\n" + getGlossaryTermMarkdownBlock(currentFilename);
+  // handle first array item
+  if (accumulator === glossaryFiles[0]) {
+    return getGlossaryTermMarkdownBlock(accumulator) + "\n" + getGlossaryTermMarkdownBlock(currentFilename);
+  }
+  return accumulator + "\n" + getGlossaryTermMarkdownBlock(currentFilename);
 };
 
 const getTermLinkMarkdownBlock = (termTitle) => {
-	let anchor = util.getMarkDownAnchor(termTitle);
-	return `* [\`${termTitle}\`](#${anchor})` + "\n";
+  let anchor = util.getMarkDownAnchor(termTitle);
+  return `* [\`${termTitle}\`](#${anchor})` + "\n";
 };
 
 const glossaryTableOfContentsReducer = (accumulator, currentFile) => {
-	if (accumulator === fileTitles[0]) {
-		return getTermLinkMarkdownBlock(accumulator) + getTermLinkMarkdownBlock(currentFile);
-	}
-	return accumulator + getTermLinkMarkdownBlock(currentFile);
+  if (accumulator === fileTitles[0]) {
+    return getTermLinkMarkdownBlock(accumulator) + getTermLinkMarkdownBlock(currentFile);
+  }
+  return accumulator + getTermLinkMarkdownBlock(currentFile);
 };
 
 try {
-	const fileContents = glossaryFiles.reduce(glossaryFilesContentReducer);
-	const TOC = "## Table of Contents\n\n" + fileTitles.reduce(glossaryTableOfContentsReducer);
+  const fileContents = glossaryFiles.reduce(glossaryFilesContentReducer);
+  const TOC = "## Table of Contents\n\n" + fileTitles.reduce(glossaryTableOfContentsReducer);
 	
-	const README =
-		"# 30-seconds-of-code JavaScript Glossary\n\n" +
-		TOC +
-		"\n\n" +
-		fileContents;
+  const README =
+    "# 30-seconds-of-code JavaScript Glossary\n\n" +
+    TOC +
+    "\n\n" +
+    fileContents;
   fs.writeFileSync('glossary/README.md', README);	
 } catch (err) {
-	console.log(`${chalk.red('ERROR!')} During glossary README generation: ${err}`);
+  console.log(`${chalk.red('ERROR!')} During glossary README generation: ${err}`);
   process.exit(1);
 }
