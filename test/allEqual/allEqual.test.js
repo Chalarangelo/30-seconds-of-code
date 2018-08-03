0b66dd6dc3b47e0503a22489938f36f5d45d1104
Berkmann18 <maxieberkmann@gmail.com>
2018-08-03 02:18:56 +0100

    Updated relevant files

--- test/allEqual/allEqual.test.js
+++ test/allEqual/allEqual.test.js
@@ -1,38 +1,38 @@
-const expect = require('expect');
-const allEqual = require('./allEqual.js');
+const expect = require("expect");
+const allEqual = require("./allEqual.js");
 
-test('allEqual is a Function', () => {
+test("allEqual is a Function", () => {
   expect(allEqual).toBeInstanceOf(Function);
 });
 
-test('Truthy numbers', () => {
+test("Truthy numbers", () => {
   expect(allEqual([4, 4, 4])).toBeTruthy();
 });
 
-test('Falsy numbers', () => {
+test("Falsy numbers", () => {
   expect(allEqual([4, 3.999, 4])).toBeFalsy();
 });
 
-test('Truthy strings', () => {
-  expect(allEqual(['lorem', 'lorem', 'lorem'])).toBeTruthy();
+test("Truthy strings", () => {
+  expect(allEqual(["lorem", "lorem", "lorem"])).toBeTruthy();
 });
 
-test('Falsy numbers', () => {
-  expect(allEqual(['lorem', 'ipsum', 'dolore'])).toBeFalsy();
+test("Falsy numbers", () => {
+  expect(allEqual(["lorem", "ipsum", "dolore"])).toBeFalsy();
 });
 
-test('Truthy trues', () => {
+test("Truthy trues", () => {
   expect(allEqual([true, true, true])).toBeTruthy();
 });
 
-test('Truthy falses', () => {
+test("Truthy falses", () => {
   expect(allEqual([false, false, false])).toBeTruthy();
 });
 
-test('Falsy trues', () => {
+test("Falsy trues", () => {
   expect(allEqual([true, true, false])).toBeFalsy();
 });
 
-test('Falsy falses', () => {
+test("Falsy falses", () => {
   expect(allEqual([false, false, true])).toBeFalsy();
 });
