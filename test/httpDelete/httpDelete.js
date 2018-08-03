c5c0375f2cb59c006e842a3f08d4a749fd180953
30secondsofcode <30secondsofcode@gmail.com>
2018-08-03 19:51:50 +0000

    Travis build: 188 [cron]

--- test/httpDelete/httpDelete.js
+++ test/httpDelete/httpDelete.js
@@ -1,6 +1,6 @@
 const httpDelete = (url, callback, err = console.error) => {
   const request = new XMLHttpRequest();
-  request.open('DELETE', url, true);
+  request.open("DELETE", url, true);
   request.onload = () => callback(request);
   request.onerror = () => err(request);
   request.send();
