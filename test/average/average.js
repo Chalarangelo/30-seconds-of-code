4c199cca6504873ee15964ae9f20edda14cb2833
Siarhei <siarhei.zharnasek@gmail.com>
2018-08-08 14:23:35 +0400

    Simplify average

--- test/average/average.js
+++ test/average/average.js
@@ -1,2 +1,2 @@
-const average = (...nums) => [...nums].reduce((acc, val) => acc + val, 0) / nums.length;
+const average = (...nums) => nums.reduce((acc, val) => acc + val, 0) / nums.length;
 module.exports = average;
