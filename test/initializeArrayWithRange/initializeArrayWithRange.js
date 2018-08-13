7cb2bff46fc648e5ecb2a68bf07a3653d3a6dffa
30secondsofcode <30secondsofcode@gmail.com>
2018-08-13 19:58:48 +0000

    Travis build: 224 [cron]

--- test/initializeArrayWithRange/initializeArrayWithRange.js
+++ test/initializeArrayWithRange/initializeArrayWithRange.js
@@ -1,3 +1,3 @@
 const initializeArrayWithRange = (end, start = 0, step = 1) =>
-  Array.from({length: Math.ceil((end - start + 1) / step)}, (v, i) => i * step + start);
+  Array.from({ length: Math.ceil((end - start + 1) / step) }, (v, i) => i * step + start);
 module.exports = initializeArrayWithRange;
