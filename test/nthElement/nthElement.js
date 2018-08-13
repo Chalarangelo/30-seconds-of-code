59183e03b3483ce72b29b1479829bba9ab4bbe83
Bruce Feldman <bfeldman@sunriselabs.com>
2018-08-12 21:01:48 -0400

    Simplified nth Element

--- test/nthElement/nthElement.js
+++ test/nthElement/nthElement.js
@@ -1,2 +1,2 @@
-const nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+const nthElement = (arr, n = 0) => (n === -1 ? arr.slice(n) : arr.slice(n, n + 1))[0];
 module.exports = nthElement;
