8e60551a23b3d61b652efec26de411d9c982e51d
30secondsofcode <30secondsofcode@gmail.com>
2018-09-01 20:05:19 +0000

    Travis build: 342 [cron]

--- test/deepFreeze/deepFreeze.js
+++ test/deepFreeze/deepFreeze.js
@@ -1,17 +1,6 @@
-const deepFreeze = obj => {
-  Object.freeze(obj);
-
-  Object.getOwnPropertyNames(obj).forEach(function(prop) {
-    if (
-      obj.hasOwnProperty(prop) &&
-      obj[prop] !== null &&
-      (typeof obj[prop] === 'object' || typeof obj[prop] === 'function') &&
-      !Object.isFrozen(obj[prop])
-    ) {
-      deepFreeze(obj[prop]);
-    }
-  });
-
-  return obj;
-};
+const deepFreeze = obj =>
+  Object.keys(obj).forEach(
+    prop =>
+      !obj[prop] instanceof Object || Object.isFrozen(obj[prop]) ? null : deepFreeze(obj[prop])
+  ) || Object.freeze(obj);
 module.exports = deepFreeze;
