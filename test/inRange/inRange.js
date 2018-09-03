bb32167637a844a5d159c57532554cd652caa292
Siarhei <siarhei.zharnasek@gmail.com>
2018-09-03 10:27:11 +0400

    Simplify inRange

--- test/inRange/inRange.js
+++ test/inRange/inRange.js
@@ -1,5 +1,5 @@
 const inRange = (n, start, end = null) => {
-  if (end && start > end) end = [start, (start = end)][0];
+  if (end && start > end) [end, start] = [start, end];
   return end == null ? n >= 0 && n < start : n >= start && n < end;
 };
 module.exports = inRange;
