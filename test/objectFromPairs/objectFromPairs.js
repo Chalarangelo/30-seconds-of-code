e14880984b741d75e8d1dc3403ea49f12fd9554e
Siarhei <siarhei.zharnasek@gmail.com>
2018-09-07 17:46:58 +0400

    Simplify objectFromPairs

--- test/objectFromPairs/objectFromPairs.js
+++ test/objectFromPairs/objectFromPairs.js
@@ -1,2 +1,2 @@
-const objectFromPairs = arr => arr.reduce((a, v) => ((a[v[0]] = v[1]), a), {});
+const objectFromPairs = arr => arr.reduce((a, [key, val]) => ((a[key] = val), a), {});
 module.exports = objectFromPairs;
