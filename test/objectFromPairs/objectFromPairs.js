9469cabbb90fb2fada9a15b72e3d2943686654fe
Siarhei <siarhei.zharnasek@gmail.com>
2018-09-10 09:03:26 +0400

    Remove parentheses for objectFromPairs argument

--- test/objectFromPairs/objectFromPairs.js
+++ test/objectFromPairs/objectFromPairs.js
@@ -1,2 +1,2 @@
-const objectFromPairs = (arr) => arr.reduce((a, [key, val]) => ((a[key] = val), a), {});
+const objectFromPairs = arr => arr.reduce((a, [key, val]) => ((a[key] = val), a), {});
 module.exports = objectFromPairs;
