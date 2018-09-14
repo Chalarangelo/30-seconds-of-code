07f0c9889a8b428cef07442b060581eac3ae6e74
Siarhei <siarhei.zharnasek@gmail.com>
2018-09-14 12:26:08 +0400

    Add unit tests for deepFreeze

--- test/deepFreeze/deepFreeze.test.js
+++ test/deepFreeze/deepFreeze.test.js
@@ -4,3 +4,32 @@ const deepFreeze = require('./deepFreeze.js');
 test('deepFreeze is a Function', () => {
   expect(deepFreeze).toBeInstanceOf(Function);
 });
+
+test('modifying deeply freezed object prop throws an error in strict mode', () => {
+  let freezedObj = deepFreeze({a: 42});
+
+  expect(() => {
+    'use strict';
+    freezedObj.a = 24;
+  }).toThrow();
+});
+
+test('should not modify deeply freezed object inside another object', () => {
+  let freezedObj = deepFreeze({
+    a: {
+      b: 42
+    }
+  });
+
+  freezedObj.a.b = 16;
+
+  expect(freezedObj.a.b).toBe(42);
+});
+
+test('should not add prop to deeply freezed empty object', () => {
+  let freezedObj = deepFreeze({});
+
+  freezedObj.a = 42;
+
+  expect(freezedObj).toEqual({});
+});
