e85aa45bbbc147198c6379a56d10a49f73d52aec
30secondsofcode <30secondsofcode@gmail.com>
2018-09-15 14:03:26 +0000

    Travis build: 466

--- test/httpPut/httpPut.js
+++ test/httpPut/httpPut.js
@@ -1,7 +1,7 @@
 const httpPut = (url, data, callback, err = console.error) => {
   const request = new XMLHttpRequest();
-  request.open('PUT', url, true);
-  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.open("PUT", url, true);
+  request.setRequestHeader('Content-type','application/json; charset=utf-8');
   request.onload = () => callback(request);
   request.onerror = () => err(request);
   request.send(data);
