77167a059de5bb10ca599eba34f1aed720e21a28
Theo Menkes Reis <theomreis@gmail.com>
2018-09-25 00:21:18 +0000

    simpler solution to mask using padStart

--- test/mask/mask.js
+++ test/mask/mask.js
@@ -1,3 +1,3 @@
 const mask = (cc, num = 4, mask = '*') =>
-  ('' + cc).slice(0, -num).replace(/./g, mask) + ('' + cc).slice(-num);
+  ('' + cc).slice(-num).padStart(('' + cc).length, mask);
 module.exports = mask;
