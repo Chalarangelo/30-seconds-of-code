596e2ca8493cc47e5a9f0696de074819b32a51c3
30secondsofcode <30secondsofcode@gmail.com>
2018-09-27 20:14:50 +0000

    Travis build: 543 [cron]

--- test/shank/shank.js
+++ test/shank/shank.js
@@ -1,5 +1,6 @@
-const shank = (arr, index = 0, delCount = 0, ...elements) => 
-  arr.slice(0, index)
-     .concat(elements)
-     .concat(arr.slice(index + delCount));
+const shank = (arr, index = 0, delCount = 0, ...elements) =>
+  arr
+    .slice(0, index)
+    .concat(elements)
+    .concat(arr.slice(index + delCount));
 module.exports = shank;
