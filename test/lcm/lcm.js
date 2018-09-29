b111778a1c195478e9af84a29d773a1f3dbdd379
30secondsofcode <30secondsofcode@gmail.com>
2018-09-29 20:15:22 +0000

    Travis build: 563 [cron]

--- test/lcm/lcm.js
+++ test/lcm/lcm.js
@@ -1,6 +1,6 @@
 const lcm = (...arr) => {
   const gcd = (x, y) => (!y ? x : gcd(y, x % y));
-  const _lcm = (x, y) => x * y / gcd(x, y);
+  const _lcm = (x, y) => (x * y) / gcd(x, y);
   return [...arr].reduce((a, b) => _lcm(a, b));
 };
 module.exports = lcm;
