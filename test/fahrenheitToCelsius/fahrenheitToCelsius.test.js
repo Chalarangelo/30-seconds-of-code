12fa85a6e2ef9ea4942892000155c469610a7968
evyatarmeged <evyatarmeged@gmail.com>
2018-10-01 15:26:36 +0300

    add celsiusToFahrenheit and fahrenheitToCelsius

---
+++ test/fahrenheitToCelsius/fahrenheitToCelsius.test.js
@@ -0,0 +1,26 @@
+const expect = require('expect');
+const fahrenheitToCelsius = require('./fahrenheitToCelsius.js');
+
+test('fahrenheitToCelsius is a Function', () => {
+  expect(fahrenheitToCelsius).toBeInstanceOf(Function);
+});
+
+test('32 Fahrenheit is 0 Celsius', () => {
+	expect(fahrenheitToCelsius(32)).toBe(0)
+})
+
+test('212 Fahrenheit is 100 Celsius', () => {
+	expect(fahrenheitToCelsius(212)).toBe(100)
+})
+
+test('150 Fahrenheit is 65.55555555555556 Celsius', () => {
+	expect(fahrenheitToCelsius(150)).toBe(65.55555555555556)
+})
+
+test('1000 Fahrenheit is 537.7777777777778', () => {
+	expect(fahrenheitToCelsius(1000)).toBe(537.7777777777778)
+})
+
+test('Not a number value is NaN', () => {
+	expect(fahrenheitToCelsius("Durr")).toBe(NaN)
+})
