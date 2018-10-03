630de45d45b9fff368415f40182bf2983385d03e
itchylol742 <42976312+itchylol742@users.noreply.github.com>
2018-10-03 13:47:41 -0400

    Update tomorrow.js
    simplified code and made it pass the tester

--- test/tomorrow/tomorrow.js
+++ test/tomorrow/tomorrow.js
@@ -1,9 +1,9 @@
 const tomorrow = (long = false) => {
   let t = new Date();
   t.setDate(t.getDate() + 1);
-  const ret = `${t.getFullYear()}-${String(t.getMonth() + 1).padStart(2, '0')}-${String(
-    t.getDate()
-  ).padStart(2, '0')}`;
-  return !long ? ret : `${ret}T00:00:00`;
+  if (long){
+    return `${t}T00:00:00`;
+  }
+  return t;
 };
 module.exports = tomorrow;
