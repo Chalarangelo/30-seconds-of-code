1e43dc7b89bb48cc7770a6c38d9495accbb2ed4b
Filip Danić <filip@spicefactory.co>
2018-10-17 11:36:41 +0200

    Improve the readability of tomorrow.js (solved #775)

--- test/tomorrow/tomorrow.js
+++ test/tomorrow/tomorrow.js
@@ -1,9 +1,7 @@
-const tomorrow = (long = false) => {
+const tomorrow = () => {
   let t = new Date();
   t.setDate(t.getDate() + 1);
-  const ret = `${t.getFullYear()}-${String(t.getMonth() + 1).padStart(2, '0')}-${String(
-    t.getDate()
-  ).padStart(2, '0')}`;
-  return !long ? ret : `${ret}T00:00:00`;
+  return t.toISOString().split('T')[0];
 };
+
 module.exports = tomorrow;
