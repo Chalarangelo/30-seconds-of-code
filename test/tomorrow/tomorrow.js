24b01ec97c61a9d55cb6621cd76f34599fd3c89d
Filip Danić <filip@spicefactory.co>
2018-10-17 21:37:54 +0200

    revert to previous implementation, but make it cleaner

--- test/tomorrow/tomorrow.js
+++ test/tomorrow/tomorrow.js
@@ -1,7 +1,10 @@
 const tomorrow = () => {
-  let t = new Date();
+  const t = new Date();
   t.setDate(t.getDate() + 1);
-  return t.toISOString().split('T')[0];
+  const year = t.getFullYear();
+  const month = String(t.getMonth() + 1).padStart(2, '0');
+  const date = String(t.getDate()).padStart(2, '0');
+  return `${year}-${month}-${date}`;
 };
 
 module.exports = tomorrow;
