61de9fe59cde690307f1c8cb3233fa4f7cb05d54
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-20 17:10:56 +0300

    Update util.js

--- scripts/util.js
+++ scripts/util.js
@@ -106,7 +106,7 @@ const capitalize = (str, lowerRest = false) =>
 // Checks if current environment is Travis CI
 const isTravisCI = () => 'TRAVIS' in process.env && 'CI' in process.env;
 const isTravisCronOrAPI = () =>
-  process.env['TRAVIS_EVENT_TYPE'] === 'cron' && process.env['TRAVIS_EVENT_TYPE'] === 'api';
+  process.env['TRAVIS_EVENT_TYPE'] === 'cron' || process.env['TRAVIS_EVENT_TYPE'] === 'api';
 const isNotTravisCronOrAPI = () => !isTravisCronOrAPI();
 // Creates a hash for a value using the SHA-256 algorithm.
 const hashData = val =>
