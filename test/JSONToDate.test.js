0ba22da4b03b7a35540ae4f19a5108522ac51d8c
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-26 21:26:34 +0300

    Additional tests

--- test/JSONToDate.test.js
+++ test/JSONToDate.test.js
@@ -4,3 +4,6 @@ const {JSONToDate} = require('./_30s.js');
 test('JSONToDate is a Function', () => {
   expect(JSONToDate).toBeInstanceOf(Function);
 });
+test('JSONToDate returns the correct date string', () => {
+  expect(JSONToDate(/Date(1489525200000)/)).toBe("14/3/2017");
+});
--- test/factors.test.js
+++ test/factors.test.js
@@ -4,3 +4,15 @@ const {factors} = require('./_30s.js');
 test('factors is a Function', () => {
   expect(factors).toBeInstanceOf(Function);
 });
+test('factors returns the correct array', () => {
+  expect(factors(12)).toEqual([2, 3, 4, 6, 12]);
+});
+test('factors returns the correct array of primes', () => {
+  expect(factors(12, true)).toEqual([2, 3]);
+});
+test('factors returns the correct array for negatives', () => {
+  expect(factors(-12)).toEqual([2, -2, 3, -3, 4, -4, 6, -6, 12, -12]);
+});
+test('factors returns the correct array of primes for negatives', () => {
+  expect(factors(-12, true)).toEqual([2, 3]);
+});
