0b16923fef2d4392ac4ecfddab53eee4df338945
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-26 21:08:37 +0300

    Add tests for some archive snippets

--- test/isArmstrongNumber.test.js
+++ test/isArmstrongNumber.test.js
@@ -4,3 +4,9 @@ const {isArmstrongNumber} = require('./_30s.js');
 test('isArmstrongNumber is a Function', () => {
   expect(isArmstrongNumber).toBeInstanceOf(Function);
 });
+test('isArmstrongNumber returns true', () => {
+  expect(isArmstrongNumber(1634)).toBeTruthy();
+});
+test('isArmstrongNumber returns false', () => {
+  expect(isArmstrongNumber(56)).toBeFalsy();
+});
