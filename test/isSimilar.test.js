1cc4f5bdd5537c8d979eba897b18aaf578e0fd33
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-26 21:08:37 +0300

    Add tests for some archive snippets

--- test/isSimilar.test.js
+++ test/isSimilar.test.js
@@ -4,3 +4,9 @@ const {isSimilar} = require('./_30s.js');
 test('isSimilar is a Function', () => {
   expect(isSimilar).toBeInstanceOf(Function);
 });
+test('isSimilar returns true', () => {
+  expect(isSimilar('rt', 'Rohit')).toBeTruthy();
+});
+test('isSimilar returns false', () => {
+  expect(isSimilar('tr', 'Rohit')).toBeFalsy();
+});
