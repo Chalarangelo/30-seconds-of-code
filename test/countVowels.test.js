8d12620b4a44f85b9c742aa3cc658ee339cfe820
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-26 21:26:34 +0300

    Additional tests

--- test/countVowels.test.js
+++ test/countVowels.test.js
@@ -4,3 +4,9 @@ const {countVowels} = require('./_30s.js');
 test('countVowels is a Function', () => {
   expect(countVowels).toBeInstanceOf(Function);
 });
+test('countVowels returns the correct count', () => {
+  expect(countVowels('foobar')).toBe(3);
+});
+test('countVowels returns the correct count', () => {
+  expect(countVowels('ggg')).toBe(0);
+});
