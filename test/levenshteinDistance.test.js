53820b94028319479b287df4ff9f542dde5fe5b1
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-26 21:03:11 +0300

    Add tests for pipeLog and levenshtein

--- test/levenshteinDistance.test.js
+++ test/levenshteinDistance.test.js
@@ -4,3 +4,12 @@ const {levenshteinDistance} = require('./_30s.js');
 test('levenshteinDistance is a Function', () => {
   expect(levenshteinDistance).toBeInstanceOf(Function);
 });
+test('levenshteinDistance returns the correct results', () => {
+  expect(levenshteinDistance('30-seconds-of-code', '30-seconds-of-python-code')).toBe(7);
+});
+test('levenshteinDistance returns the correct result for 0-length string as first argument', () => {
+  expect(levenshteinDistance('', 'foo')).toBe(3);
+});
+test('levenshteinDistance returns the correct result for 0-length string as second argument', () => {
+  expect(levenshteinDistance('bar', '')).toBe(3);
+});
--- test/pipeLog.test.js
+++ test/pipeLog.test.js
@@ -4,3 +4,6 @@ const {pipeLog} = require('./_30s.js');
 test('pipeLog is a Function', () => {
   expect(pipeLog).toBeInstanceOf(Function);
 });
+test('pipeLog returns the given value', () => {
+  expect(pipeLog('hi')).toBe('hi');
+});
