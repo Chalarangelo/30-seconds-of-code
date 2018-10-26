6090836233f244549f837d1d07e4d16b068a9a5c
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-26 21:15:14 +0300

    Add archive snippet tests

--- test/heronArea.test.js
+++ test/heronArea.test.js
@@ -4,3 +4,6 @@ const {heronArea} = require('./_30s.js');
 test('heronArea is a Function', () => {
   expect(heronArea).toBeInstanceOf(Function);
 });
+test('howManyTimes returns the correct result', () => {
+  expect(heronArea(3, 4, 5)).toBe(6);
+});
