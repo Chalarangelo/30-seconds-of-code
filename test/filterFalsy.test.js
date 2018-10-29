5c693aee76e0daa6dd0c63ce0bad90df1330d4dc
Mykolas Krupauskas <mkrupauskas@gmail.com>
2018-10-29 23:19:01 +0200

    add filterFalsy snippet, tags, and tests

---
+++ test/filterFalsy.test.js
@@ -0,0 +1,18 @@
+const expect = require('expect');
+const {filterFalsy} = require('._30s.js');
+
+test('filterFalsy is a Function', () => {
+  expect(filterFalsy).toBeInstanceOf(Function);
+});
+
+test('filterFalsy filters different types of falsy values', () => {
+  expect(filterFalsy(['', true, {}, false, 'sample', 1, 0])).toEqual([true, {}, 'sample', 1]);
+});
+
+test('filterFalsy returns an empty array if you pass it an array of falsy values', () => {
+  expect(filterFalsy(['', 0, false, '', false, 0])).toEqual([]);
+});
+
+test('filterFalsy returns all of the truthy elements in an array', () => {
+  expect(filterFalsy([true, null, 'test', {}, []])).toEqual([true, 'test', {}, []]);
+});
