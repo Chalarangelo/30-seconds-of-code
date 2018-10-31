b747a82395d61e5d45bf9f895630c7eb3840388f
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-31 20:00:32 +0200

    Add tests for 2 browser snippets

--- test/createElement.test.js
+++ test/createElement.test.js
@@ -4,3 +4,10 @@ const {createElement} = require('./_30s.js');
 test('createElement is a Function', () => {
   expect(createElement).toBeInstanceOf(Function);
 });
+test('createElement creates an element', () => {
+  expect(createElement(
+    `<div class="container">
+    <p>Hello!</p>
+  </div>`
+  ).className).toEqual('container');
+});
