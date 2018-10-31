082baa86138217b5df771c7c4fbde48a549c748e
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-31 20:00:32 +0200

    Add tests for 2 browser snippets

--- test/currentURL.test.js
+++ test/currentURL.test.js
@@ -4,3 +4,6 @@ const {currentURL} = require('./_30s.js');
 test('currentURL is a Function', () => {
   expect(currentURL).toBeInstanceOf(Function);
 });
+test('currentURL returns the appropriate value', () => {
+  expect(currentURL()).toEqual(global.location.href);
+});
