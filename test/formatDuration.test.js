0ee9222291074fd7cce78d8c2e8dd36cb2d43c22
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-31 20:41:38 +0200

    Additional tests

--- test/formatDuration.test.js
+++ test/formatDuration.test.js
@@ -7,6 +7,9 @@ test('formatDuration is a Function', () => {
 test('Returns the human readable format of the given number of milliseconds', () => {
   expect(formatDuration(1001)).toBe('1 second, 1 millisecond');
 });
+test('Returns the human readable format of the given number of milliseconds (negative)', () => {
+  expect(formatDuration(-1001)).toBe('1 second, 1 millisecond');
+});
 test('Returns the human readable format of the given number of milliseconds', () => {
   expect(formatDuration(34325055574)).toBe(
     '397 days, 6 hours, 44 minutes, 15 seconds, 574 milliseconds'
--- test/removeVowels.test.js
+++ test/removeVowels.test.js
@@ -4,3 +4,9 @@ const {removeVowels} = require('./_30s.js');
 test('removeVowels is a Function', () => {
   expect(removeVowels).toBeInstanceOf(Function);
 });
+test('Removes vowels.', () => {
+  expect(removeVowels('foobAr')).toBe('fbr');
+});
+test('Replaces vowels.', () => {
+  expect(removeVowels('foobAr', '*')).toBe('f**b*r');
+});
