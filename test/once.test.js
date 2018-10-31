91c1de3a451a8a6ef846f35a273bbe7e5f761710
Angelos Chalaris <chalarangelo@gmail.com>
2018-10-31 21:04:22 +0200

    Additional tests

--- test/once.test.js
+++ test/once.test.js
@@ -4,3 +4,6 @@ const {once} = require('./_30s.js');
 test('once is a Function', () => {
   expect(once).toBeInstanceOf(Function);
 });
+test('once is a Function', () => {
+  expect(typeof once(x => 10)).toBe('function');
+});
