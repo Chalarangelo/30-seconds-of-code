53c5d9ae08adca81df673ae9abd4a3a57e88701c
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-01 14:47:28 +0200

    Additional tests

--- test/initialize2DArray.test.js
+++ test/initialize2DArray.test.js
@@ -7,3 +7,6 @@ test('initialize2DArray is a Function', () => {
 test('Initializes a 2D array of given width and height and value', () => {
   expect(initialize2DArray(2, 2, 0)).toEqual([[0, 0], [0, 0]]);
 });
+test('Initializes a 2D array of given width and height and value (no fill)', () => {
+  expect(initialize2DArray(2, 2)).toEqual([[null, null], [null, null]]);
+});
