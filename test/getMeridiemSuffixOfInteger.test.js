11270f8b8593678ef4809042a8a5bcc9e8d34781
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-01 14:35:17 +0200

    Additional tests

--- test/getMeridiemSuffixOfInteger.test.js
+++ test/getMeridiemSuffixOfInteger.test.js
@@ -4,3 +4,18 @@ const {getMeridiemSuffixOfInteger} = require('./_30s.js');
 test('getMeridiemSuffixOfInteger is a Function', () => {
   expect(getMeridiemSuffixOfInteger).toBeInstanceOf(Function);
 });
+test('Gets the correct meridiem suffix.', () => {
+  expect(getMeridiemSuffixOfInteger(0)).toEqual('12am');
+});
+test('Gets the correct meridiem suffix.', () => {
+  expect(getMeridiemSuffixOfInteger(11)).toEqual('11am');
+});
+test('Gets the correct meridiem suffix.', () => {
+  expect(getMeridiemSuffixOfInteger(12)).toEqual('12pm');
+});
+test('Gets the correct meridiem suffix.', () => {
+  expect(getMeridiemSuffixOfInteger(13)).toEqual('1pm');
+});
+test('Gets the correct meridiem suffix.', () => {
+  expect(getMeridiemSuffixOfInteger(25)).toEqual('1pm');
+});
