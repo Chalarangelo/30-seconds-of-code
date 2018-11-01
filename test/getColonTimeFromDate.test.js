a097c3de360cce64c54c0555edf01bdd3ddb1695
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-01 14:35:17 +0200

    Additional tests

--- test/getColonTimeFromDate.test.js
+++ test/getColonTimeFromDate.test.js
@@ -4,3 +4,7 @@ const {getColonTimeFromDate} = require('./_30s.js');
 test('getColonTimeFromDate is a Function', () => {
   expect(getColonTimeFromDate).toBeInstanceOf(Function);
 });
+test('Gets the time in the proper format.', () => {
+  let date = new Date();
+  expect(getColonTimeFromDate(date)).toEqual(date.toTimeString().slice(0, 8));
+});
