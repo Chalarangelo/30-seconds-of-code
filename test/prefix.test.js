8a66f92fe3c785b9b285622152128101659a2bfa
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-07 15:21:43 +0200

    Tests for prefix

--- test/prefix.test.js
+++ test/prefix.test.js
@@ -4,3 +4,6 @@ const {prefix} = require('./_30s.js');
 test('prefix is a Function', () => {
   expect(prefix).toBeInstanceOf(Function);
 });
+test('prefix is a Function', () => {
+  expect(typeof prefix('appearance')).toBe('string');
+});
