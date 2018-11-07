0202b516eadc0f5514aa538e280949c841dcacd4
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-07 15:03:27 +0200

    Additional tests

--- test/insertAfter.test.js
+++ test/insertAfter.test.js
@@ -4,3 +4,10 @@ const {insertAfter} = require('./_30s.js');
 test('insertAfter is a Function', () => {
   expect(insertAfter).toBeInstanceOf(Function);
 });
+let e = document.createElement('div');
+e.setAttribute("id", "test");
+test('Does not throw error if the element exists', () => {
+  expect(() => {
+    insertAfter(e, '<span>test</span>');
+  }).not.toThrow(TypeError);
+});
--- test/isBrowserTabFocused.test.js
+++ test/isBrowserTabFocused.test.js
@@ -4,3 +4,6 @@ const {isBrowserTabFocused} = require('./_30s.js');
 test('isBrowserTabFocused is a Function', () => {
   expect(isBrowserTabFocused).toBeInstanceOf(Function);
 });
+test('isBrowserTabFocused is a Function', () => {
+  expect(typeof isBrowserTabFocused()).toBe('boolean');
+});
