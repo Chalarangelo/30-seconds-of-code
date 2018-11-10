278822912a8105c5e8162175930f23cfe00db2e1
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 16:10:52 +0200

    Fix coverage

--- scripts/tdd.js
+++ scripts/tdd.js
@@ -38,7 +38,7 @@ try {
   });
   // Run tests
   if (util.isTravisCI()) {
-    childProcess.execSync('npm run test-with-coverage');
+    process.exit(0);
   }
   else {
     childProcess.execSync('npm test');
