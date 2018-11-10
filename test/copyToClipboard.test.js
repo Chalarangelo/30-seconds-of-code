1e82f5f06f2c9a6d4c25fef04845f70910fe0326
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 13:38:34 +0200

    Additional tests

--- test/copyToClipboard.test.js
+++ test/copyToClipboard.test.js
@@ -4,3 +4,28 @@ const {copyToClipboard} = require('./_30s.js');
 test('copyToClipboard is a Function', () => {
   expect(copyToClipboard).toBeInstanceOf(Function);
 });
+test('copyToClipboard does not throw errors', () => {
+  document.getSelection = function () {
+    return {
+      rangeCount: 0,
+      removeAllRanges() { return; },
+      addRange(x) { return x; }
+    };
+  }
+  document.execCommand = function (x) { return x; }
+
+  expect(copyToClipboard('hi')).toBe(undefined);
+});
+test('copyToClipboard does not throw errors', () => {
+  document.getSelection = function () {
+    return {
+      rangeCount: 1,
+      getRangeAt(x) { return x+1; },
+      removeAllRanges() { return; },
+      addRange(x) { return x; }
+    };
+  }
+  document.execCommand = function (x) { return x; }
+
+  expect(copyToClipboard('hi')).toBe(undefined);
+});
