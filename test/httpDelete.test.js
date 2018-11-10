27212f55e7569836c65abce1201f374d9fb042c5
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 13:55:04 +0200

    Additional tests

--- test/httpDelete.test.js
+++ test/httpDelete.test.js
@@ -4,3 +4,9 @@ const {httpDelete} = require('./_30s.js');
 test('httpDelete is a Function', () => {
   expect(httpDelete).toBeInstanceOf(Function);
 });
+test('httpDelete does not throw errors', () => {
+  expect(() => {
+    httpDelete('http://localhost', x => x, console.log);
+    httpDelete('http://localhost', x => x);
+  }).not.toThrow(TypeError);
+});
--- test/httpPut.test.js
+++ test/httpPut.test.js
@@ -4,3 +4,9 @@ const {httpPut} = require('./_30s.js');
 test('httpPut is a Function', () => {
   expect(httpPut).toBeInstanceOf(Function);
 });
+test('httpPut does not throw errors', () => {
+  expect(() => {
+    httpPut('http://localhost', x => x, console.log);
+    httpPut('http://localhost', x => x);
+  }).not.toThrow(TypeError);
+});
