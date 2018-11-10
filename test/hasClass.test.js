0f46d4773dd2b84ad12f7fce36bd5dd009596d44
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 11:12:59 +0200

    Additional tests

--- test/hasClass.test.js
+++ test/hasClass.test.js
@@ -4,3 +4,8 @@ const {hasClass} = require('./_30s.js');
 test('hasClass is a Function', () => {
   expect(hasClass).toBeInstanceOf(Function);
 });
+test('hasClass returns the proper value', () => {
+  let el = document.createElement('div');
+  el.classList.add('myClass');
+  expect(hasClass(el, 'myClass')).toBeTruthy();
+});
