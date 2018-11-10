fef835355b692e2ee2833b39002c358e7e70fca5
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 13:55:04 +0200

    Additional tests

--- test/httpGet.test.js
+++ test/httpGet.test.js
@@ -4,3 +4,9 @@ const {httpGet} = require('./_30s.js');
 test('httpGet is a Function', () => {
   expect(httpGet).toBeInstanceOf(Function);
 });
+test('httpGet does not throw errors', () => {
+  expect(() => {
+    httpGet('http://localhost', x => x, console.log);
+    httpGet('http://localhost', x => x);
+  }).not.toThrow(TypeError);
+});
