39ff17ae2af1706271e31d9173be00ba2cab6a4c
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 13:04:37 +0200

    Additional tests

--- test/triggerEvent.test.js
+++ test/triggerEvent.test.js
@@ -4,3 +4,11 @@ const {triggerEvent} = require('./_30s.js');
 test('triggerEvent is a Function', () => {
   expect(triggerEvent).toBeInstanceOf(Function);
 });
+test('triggerEvent triggers an event', () => {
+  let el = document.createElement('div');
+  let val = false;
+  const fn = () => val = true;
+  el.addEventListener('click', fn);
+  triggerEvent(el, 'click', {})
+  expect(val).toBeTruthy();
+});
