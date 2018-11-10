9ef51d35862ee7dfcc69634506adddc3206961a4
Angelos Chalaris <chalarangelo@gmail.com>
2018-11-10 14:48:56 +0200

    Cleanup

--- test/copyToClipboard.test.js
+++ test/copyToClipboard.test.js
@@ -5,27 +5,27 @@ test('copyToClipboard is a Function', () => {
   expect(copyToClipboard).toBeInstanceOf(Function);
 });
 test('copyToClipboard does not throw errors', () => {
-  document.getSelection = function () {
+  document.getSelection = function() {
     return {
       rangeCount: 0,
       removeAllRanges() { return; },
       addRange(x) { return x; }
     };
-  }
-  document.execCommand = function (x) { return x; }
+  };
+  document.execCommand = function(x) { return x; };
 
   expect(copyToClipboard('hi')).toBe(undefined);
 });
 test('copyToClipboard does not throw errors', () => {
-  document.getSelection = function () {
+  document.getSelection = function() {
     return {
       rangeCount: 1,
-      getRangeAt(x) { return x+1; },
+      getRangeAt(x) { return x + 1; },
       removeAllRanges() { return; },
       addRange(x) { return x; }
     };
-  }
-  document.execCommand = function (x) { return x; }
+  };
+  document.execCommand = function(x) { return x; };
 
   expect(copyToClipboard('hi')).toBe(undefined);
 });
