0b8c7c7efcbcf7dc45e497b39f6f95a3fb79b3cd
azou1 <azou1@myseneca.com>
2018-11-11 14:49:33 -0500

    added edge cases fo everyNth test

--- test/everyNth.test.js
+++ test/everyNth.test.js
@@ -7,3 +7,10 @@ test('everyNth is a Function', () => {
 test('Returns every nth element in an array', () => {
   expect(everyNth([1, 2, 3, 4, 5, 6], 2)).toEqual([2, 4, 6]);
 });
+test('When N is bigger than array size, return nothing', () => {
+  expect(everyNth([1, 2, 3, 4], 5)).toEqual([]);
+});
+test('Works with different variable types in the same array', () => {
+  expect(everyNth([1, "two", 3, false, 5, 6.1], 2)).toEqual(["two", false, 6.1]);
+});
+
