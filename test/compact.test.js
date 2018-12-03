eb738d972b1204566d615b197807748230f6bc57
azou1 <itchylol742@gmail.com>
2018-12-03 14:12:04 -0500

    added null and undefined to compact test

--- test/compact.test.js
+++ test/compact.test.js
@@ -5,7 +5,7 @@ test('compact is a Function', () => {
   expect(compact).toBeInstanceOf(Function);
 });
 test('Removes falsey values from an array', () => {
-  expect(compact([0, 1, false, 2, '', 3, 'a', 'e' * 23, NaN, 's', 34])).toEqual([
+  expect(compact([0, 1, false, 2, '', 3, 'a', 'e' * 23, NaN, 's', 34, null, undefined])).toEqual([
     1,
     2,
     3,
