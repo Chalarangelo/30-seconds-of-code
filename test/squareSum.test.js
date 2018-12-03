64ee0fe01243b04ff218d75a167d299e3f002709
azou1 <itchylol742@gmail.com>
2018-12-03 14:05:22 -0500

    Added edge cases to tests

--- test/squareSum.test.js
+++ test/squareSum.test.js
@@ -7,3 +7,11 @@ test('squareSum is a Function', () => {
 test('squareSum returns the proper result', () => {
   expect(squareSum(2, 3, 4)).toBe(29);
 });
+
+test('works with negative numbers', () => {
+  expect(squareSum(-2, 3, -4)).toBe(29);
+});
+
+test('NaN when parameters have text', () => {
+  expect(squareSum(-2, 3, -4, 'text')).toBe(NaN);
+});
--- test/uniqueSymmetricDifference.test.js
+++ test/uniqueSymmetricDifference.test.js
@@ -10,3 +10,13 @@ test('Returns the symmetric difference between two arrays.', () => {
 test('Does not return duplicates from one array', () => {
   expect(uniqueSymmetricDifference([1, 2, 2], [1, 3, 1])).toEqual([2, 3]);
 });
+test('Works with different size arrays', () => {
+  expect(uniqueSymmetricDifference([1, 2, 2, 4], [1, 3, 1, 4, 5, 6])).toEqual([2, 3, 5, 6]);
+});
+test('Works with different types in the same array', () => {
+  expect(uniqueSymmetricDifference([1, 2, 'duplicate text', 'unique text'], [1, 3, 'duplicate text'])).toEqual([2, 'unique text', 3]);
+});
+test('Works when one array is empty', () => {
+  expect(uniqueSymmetricDifference([1, 2, 2, 3], [])).toEqual([1, 2, 3]);
+});
+
