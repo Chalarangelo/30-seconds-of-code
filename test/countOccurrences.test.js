52a19f2c7569ac55e0c4d3bcceff34448c73546b
azou1 <itchylol742@gmail.com>
2018-12-03 14:20:50 -0500

    countOccurrences test edge case

--- test/countOccurrences.test.js
+++ test/countOccurrences.test.js
@@ -7,3 +7,6 @@ test('countOccurrences is a Function', () => {
 test('Counts the occurrences of a value in an array', () => {
   expect(countOccurrences([1, 1, 2, 1, 2, 3], 1)).toEqual(3);
 });
+test('Works with different types in the same array', () => {
+  expect(countOccurrences([1, 1, 2, 1, 2, 3, '1', true], 1)).toEqual(3);
+});
