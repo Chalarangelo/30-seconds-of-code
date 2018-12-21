8cb2c24a973bec0067aa009db4448996735fffb5
Alexander Covington <covinga@honeydew.reshall.wwu.edu>
2018-12-21 09:20:41 -0800

    Updated deepClone test

--- test/deepClone.test.js
+++ test/deepClone.test.js
@@ -8,6 +8,8 @@ const a = { foo: 'bar', obj: { a: 1, b: 2 } };
 const b = deepClone(a);
 const c = [{ foo: 'bar' }];
 const d = deepClone(c);
+const e = 'this is a primitive';
+const f = deepClone(e);
 test('Shallow cloning works', () => {
   expect(a).not.toBe(b);
 });
@@ -19,4 +21,7 @@ test('Array shallow cloning works', () => {
 });
 test('Array deep cloning works', () => {
   expect(c[0]).not.toBe(d[0]);
+});
+test('Deep cloning primitive doesn\'t mangle result', () => {
+  expect(f).toBe(e);
 });
