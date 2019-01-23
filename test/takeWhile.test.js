c7cf8a9eef72a3eca7dde2bbaf9681bafda61e84
abdellah hariti <haritiabdellah@gmail.com>
2019-01-23 11:05:14 +0000

    change the behavior of takeWhile

--- test/takeWhile.test.js
+++ test/takeWhile.test.js
@@ -4,9 +4,9 @@ const {takeWhile} = require('./_30s.js');
 test('takeWhile is a Function', () => {
   expect(takeWhile).toBeInstanceOf(Function);
 });
-test('Removes elements until the function returns true', () => {
-  expect(takeWhile([1, 2, 3, 4], n => n >= 3)).toEqual([1, 2]);
+test('Returns the first elements where the function returns true', () => {
+  expect(takeWhile([1, 2, 3, 4], n => n <= 3)).toEqual([1, 2, 3]);
 });
-test('Removes elements until the function returns true', () => {
-  expect(takeWhile([1, 2, 3, 4], n => false)).toEqual([1, 2, 3, 4]);
+test('Returns the first elements where the function returns true', () => {
+  expect(takeWhile([1, 2, 3, 4], n => true)).toEqual([1, 2, 3, 4]);
 });
