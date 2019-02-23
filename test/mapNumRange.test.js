b12de6d1d9ac40d6834c6716b57fb4eb07cd01dd
Angelos Chalaris <chalarangelo@gmail.com>
2019-02-23 12:47:03 +0200

    Fix issues with #922

--- test/mapNumRange.test.js
+++ test/mapNumRange.test.js
@@ -5,5 +5,5 @@ test('mapNumRange is a Function', () => {
   expect(mapNumRange).toBeInstanceOf(Function);
 });
 test('Maps 5 to the range 0-100 from 0-10', () => {
-  expect(distance(5,0,10,0,100)).toEqual(50);
+  expect(mapNumRange(5,0,10,0,100)).toEqual(50);
 });
