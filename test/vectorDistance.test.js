c02d4165ce144f2a58d4e9b8391f4179001592bf
Angelos Chalaris <chalarangelo@gmail.com>
2019-02-25 08:39:44 +0200

    Update vectorDistance.test.js

--- test/vectorDistance.test.js
+++ test/vectorDistance.test.js
@@ -5,5 +5,5 @@ test('vectorDistance is a Function', () => {
   expect(vectorDistance).toBeInstanceOf(Function);
 });
 test('Calculates the distance between two vectors', () => {
-  expect(distance(10, 0, 5, 20, 0, 10)).toBeCloseTo(11.180339887498949, 5);
+  expect(vectorDistance(10, 0, 5, 20, 0, 10)).toBeCloseTo(11.180339887498949, 5);
 });
