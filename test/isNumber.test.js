0daced5fd585ccaed53bb9e14e643be8c78c6608
Marcin Kopa <marcin.info@gmail.com>
2019-03-15 10:45:05 +0100

    Fix isNumber snippet and add test for NaN value

--- test/isNumber.test.js
+++ test/isNumber.test.js
@@ -10,3 +10,6 @@ test('passed argument is a number', () => {
 test('passed argument is not a number', () => {
   expect(isNumber('1')).toBeFalsy();
 });
+test('passed argument is not a number', () => {
+  expect(isNumber(NaN)).toBeFalsy();
+});
