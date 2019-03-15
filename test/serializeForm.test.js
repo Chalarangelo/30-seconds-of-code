76e6286c98762e4d9fad814afc2db9b355d1f3f3
Nikita <eddimensi@gmail.com>
2019-03-15 12:54:57 +0300

    add test, update tags, update description

---
+++ test/serializeForm.test.js
@@ -0,0 +1,18 @@
+const expect = require('expect');
+const {serializeForm} = require('./_30s.js');
+
+test('serializeForm is a Function', () => {
+  expect(serializeForm).toBeInstanceOf(Function);
+});
+
+test('serializeForm correctly converts to query string', () => {
+  document.body.innerHTML = `
+    <form id="form">
+      <input name="name" value="Test Name" />
+      <input name="email" value="test@mail.com" />
+    </form>
+  `;
+
+  const form = document.querySelector('#form');
+  expect(serializeForm(form)).toBe('name=Test%20Name&email=test%40mail.com');
+});
