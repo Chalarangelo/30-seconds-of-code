80164c8a5275cf7aa727e9ae6e0aa058915c5d87
fatedeity <799298142@qq.com>
2019-08-11 16:20:26 +0800

    return default index when not found

--- test/findLastIndex.test.js
+++ test/findLastIndex.test.js
@@ -6,3 +6,6 @@ test('findLastIndex is a Function', () => {
 test('Finds last index for which the given function returns true', () => {
   expect(findLastIndex([1, 2, 3, 4], n => n % 2 === 1)).toBe(2);
 });
+test('Return default index when not found', () => {
+  expect(findLastIndex([1, 2, 3, 4], n => n === 5)).toBe(-1);
+});
