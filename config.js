233815cc2217d59aa95ab18d248eda2278b50765
Angelos Chalaris <chalarangelo@gmail.com>
2019-08-13 11:20:10 +0300

    Import gatsby from starter

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Project metadata
+  name: `30 seconds starter`,
+  description: `Kick off your next, great 30 seconds project with this starter.`,
+  shortName: `30s`,
+  repositoryUrl: `https://github.com/30-seconds/30-seconds-starter`,
+  // Path information
+  snippetPath: `snippets`,
+  snippetArchivePath: `snippets_archive`,
+  snippetDataPath: `snippet_data`,
+  assetPath: `assets`,
+  pagePath: `src/docs/pages`,
+  staticPartsPath: `src/static-parts`,
+  // General information
+  language: `js`,
+};
