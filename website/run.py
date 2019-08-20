0eaa2debd57243b90e35ee127c58f36de7eb2dfd
Stefan Fejes <ns.fejes.stefan@gmail.com>
2019-08-20 10:04:20 +0200

    remove old website

--- website/run.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-if __name__ == '__main__':
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host='0.0.0.0',port=80)
