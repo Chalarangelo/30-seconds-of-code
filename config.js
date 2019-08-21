c0d36df01c75ca677be06b5290cd4435e8fdf2fb
Angelos Chalaris <chalarangelo@gmail.com>
2019-08-21 10:12:42 +0300

    [CHORE] Cleanup scripts, setup most of Travis
    Also updated logos!

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Project metadata
-  name: `30 seconds of python code`,
+  name: `30 seconds of python`,
   description: `A curated collection of useful Python snippets that you can understand in 30 seconds or less.`,
   shortName: `30s`,
   repositoryUrl: `https://github.com/30-seconds/30-seconds-of-python-code`,
@@ -11,5 +11,5 @@ module.exports = {
   pagePath: `src/docs/pages`,
   staticPartsPath: `src/static-parts`,
   // General information
-  language: `python`,
+  language: `py`,
 };
