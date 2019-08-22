fe4aa164d14aae66e34c9e2ee61d5788dee417b9
Angelos Chalaris <chalarangelo@gmail.com>
2019-08-22 10:34:38 +0300

    Add sitemap generation

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
   description: `Curated collection of useful JavaScript snippets that you can understand in 30 seconds or less.`,
   shortName: `30s`,
   repositoryUrl: `https://github.com/30-seconds/30-seconds-of-code`,
+  siteUrl: `https://www.30secondsofcode.org`,
   // Path information
   snippetPath: `snippets`,
   snippetArchivePath: `snippets_archive`,
