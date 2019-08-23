6990b61e443e8c7d065108ef5d0515432e3330c0
Angelos Chalaris <chalarangelo@gmail.com>
2019-08-23 11:02:57 +0300

    Add webber script (WIP)

---
+++ src/docs/components/SnippetCard.js
@@ -0,0 +1,167 @@
+import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import config from '../../../config';
+
+import { getTextualContent, getCodeBlocks, optimizeAllNodes } from '../util';
+// import ShareIcon from './SVGs/ShareIcon';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import CollapseOpenIcon from './SVGs/CollapseOpenIcon';
+import CollapseClosedIcon from './SVGs/CollapseClosedIcon';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
+
+// ===================================================
+// Snippet Card HOC - check components below for more
+// ===================================================
+const SnippetCard = ({ short, snippetData, ...rest }) => {
+  let difficulty = snippetData.tags.includes('advanced')
+    ? 'advanced'
+    : snippetData.tags.includes('beginner')
+    ? 'beginner'
+    : 'intermediate';
+  return short ? (
+    <ShortCard snippetData={snippetData} difficulty={difficulty} {...rest} />
+  ) : (
+    <FullCard snippetData={snippetData} difficulty={difficulty} {...rest} />
+  );
+};
+
+// ===================================================
+// Simple card corner for difficulty display
+// ===================================================
+const CardCorner = ({ difficulty = 'intermediate' }) => (
+  <div
+    className={`card-corner ${difficulty}`}
+    aria-label={difficulty}
+    title={difficulty}
+  />
+);
+
+// ===================================================
+// Full snippet view (tags, code, title, description)
+// ===================================================
+const FullCard = ({ snippetData, difficulty, isDarkMode }) => {
+  const [examplesOpen, setExamplesOpen] = React.useState(false);
+  const tags = snippetData.tags;
+  let cardCodeHtml = `${optimizeAllNodes(
+    getCodeBlocks(snippetData.html).html,
+  )}`;
+  let cardCodeCss = `${optimizeAllNodes(
+    getCodeBlocks(snippetData.html).css,
+  )}`;
+  let cardCodeJs = `${optimizeAllNodes(
+    getCodeBlocks(snippetData.html).js,
+  )}`;
+  return (
+    <div className='card'>
+      <CardCorner difficulty={difficulty} />
+      <h4 className='card-title'>{snippetData.title}</h4>
+      {tags.map(tag => (
+        <span className='tag' key={`tag_${tag}`}>{tag}</span>
+      ))}
+      <div
+        className='card-description'
+        dangerouslySetInnerHTML={{
+          __html: `${getTextualContent(snippetData.html)}`,
+        }}
+      />
+      <div className='card-bottom'>
+        <pre
+          className={`card-code language-${config.secondLanguage}`}
+          dangerouslySetInnerHTML={{ __html: cardCodeHtml }}
+        />
+        <pre
+          className={`card-code language-${config.language}`}
+          dangerouslySetInnerHTML={{ __html: cardCodeCss }}
+        />
+        {
+          cardCodeJs && <pre
+            className={`card-code language-${config.optionalLanguage}`}
+            dangerouslySetInnerHTML={{ __html: cardCodeJs }}
+          />
+        }
+        {/* <button
+          className='button button-example-toggler'
+          onClick={() => setExamplesOpen(!examplesOpen)}
+        >
+          {examplesOpen ? <CollapseOpenIcon /> : <CollapseClosedIcon />}Examples
+        </button>
+        <ReactCSSTransitionReplace
+          transitionName='roll-up'
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          {examplesOpen && (
+            <pre
+              className={`section card-examples language-${config.language}`}
+              dangerouslySetInnerHTML={{ __html: cardExamplesHtml }}
+            />
+          )}
+        </ReactCSSTransitionReplace> */}
+      </div>
+    </div>
+  );
+};
+
+// ===================================================
+// Short snippet view (title, description, code*)
+// ===================================================
+const ShortCard = ({
+  snippetData,
+  withCode = false,
+  difficulty,
+  isDarkMode
+}) => {
+  let cardCodeHtml;
+  if (withCode)
+    cardCodeHtml = `${optimizeAllNodes(
+      getCodeBlocks(snippetData.html).code,
+    )}`;
+  return (
+    <div className='card short'>
+      <CardCorner difficulty={difficulty} />
+      <h4 className='card-title'>
+        <AniLink
+          paintDrip
+          to={`/snippet/${snippetData.id}`}
+          hex={isDarkMode ? '#434E76' : '#FFFFFF'}
+        >
+          {snippetData.title}
+        </AniLink>
+      </h4>
+      <div
+        className='card-description'
+        dangerouslySetInnerHTML={{
+          __html: `${getTextualContent(snippetData.html)}`,
+        }}
+      />
+      {withCode ? <div className='card-bottom'>
+        <CopyToClipboard
+          text={snippetData.code}
+          onCopy={() => {
+            let tst = document.createElement('div');
+            tst.classList = 'toast';
+            tst.innerHTML = 'Snippet copied to clipboard!';
+            document.body.appendChild(tst);
+            setTimeout(function() {
+              tst.style.opacity = 0;
+              setTimeout(function() {
+                document.body.removeChild(tst);
+              }, 300);
+            }, 1700);
+          }}
+        >
+          <button
+            className='button button-a button-copy'
+            aria-label='Copy to clipboard'
+          />
+        </CopyToClipboard>
+        <pre
+          className={`card-code language-${config.language}`}
+          dangerouslySetInnerHTML={{ __html: cardCodeHtml }}
+        />
+      </div> : '' }
+    </div>
+  );
+};
+
+export default SnippetCard;
