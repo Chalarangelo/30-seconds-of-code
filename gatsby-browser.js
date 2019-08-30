abaa07a82f5b5e2096074236e84d585be8bde3df
Angelos Chalaris <chalarangelo@gmail.com>
2019-08-30 19:12:47 +0300

    Add scroll memory

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,26 @@
  */
 
 // You can delete this file if you're not using it
-export { default as wrapRootElement } from './src/docs/state/ReduxWrapper'
+
+let locationScrollTops = [];
+
+const onPreRouteUpdate = ({ location, prevLocation }) => {
+  try {
+    let scrollTop = document.querySelector('.content').scrollTop;
+    locationScrollTops[prevLocation.pathname] = scrollTop;
+  }
+  catch (e) {}
+};
+
+const onRouteUpdate = ({ location, prevLocation }) => {
+  console.log(locationScrollTops)
+  try {
+    if (locationScrollTops[location.pathname]) {
+      document.querySelector('.content').scrollTop = locationScrollTops[location.pathname];
+    }
+  }
+  catch (e) { }
+}
+
+export { default as wrapRootElement } from './src/docs/state/ReduxWrapper';
+export { onPreRouteUpdate, onRouteUpdate };
