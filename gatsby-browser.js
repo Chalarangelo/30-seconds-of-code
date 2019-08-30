16f820b7e24e81ce4bb32ee4098c692365935dff
Angelos Chalaris <chalarangelo@gmail.com>
2019-08-30 19:58:35 +0300

    Template final touches

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+
+let locationScrollTops = [];
+
+const onPreRouteUpdate = ({ location, prevLocation }) => {
+  try {
+    let scrollTop = document.querySelector('.content').scrollTop;
+    locationScrollTops[prevLocation.pathname] = scrollTop;
+  }
+  catch (e) { }
+};
+
+const onRouteUpdate = ({ location, prevLocation }) => {
+  try {
+    if (locationScrollTops[location.pathname]) {
+      document.querySelector('.content').scrollTop = locationScrollTops[location.pathname];
+    }
+  }
+  catch (e) { }
+}
+
 export { default as wrapRootElement } from './src/docs/state/ReduxWrapper';
+export { onPreRouteUpdate, onRouteUpdate };
