cd85be6064f74bd5480d027c18bffc6670599e1c
Stefan Fejes <ns.fejes.stefan@gmail.com>
2019-09-03 09:58:14 +0200

    add about section on initial page, style sorting

--- src/docs/pages/index.js
+++ src/docs/pages/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { connect } from 'react-redux';
-import { pushNewPage, pushNewQuery } from '../state/app';
-
-import Shell from '../components/Shell';
-import Meta from '../components/Meta';
-import Search from '../components/Search';
-import SnippetCard from '../components/SnippetCard';
+import React from 'react';
 
 import { getRawCodeBlocks as getCodeBlocks } from '../util';
+import { pushNewPage, pushNewQuery } from '../state/app';
+import SnippetCard from '../components/SnippetCard';
+import SimpleCard from '../components/SimpleCard';
+import Search from '../components/Search';
+import Shell from '../components/Shell';
+import Meta from '../components/Meta';
+
 
 // ===================================================
 // Home page (splash and search)
@@ -78,21 +79,38 @@ const IndexPage = props => {
             <strong>{searchQuery}</strong>.
           </p>
         ) : (
-          <>
-            <p className='light-sub'>
-              Click on a snippet's name to view its code.
+              <>
+                <p className='light-sub'>
+                  Click on a snippet's name to view its code.
             </p>
-            <h2 className='page-sub-title'>Search results</h2>
-            {searchResults.map(snippet => (
-              <SnippetCard
-                short
-                key={`snippet_${snippet.id}`}
-                snippetData={snippet}
-                isDarkMode={props.isDarkMode}
-              />
-            ))}
-          </>
-        )}
+                <h2 className='page-sub-title'>Search results</h2>
+                {searchResults.map(snippet => (
+                  <SnippetCard
+                    short
+                    key={`snippet_${snippet.id}`}
+                    snippetData={snippet}
+                    isDarkMode={props.isDarkMode}
+                  />
+                ))}
+              </>
+            )}
+        <SimpleCard title='About us'>
+          <p style={{ textAlign: 'justify' }}><strong>30 seconds</strong> provides high-quality learning resources for developers of all skill levels in the form of snippet collections in various programming languages since its inception in 2017. Today, <strong>30 seconds</strong> consists of a community of over 250 contributors and  more than 10 maintainers, dedicated to creating the best short-form learning resources for software developers. Our goal is to make software development more accessible and help the open-source community grow by helping people learn to code for free.<br /><br /><Link to='/about'>Read more...</Link></p>
+        </SimpleCard>
+        <SimpleCard title='Our other projects'>
+          <ul>
+            {
+              [
+                { name: '30 seconds of Code', url: 'https://30secondsofcode.org/' },
+                { name: '30 seconds of CSS', url: 'https://css.30secondsofcode.org/' },
+                { name: '30 seconds of Interviews', url: 'https://30secondsofinterviews.org/' },
+                { name: '30 seconds of Knowledge', url: 'https://30secondsofknowledge.org/' },
+                { name: '30 seconds of React', url: 'https://react.30secondsofcode.org/' },
+                { name: '30 seconds of PHP', url: 'https://php.30secondsofcode.org/' },
+              ].map(v => (<li><a href={v.url} key={`link_${v.name}`} target='_blank' rel='noopener noreferrer'>{v.name}</a></li>))
+            }
+          </ul>
+        </SimpleCard>
       </Shell>
     </>
   );
