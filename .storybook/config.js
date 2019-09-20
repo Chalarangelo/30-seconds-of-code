d913f168678d0d59874cdeb95df3a3a527570167
Angelos Chalaris <chalarangelo@gmail.com>
2019-09-21 00:11:25 +0300

    Configure Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import 'index.scss';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.js$/), module);
