9448a056c0b6a066fdf9e6f49f553d71beffb06d
Angelos Chalaris <chalarangelo@gmail.com>
2019-09-21 00:44:54 +0300

    Add PropTypes to expertise atom

--- src/atoms/expertise/index.jsx
+++ src/atoms/expertise/index.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import {trimWhiteSpace} from 'helpers/trimWhiteSpace';
 
 const Expertise = ({
-  level
+  level = 'intermediate'
 }) => (
   <div className={trimWhiteSpace`expertise ${level}`} />
 );
+
+Expertise.propTypes = {
+  level: PropTypes.oneOf(['beginner', 'intermediate', 'advanced'])
+};
 
 export default Expertise;
