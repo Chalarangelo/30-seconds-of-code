e648482588d5a9c6cf15c757fa83787906341acb
Angelos Chalaris <chalarangelo@gmail.com>
2019-09-21 11:16:21 +0300

    Add ESLint configuration

--- src/atoms/expertise/expertise.stories.js
+++ src/atoms/expertise/expertise.stories.js
@@ -7,21 +7,21 @@ export default {
   title: 'Atoms|Expertise',
   component: Expertise,
   decorators: [
-    withDesign
+    withDesign,
   ],
   parameters: {
     design: {
       name: 'beginner',
       type: 'figma',
-      url: 'https://www.figma.com/file/oY0oRyqDxQZMeMqG4BSwrf/30-seconds-web?node-id=80%3A10'
-    }
-  }
-}
+      url: 'https://www.figma.com/file/oY0oRyqDxQZMeMqG4BSwrf/30-seconds-web?node-id=80%3A10',
+    },
+  },
+};
 
 export const component = () => {
   const level = select('level', EXPERTISE_LEVELS, 'intermediate');
 
   return (
     <Expertise level={level} />
-  )
+  );
 };
