49e92ccbdd5d06abe19d607a3b811748efef665d
Angelos Chalaris <chalarangelo@gmail.com>
2019-09-25 20:02:59 +0300

    Add remaining data for Gatsby
    Snippet data, robots and config file

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Project metadata
+  name: `30 seconds starter`,
+  description: `Kick off your next, great 30 seconds project with this starter.`,
+  shortName: `30s`,
+  repositoryUrl: `https://github.com/30-seconds/30-seconds-starter`,
+  siteUrl: `https://30secondsofcode.org`,
+  // Path information
+  snippetPath: `snippets`,
+  snippetDataPath: `snippet_data`,
+  assetPath: `assets`,
+  pagePath: `src/docs/pages`,
+  staticPartsPath: `src/static-parts`,
+  // General information
+  language: `js`,
+};
