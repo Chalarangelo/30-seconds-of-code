23355dba6cb997b7728e7dfda7da3132229b5737
Chalarangelo <chalarangelo@gmail.com>
2019-10-05 11:24:33 +0300

    Add missing Gatsby config to Storybook
    Links from Gatsby should work now.

--- .storybook/config.js
+++ .storybook/config.js
@@ -24,6 +24,19 @@ addParameters({
   },
 });
 
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+}
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__PATH_PREFIX__ = ""
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+window.___navigate = pathname => {
+  action("NavigateTo:")(pathname)
+}
+
 // automatically import all files ending in *.stories.js
 configure(
   [
