cd2e07dc17c43ba7cb6b3aaa99d3fdf567d8d4e9
Chalarangelo <chalarangelo@gmail.com>
2019-10-05 12:35:46 +0300

    Remove design from Anchor story

--- src/atoms/anchor/anchor.stories.js
+++ src/atoms/anchor/anchor.stories.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import Anchor from 'atoms/anchor';
 import { text, boolean } from '@storybook/addon-knobs';
-import { withDesign } from 'storybook-addon-designs';
 import mdx from './docs.mdx';
 
 export default {
   title: 'Atoms|Anchor',
   component: Anchor,
-  decorators: [
-    withDesign,
-  ],
   parameters: {
     docs: {
       page: mdx,
-    },
-    design: {
-      name: 'anchor',
-      type: 'figma',
-      url: 'https://www.figma.com/file/oY0oRyqDxQZMeMqG4BSwrf/30-seconds-web?node-id=193%3A0',
     },
     jest: [
       'anchor',
