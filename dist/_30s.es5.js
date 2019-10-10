cd719a57d9d45fe5fa423f098ee20410db24607e
30secondsofcode <30secondsofcode@gmail.com>
2019-10-10 17:00:08 +0000

    Travis build: 1493 [cron]

--- dist/_30s.es5.js
+++ dist/_30s.es5.js
@@ -724,7 +724,6 @@
     if (a === b) return true;
     if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
     if (!a || !b || _typeof(a) !== 'object' && _typeof(b) !== 'object') return a === b;
-    if (a === null || a === undefined || b === null || b === undefined) return false;
     if (a.prototype !== b.prototype) return false;
     var keys = Object.keys(a);
     if (keys.length !== Object.keys(b).length) return false;
