e1bcbecdd18560d4fc0c433a02186ae48b387a1f
Angelos Chalaris <chalarangelo@gmail.com>
2019-10-14 22:38:06 +0300

    Add PreviewCard component

---
+++ src/molecules/previewCard/previewCard.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PreviewCard from 'molecules/previewCard';
+import mdx from './docs.mdx';
+
+export default {
+  title: 'Molecules|PreviewCard',
+  component: PreviewCard,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+    jest: [
+      'previewCard',
+    ],
+  },
+};
+
+export const component = () => {
+  const snippet = {
+    title: 'compose',
+    tags: ['function', 'intermediate'],
+    expertise: 'intermediate',
+    descriptionHtml: '<p>Performs right-to-left function composition.</p>',
+    explanationHtml: '<p> Use <code class="language-text"> Array.prototype.reduce()</code> to perform right-to-left function composition.\nThe last(rightmost) function can accept one or more arguments; the remaining functions must be unary.</p>',
+    codeHtml: '<span class="token keyword">const</span> <span class="token function-variable function">compose</span> <span class="token operator">=</span> <span class="token punctuation">(</span><span class="token parameter"><span class="token operator">...</span>fns</span><span class="token punctuation">)</span> <span class="token operator">=></span> fns<span class="token punctuation">.</span><span class="token function">reduce</span><span class="token punctuation">((</span><span class="token parameter">f<span class="token punctuation">,</span> g</span><span class="token punctuation">)</span> <span class="token operator">=></span> <span class="token punctuation">(</span><span class="token parameter"><span class="token operator">...</span>args</span><span class="token punctuation">)</span> <span class="token operator">=></span> <span class="token function">f</span><span class="token punctuation">(</span><span class="token function">g</span><span class="token punctuation">(</span><span class="token operator">...</span>args<span class="token punctuation">)));</span>',
+    exampleHtml: '<span class="token keyword">const</span> <span class="token function-variable function">add5</span> <span class="token operator">=</span> <span class="token parameter">x</span> <span class="token operator">=></span> x <span class="token operator">+</span> <span class="token number">5</span><span class="token punctuation">;</span>\n<span class= "token keyword" >const</span> <span class="token function-variable function">multiply</span> <span class="token operator">=</span> <span class="token punctuation">(</span> <span class="token parameter">x<span class="token punctuation">,</span> y</span> <span class="token punctuation">)</span> <span class="token operator">=></span> x <span class="token operator">*</span> y <span class="token punctuation" >;</span >\n<span class="token keyword">const</span> multiplyAndAdd5 <span class="token operator" >=</span > <span class="token function">compose</span> <span class="token punctuation">(</span>add5 <span class="token punctuation" >,</span >multiply<span class="token punctuation" >);</span >\n<span class="token function">multiplyAndAdd5</span> <span class="token punctuation">(</span> <span class="token number">5</span> <span class="token punctuation">,</span> <span class="token number">2</span> <span class="token punctuation">);</span> <span class="token comment">// 15</span>',
+    code: 'const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));',
+    url: 'snippets/compose',
+  };
+
+  return (
+    <PreviewCard snippet={ snippet } />
+  );
+};
---
+++ src/molecules/previewCard/previewCard.test.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import PreviewCard from './index';
+
+configure({ adapter: new Adapter() });
+console.warn = jest.fn();
+
+describe('<PreviewCard />', () => {
+  const snippet = {
+    title: 'compose',
+    language: 'JavaScript',
+    tags: ['function', 'recursion'],
+    expertise: 'intermediate',
+    descriptionHtml: '<p>Performs right-to-left function composition.</p>',
+    url: 'snippets/compose',
+  };
+  let wrapper, card, expertise, anchor;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <PreviewCard snippet={ snippet } />
+    );
+    anchor = wrapper.find('Anchor');
+    card = wrapper.find('Card');
+    expertise = wrapper.find('Expertise');
+  });
+
+  describe('should render', () => {
+    it('an Anchor component', () => {
+      expect(wrapper).toContainMatchingElement('Anchor');
+    });
+
+    it('a Card component', () => {
+      expect(anchor).toContainMatchingElement('Card');
+    });
+
+    it('the card title', () => {
+      expect(card).toContainMatchingElement('h4.card-title');
+    });
+
+    it('an Expertise component', () => {
+      expect(card).toContainMatchingElement('Expertise');
+    });
+
+    it('the card description', () => {
+      expect(card).toContainMatchingElement('.card-description');
+    });
+  });
+
+  it('should have the correct card title', () => {
+    expect(card.find('h4.card-title').text()).toBe(snippet.title);
+  });
+
+  it('should pass the expertise data to the Expertise component', () => {
+    expect(expertise.prop('level')).toBe(snippet.expertise);
+  });
+
+  it('should render the correct description', () => {
+    expect(card.find('.card-description').html()).toContain(snippet.descriptionHtml);
+  });
+
+  it('should link to the correct url', () => {
+    expect(anchor.prop('link').url).toBe(snippet.url);
+  });
+});
+
