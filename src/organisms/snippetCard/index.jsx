778824d12e4f5b6348865eb6d32fd563b7e8b0c1
Angelos Chalaris <chalarangelo@gmail.com>
2019-10-17 20:03:05 +0300

    Add toast when snippet is copied
    Expects a container for the toast to render.

--- src/organisms/snippetCard/index.jsx
+++ src/organisms/snippetCard/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Card from 'atoms/card';
 import TagList from 'molecules/tagList';
 import Expertise from 'atoms/expertise';
 import CodeBlock from 'atoms/codeBlock';
-import {CopyButton} from 'atoms/button';
+import { CopyButton } from 'atoms/button';
+import Toast from 'atoms/toast';
 import { Snippet as SnippetPropType } from 'proptypes';
 import { trimWhiteSpace } from 'functions/utils';
 import _ from 'lang';
@@ -13,6 +15,7 @@ const _l = _('en');
 const SnippetCard = ({
   snippet,
   className,
+  toastContainer,
   ...rest
 }) => (
   <Card className={ trimWhiteSpace`snippet-card ${className}` } { ...rest } >
@@ -26,6 +29,15 @@ const SnippetCard = ({
     <div className='card-source-content'>
       <CopyButton
         text={ snippet.code }
+        onCopy={ () => {
+          ReactDOM.render(
+            <Toast message='Snippet copied to clipboard!'/>,
+            toastContainer
+          );
+          setTimeout(() => {
+            ReactDOM.unmountComponentAtNode(toastContainer);
+          }, 2000);
+        } }
       />
       <CodeBlock
         language={ snippet.language }
@@ -47,6 +59,8 @@ SnippetCard.propTypes = {
   snippet: SnippetPropType,
   /** Additional classes for the card */
   className: PropTypes.string,
+  /** A DOMNode used to render the toast when copying the snippet */
+  toastContainer: PropTypes.node,
   /** Any other arguments to be passed to the card */
   rest: PropTypes.any,
 };
