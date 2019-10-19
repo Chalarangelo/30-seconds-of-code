4ce8579512b1fd25055e7d173623abbbfc3b0076
Chalarangelo <chalarangelo@gmail.com>
2019-10-19 12:22:47 +0300

    Add Meta component
    Add complementary literals (temp).
    Add complementary PropType definition.
    Meta has no tests, due to Helmet being hard to test.

--- src/lang/literals_en.js
+++ src/lang/literals_en.js
@@ -1,6 +1,11 @@
 import { capitalize } from 'functions/utils';
 
 export default {
+  // Site metadata - TODO: Use config
+  'site.title': '30 seconds of code',
+  'site.description': 'Curated collection of code snippets you can understand in 30 seconds or less.',
+  'site.author': '30-seconds',
+  // Literals
   'Expertise': level => `Expertise: ${capitalize(level, true)}`,
   'Search snippets': 'Search snippets',
   'Search...': 'Search...',
---
+++ src/proptypes/meta.js
@@ -0,0 +1,6 @@
+import { shape, string, any } from 'prop-types';
+
+export default shape({
+  name: string,
+  content: any,
+});
