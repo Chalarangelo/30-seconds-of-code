be5108d898b469affa3284c83a2acf9c7cfef1df
Angelos Chalaris <chalarangelo@gmail.com>
2019-10-22 22:23:19 +0300

    Extract createPages method
    Creates pages from a query and a templates object.
    Create individual snippet pages.

--- src/functions/build/index.js
+++ src/functions/build/index.js
@@ -1,9 +1,11 @@
 import onCreateNode from './onCreateNode';
 import sourceNodes from './sourceNodes';
 import createResolvers from './createResolvers';
+import createPages from './createPages';
 
 export {
   onCreateNode,
   sourceNodes,
   createResolvers,
+  createPages,
 };
