9ab1f0a8a7ddbcd3d769d56ea9e81d987f4880a8
Chalarangelo <chalarangelo@gmail.com>
2019-10-26 12:18:25 +0300

    Add 30-seconds-of-php submodule

---
+++ content/configs/30php.js
@@ -0,0 +1,10 @@
+export default {
+  name: '30 seconds of PHP',
+  dirName: '30php',
+  repoUrl: 'https://github.com/30-seconds/30-seconds-of-php',
+  snippetPath: 'snippets',
+  requirables: [
+    'snippet_data/snippets.json',
+  ],
+  slug: 'php',
+};
