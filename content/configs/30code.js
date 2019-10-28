ad4abc2471a7bb4d9caf67a1e2c5718474b4ef1d
Chalarangelo <chalarangelo@gmail.com>
2019-10-28 12:02:31 +0200

    Add 30-seconds-of-code submodule

---
+++ content/configs/30code.js
@@ -0,0 +1,11 @@
+export default {
+  name: '30 seconds of code',
+  dirName: '30code',
+  repoUrl: 'https://github.com/30-seconds/30-seconds-of-code',
+  snippetPath: 'snippets',
+  requirables: [
+    'snippet_data/snippets.json',
+  ],
+  slug: 'js',
+  reducer: 'es6Reducer',
+};
