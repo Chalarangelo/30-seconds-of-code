511cdd41e8c17b2d3157ef3fed714ce426cf4cf8
Chalarangelo <chalarangelo@gmail.com>
2019-10-28 12:02:31 +0200

    Add 30-seconds-of-code submodule

---
+++ content/configs/30codeArchive.js
@@ -0,0 +1,12 @@
+export default {
+  name: '30 seconds of code - Archive',
+  dirName: '30code',
+  repoUrl: 'https://github.com/30-seconds/30-seconds-of-code',
+  snippetPath: 'snippets_archive',
+  requirables: [
+    'snippet_data/archivedSnippets.json',
+  ],
+  slug: 'js',
+  reducer: 'es6Reducer',
+  isArchived: true,
+};
