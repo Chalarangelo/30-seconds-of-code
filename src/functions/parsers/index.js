ba641eff4edddc6f1319e6260b1d58734874a8fe
Chalarangelo <chalarangelo@gmail.com>
2019-10-28 12:03:42 +0200

    Introduce reducer parsing
    Reducers allow each submodule to provide its own resolution strategy for snippet nodes.
    Create standard reducer (default).
    Crete ES6 reducer for 30code.

--- src/functions/parsers/index.js
+++ src/functions/parsers/index.js
@@ -2,10 +2,12 @@ import parseHtml from './parseHtml';
 import parseQueries from './parseQueries';
 import parseConfigs from './parseConfigs';
 import parseRequirables from './parseRequirables';
+import parseReducers from './parseReducers';
 
 export {
   parseHtml,
   parseQueries,
   parseConfigs,
-  parseRequirables
+  parseRequirables,
+  parseReducers
 };
