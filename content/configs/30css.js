291a7e0d93b8c9499546fb5e498cd82317dee99f
Chalarangelo <chalarangelo@gmail.com>
2019-10-28 13:31:42 +0200

    Add 30-seconds-of-css submodule

---
+++ content/configs/30css.js
@@ -0,0 +1,12 @@
+export default {
+  name: '30 seconds of CSS',
+  dirName: '30css',
+  repoUrl: 'https://github.com/30-seconds/30-seconds-of-css',
+  snippetPath: 'snippets',
+  requirables: [
+    'snippet_data/snippets.json',
+  ],
+  slug: 'css',
+  reducer: 'cssReducer',
+  resolver: 'cssResolver',
+};
