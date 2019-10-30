f5dc574ecaf3953157b581a286db0c1dd4154c5e
Chalarangelo <chalarangelo@gmail.com>
2019-10-30 20:57:28 +0200

    Add 30-seconds-of-react submodule

---
+++ content/configs/30react.js
@@ -0,0 +1,12 @@
+export default {
+  name: '30 seconds of React',
+  dirName: '30react',
+  repoUrl: 'https://github.com/30-seconds/30-seconds-of-react',
+  snippetPath: 'snippets',
+  requirables: [
+    'snippet_data/snippets.json',
+  ],
+  slug: 'react',
+  reducer: 'jsxReducer',
+  resolver: 'jsxResolver',
+};
