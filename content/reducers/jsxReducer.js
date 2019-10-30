ce9e2a5281d9148d3f295ba163e74f8ff09dac7a
Chalarangelo <chalarangelo@gmail.com>
2019-10-30 20:58:21 +0200

    Add JSX reducer and resolver
    Custom strategies for React submodule.

---
+++ content/reducers/jsxReducer.js
@@ -0,0 +1,30 @@
+import { determineExpertiseFromTags } from 'functions/utils';
+
+export default (id, snippetNode, markdownNode) => {
+  return {
+    id,
+    tags: {
+      all: snippetNode.attributes.tags,
+      primary: snippetNode.attributes.tags[0],
+    },
+    expertise: determineExpertiseFromTags(snippetNode.attributes.tags),
+    title: snippetNode.title,
+    code: {
+      src: snippetNode.attributes.codeBlocks.code,
+      example: snippetNode.attributes.codeBlocks.example,
+      style: snippetNode.attributes.codeBlocks.style,
+    },
+    slug: `/${snippetNode.slugPrefix}${markdownNode.fields.slug}`,
+    path: markdownNode.fileAbsolutePath,
+    text: {
+      full: snippetNode.attributes.text,
+      short: snippetNode.attributes.text.slice(0, snippetNode.attributes.text.indexOf('\n\n')),
+    },
+    archived: snippetNode.archived,
+    language: {
+      ...snippetNode.language,
+      otherLanguages: snippetNode.otherLanguages,
+    },
+    browserSupport: snippetNode.attributes.browserSupport,
+  };
+};
