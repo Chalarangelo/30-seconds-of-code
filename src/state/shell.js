e66d8cdd82316961b561e769d567647c4720ca5a
Chalarangelo <chalarangelo@gmail.com>
2019-11-02 13:24:16 +0200

    Separate state into individual reducers

---
+++ src/state/shell.js
@@ -0,0 +1,25 @@
+// Defalt state
+const initialState = {
+  isDarkMode: undefined,
+};
+
+// Actions
+const TOGGLE_DARKMODE = 'TOGGLE_DARKMODE';
+
+export const toggleDarkMode = isDarkMode => ({
+  type: TOGGLE_DARKMODE,
+  isDarkMode,
+});
+
+// Reducer
+export default (state = initialState, action) => {
+  switch (action.type) {
+  case TOGGLE_DARKMODE:
+    return {
+      ...state,
+      isDarkMode: action.isDarkMode,
+    };
+  default:
+    return state;
+  }
+};
