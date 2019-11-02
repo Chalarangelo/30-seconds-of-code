d91f567cb7d828e009d32cf68d00f15c07c141f2
Chalarangelo <chalarangelo@gmail.com>
2019-11-02 10:46:44 +0200

    Create PageBackdrop molecule

---
+++ src/molecules/pageBackdrop/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PageGraphic from 'atoms/pageGraphic';
+import { trimWhiteSpace } from 'functions/utils';
+
+const PageBackdrop = ({
+  graphicName,
+  mainText,
+  mainTextClassName,
+  subText,
+  subTextClassName,
+  children,
+  ...rest
+}) => (
+  <PageGraphic
+    className={ graphicName }
+    { ...rest }
+  >
+    <p className={ trimWhiteSpace`page-backdrop-text ${mainTextClassName}` }>
+      { mainText }
+    </p>
+    { subText ?
+      <p className={ trimWhiteSpace`page-backdrop-subtext ${subTextClassName}` }>
+        { subText }
+      </p>
+      : null
+    }
+    { children }
+  </PageGraphic>
+);
+
+PageBackdrop.propTypes = {
+  /** The name of the graphic for the backdrop */
+  graphicName: PropTypes.string,
+  /** Main text for the backdrop */
+  mainText: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  /** Additional classnames to be passed to the main text */
+  mainTextClassName: PropTypes.string,
+  /** Secondary text for the backdrop */
+  subText: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  /** Additional classnames to be passed to the secondary text */
+  subTextClassName: PropTypes.string,
+  /** Additional children elements for the backdrop */
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  /** Any other arguments to be passed to the backdrop */
+  rest: PropTypes.any,
+};
+
+export default PageBackdrop;
