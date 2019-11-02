f2d9d9afab7aeebeac2a33fb8bb6a22e87721815
Chalarangelo <chalarangelo@gmail.com>
2019-11-02 12:52:10 +0200

    Add getURLParameters
    Utility function for getting url params.

--- src/functions/utils/string.js
+++ src/functions/utils/string.js
@@ -58,3 +58,10 @@ export const optimizeAllNodes = html => {
   );
   return output;
 };
+
+/** Returns an object containing the parameters of the current URL. */
+export const getURLParameters = url =>
+  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+    (a, v) => ((a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1)), a),
+    {}
+  );
