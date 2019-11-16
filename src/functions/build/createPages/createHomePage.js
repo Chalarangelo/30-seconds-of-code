56bdebc0e500c4c9ffbf6b3baf62bb831937ad90
Chalarangelo <chalarangelo@gmail.com>
2019-11-16 17:26:10 +0200

    Refactor listing and home page creation
    Use individual config/requirables.
    Include only the required information.
    Avoid code duplication.
    Improve performance.

--- src/functions/build/createPages/createHomePage.js
+++ src/functions/build/createPages/createHomePage.js
@@ -1,9 +1,20 @@
-const createHomePage = (homePage, createPage, context) => {
+const createHomePage = (listingMetas, homePage, createPage, context) => {
+  const featuredSources = listingMetas
+    .filter(meta => meta.featured > 0)
+    .sort((a, b) => a.featured - b.featured)
+    .map(meta => ({
+      link: meta.link,
+      name: meta.name,
+      style: meta.style,
+      count: meta.count,
+    }));
+
   createPage({
     path: '/',
     component: homePage,
     context: {
       ...context,
+      listingAnchors: featuredSources,
     },
   });
 };
