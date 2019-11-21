6e738c57f47448227f3e87ba88ba8e231725179b
Angelos Chalaris <chalarangelo@gmail.com>
2019-11-21 22:30:37 +0200

    Update prepare cache key job
    Echo a log in the console.

--- src/jobs/prepare_cache_key.sh
+++ src/jobs/prepare_cache_key.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 prepare_cache_key() {
+  echo "Generating cache key..."
   mkdir -p .build
   echo "const cacheKey = '"$(date +%s%N)"';export default cacheKey;" > "./.build/cacheKey.js"
 }
