a3ab239d589f60abe1c4e28d9fd9a38c222c782b
Chalarangelo <chalarangelo@gmail.com>
2019-11-23 14:06:49 +0200

    Enable Travis CI
    Add bash script to call deploy if necessary.
    Add Travis CI configuration file.

--- src/jobs/deploy.sh
+++ src/jobs/deploy.sh
@@ -2,10 +2,9 @@
 deploy_branch() {
   git config --global user.email "30secondsofcode@gmail.com"
   git config --global user.name "30secondsofcode"
-  # git push -d origin $1
-  # git checkout -b $1
-  git push --force --quiet "https://${GH_TOKEN}@github.com/30-seconds/30-seconds-web.git" production > dev/null 2>&1
-  # git push origin $1
+  git checkout -b $1
+  git commit -m "Deployment from Travis build $TRAVIS_BUILD_NUMBER" --allow-empty
+  git push --force --quiet "https://${GH_TOKEN}@github.com/30-seconds/30-seconds-web.git" $1 > /dev/null 2>&1
 }
 
 deploy_production() {
