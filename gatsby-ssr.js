c0c5c9fb5039cfe515bb6735d07702f606d9903b
Angelos Chalaris <chalarangelo@gmail.com>
2019-12-03 10:24:31 +0200

    Update for integration tools

--- gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
-export { default as wrapRootElement } from './src/docs/state/ReduxWrapper';
