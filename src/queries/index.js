c3d8b487c2bcf7fd369c4004c5fa266eee95f0ff
Chalarangelo <chalarangelo@gmail.com>
2020-01-10 13:17:04 +0200

    Add images query to GraphQL

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,9 +1,11 @@
 import createPagesQuery from './createPagesQuery';
 import getLogoSrc from './getLogoSrc';
+import getImages from './getImages';
 import getSearchIndex from './getSearchIndex';
 
 export {
   createPagesQuery,
   getLogoSrc,
+  getImages,
   getSearchIndex
 };
