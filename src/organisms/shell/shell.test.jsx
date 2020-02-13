66cb335a3ae7ed5c70e3eadd5ede2a0f8881b734
Angelos Chalaris <chalarangelo@gmail.com>
2020-02-13 21:54:05 +0200

    Update Shell to new navigation layout
    Handle logic appropriately.
    Update layout.

--- src/organisms/shell/shell.test.jsx
+++ src/organisms/shell/shell.test.jsx
@@ -20,14 +20,13 @@ const expectArrayToContainMatchingObject = (arr, obj) => {
 
 describe('<Shell />', () => {
   const logoSrc = '/assets/logo.png';
-  let wrapper, pageContainer, navBar, content, title, logo;
+  let wrapper, pageContainer;
   const innerText = 'Hi there!';
 
   beforeEach(() => {
     wrapper = mount(
       <Provider store={ store }>
         <Shell
-          isListing={ false }
           isSearch={ false }
           logoSrc={ logoSrc }
         >
@@ -36,8 +35,6 @@ describe('<Shell />', () => {
       </Provider>
     );
     pageContainer = wrapper.find('div.page-container');
-    navBar = wrapper.find('NavBar');
-    content = wrapper.find('div.content');
   });
 
   describe('should render', () => {
@@ -51,119 +48,6 @@ describe('<Shell />', () => {
 
     it('the content container', () => {
       expect(pageContainer).toContainMatchingElement('div.content');
-    });
-  });
-
-  it('should create a NavBar with 4 buttons', () => {
-    expect(navBar.prop('buttons').length).toBe(4);
-  });
-
-  describe('should pass a button to the NavBar', () => {
-    it('for the search page', () => {
-      expectArrayToContainMatchingObject(navBar.prop('buttons'), {
-        icon: 'search',
-      });
-    });
-    it('for the list page', () => {
-      expectArrayToContainMatchingObject(navBar.prop('buttons'), {
-        icon: 'list',
-      });
-    });
-    it('for the github link', () => {
-      expectArrayToContainMatchingObject(navBar.prop('buttons'), {
-        icon: 'github',
-      });
-    });
-    it('for the mode toggle', () => {
-      expectArrayToContainMatchingObject(navBar.prop('buttons'), {
-        icon: 'moon',
-      });
-    });
-  });
-
-  describe('with title and icon', () => {
-    beforeEach(() => {
-      wrapper = mount(
-        <Provider store={ store }>
-          <Shell
-            isList={ false }
-            isSearch={ false }
-            logoSrc={ logoSrc }
-            withTitle
-            withIcon
-          />
-        </Provider>
-      );
-      pageContainer = wrapper.find('div.page-container');
-      navBar = wrapper.find('NavBar');
-      content = wrapper.find('div.content');
-      title = wrapper.find('.website-title');
-      logo = wrapper.find('.website-logo');
-    });
-
-    it('should render the website title', () => {
-      expect(content).toContainMatchingElement('.website-title');
-    });
-    it('should render the website logo', () => {
-      expect(title).toContainMatchingElement('.website-logo');
-    });
-    it('should pass the logoSrc to the logo', () => {
-      expect(logo.prop('src')).toBe(logoSrc);
-    });
-    it('should pass an alt attribute to the logo', () => {
-      expect(logo.prop('alt')).not.toBe(undefined);
-    });
-  });
-
-  describe('without title or icon', () => {
-    beforeEach(() => {
-      wrapper = mount(
-        <Provider store={ store }>
-          <Shell
-            isList={ false }
-            isSearch={ false }
-            logoSrc={ logoSrc }
-            withTitle={ false }
-            withIcon={ false }
-          />
-        </Provider>
-      );
-      pageContainer = wrapper.find('div.page-container');
-      navBar = wrapper.find('NavBar');
-      content = wrapper.find('div.content');
-    });
-
-    it('should not render a title or icon', () => {
-      expect(content).not.toContainMatchingElement('.website-title');
-      expect(content).not.toContainMatchingElement('.website-logo');
-    });
-  });
-
-  describe('when the mode toggle is clicked', () => {
-    beforeEach(() => {
-      wrapper = mount(
-        <Provider store={ store }>
-          <Shell
-            isList={ false }
-            isSearch={ false }
-            logoSrc={ logoSrc }
-            withTitle
-            withIcon
-          />
-        </Provider>
-      );
-    });
-
-    it('should change the mode', () => {
-      wrapper.find('.icon-moon').at(0).simulate('click');
-      expect(wrapper).toContainMatchingElement('.nav-btn.icon-sun');
-      expect(wrapper).not.toContainMatchingElement('.nav-btn.icon-moon');
-    });
-
-    it('should change the mode back when clicked a second time', () => {
-      wrapper.find('.icon-sun').at(0).simulate('click');
-      expect(wrapper).toContainMatchingElement('.nav-btn.icon-moon');
-      expect(wrapper).not.toContainMatchingElement('.nav-btn.icon-sun');
     });
   });
 
