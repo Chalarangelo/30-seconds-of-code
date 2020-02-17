d4b85c0b0b91fe08d95e8ef5674444e3d5228d2e
Chalarangelo <chalarangelo@gmail.com>
2020-02-17 21:23:05 +0200

    Create a job for adding new content sources
    Resolves #17

---
+++ src/jobs/add_content_source.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Usage/arguments:
+#  - $1: GitHub repository URL  (e.g. https://github.com/30-seconds/30-seconds-of-yada)
+#  - $2: Directory name         (e.g. 30yada)
+#  - $3: Name                   (e.g. "30 seconds of yada")
+#  - $4: Slug                   (e.g. yada)
+# Example:
+#  add_content_source https://github.com/30-seconds/30-seconds-of-yada 30yada "30 seconds of yada" yada
+add_content_source() {
+  git submodule add $1 "./content/sources/$2"
+  echo "export default {
+  name: '$3',
+  dirName: '$2',
+  repoUrl: '$1',
+  snippetPath: 'snippets',
+  requirables: [
+    'snippet_data/snippets.json',
+  ],
+  slug: '$4',
+  featured: 500,
+  theme: {
+    backColor: '#ffffff',
+    foreColor: '#000000',
+    iconName: '$4',
+  },
+  biasPenaltyMultiplier: 1.00,
+  tagScores: {
+  },
+  keywordScores: {
+  },
+};" >> "./content/configs/$2.js"
+}
+
+add_content_source $1 $2 "$3" $4
