77ac1aa38b701a8703bc847c5e640d54cd8b4547
Chalarangelo <chalarangelo@gmail.com>
2020-02-22 14:43:45 +0200

    Add breadcrumbs transformer

---
+++ src/functions/transformers/transformBreadcrumbs.js
@@ -0,0 +1,31 @@
+import { transformTagName } from './transformTags';
+/**
+* Given a snippet object with key-value pairs, create the
+* necessary breadcrumb data.
+*
+* Used in snippet pages to render breadcrumbs and set up structured data.
+*/
+export const transformBreadcrumbs = (snippet, cardTemplate) => {
+  const slugParts = snippet.slug.split('/').filter(Boolean).slice(0, -2);
+  let breadcrumbs = [
+    {
+      link: {
+        internal: true,
+        url: `/${slugParts[0]}/p/1`,
+      },
+      name: cardTemplate === 'blog' ? transformTagName('blog') : snippet.language.long,
+    },
+  ];
+
+  if(cardTemplate !== 'blog') {
+    breadcrumbs.push({
+      link: {
+        internal: true,
+        url: `/${slugParts[0]}/${snippet.tags.primary.toLowerCase()}/p/1`,
+      },
+      name: `${snippet.language.long} ${transformTagName(snippet.tags.primary)}`,
+    });
+  }
+
+  return breadcrumbs;
+};
