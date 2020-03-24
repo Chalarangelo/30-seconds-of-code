1271cb1bf9e88592bdd18ea4f619b6ef43bc9e52
Chalarangelo <chalarangelo@gmail.com>
2020-03-24 19:45:39 +0200

    Cleanup recommendation engine
    Recommendations without context are redundant, defaulting to ranking.

--- src/engines/recommendationEngine.js
+++ src/engines/recommendationEngine.js
@@ -1,49 +1,8 @@
 /* eslint-disable brace-style */
-import config from '../../config';
-const { recomendationEngine } = config;
-import { mapNumRange, similarity } from 'functions/utils';
+import { similarity } from 'functions/utils';
 import { determineLanguageFromTags, stripLanguageFromTags } from 'functions/transformers';
 
-const determineRecommendedSnippetsWithoutContext = snippetNodes => {
-  const freshnessSortedNodes = Object.entries(snippetNodes)
-    .filter(v => !v[1].archived)
-    .sort((a, b) => +b[1].meta.lastUpdated - a[1].meta.lastUpdated);
-
-  const freshNodes = freshnessSortedNodes.slice(0, Math.ceil(0.1 * freshnessSortedNodes.length));
-  const freshnessMaxValue = Math.max(...freshNodes.map(v => +v[1].meta.lastUpdated));
-  const freshnessMaxDifference = freshnessMaxValue - Math.min(...freshNodes.map(v => +v[1].meta.lastUpdated));
-
-  return freshNodes.map(node => {
-    const freshnessValue = mapNumRange(freshnessMaxValue - node[1].meta.lastUpdated, freshnessMaxDifference, 0, 0.0, recomendationEngine.noContextFreshnessMultiplier).toFixed(4);
-    const indexableContent = [
-      node[1].title,
-      node[1].attributes.codeBlocks && node[1].attributes.codeBlocks.src || '',
-      node[1].attributes.codeBlocks && node[1].attributes.codeBlocks.es6 || '',
-      node[1].attributes.codeBlocks && node[1].attributes.codeBlocks.css || '',
-      node[1].attributes.codeBlocks && node[1].attributes.codeBlocks.html || '',
-      node[1].attributes.codeBlocks && node[1].attributes.codeBlocks.js || '',
-      node[1].attributes.codeBlocks && node[1].attributes.codeBlocks.style || '',
-      node[1].attributes.text,
-    ].join(' ');
-
-    const keywordMaxValue = Object.values(node[1].keywordScores || []).reduce((acc, v) => acc + v, 0);
-    let keywordValue = 0;
-
-    Object.keys(node[1].keywordScores || []).forEach(k => {
-      if (indexableContent.indexOf(k) !== -1) keywordValue += node[1].keywordScores[k];
-    });
-
-    keywordValue = mapNumRange(keywordValue, 0, keywordMaxValue, 0.0, recomendationEngine.noContextKeywordMultiplier).toFixed(4);
-    keywordValue = isNaN(keywordValue) ? 0 : keywordValue;
-
-    return {
-      recommendationRanking: +freshnessValue + +keywordValue,
-      snippetKey: node[0],
-    };
-  }).sort((a, b) => b.recommendationRanking - a.recommendationRanking );
-};
-
-const determineRecommendedSnippetsWithContext = (snippetNodes, snippetContext) => {
+const determineRecommendedSnippets = (snippetNodes, snippetContext) => {
   const _snippetContext = {
     language: snippetContext.node.blog
       ? determineLanguageFromTags(snippetContext.node.tags.all)
@@ -94,12 +53,6 @@ const determineRecommendedSnippetsWithContext = (snippetNodes, snippetContext) =
   return [...new Set(relatedSnippets.sort((a, b) =>
     b.similarity - a.similarity || b.ranking - a.ranking
   ))].filter(v => v.node.url !== snippetContext.node.url);
-};
-
-const determineRecommendedSnippets = (data, context) => {
-  if(typeof context !== 'undefined')
-    return determineRecommendedSnippetsWithContext(data, context);
-  return determineRecommendedSnippetsWithoutContext(data);
 };
 
 export default determineRecommendedSnippets;
