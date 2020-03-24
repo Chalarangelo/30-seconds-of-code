18c9a4833ccb15e4293c6afa152a47ccd9080b09
Chalarangelo <chalarangelo@gmail.com>
2020-03-24 19:46:36 +0200

    Update search page components
    Show recommendations when available.

--- src/organisms/searchResults/index.jsx
+++ src/organisms/searchResults/index.jsx
@@ -2,38 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PageBackdrop from 'molecules/pageBackdrop';
 import PropTypes from 'prop-types';
+import { Snippet as SnippetPropType } from 'typedefs';
 import _ from 'lang';
-import PageSubtitle from 'atoms/pageSubtitle';
+import PageTitle from 'atoms/pageTitle';
 import PreviewCard from 'molecules/previewCard';
+import RecommendationList from 'organisms/recommendationList';
 const _l = _('en');
 
 // eslint-disable-next-line complexity
 const SearchResults = ({
   searchQuery,
   searchResults,
-}) =>
-  searchQuery.trim().length <= 1 ? (
-    <PageBackdrop
-      graphicName='search-empty'
-      mainText={ _l('Start typing a keyphrase to see matching snippets.') }
-      mainTextClassName='search-page-text'
-    />
-  ) : searchResults.length === 0 ? (
-    <PageBackdrop
-      graphicName='search-no-results'
-      mainText={ (
-        <>
-          { _l('We couldn\'t find any results for the keyphrase ') }<strong>{ searchQuery }</strong>
-          { _l('.') }
-        </>
-      ) }
-      mainTextClassName='search-page-text'
-    />
-  ) : (
+  recommendedSnippets = [],
+}) => {
+  const hasResults = searchQuery.trim().length > 1 && searchResults.length !== 0;
+  return hasResults ? (
     <>
-      <PageSubtitle isLight className='search-results-title'>
+      <PageTitle isLight>
         { _l('Search results') }
-      </PageSubtitle>
+      </PageTitle>
       { searchResults.map(snippet => (
         <PreviewCard
           key={ `snippet_${snippet.url}` }
@@ -41,13 +28,35 @@ const SearchResults = ({
         />
       )) }
     </>
+  ) : (
+    <>
+      <PageBackdrop
+        graphicName={ searchQuery.trim().length <= 1 ? 'search-empty' : 'search-no-results' }
+        mainText={ searchQuery.trim().length <= 1
+          ? _l('Start typing a keyphrase to see matching snippets.')
+          : (
+            <>
+              { _l('We couldn\'t find any results for the keyphrase ') }<strong>{ searchQuery }</strong>
+              { _l('.') }
+            </>
+          )
+        }
+        mainTextClassName='search-page-text'
+      />
+      { recommendedSnippets.length ? (
+        <RecommendationList snippetList={ recommendedSnippets } />
+      ) : null }
+    </>
   );
+};
 
 SearchResults.propTypes = {
   /** Search query */
   searchQuery: PropTypes.string,
   /** Search results */
   searchResults: PropTypes.arrayOf(PropTypes.shape({})),
+  /** List of recommended snippets */
+  recommendedSnippets: PropTypes.arrayOf(SnippetPropType),
 };
 
 export default connect(
