f9e8e5913a3e782cafaa4c84da4164f2678fb4f1
Chalarangelo <chalarangelo@gmail.com>
2020-03-25 20:04:58 +0200

    Extract footer component literals and to new files

---
+++ src/lang/en/client/footer.js
@@ -0,0 +1,16 @@
+const literals = {
+  about: 'About',
+  github: 'GitHub',
+  twitter: 'Twitter',
+  cookies: 'Cookies',
+  discord: 'Discord',
+  copyright: 'Website, name & logo © 2017-2020 ',
+  snippetLicense: 'Individual snippets licensed under ',
+  ccLicense: 'CC0-1.0',
+  poweredBy: 'Powered by ',
+  netlify: 'Netlify',
+  gatsby: 'Gatsby',
+  travis: 'Travis CI',
+};
+
+export default literals;
