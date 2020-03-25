33a935171c70fd4c32cf7003f3e777381e0a6bf7
Chalarangelo <chalarangelo@gmail.com>
2020-03-25 16:57:11 +0200

    Migrate string literals to new directory

--- src/lang/en/index.js
+++ src/lang/en/index.js
@@ -1,7 +1,11 @@
+import site from './site';
 import about from './about';
 import cookies from './cookies';
+import settings from './settings';
 
 export default {
+  site,
   about,
   cookies,
+  settings,
 };
---
+++ src/lang/en/site/index.js
@@ -0,0 +1,9 @@
+import config from '../../../../config';
+
+const literals = {
+  siteTitle: config.name,
+  siteDescription: config.description,
+  siteAuthor: config.author,
+};
+
+export default literals;
