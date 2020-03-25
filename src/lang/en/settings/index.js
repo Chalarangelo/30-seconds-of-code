93d782fc5bde7ab93de5cd7825160f6b5554051d
Chalarangelo <chalarangelo@gmail.com>
2020-03-25 16:57:11 +0200

    Migrate string literals to new directory

---
+++ src/lang/en/settings/index.js
@@ -0,0 +1,13 @@
+import config from '../../../../config';
+
+const literals = {
+  title: 'Settings',
+  pageDescription: `Customize your ${config.name} experience.`,
+  settings: {
+    darkMode: 'Dark mode',
+    githubLinks: 'GitHub links',
+    cookies: 'cookies',
+  },
+};
+
+export default literals;
