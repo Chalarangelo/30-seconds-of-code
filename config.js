5b92a86d97d85d0b1b8aa7cce0c22d5d94dceefc
Chalarangelo <chalarangelo@gmail.com>
2020-03-26 18:51:55 +0200

    Extract stateName property to persist config
    No reason to live in shared state, as it is only used in that file.

--- config.js
+++ config.js
@@ -20,7 +20,6 @@ module.exports = {
       send_page_view: false,  // Prevent duplicate page views
     },
   },
-  stateName: `30-sec-app`,
   // Path information
   assetPath: `assets`,
   contentPath: `${__dirname}/content`,
