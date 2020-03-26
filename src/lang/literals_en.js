9542297ce89b05e432cc7ef37a6b58a663fbbde7
Chalarangelo <chalarangelo@gmail.com>
2020-03-26 18:29:49 +0200

    Extract cookie consent popup literals to client language file

--- src/lang/literals_en.js
+++ src/lang/literals_en.js
@@ -7,12 +7,6 @@ const literals = {
   'site.author': config.author,
   // Literals
   'Logo': 'Logo',
-  'Our website uses tools such as cookies to provide a high quality personalized experience and gather anonymized data for statistical analisis of the website\'s traffic.': `${config.name} uses cookies to provide a high quality user experience and gather anonymized data for statistical analysis of the website's traffic.`,
-  'By clicking "Accept" you accept their installation.': 'By clicking "Accept" you accept their installation.',
-  'Accept': 'Accept',
-  'Decline': 'Decline',
-  'You can learn more by reading our ': ' You can learn more by reading our ',
-  'cookie policy': 'cookie policy',
 };
 
 export default literals;
