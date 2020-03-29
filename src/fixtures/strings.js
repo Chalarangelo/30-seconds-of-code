adbc7a230936c43ae3ad4fc6331361f74318532f
Chalarangelo <chalarangelo@gmail.com>
2020-03-29 19:48:01 +0300

    Cleanup snippetPreview
    Extract fixtures, cleanup component implementation, increase test coverage.

--- src/fixtures/strings.js
+++ src/fixtures/strings.js
@@ -4,3 +4,10 @@ export const codepenCssCode = `.my-special-snippet {
   background: red;
   color: white;
 }`;
+
+export const scopedHtmlCode = '<p class="my-special-snippet">Hello, this is white on red.</p>';
+
+export const scopedCssCode = `[data-scope="my-special-snippet] .my-special-snippet {
+  background: red;
+  color: white;
+}`;
