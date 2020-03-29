df95acef5e9325959ada57adc28f1bc4d98ff02d
Chalarangelo <chalarangelo@gmail.com>
2020-03-29 14:35:58 +0300

    Cleanup card component
    Improve tests.

--- src/components/atoms/card/index.jsx
+++ src/components/atoms/card/index.jsx
@@ -2,32 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { trimWhiteSpace } from 'functions/utils';
 
+const propTypes = {
+  className: PropTypes.string,
+  rest: PropTypes.any,
+};
+
 /**
- * Generic card component.
+ * Generic card component. Renders a simple `<div>` element with a base class
+ * and passes everything else to the element.
  */
 const Card = ({
   className,
-  children,
   ...rest
 }) => (
   <div
     className={ trimWhiteSpace`card ${className}` }
     { ...rest }
-  >
-    { children }
-  </div>
+  />
 );
 
-Card.propTypes = {
-  /** Additional classes for the card */
-  className: PropTypes.string,
-  /** The card's children */
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node),
-  ]),
-  /** Any other arguments to be passed to the card */
-  rest: PropTypes.any,
-};
+Card.propTypes = propTypes;
 
 export default Card;
