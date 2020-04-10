2c1205f22236795d742f9d92738748ec8562eea9
Chalarangelo <chalarangelo@gmail.com>
2020-04-10 18:54:25 +0300

    Move resolvers inside the build directory
    Decouple code from content.

--- src/build/parsers/index.js
+++ src/build/parsers/index.js
@@ -1,7 +1,6 @@
 import parseQueries from './parseQueries';
 import parseConfigs from './parseConfigs';
 import parseRequirables from './parseRequirables';
-import parseResolvers from './parseResolvers';
 import parseTemplates from './parseTemplates';
 import parseListingMetas from './parseListingMetas';
 
@@ -9,7 +8,6 @@ export {
   parseQueries,
   parseConfigs,
   parseRequirables,
-  parseResolvers,
   parseTemplates,
   parseListingMetas
 };
