5f1f5ecfe800fb41ad2a0440f912c258bcc44e58
Angelos Chalaris <chalarangelo@gmail.com>
2020-04-16 11:32:23 +0300

    Remove cleanObj

--- test/cleanObj.test.js
+++
@@ -1,9 +0,0 @@
-const {cleanObj} = require('./_30s.js');
-
-test('cleanObj is a Function', () => {
-  expect(cleanObj).toBeInstanceOf(Function);
-});
-const testObj = { a: 1, b: 2, children: { a: 1, b: 2 } };
-test('Removes any properties except the ones specified from a JSON object', () => {
-  expect(cleanObj(testObj, ['a'], 'children')).toEqual({ a: 1, children: { a: 1 } });
-});
