5d4f6455952c0e92554394ea557bbb86854bd189
Angelos Chalaris <chalarangelo@gmail.com>
2020-04-16 11:09:02 +0300

    Remove JSONToDate

--- test/JSONToDate.test.js
+++
@@ -1,9 +0,0 @@
-const {JSONToDate} = require('./_30s.js');
-
-test('JSONToDate is a Function', () => {
-  expect(JSONToDate).toBeInstanceOf(Function);
-});
-test('JSONToDate returns the correct date string', () => {
-  var reg = new RegExp(`Date(${Date.parse('March 14,2017')})`);
-  expect(JSONToDate(reg)).toBe('14/3/2017');
-});
