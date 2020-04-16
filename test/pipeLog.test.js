69be08ff4bde6b946f9cb5fa40913ecd65bc27e4
Angelos Chalaris <chalarangelo@gmail.com>
2020-04-16 11:12:17 +0300

    Remove pipeLog

--- test/pipeLog.test.js
+++
@@ -1,8 +0,0 @@
-const {pipeLog} = require('./_30s.js');
-
-test('pipeLog is a Function', () => {
-  expect(pipeLog).toBeInstanceOf(Function);
-});
-test('pipeLog returns the given value', () => {
-  expect(pipeLog('hi')).toBe('hi');
-});
