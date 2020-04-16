bd00bc75ee2572b0bfcad01da592145ecc72cd9e
Angelos Chalaris <chalarangelo@gmail.com>
2020-04-16 11:04:51 +0300

    Remove mphToKmph

--- test/mphToKmph.test.js
+++
@@ -1,8 +0,0 @@
-const {mphToKmph} = require('./_30s.js');
-
-test('mphToKmph is a Function', () => {
-  expect(mphToKmph).toBeInstanceOf(Function);
-});
-test('Returns kph from mph.', () => {
-  expect(mphToKmph(10)).toBe(16.09344000614692);
-});
