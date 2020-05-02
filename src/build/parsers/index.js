d1fb073cb55973610699e5ffc793e9b70bb1cee5
Chalarangelo <chalarangelo@gmail.com>
2020-05-02 17:25:00 +0300

    Update parsers
    Require each JSON as-is, snippet parser will handle the rest.

--- src/build/parsers/index.js
+++ src/build/parsers/index.js
@@ -3,11 +3,13 @@ import parseConfigs from './parseConfigs';
 import parseRequirables from './parseRequirables';
 import parseTemplates from './parseTemplates';
 import parseListingMetas from './parseListingMetas';
+import parseSnippets from './parseSnippets';
 
 export {
   parseQueries,
   parseConfigs,
   parseRequirables,
   parseTemplates,
-  parseListingMetas
+  parseListingMetas,
+  parseSnippets
 };
