e0fd77a9f08467e97ca5fc49285522cfc348b483
Chalarangelo <chalarangelo@gmail.com>
2020-05-06 18:29:19 +0300

    Replace 30css reducer with std

--- src/build/reducers/index.js
+++ src/build/reducers/index.js
@@ -1,9 +1,7 @@
 import stdReducer from './stdReducer';
-import cssReducer from './cssReducer';
 import blogReducer from './blogReducer';
 
 export default {
   stdReducer,
-  cssReducer,
   blogReducer,
 };
