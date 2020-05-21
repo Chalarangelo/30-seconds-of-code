8b45b40c55a2622bc07cd7e57401f20e32f0ed90
Chalarangelo <chalarangelo@gmail.com>
2020-05-21 22:24:07 +0300

    Introduce insertAt util for array

--- src/utils/array.test.js
+++ src/utils/array.test.js
@@ -1,5 +1,6 @@
 import {
   uniqueElements,
+  insertAt,
   chunk
 } from './array';
 
@@ -15,5 +16,12 @@ describe('chunk', () => {
   });
   it('chunks an empty array', () => {
     expect(chunk([])).toEqual([]);
+  });
+});
+
+describe('insertAt', () => {
+  it('inserts the element at the given index', () => {
+    let arr = [1, 2, 3];
+    expect(insertAt(1, 4, arr)).toEqual([1, 2, 4, 3]);
   });
 });
