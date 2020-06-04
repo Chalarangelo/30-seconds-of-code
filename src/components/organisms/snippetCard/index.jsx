5de64e01b6ae0d7a53de801d1fe87d56d6b54620
Chalarangelo <chalarangelo@gmail.com>
2020-06-04 10:39:12 +0300

    Remove HOC from SnippetCard

--- src/components/organisms/snippetCard/index.jsx
+++ src/components/organisms/snippetCard/index.jsx
@@ -1,70 +1,9 @@
-import React from 'react';
-import PropTypes from 'typedefs/proptypes';
-import { connect } from 'react-redux';
-
 import StandardSnippetCard from './standardSnippetCard';
 import CssSnippetCard from './cssSnippetCard';
 import BlogSnippetCard from './blogSnippetCard';
 
-export {
+export default {
   StandardSnippetCard,
   CssSnippetCard,
   BlogSnippetCard,
 };
-
-const propTypes = {
-  cardTemplate: PropTypes.string,
-  hasGithubLinksEnabled: PropTypes.bool,
-  dispatch: PropTypes.func,
-  rest: PropTypes.any,
-};
-
-/**
- * Renders a snippet card. (Redux-connected)
- * Used in snippet pages.
- * Dependent on its internal components to implement the desired functionality.
- */
-const SnippetCard = ({
-  cardTemplate,
-  hasGithubLinksEnabled,
-  // Keep dispatch here, as to not pass it down the component tree and get errors.
-  // eslint-disable-next-line no-unused-vars
-  dispatch,
-  ...rest
-}) => {
-  switch (cardTemplate) {
-  case 'blog':
-    return (
-      <BlogSnippetCard
-        hasGithubLinksEnabled={ !!hasGithubLinksEnabled }
-        { ...rest }
-      />
-    );
-  case 'css':
-    return (
-      <CssSnippetCard
-        hasGithubLinksEnabled={ !!hasGithubLinksEnabled }
-        { ...rest }
-      />
-    );
-  case 'standard':
-  default:
-    return (
-      <StandardSnippetCard
-        hasGithubLinksEnabled={ !!hasGithubLinksEnabled }
-        { ...rest }
-      />
-    );
-  }
-};
-
-SnippetCard.displayName = 'SnippetCardWrapper';
-
-SnippetCard.propTypes = propTypes;
-
-export default connect(
-  state => ({
-    hasGithubLinksEnabled: state.shell.hasGithubLinksEnabled,
-  }),
-  null
-)(SnippetCard);
