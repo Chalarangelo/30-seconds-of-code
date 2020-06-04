f3ab9c401b6730da95a60d1b268b5d5ec4218385
Chalarangelo <chalarangelo@gmail.com>
2020-06-04 10:39:29 +0300

    Update SnippetPage to use non-HOC SnippetCard components

--- src/components/templates/snippetPage/index.jsx
+++ src/components/templates/snippetPage/index.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import Meta from 'components/organisms/meta';
 import Breadcrumbs from 'components/molecules/breadcrumbs';
 import Shell from 'components/organisms/shell';
-import SnippetCard from 'components/organisms/snippetCard';
+import cardComponents from 'components/organisms/snippetCard';
 import RecommendationList from 'components/organisms/recommendationList';
 import CTA from 'components/organisms/cta';
 
@@ -23,6 +23,7 @@ const propTypes = {
       })
     ),
   }),
+  hasGithubLinksEnabled: PropTypes.bool,
   lastPageTitle: PropTypes.string.isRequired,
   lastPageUrl: PropTypes.string.isRequired,
 };
@@ -41,50 +42,55 @@ const SnippetPage = ({
     breadcrumbs,
     pageDescription,
   },
+  hasGithubLinksEnabled,
   lastPageTitle,
   lastPageUrl,
-}) => (
-  <>
-    <Meta
-      title={ snippet.title }
-      description={ pageDescription }
-      logoSrc={ cardTemplate === 'blog' ? snippet.cover.src : splashLogoSrc }
-      structuredData={ {
-        title: snippet.title,
-        description: snippet.description,
-        slug: snippet.slug,
-        orgLogoSrc: logoSrc,
-        firstSeen: snippet.firstSeen,
-        lastUpdated: snippet.lastUpdated,
-      } }
-      breadcrumbsData={ breadcrumbs }
-      canonical={ snippet.slug }
-    />
-    <Shell logoSrc={ logoSrc } >
-      <Breadcrumbs
-        lastPage={ {
-          link: {
-            url: lastPageUrl,
-            internal: true,
-          },
-          name: lastPageTitle,
+}) => {
+  const SnippetCard = cardComponents[cardTemplate];
+  return (
+    <>
+      <Meta
+        title={ snippet.title }
+        description={ pageDescription }
+        logoSrc={ cardTemplate === 'BlogSnippetCard' ? snippet.cover.src : splashLogoSrc }
+        structuredData={ {
+          title: snippet.title,
+          description: snippet.description,
+          slug: snippet.slug,
+          orgLogoSrc: logoSrc,
+          firstSeen: snippet.firstSeen,
+          lastUpdated: snippet.lastUpdated,
         } }
-        breadcrumbs={ breadcrumbs }
+        breadcrumbsData={ breadcrumbs }
+        canonical={ snippet.slug }
       />
-      <SnippetCard
-        cardTemplate={ cardTemplate }
-        snippet={ snippet }
-      />
-      <CTA/>
-      <RecommendationList snippetList={ recommendedSnippets } />
-    </Shell>
-  </>
-);
+      <Shell logoSrc={ logoSrc } >
+        <Breadcrumbs
+          lastPage={ {
+            link: {
+              url: lastPageUrl,
+              internal: true,
+            },
+            name: lastPageTitle,
+          } }
+          breadcrumbs={ breadcrumbs }
+        />
+        <SnippetCard
+          snippet={ snippet }
+          hasGithubLinksEnabled={ !!hasGithubLinksEnabled }
+        />
+        <CTA/>
+        <RecommendationList snippetList={ recommendedSnippets } />
+      </Shell>
+    </>
+  );
+};
 
 SnippetPage.propTypes = propTypes;
 
 export default connect(
   state => ({
+    hasGithubLinksEnabled: state.shell.hasGithubLinksEnabled,
     lastPageTitle: state.navigation.lastPageTitle,
     lastPageUrl: state.navigation.lastPageUrl,
   }),
