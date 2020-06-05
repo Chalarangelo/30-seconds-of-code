d247d79cfeaaa74920f2fbc374eaec7c5430c86f
Chalarangelo <chalarangelo@gmail.com>
2020-06-05 16:47:38 +0300

    Deprecate AnchorButton
    Use raw anchors with a.btn instead.

--- src/components/atoms/button/index.jsx
+++ src/components/atoms/button/index.jsx
@@ -1,11 +1,9 @@
 import Button from './regularButton';
 import CopyButton from './copyButton';
-import AnchorButton from './anchorButton';
 import CodepenButton from './codepenButton';
 
 export {
   Button,
   CopyButton,
-  AnchorButton,
   CodepenButton,
 };
