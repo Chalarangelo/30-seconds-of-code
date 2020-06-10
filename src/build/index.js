468e3a796d387d5a4b559a0960718ff1c31e38df
Chalarangelo <chalarangelo@gmail.com>
2020-06-10 20:34:29 +0300

    Deprecate onCreateNode
    Unused due to parsing changes.

--- src/build/index.js
+++ src/build/index.js
@@ -1,11 +1,9 @@
-import onCreateNode from './onCreateNode';
 import sourceNodes from './sourceNodes';
 import createResolvers from './createResolvers';
 import createPages from './createPages';
 import onCreateWebpackConfig from './onCreateWebpackConfig';
 
 export {
-  onCreateNode,
   sourceNodes,
   createResolvers,
   createPages,
