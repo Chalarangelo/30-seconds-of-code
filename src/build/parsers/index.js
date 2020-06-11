5c59468169f733282c816b8085c2cb88a85dc7f5
Chalarangelo <chalarangelo@gmail.com>
2020-06-11 10:12:37 +0300

    Add markdown parser

--- src/build/parsers/index.js
+++ src/build/parsers/index.js
@@ -3,11 +3,13 @@ import parseConfigs from './parseConfigs';
 import parseRequirables from './parseRequirables';
 import parseTemplates from './parseTemplates';
 import parseSnippets from './parseSnippets';
+import parseMarkdown from './parseMarkdown';
 
 export {
   parseQueries,
   parseConfigs,
   parseRequirables,
   parseTemplates,
+  parseMarkdown,
   parseSnippets
 };
