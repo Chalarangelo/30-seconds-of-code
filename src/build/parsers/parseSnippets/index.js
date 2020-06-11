d51c42214d0ba0d0a71c39bb8f4d83d132c6f7c3
Chalarangelo <chalarangelo@gmail.com>
2020-06-11 16:52:40 +0300

    Move recommendation generation to extraction phase
    Decouple from page creation, output to JSON.
    Update recommendation engine to match data format.

--- src/build/parsers/parseSnippets/index.js
+++ src/build/parsers/parseSnippets/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'fs-extra';
 import { blue, green, red } from 'kleur';
 import rankSnippet from 'engines/rankingEngine';
+import recommendationEngine from 'engines/recommendationEngine';
 import { convertToSeoSlug, uniqueElements } from 'utils';
 import standardParser from './standardParser';
 import cssParser from './cssParser';
@@ -19,7 +20,7 @@ const parsers = {
  * Parses a repository's data using and generates the extracted JSON files.
  * @param {string} contentDirPath - The path to the content directory.
  */
-const parseSnippets = contentDirPath => {
+const parseSnippets = async contentDirPath => {
   // Load configurations
   let configs = [];
   glob.sync(`${contentDirPath}/configs/*.json`)
@@ -39,8 +40,10 @@ const parseSnippets = contentDirPath => {
       icon: cfg.theme.iconName,
     }));
 
+  let allData = [];
+
   // Extract snippet JSON files
-  configs.forEach(cfg => {
+  await Promise.all(configs.map(cfg => {
     const snippetsPath = `${contentDirPath}/sources/${cfg.dirName}/${cfg.snippetPath}`;
     const outputJson = `${contentDirPath}/data/${cfg.dirName}.json`;
     console.log(`${blue('Extracting snippets from: ')}${snippetsPath}`);
@@ -67,51 +70,102 @@ const parseSnippets = contentDirPath => {
       commonData.language.otherLanguages = otherLanguages;
     }
 
-    parser.readSnippets(snippetsPath, cfg, langData)
-      .then(snippets => {
-        let snippetsArray = Object.keys(snippets).reduce((acc, snippet) => {
-          acc.push({
-            ...commonData,
-            ...snippets[snippet],
-            slug: `/${slugPrefix}${convertToSeoSlug(snippet.slice(0, -3))}`,
-            url: `${repoUrlPrefix}/${snippet}`,
-            ranking: rankSnippet({
+    return new Promise((resolve, reject) =>
+      parser.readSnippets(snippetsPath, cfg, langData)
+        .then(snippets => {
+          let snippetsArray = Object.keys(snippets).reduce((acc, snippet) => {
+            acc.push({
+              ...commonData,
               ...snippets[snippet],
-              language: commonData.language,
-              biasPenaltyMultiplier: cfg.biasPenaltyMultiplier
-                ? cfg.biasPenaltyMultiplier
-                : 1.0,
-            }),
+              slug: `/${slugPrefix}${convertToSeoSlug(snippet.slice(0, -3))}`,
+              url: `${repoUrlPrefix}/${snippet}`,
+              ranking: rankSnippet({
+                ...snippets[snippet],
+                language: commonData.language,
+                biasPenaltyMultiplier: cfg.biasPenaltyMultiplier
+                  ? cfg.biasPenaltyMultiplier
+                  : 1.0,
+              }),
+            });
+            return acc;
+          }, []);
+
+          const completeData = {
+            data: [...snippetsArray],
+            meta: {
+              name: isBlog ? literals.blog : literals.codelang(cfg.language.long),
+              count: literals.snippetCount(snippetsArray.length),
+              tags: uniqueElements(
+                snippetsArray.map(snippet => snippet.tags.primary)
+              ).sort((a, b) => a.localeCompare(b)),
+              url: `/${slugPrefix.slice(0, slugPrefix.indexOf('/'))}/p/1`,
+              slugPrefix: `/${cfg.slug}`,
+              featured: cfg.featured ? cfg.featured : 0,
+              blog: isBlog,
+              icon: cfg.theme && cfg.theme.iconName,
+            },
+          };
+          console.log(`${green('Finished extracting ')}${snippetsPath}`);
+          resolve({
+            outputFile: outputJson,
+            data: completeData,
           });
-          return acc;
-        }, []);
+        })
+        .catch(err => {
+          console.log(`${red('[ERROR]')} Encountered an error: ${err}`);
+          reject(err);
+        })
+    );
+  })).then(results => { allData = results; });
 
-        const completeData = {
-          data: [...snippetsArray],
-          meta: {
-            name: isBlog ? literals.blog : literals.codelang(cfg.language.long),
-            count: literals.snippetCount(snippetsArray.length),
-            tags: uniqueElements(
-              snippetsArray.map(snippet => snippet.tags.primary)
-            ).sort((a, b) => a.localeCompare(b)),
-            url: `/${slugPrefix.slice(0, slugPrefix.indexOf('/'))}/p/1`,
-            slugPrefix: `/${cfg.slug}`,
-            featured: cfg.featured ? cfg.featured : 0,
-            blog: isBlog,
-            icon: cfg.theme && cfg.theme.iconName,
-          },
-        };
+  // Write snippet JSON files
+  const allSnippetData = allData.reduce((acc, r) => [...acc, ...r.data.data], []);
 
-        fs.writeFileSync(
-          outputJson,
-          JSON.stringify(completeData, null, 2)
-        );
-        console.log(`${green('Finished writing ')}${outputJson}`);
-      })
-      .catch(err => {
-        console.log(`${red('[ERROR]')} Encountered an error: ${err}`);
-      });
-  });
+  await Promise.all(
+    allData.map(({ outputFile, data }) => new Promise((resolve, reject) => {
+      const outputData = {
+        data: data.data.map(snippet => {
+          const recommendedSnippets = recommendationEngine(allSnippetData, snippet)
+            .map(({
+              id, title, expertise, tags, language,
+              icon, html, slug, searchTokens,
+              recommendationRanking,
+            }) => ({
+              id, title, expertise, icon, slug, searchTokens,
+              recommendationRanking: +recommendationRanking,
+              html: {
+                description: html.description,
+              },
+              tags: {
+                primary: tags.primary,
+              },
+              language: {
+                short: language.short,
+                long: language.long,
+              },
+            }));
+          return {
+            ...snippet,
+            recommendedSnippets,
+          };
+        }),
+        meta: data.meta,
+      };
+      fs.writeFile(
+        outputFile,
+        JSON.stringify(outputData, null, 2),
+        err => {
+          if (err) {
+            console.log(`${red('[ERROR]')} Encountered an error: ${err}`);
+            reject(err);
+          } else {
+            console.log(`${green('Finished writing ')}${outputFile}`);
+            resolve();
+          }
+        }
+      );
+    }))
+  );
 };
 
 export default parseSnippets;
