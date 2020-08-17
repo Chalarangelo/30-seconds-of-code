9fcb7215f0d8b3a0dae475884701c14922b1a5f6
Chalarangelo <chalarangelo@gmail.com>
2020-08-17 15:30:17 +0300

    Install sharp, deprecate sharp gatsby plugins
    Use in-house code to generate images, remove gatsby
    plugins that did the same job previously.

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
