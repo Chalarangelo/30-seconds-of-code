f933306427f45b525fca22a66ef98f12517ea14e
Chalarangelo <chalarangelo@gmail.com>
2020-08-20 16:32:51 +0300

    Migrate template tests to rtl

--- src/components/templates/settingsPage/index.test.jsx
+++ src/components/templates/settingsPage/index.test.jsx
@@ -1,80 +1,67 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
-import { Provider } from 'react-redux';
-import createStore from 'state';
-import { mount, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Helmet from 'react-helmet';
+import { cleanup, fireEvent } from '@testing-library/react';
+import { renderConnected } from 'test/utils';
 import literals from 'lang/en/settings';
-
 import SettingsPage from './index';
 
-configure({ adapter: new Adapter() });
 console.warn = jest.fn();
 
-const { store } = createStore();
-
 describe('<SettingsPage />', () => {
-  let wrapper, shell, meta;
+  let wrapper, shell, store, meta, getByLabelText;
 
   beforeEach(() => {
-    wrapper = mount(
-      <Provider store={ store }>
-        <SettingsPage pageContext={ { stringLiterals: literals } } />
-      </Provider>
+    const utils = renderConnected(
+      <SettingsPage pageContext={ { stringLiterals: literals } } />
     );
-    shell = wrapper.find('Shell');
-    meta = wrapper.find('Meta');
+    wrapper = utils.container;
+    store = utils.store;
+    shell = wrapper.querySelector('.page-container');
+    meta = Helmet.peek();
+    getByLabelText = utils.getByLabelText;
   });
+
+  afterEach(cleanup);
 
   describe('should render', () => {
     it('a Shell component', () => {
-      expect(wrapper).toContainMatchingElement('Shell');
-    });
-
-    it('a Meta component', () => {
-      expect(wrapper).toContainMatchingElement('Meta');
+      expect(wrapper.querySelectorAll('.page-container')).toHaveLength(1);
     });
 
     it('a PageTitle component', () => {
-      expect(wrapper).toContainMatchingElement('PageTitle');
+      expect(wrapper.querySelectorAll('.page-title')).toHaveLength(1);
     });
 
-    it('a SimpleCard components', () => {
-      expect(wrapper).toContainMatchingElement('SimpleCard');
+    it('a SimpleCard component', () => {
+      expect(wrapper.querySelectorAll('.card')).toHaveLength(1);
     });
   });
 
   it('should pass the correct data to the Shell component', () => {
-    expect(shell.prop('isSettings')).toBe(true);
+    expect(shell.querySelector('.nav-btn.icon-settings').href.endsWith('/settings')).toBe(false);
   });
 
   it('should pass the correct data to the Meta component', () => {
-    expect(meta.prop('title')).toBe(literals.title);
+    expect(meta.title).toContain(literals.title);
   });
 
   describe('when the dark mode toggle is clicked', () => {
     it('changes the dark mode setting', () => {
-      act(() => {
-        wrapper.find(`Toggle[children="${literals.settings.darkMode}"]`).prop('onChange')();
-      });
+      fireEvent.click(getByLabelText(literals.settings.darkMode));
       expect(store.getState().shell.isDarkMode).toBe(true);
     });
   });
 
   describe('when the github links is clicked', () => {
     it('changes the dark mode setting', () => {
-      act(() => {
-        wrapper.find(`Toggle[children="${literals.settings.githubLinks}"]`).prop('onChange')();
-      });
+      fireEvent.click(getByLabelText(literals.settings.githubLinks));
       expect(store.getState().shell.hasGithubLinksEnabled).toBe(true);
     });
   });
 
   describe('when the cookies is clicked', () => {
     it('changes the cookies setting', () => {
-      act(() => {
-        wrapper.find(`Toggle[children="${literals.settings.cookies}"]`).prop('onChange')();
-      });
+      fireEvent.click(getByLabelText(literals.settings.cookies));
       expect(store.getState().shell.acceptsCookies).toBe(true);
     });
   });
