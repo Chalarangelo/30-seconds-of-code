880aebd42e29fbe28173eca632c30b3aa3033e85
Chalarangelo <chalarangelo@gmail.com>
2020-08-21 12:22:07 +0300

    Cleanup testing-related babel dependencies
    Replace shims and preprocessor for jest.

--- .jest/jest-preprocess.js
+++
@@ -1,6 +0,0 @@
-const babelOptions = {
-  presets: ['babel-preset-gatsby'],
-};
-
-module.exports = require('babel-jest').createTransformer(babelOptions)
-;
--- .jest/style-mock.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  process() {
-    return ''
-  }
-}
