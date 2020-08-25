50c25a78533fbdd16b9f5c44bd5711346d03b32c
Chalarangelo <chalarangelo@gmail.com>
2020-08-26 00:14:46 +0300

    Post-process snippet recommendations
    Rewrite system to output to individual files.

--- src/yild/extractSnippets/postProcess.js
+++ src/yild/extractSnippets/postProcess.js
@@ -4,51 +4,41 @@ import recommendationEngine from 'engines/recommendationEngine';
 const postProcess = (allData, allSnippetData, parentLog) => {
   const boundLog = parentLog.rebind('postProcess');
 
-  return allData.map(({ outputFile, data }) => new Promise((resolve, reject) => {
-    boundLog(`Post-processing snippets for ${outputFile}`, 'info');
-    const outputData = {
-      data: data.data.map(snippet => {
-        const recommendedSnippets = recommendationEngine(allSnippetData, snippet)
-          .map(({
-            id, title, expertise, tags, language,
-            icon, html, slug, searchTokens,
-            recommendationRanking,
-          }) => ({
-            id, title, expertise, icon, slug, searchTokens,
-            recommendationRanking: +recommendationRanking,
-            html: {
-              description: html.description,
-            },
-            tags: {
-              primary: tags.primary,
-            },
-            language: {
-              short: language.short,
-              long: language.long,
-            },
-          }));
-        return {
-          ...snippet,
-          recommendedSnippets,
-        };
-      }),
-      meta: data.meta,
-    };
-    fs.writeFile(
-      outputFile,
-      JSON.stringify(outputData, null, 2),
-      err => {
-        if (err) {
-          boundLog(`Encountered an error while writing ${outputFile}`, 'error');
-          boundLog(`${err}`, 'error');
-          reject(err);
-        } else {
-          boundLog(`Finished writing ${outputFile}`, 'success');
-          resolve();
-        }
-      }
-    );
-  }));
+  return allData.map(async({ snippetsPath, data }) => {
+    const {
+      contentPath: contentOutDir,
+    } = global._yild_instance.config.paths;
+    boundLog(`Post-processing snippets for ${snippetsPath}`, 'info');
+
+    for (let snippet of data.data) {
+      const recommendedSnippets = recommendationEngine(allSnippetData, snippet)
+        .map(({
+          id, title, expertise, tags, language,
+          icon, html, slug, searchTokens,
+          recommendationRanking,
+        }) => ({
+          id, title, expertise, icon, slug, searchTokens,
+          recommendationRanking: +recommendationRanking,
+          html: {
+            description: html.description,
+          },
+          tags: {
+            primary: tags.primary,
+          },
+          language: {
+            short: language.short,
+            long: language.long,
+          },
+        }));
+
+      const outDir = `${contentOutDir}/${snippet.slug.slice(1)}`;
+      fs.ensureDirSync(outDir);
+      await fs.writeFile(
+        `${outDir}/recommendations.json`,
+        JSON.stringify(recommendedSnippets, null, 2)
+      );
+    }
+  });
 };
 
 export default postProcess;
