08caa34904b52b9f6d1fa717191fad79f471cf48
Chalarangelo <chalarangelo@gmail.com>
2020-08-26 22:58:05 +0300

    Simplify parseRequirements API
    Minimize logic in gatsby-node.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,12 @@
 const { green } = require('chalk');
-const env = require('./.build/env').default;
-
 const {
   createPages,
   onCreateWebpackConfig,
   parseRequirements,
 } = require(`./src/gatsby`);
-const paths = require(`./src/config/paths`);
 
-const { requirables, templates} = parseRequirements(
-  env === 'DEVELOPMENT' ? paths.devTemplates : paths.templates,
-  paths.templatesPath,
-  paths.contentPath
-);
+const { requirables, templates} = parseRequirements();
 console.log(`${green('success')} parse requirements`);
 
 exports.createPages = createPages(templates, requirables);
-
 exports.onCreateWebpackConfig = onCreateWebpackConfig;
