f3db29474cb739cb4498e9a131a992449b20aca0
Chalarangelo <chalarangelo@gmail.com>
2020-09-03 16:13:45 +0300

    Generate sitemap for routes

---
+++ src/build/sitemap/index.js
@@ -0,0 +1,29 @@
+import util from 'util';
+import fs from 'fs-extra';
+import handlebars from 'handlebars';
+import { parseRequirements } from 'build/requirements';
+import { bindLogger } from 'build/core';
+
+const writeFile = util.promisify(fs.writeFile);
+
+export const generateSitemap = async() => {
+  const boundLog = bindLogger('makeIcons');
+  const { sitemapTemplatePath } = global.yild.sitemap;
+  const { xmlPath } = global.yild.paths;
+  const template = handlebars.compile(
+    fs.readFileSync(sitemapTemplatePath, 'utf-8')
+  );
+  const requirables = parseRequirements().requirables;
+  boundLog(`Generating sitemap for ${requirables.length} routes`, 'info');
+
+  const sitemap = template({
+    nodes: [
+      { fullRoute: global.yild.mainConfig.websiteUrl, priority: 1.0},
+      ...requirables.filter(n => !n.relRoute.endsWith('404')),
+    ],
+  });
+
+  await writeFile(`${xmlPath}sitemap.xml`, sitemap);
+
+  boundLog('Generating sitemap complete', 'success');
+};
--- src/scripts/build.js
+++ src/scripts/build.js
@@ -2,14 +2,19 @@ import { logger, format } from 'build/core';
 import { loadContentConfigs } from 'build/config';
 import { updateContent } from 'build/content';
 import { makeIcons, prepareAssets } from 'build/assets';
+import { generateSitemap } from 'build/sitemap';
 import { extractData } from 'build/extract';
+import globalConfig from 'config/global';
 import pathConfig from 'config/paths';
 import iconConfig from 'config/icons';
+import sitemapConfig from 'config/sitemap';
 
 export const build = async() => {
   global.yild = global.yild || {};
   global.yild.paths = pathConfig;
   global.yild.icons = iconConfig;
+  global.yild.sitemap = sitemapConfig;
+  global.yild.mainConfig = globalConfig;
   global.yild.env = 'PRODUCTION';
 
   logger.log(`${format('yild', 'bold')} is starting up...`, 'info');
@@ -25,6 +30,10 @@ export const build = async() => {
   await Promise.all([
     extractData(),
     prepareAssets(),
+  ]);
+
+  await Promise.all([
+    generateSitemap(),
   ]);
 };
 
