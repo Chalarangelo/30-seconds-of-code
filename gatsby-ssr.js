587518ef465d89b3847e9a1ba4aa32a0dbccb15f
Chalarangelo <chalarangelo@gmail.com>
2020-09-06 17:25:48 +0300

    Fix XMLs injected with data-react-helmet
    Fixes #162

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,32 @@
 /*
   Gatsby SSR API
 */
-export { default as wrapRootElement } from 'state/ReduxWrapper';
+import ReduxWrapper from 'state/ReduxWrapper';
+import React from 'react';
+
+/**
+ * Wraps the whole application in a redux Provider.
+ */
+export const wrapRootElement = ReduxWrapper;
+
+/**
+ * Called after rendering a page. Injects sitemap and opensearch XMLs.
+ * TODO: Improve and extract outside of this file, if/when possible.
+ */
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="link-sitemap"
+      rel="sitemap"
+      href="/sitemap.xml"
+      type="application/xml"
+    />,
+    <link
+      key="link-opensearch"
+      rel="search"
+      href="/opensearch.xml"
+      type="application/opensearchdescription+xml"
+      title="Snippet search"
+    />,
+  ]);
+};
