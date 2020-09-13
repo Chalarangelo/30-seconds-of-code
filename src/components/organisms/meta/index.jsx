c0f9c91c92762e91dd437554a543578757e7d880
Chalarangelo <chalarangelo@gmail.com>
2020-09-13 03:18:54 +0300

    Attempt to install Hotjar
    This might not work correctly, will revert changes if necessary.

--- src/components/organisms/meta/index.jsx
+++ src/components/organisms/meta/index.jsx
@@ -116,7 +116,7 @@ const Meta = ({
       async: '',
       src: `https://www.googletagmanager.com/gtag/js?id=${config.googleAnalytics.id}`,
     });
-
+    // GTAG
     scripts.push({
       innerHTML: `
       window.dataLayer = window.dataLayer || [];
@@ -140,6 +140,20 @@ const Meta = ({
         }`,
       });
     }
+
+    // Hotjar
+    scripts.push({
+      innerHTML: `
+      (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1988882,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+    });
   }
 
   return (
