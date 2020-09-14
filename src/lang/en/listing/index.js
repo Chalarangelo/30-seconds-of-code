490e65a080ab38cec45566dab9adad46a2171244
Chalarangelo <chalarangelo@gmail.com>
2020-09-14 20:00:00 +0300

    Rename 'Popularity' to 'Recommended'
    Change default ranking string literal

--- src/lang/en/listing/index.js
+++ src/lang/en/listing/index.js
@@ -3,7 +3,7 @@ import config from 'config/global';
 /* istanbul ignore next */
 const literals = {
   orders: {
-    popularity: 'Popularity',
+    popularity: 'Recommended',
     alphabetical: 'Alphabetical',
     expertise: 'Expertise',
   },
