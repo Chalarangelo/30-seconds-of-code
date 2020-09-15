97b26588f1b1bd24303361d62b07d7a6c761068c
Chalarangelo <chalarangelo@gmail.com>
2020-09-15 14:39:01 +0300

    Add dedicated utility for structured data

--- src/utils/object.test.js
+++ src/utils/object.test.js
@@ -1,8 +1,11 @@
 import {
+  generateStructuredData,
   hasKey,
   hasKeys,
   get
 } from './object';
+
+import { listingStructuredData, snippetStructuredData } from 'fixtures/metadata';
 
 describe('hasKey', () => {
   it('returns the correct result for a regular key', () => {
@@ -36,5 +39,41 @@ describe('get', () => {
       { a: { b: { c: 'yes'}, d: 1 }, e: true },
       ['e', ['a', 'b', 'c'], ['a', 'd']]
     )).toEqual([true, 'yes', 1]);
+  });
+});
+
+describe('generateStructuredData', () => {
+  describe('with a snippet', () => {
+    let result;
+
+    beforeAll(() => {
+      result = generateStructuredData(
+        snippetStructuredData,
+        { websiteUrl: 'https://30secondsofcode.org', orgName: '30 seconds' },
+        '/assets/logo.png'
+      );
+    });
+
+    it('result has correct attributes', () => {
+      expect(result['@context']).toBe('https://schema.org');
+      expect(result['@type']).toBe('TechArticle');
+    });
+  });
+
+  describe('with a listing', () => {
+    let result;
+
+    beforeAll(() => {
+      result = generateStructuredData(
+        listingStructuredData,
+        { websiteUrl: 'https://30secondsofcode.org', orgName: '30 seconds' },
+        '/assets/logo.png'
+      );
+    });
+
+    it('result has correct attributes', () => {
+      expect(result['@context']).toBe('https://schema.org');
+      expect(result['@type']).toBe('ItemList');
+    });
   });
 });
