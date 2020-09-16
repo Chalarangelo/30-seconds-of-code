83272cc31ee39fb0cc1e9d38c2da8198adf0828d
Chalarangelo <chalarangelo@gmail.com>
2020-09-16 22:37:15 +0300

    Replace @30-seconds/integration-tools
    Fixes #174

--- src/gatsby/onCreateDevServer.js
+++ src/gatsby/onCreateDevServer.js
@@ -1,6 +1,14 @@
-import { execSync } from 'child_process';
+import fs from 'fs-extra';
 import bodyParser from 'body-parser';
 import { convertToSeoSlug } from 'utils';
+
+const createNewSnippet = (dirName, snippetName) => {
+  const template = fs.readFileSync(`${dirName}/snippet-template.md`, 'utf8');
+  const snippetDirectory = fs.existsSync(`${dirName}/blog_posts`) ? 'blog_posts' : 'snippets';
+  fs.ensureDirSync(`${dirName}/${snippetDirectory}`);
+  const fileData = template.replace(/title:\s*.*\n/, `title: ${snippetName}\n`);
+  fs.writeFileSync(`${dirName}/${snippetDirectory}/${snippetName}.md`, fileData );
+};
 
 /**
  * When called, allows modifying the development Express server.
@@ -10,11 +18,13 @@ const onCreateDevServer = ({ app }) => {
   app.use(bodyParser.json());
   app.post('/create', (req, res) => {
     const { dirName, snippetName, slugPrefix } = req.body;
-    execSync(`cd ./content/sources/${dirName.slice(0, dirName.lastIndexOf('/'))}; ../../../node_modules/@30-seconds/integration-tools/bin/newSnippet.js ${snippetName}`);
+    createNewSnippet(
+      `./content/sources/${dirName.slice(0, dirName.lastIndexOf('/'))}`,
+      snippetName
+    );
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ url: `${slugPrefix}${convertToSeoSlug(snippetName)}` }));
   });
 };
 
 export default onCreateDevServer;
-// /home/achalaris/personal/30-seconds-web/content/sources/30blog/blog_posts/testing-farts.md
