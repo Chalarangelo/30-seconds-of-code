9492c80c4ad8f561a01ada37db7ba7c2c82e0954
Chalarangelo <chalarangelo@gmail.com>
2020-09-28 20:36:55 +0300

    Remove custom scroll handling
    Useless due to the new HTML structure.
    Remove smooth behavior as it slows down users.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,6 @@
 /*
   Gatsby Browser API
 */
-let locationScrollTops = [];
-
 const sendPageView = () => {
   const pagePath = location
     ? location.pathname + location.search + location.hash
@@ -10,22 +8,7 @@ const sendPageView = () => {
   window.gtag(`event`, `page_view`, { page_path: pagePath });
 };
 
-const onPreRouteUpdate = ({ location, prevLocation }) => {
-  try {
-    let scrollTop = document.querySelector('.content').scrollTop;
-    locationScrollTops[prevLocation.pathname] = scrollTop;
-  } catch (e) { return; }
-};
-
-const onRouteUpdate = ({ location, prevLocation }) => {
-  try {
-    if (locationScrollTops[location.pathname] && prevLocation.includes('/s/'))
-      document.querySelector('.content').scrollTop = locationScrollTops[location.pathname];
-    else if(location.pathname.match(/\/[ape]\/\d+/) && prevLocation.pathname.match(/\/[ape]\/\d+/))
-      document.querySelector('.content').scrollTop = 0;
-
-  } catch (e) { return; }
-
+const onRouteUpdate = () => {
   if (process.env.NODE_ENV === `production` && typeof gtag === `function`) {
     if (`requestAnimationFrame` in window) {
       requestAnimationFrame(() => {
@@ -39,4 +22,4 @@ const onRouteUpdate = ({ location, prevLocation }) => {
 };
 
 export { default as wrapRootElement } from 'state/ReduxWrapper';
-export { onPreRouteUpdate, onRouteUpdate };
+export { onRouteUpdate };
