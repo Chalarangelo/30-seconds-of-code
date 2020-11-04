1f403eaa255fe4b5d608ace18c960c2afe85cf2b
Chalarangelo <chalarangelo@gmail.com>
2020-11-04 14:43:09 +0200

    Update tests to account for Redux
    Additional connections due to hooks.

--- src/components/atoms/button/codepenButton/index.test.jsx
+++ src/components/atoms/button/codepenButton/index.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
+import { renderConnected } from 'test/utils';
+import { cleanup } from '@testing-library/react';
 import literals from 'lang/en/client/common';
 import { codepenHtmlCode, codepenCssCode } from 'fixtures/strings';
 import CodepenButton from './index';
@@ -12,7 +13,7 @@ describe('<CodepenButton />', () => {
   let button, input;
 
   beforeEach(() => {
-    wrapper = render(
+    wrapper = renderConnected(
       <CodepenButton htmlCode={ codepenHtmlCode } cssCode={ codepenCssCode } />
     ).container;
     button = wrapper.querySelector('button');
