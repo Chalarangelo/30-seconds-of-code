c92b8f75f10fe88a590f7df9fdb361888ae7874b
Chalarangelo <chalarangelo@gmail.com>
2020-12-02 16:30:11 +0200

    Introduce FileParser

--- src/build/snippet/snippetData.js
+++ src/build/snippet/snippetData.js
@@ -6,36 +6,21 @@ import frontmatter from 'front-matter';
 import { exec } from 'child_process';
 
 const readFile = util.promisify(fs.readFile);
-const readDir = util.promisify(fs.readdir);
 
 const mdCodeFence = '```';
-const codeMatcher = new RegExp(
-  `${mdCodeFence}[.\\S\\s]*?${mdCodeFence}`,
-  'g'
-);
-
-/**
- * Synchronously reads all files in a directory and returns the resulting array.
- * @param {string} directoryPath - The path of the directory to read.
- */
-export const getFilesInDir = directoryPath => new Promise((resolve, reject) =>
-  readDir(directoryPath)
-    .then(files => resolve(
-      files.sort((a, b) => a.toLowerCase() < b.toLowerCase() ? -1 : 1)
-    ))
-    .catch(err => reject(err))
-);
+const codeMatcher = new RegExp(`${mdCodeFence}[.\\S\\s]*?${mdCodeFence}`, 'g');
 
 /**
  * Synchronously gets the data from a snippet file in a usable format, using frontmatter.
  * @param {string} snippetsPath - The path of the snippets directory.
  * @param {string} snippet - The name of the snippet file.
  */
-export const getData = (snippetsPath, snippet) => new Promise((resolve, reject) =>
-  readFile(path.join(snippetsPath, snippet), 'utf8')
-    .then(content => resolve(frontmatter(content)))
-    .catch(err => reject(err))
-);
+export const getData = (snippetsPath, snippet) =>
+  new Promise((resolve, reject) =>
+    readFile(path.join(snippetsPath, snippet), 'utf8')
+      .then(content => resolve(frontmatter(content)))
+      .catch(err => reject(err))
+  );
 
 /**
  * Gets the code blocks for a snippet.
@@ -49,54 +34,61 @@ export const getCodeBlocks = (
     hasOptionalLanguage = false,
     languages = [],
     snippetName = '',
-  }) => new Promise(resolve => {
-  const replacer = new RegExp(
-    `^${mdCodeFence}(${languages})?`,
-    'gm'
-  );
-  const raw = Array.from([...str.matchAll(codeMatcher)].map(m => m[0]));
-  const results = raw.map(v => v.replace(replacer, '').trim());
-
-  if(isCssSnippet) {
-    const scopedCss = sass
-      .renderSync({ data: `[data-scope="${snippetName}"] { ${results[1]} }`})
-      .css
-      .toString();
-
-    return resolve([{
-      html: results[0],
-      css: results[1],
-      js: results.length > 2 ? results[2] : '',
-      scopedCss,
-    }, {
-      html: raw[0],
-      css: raw[1],
-      js: raw.length > 2 ? raw[2] : '',
-    }]);
   }
+) =>
+  new Promise(resolve => {
+    const replacer = new RegExp(`^${mdCodeFence}(${languages})?`, 'gm');
+    const raw = Array.from([...str.matchAll(codeMatcher)].map(m => m[0]));
+    const results = raw.map(v => v.replace(replacer, '').trim());
 
-  if(hasOptionalLanguage && results.length > 2) {
-    return resolve([{
-      style: results[0],
-      src: results[1],
-      example: results[2],
-    }, {
-      style: raw[0],
-      code: raw[1],
-      example: raw[2],
-    }]);
-  }
+    if (isCssSnippet) {
+      const scopedCss = sass
+        .renderSync({ data: `[data-scope="${snippetName}"] { ${results[1]} }` })
+        .css.toString();
 
-  return resolve([{
-    style: hasOptionalLanguage ? '' : undefined,
-    src: results[0],
-    example: results[1],
-  }, {
-    style: hasOptionalLanguage ? '' : undefined,
-    code: raw[0],
-    example: raw[1],
-  }]);
-});
+      return resolve([
+        {
+          html: results[0],
+          css: results[1],
+          js: results.length > 2 ? results[2] : '',
+          scopedCss,
+        },
+        {
+          html: raw[0],
+          css: raw[1],
+          js: raw.length > 2 ? raw[2] : '',
+        },
+      ]);
+    }
+
+    if (hasOptionalLanguage && results.length > 2) {
+      return resolve([
+        {
+          style: results[0],
+          src: results[1],
+          example: results[2],
+        },
+        {
+          style: raw[0],
+          code: raw[1],
+          example: raw[2],
+        },
+      ]);
+    }
+
+    return resolve([
+      {
+        style: hasOptionalLanguage ? '' : undefined,
+        src: results[0],
+        example: results[1],
+      },
+      {
+        style: hasOptionalLanguage ? '' : undefined,
+        code: raw[0],
+        example: raw[1],
+      },
+    ]);
+  });
 
 /**
  * Gets the textual content for a snippet.
@@ -109,21 +101,28 @@ export const getTextualContent = str =>
  * Asynchronously gets the git metadata for a snippet.
  * @param {string} snippet - The name of the snippet file.
  */
-export const getGitMetadata = (snippet, snippetsPath) => new Promise(resolve => Promise.all([
-  new Promise(rsl => exec(
-    `cd ${snippetsPath}; git log --diff-filter=A --pretty=format:%at -- ${snippet} | head -1`,
-    (error, stdout) => rsl(stdout.toString().replace('\n', ''))
-  )),
-  new Promise(rsl => exec(
-    `cd ${snippetsPath}; git log -n 1 --pretty=format:%at -- ${snippet} | head -1`,
-    (error, stdout) => rsl(stdout.toString().replace('\n', ''))
-  )),
-]).then(values =>
-  resolve({
-    firstSeen: new Date(+`${values[0]}000`),
-    lastUpdated: new Date(+`${values[1]}000`),
-  }))
-);
+export const getGitMetadata = (snippet, snippetsPath) =>
+  new Promise(resolve =>
+    Promise.all([
+      new Promise(rsl =>
+        exec(
+          `cd ${snippetsPath}; git log --diff-filter=A --pretty=format:%at -- ${snippet} | head -1`,
+          (error, stdout) => rsl(stdout.toString().replace('\n', ''))
+        )
+      ),
+      new Promise(rsl =>
+        exec(
+          `cd ${snippetsPath}; git log -n 1 --pretty=format:%at -- ${snippet} | head -1`,
+          (error, stdout) => rsl(stdout.toString().replace('\n', ''))
+        )
+      ),
+    ]).then(values =>
+      resolve({
+        firstSeen: new Date(+`${values[0]}000`),
+        lastUpdated: new Date(+`${values[1]}000`),
+      })
+    )
+  );
 
 /**
  * Gets the tag array for a snippet from the tags string.
@@ -136,5 +135,5 @@ export const getTags = tagStr => [...new Set(tagStr.toLowerCase().split(','))];
  * @param {string} snippetFilename - Filename of the snippet.
  * @param {string} sourceDir - The name of the source directory.
  */
-export const getId = (snippetFilename, sourceDir) => `${sourceDir}/${snippetFilename.slice(0, -3)}`;
-
+export const getId = (snippetFilename, sourceDir) =>
+  `${sourceDir}/${snippetFilename.slice(0, -3)}`;
