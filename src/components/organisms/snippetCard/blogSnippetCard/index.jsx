e13bf80d594a6e64197ed232befcafa95db68542
Chalarangelo <chalarangelo@gmail.com>
2020-12-22 12:55:31 +0200

    Add empty alt attribute to blog covers
    Resolves #204

--- src/components/organisms/snippetCard/blogSnippetCard/index.jsx
+++ src/components/organisms/snippetCard/blogSnippetCard/index.jsx
@@ -38,7 +38,7 @@ const SnippetCard = ({ snippet }) => (
       <TagList tags={snippet.tags.all} />
     </div>
     {snippet.cover && snippet.cover && (
-      <img className='card-cover-image' src={snippet.cover} />
+      <img className='card-cover-image' src={snippet.cover} alt='' />
     )}
     <div
       className='card-description'
