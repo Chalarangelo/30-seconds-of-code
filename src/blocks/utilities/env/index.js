133c68a60cd244e8af249b3cf7dd83e968cc3153
Chalarangelo <chalarangelo@gmail.com>
2021-01-02 12:30:52 +0200

    Convert env util to a class
    Allow for Content loading if not initialized.

--- src/blocks/utilities/env/index.js
+++ src/blocks/utilities/env/index.js
@@ -3,37 +3,53 @@ import pathConfig from 'settings/paths';
 import { JSONParser } from 'blocks/parsers/json';
 import { ContentConfig } from 'blocks/entities/contentConfig';
 import { Logger } from 'blocks/utilities/logger';
+import { Content } from 'blocks/utilities/content';
 
 /**
- * Sets up the global environment variables, so that they are available everywhere.
- * Make sure this is the first thing your code runs before executing anything requiring
- * global variables.
+ * Environment setup utility.
  */
-export const setupEnv = (env = 'PRODUCTION') => {
-  const boundLog = Logger.bind('utilities.env.setupEnv');
-  boundLog(`Setting up environment in "${env}" mode.`, 'info');
-  // This is the only function in a sea of classes.
-  // It needs to stand out as it's unique and has a special role.
-  const settings = JSONParser.fromGlob(pathConfig.settingsPath, {
-    withNames: true,
-  }).reduce(
-    (acc, [fileName, fileData]) => {
-      const keyName = fileName.slice(fileName.lastIndexOf('/') + 1, -5);
-      acc[keyName] = fileData;
-      return acc;
-    },
-    {
-      ...globalConfig,
-      paths: { ...pathConfig },
-      configs: JSONParser.fromGlob(
-        `${pathConfig.rawContentPath}/configs/*.json`
-      ).map(cfg => new ContentConfig(cfg)),
-      env,
-    }
-  );
-  boundLog('Finished loading settings and configuration files.', 'success');
-  boundLog(`Loaded ${settings.configs.length} config files.`, 'success');
+export class Env {
+  /**
+   * Sets up the global environment variables, so that they are available everywhere.
+   * Make sure this is the first thing your code runs before executing anything requiring
+   * global variables.
+   * Use this only the first time you are setting up, otherwise prefer `setup`.
+   */
+  static init = async (env = 'PRODUCTION') => {
+    if (env === 'PRODUCTION') await Content.update();
+    return this.setup(env);
+  };
 
-  global.settings = settings;
-  return settings;
-};
+  /**
+   * Sets up the global environment variables, so that they are available everywhere.
+   * Make sure this is the first thing your code runs before executing anything requiring
+   * global variables.
+   * If this is the first time you are setting up, use `init` instead.
+   */
+  static setup = (env = 'PRODUCTION') => {
+    const boundLog = Logger.bind('utilities.env.setup');
+    boundLog(`Setting up environment in "${env}" mode.`, 'info');
+    const settings = JSONParser.fromGlob(pathConfig.settingsPath, {
+      withNames: true,
+    }).reduce(
+      (acc, [fileName, fileData]) => {
+        const keyName = fileName.slice(fileName.lastIndexOf('/') + 1, -5);
+        acc[keyName] = fileData;
+        return acc;
+      },
+      {
+        ...globalConfig,
+        paths: { ...pathConfig },
+        configs: JSONParser.fromGlob(
+          `${pathConfig.rawContentPath}/configs/*.json`
+        ).map(cfg => new ContentConfig(cfg)),
+        env,
+      }
+    );
+    boundLog('Finished loading settings and configuration files.', 'success');
+    boundLog(`Loaded ${settings.configs.length} config files.`, 'success');
+
+    global.settings = settings;
+    return settings;
+  };
+}
