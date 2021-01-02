76708ac2dac13c75b76722153cb4a3dd35b8dbbd
Chalarangelo <chalarangelo@gmail.com>
2021-01-02 12:31:54 +0200

    Amend build process
    Content update is done via the `Env.init()` call.
    Icon serialization is run alongside extraction and
    assets.

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,6 +1,5 @@
-import { setupEnv } from 'blocks/utilities/env';
+import { Env } from 'blocks/utilities/env';
 import { Logger } from 'blocks/utilities/logger';
-import { Content } from 'blocks/utilities/content';
 import { IconSerializer } from 'blocks/serializers/icon';
 import { SitemapSerializer } from 'blocks/serializers/sitemap';
 import { FeedSerializer } from 'blocks/serializers/feed';
@@ -9,21 +8,20 @@ import { AssetSerializer } from 'blocks/serializers/asset';
 import { Extractor } from 'blocks/utilities/extractor';
 
 export const build = async () => {
-  setupEnv('PRODUCTION');
-
   Logger.log('Build process is starting up...', 'info');
   Logger.logProcessInfo();
   Logger.breakLine();
 
+  await Env.init('PRODUCTION');
+
   await Promise.all([
-    Content.update(),
+    Extractor.extract(),
+    AssetSerializer.serialize(),
     IconSerializer.serialize(
       FileParser.fromGlob(global.settings.paths.rawIconPath),
       global.settings.configs
     ),
   ]);
-
-  await Promise.all([Extractor.extract(), AssetSerializer.serialize()]);
 
   await Promise.all([
     SitemapSerializer.serialize(),
