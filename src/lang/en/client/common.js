2eaa839b70749f6d24dc405127528a62c2c7f45c
Chalarangelo <chalarangelo@gmail.com>
2021-01-04 23:19:14 +0200

    Add development controls
    Resolves #234

--- src/lang/en/client/common.js
+++ src/lang/en/client/common.js
@@ -8,7 +8,6 @@ const literals = {
   preview: 'Preview',
   viewOnGitHub: 'View on GitHub',
   share: 'Share',
-  openInVscode: 'Open in VS Code',
   home: 'Home',
   recommendedSnippets: 'Recommended snippets',
   blog: 'Blog',
@@ -16,6 +15,13 @@ const literals = {
   siteName: settings.websiteName,
   siteAuthor: settings.orgName,
   siteDescription: settings.websiteDescription,
+  // Development mode literals
+  developerControls: 'Development mode controls',
+  contentManager: 'Content manager',
+  openInVscode: 'Open in VS Code',
+  toggleDarkMode: 'Toggle dark mode',
+  editCoverImage: 'Edit cover image',
+  viewLive: 'View live',
 };
 
 export default literals;
