016e2a4a9964b90b0c759628d3aa2f54ed553e5f
Chalarangelo <chalarangelo@gmail.com>
2021-01-04 19:26:07 +0200

    Add predevelop script
    Same as build, except it doesn't handle
    content updating or post-processing.

---
+++ src/scripts/develop.js
@@ -0,0 +1,25 @@
+import { Env } from 'blocks/utilities/env';
+import { Logger } from 'blocks/utilities/logger';
+import { IconSerializer } from 'blocks/serializers/icon';
+import { FileParser } from 'blocks/parsers/file';
+import { AssetSerializer } from 'blocks/serializers/asset';
+import { Extractor } from 'blocks/utilities/extractor';
+
+export const build = async () => {
+  Logger.log('Build process is starting up...', 'info');
+  Logger.logProcessInfo();
+  Logger.breakLine();
+
+  await Env.init('DEVELOPMENT');
+
+  await Promise.all([
+    Extractor.extract(),
+    AssetSerializer.serialize(),
+    IconSerializer.serialize(
+      FileParser.fromGlob(global.settings.paths.rawIconPath),
+      global.settings.configs
+    ),
+  ]);
+};
+
+build();
