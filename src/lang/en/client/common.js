265dfab82a9a9fc4d0553a578dce9dd2379e71ef
Chalarangelo <chalarangelo@gmail.com>
2021-01-05 13:17:18 +0200

    Lazy load development controls
    Avoid adding them to the production bundle.

--- src/lang/en/client/common.js
+++ src/lang/en/client/common.js
@@ -15,13 +15,6 @@ const literals = {
   siteName: settings.websiteName,
   siteAuthor: settings.orgName,
   siteDescription: settings.websiteDescription,
-  // Development mode literals
-  developerControls: 'Development mode controls',
-  contentManager: 'Content manager',
-  openInVscode: 'Open in VS Code',
-  toggleDarkMode: 'Toggle dark mode',
-  editCoverImage: 'Edit cover image',
-  viewLive: 'View live',
 };
 
 export default literals;
