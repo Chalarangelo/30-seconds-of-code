61d6fb161f12fb3fa29c1950c0b79234338919cd
Chalarangelo <chalarangelo@gmail.com>
2021-01-10 22:44:35 +0200

    Add CollectionConfig entity

---
+++ src/fixtures/blocks/collectionConfigs.js
@@ -0,0 +1,12 @@
+export const rawCollections = {
+  collection: {
+    slug: 'c/js-collection',
+    name: 'JavaScript Collection',
+    featured: 2,
+    snippetIds: ['dart/s/any'],
+    theme: {
+      iconName: 'js',
+    },
+    description: 'Lorem ipsum dolor sit amet',
+  },
+};
