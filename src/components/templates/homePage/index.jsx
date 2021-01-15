813cec180be892b209b7c6ad98ff243e734eda5c
Chalarangelo <chalarangelo@gmail.com>
2021-01-15 16:20:53 +0200

    Add HomePage template

---
+++ src/components/templates/homePage/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'typedefs/proptypes';
+import Meta from 'components/organisms/meta';
+import Shell from 'components/organisms/shell';
+import Shelf from 'components/organisms/shelf';
+import literals from 'lang/en/client/common';
+
+const propTypes = {
+  pageContext: PropTypes.shape({
+    pageDescription: PropTypes.string.isRequired,
+    shelves: PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.oneOf(['snippets', 'collections']),
+        shelfName: PropTypes.string,
+        shelfUrl: PropTypes.string,
+        snippetList: PropTypes.arrayOf(PropTypes.snippet),
+        chipList: PropTypes.arrayOf(
+          PropTypes.shape({
+            url: PropTypes.string,
+            name: PropTypes.string,
+            icon: PropTypes.string,
+          })
+        ),
+      })
+    ),
+  }),
+};
+
+/**
+ * Renders the home page.
+ * Used to render the / page (home).
+ */
+const HomePage = ({ pageContext: { shelves, pageDescription } }) => {
+  return (
+    <>
+      <Meta
+        title={''}
+        description={pageDescription}
+        canonical={'/'}
+        structuredData={{
+          type: 'home',
+        }}
+      />
+      <Shell>
+        <h1 className='home-title'>
+          <img
+            src='/assets/30s-icon.png'
+            alt={literals.siteName}
+            className='home-logo'
+            width='64'
+            height='64'
+          />
+          <span className='home-title-text'>{literals.siteName}</span>
+        </h1>
+        <p className='home-sub-title'>{literals.siteDescription}</p>
+        {shelves.map(s => (
+          <Shelf shelf={s} key={`shelf_${s.shelfType}.${s.shelfUrl}`} />
+        ))}
+      </Shell>
+    </>
+  );
+};
+
+HomePage.propTypes = propTypes;
+
+export default HomePage;
---
+++ src/components/templates/homePage/index.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import { renderConnected } from 'test/utils';
+import HomePage from './index';
+import { collectionShelf, snippetShelf } from 'fixtures/shelves';
+
+console.warn = jest.fn();
+console.error = jest.fn();
+
+describe('<HomePage />', () => {
+  const shelves = [collectionShelf, snippetShelf];
+  const pageDescription = 'Browse 20 snippet collections on 30 seconds of code';
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = renderConnected(
+      <HomePage
+        pageContext={{
+          shelves,
+          pageDescription,
+        }}
+      />
+    ).container;
+  });
+
+  afterEach(cleanup);
+
+  describe('should render', () => {
+    it('a Shell component', () => {
+      expect(wrapper.querySelectorAll('.page-container')).toHaveLength(1);
+    });
+
+    it('the home page title', () => {
+      expect(wrapper.querySelectorAll('.home-title')).toHaveLength(1);
+    });
+
+    it('the correct number of shelves component', () => {
+      expect(wrapper.querySelectorAll('.collections-shelf-title')).toHaveLength(
+        1
+      );
+      expect(wrapper.querySelectorAll('.snippets-shelf-title')).toHaveLength(1);
+    });
+  });
+});
