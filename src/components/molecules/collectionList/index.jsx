4cfd64a8eb34ee761633a0147642f684712e2e39
Chalarangelo <chalarangelo@gmail.com>
2021-01-15 15:20:57 +0200

    Add CollectionList molecule

---
+++ src/components/molecules/collectionList/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'typedefs/proptypes';
+import PageTitle from 'components/atoms/pageTitle';
+import CollectionChip from 'components/atoms/collectionChip';
+
+const propTypes = {
+  chipList: PropTypes.arrayOf(PropTypes.chip),
+  listingName: PropTypes.string,
+};
+
+/**
+ * Renders a list of collection chips.
+ * Dependent on the `CollectionChip` component.
+ */
+const CollectionList = ({ chipList, listingName }) => {
+  return chipList.length ? (
+    <>
+      <PageTitle>{listingName}</PageTitle>
+      <ul className='collection-list'>
+        {chipList.map(chip => (
+          <CollectionChip key={`chip_${chip.url}`} chip={chip} />
+        ))}
+      </ul>
+    </>
+  ) : null;
+};
+
+CollectionList.propTypes = propTypes;
+
+export default CollectionList;
