0e9b13c20792bb554babe7b9296eea5964011545
Chalarangelo <chalarangelo@gmail.com>
2021-02-17 16:48:29 +0200

    Add createStateProvider utility

---
+++ src/state/utils.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+/**
+ * Creates all the necessary components for a state context provider.
+ */
+const createStateProvider = ({
+  initialState = {},
+  persistKey = null,
+  reducer = state => state,
+  stateContextName = 'StateContext',
+  dispatchContextName = 'DispatchContext',
+}) => {
+  // Persisted initializer
+  const initializeState = () => {
+    if (typeof window === 'undefined' || persistKey === null)
+      return initialState;
+    try {
+      const persistedData = window.localStorage.getItem(persistKey);
+      return JSON.parse(persistedData) || initialState;
+    } catch (e) {
+      return initialState;
+    }
+  };
+
+  // Contexts
+  const StateContext = React.createContext();
+  const DispatchContext = React.createContext();
+  StateContext.displayName = stateContextName;
+  DispatchContext.displayName = dispatchContextName;
+
+  // Hooks
+  const useState = () => {
+    const context = React.useContext(StateContext);
+    return context;
+  };
+  const useDispatch = () => {
+    const context = React.useContext(DispatchContext);
+    return context;
+  };
+  const useStateDispatch = () => {
+    return [useState(), useDispatch()];
+  };
+
+  const StateProvider = ({ children, initialState }) => {
+    const [state, dispatch] = React.useReducer(
+      reducer,
+      initialState ? initialState : initializeState()
+    );
+
+    React.useEffect(() => {
+      if (persistKey === null) return;
+      try {
+        window.localStorage.setItem(persistKey, JSON.stringify(state));
+      } catch (e) {
+        console.warn(e);
+      }
+    }, [state]);
+
+    return (
+      <StateContext.Provider value={state}>
+        <DispatchContext.Provider value={dispatch}>
+          {children}
+        </DispatchContext.Provider>
+      </StateContext.Provider>
+    );
+  };
+
+  return {
+    StateProvider,
+    useState,
+    useDispatch,
+    useStateDispatch,
+  };
+};
+
+export default createStateProvider;
