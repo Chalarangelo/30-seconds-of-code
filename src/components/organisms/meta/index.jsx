61d24aaa99b24142c82948c29d3b0456f2086a13
Chalarangelo <chalarangelo@gmail.com>
2021-02-17 23:59:40 +0200

    Remove Hotjar script

--- src/components/organisms/meta/index.jsx
+++ src/components/organisms/meta/index.jsx
@@ -111,20 +111,6 @@ const Meta = ({
         }`,
       });
     }
-
-    // Hotjar
-    scripts.push({
-      innerHTML: `
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1988882,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-      `,
-    });
   }
 
   return (
