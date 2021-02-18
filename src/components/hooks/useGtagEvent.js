7a6a9eb128817ef21c101b41df349516dd7667b3
Chalarangelo <chalarangelo@gmail.com>
2021-02-18 18:58:26 +0200

    Send gtag events except for bots

--- src/components/hooks/useGtagEvent.js
+++ src/components/hooks/useGtagEvent.js
@@ -5,18 +5,16 @@ import { useShellState } from 'state/shell';
  * A hook that handles gtag-enabled event tracking gracefully.
  */
 const useGtagEvent = name => {
-  const { acceptsCookies } = useShellState();
+  const { isBot } = useShellState();
   const canSendEvent =
-    typeof window !== 'undefined' && typeof gtag === `function`;
+    typeof window !== 'undefined' && !isBot && typeof gtag === `function`;
 
-  return useCallback(
-    params => {
-      if (acceptsCookies && canSendEvent) window.gtag('event', name, params);
-      else if (process.env.NODE_ENV === 'development')
-        console.log('event', name, params);
-    },
-    [acceptsCookies]
-  );
+  return useCallback(params => {
+    if (canSendEvent && process.env.NODE_ENV !== 'development')
+      window.gtag('event', name, params);
+    else if (process.env.NODE_ENV === 'development')
+      console.log('event', name, params);
+  }, []);
 };
 
 export default useGtagEvent;
