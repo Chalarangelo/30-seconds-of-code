91d64eee3a30ea568150318a0b473b497c97eda4
Chalarangelo <chalarangelo@gmail.com>
2021-03-13 21:19:28 +0200

    Add inline-block layout utility

--- src/components/atoms/collectionChip/index.jsx
+++ src/components/atoms/collectionChip/index.jsx
@@ -15,7 +15,7 @@ const CollectionChip = ({ chip }) => {
   const hasDescription = chip.description && chip.description.length;
   return hasDescription ? (
     <li className='card srfc-02dp txt-100 list-card'>
-      <div className={`card-icon br-xlicon icon-${chip.icon}`}></div>
+      <div className={`card-icon br-xl icon icon-${chip.icon}`}></div>
       <div className='card-data'>
         <h3 className='card-title txt-200 fs-xl f-alt'>
           <a href={chip.url}>{chip.title}</a>
