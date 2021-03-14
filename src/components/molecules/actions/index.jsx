2a54f533bdb16f08e9892d27d81b27b2172d23c8
Chalarangelo <chalarangelo@gmail.com>
2021-03-14 11:22:39 +0200

    Add media query font sizes

--- src/components/molecules/actions/index.jsx
+++ src/components/molecules/actions/index.jsx
@@ -49,7 +49,7 @@ const Actions = ({ snippet }) => {
         />
       )}
       <a
-        className='btn icon icon-github'
+        className='btn fs-no md:fs-sm icon icon-github '
         href={snippet.url}
         rel='nofollow noopener noreferrer'
         target='_blank'
