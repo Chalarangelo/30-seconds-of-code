3227a6c2297434626475905c36316180917a60b5
Chalarangelo <chalarangelo@gmail.com>
2021-03-31 18:04:16 +0300

    Allow CollectionConfig to accept a typeMatcher
    Match snippet types instead of ids.

--- src/blocks/entities/collectionConfig/index.js
+++ src/blocks/entities/collectionConfig/index.js
@@ -10,6 +10,7 @@ export class CollectionConfig {
    * @param {object} config - Collection configuration data. Must contain:
    *   - `name` - The name of the configuration.
    *   - `snippetIds` - Ids of the snippets that make up the collection.
+   *   - `typeMatcher` - Type matcher for the snippets that make up the collection.
    *   - `featured` - > 0 if the content is listed, -1 if it's not.
    *   - `slug` - Base url for the content pages.
    * @throws Will throw an error if any of the necessary keys is not present.
@@ -18,14 +19,19 @@ export class CollectionConfig {
     name,
     slug,
     snippetIds,
+    typeMatcher,
     featured,
     description,
     iconName = null,
     ...rest
   }) {
-    if (!name || !slug || !snippetIds || !snippetIds.length) {
+    if (
+      !name ||
+      !slug ||
+      ((!snippetIds || !snippetIds.length) && !typeMatcher)
+    ) {
       throw new ArgsError(
-        "Missing required keys. One or more of the following keys were not specified: 'name', 'slug', 'featured', 'snippetIds'"
+        "Missing required keys. One or more of the following keys were not specified: 'name', 'slug', 'featured', 'snippetIds', 'typeMatcher'"
       );
     }
 
@@ -35,6 +41,7 @@ export class CollectionConfig {
     this.featured = Boolean(featured);
     this.iconName = iconName;
     this.snippetIds = snippetIds;
+    this.typeMatcher = typeMatcher;
     Object.keys(rest).forEach(key => {
       this[key] = rest[key];
     });
