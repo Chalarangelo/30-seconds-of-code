3229feffc99ac141003d2cfc02e029ba522fd4fe
Chalarangelo <chalarangelo@gmail.com>
2021-04-18 13:17:33 +0300

    Add create script
    Creates a new snippet in a repo.
    Resolves #254

---
+++ src/scripts/create.js
@@ -0,0 +1,21 @@
+import { Env } from 'blocks/utilities/env';
+import { Logger } from 'blocks/utilities/logger';
+import { Content } from 'blocks/utilities/content';
+
+export const build = async () => {
+  Logger.log('Creation process is starting up...', 'info');
+  Logger.breakLine();
+
+  if (process.argv < 4) {
+    Logger.log(
+      'Not enough arguments, 2 expected (submodule name and snippet name). Terminating...',
+      'error'
+    );
+    return;
+  }
+
+  await Env.init('DEVELOPMENT');
+  Content.createSnippet(process.argv[2], process.argv[3]);
+};
+
+build();
