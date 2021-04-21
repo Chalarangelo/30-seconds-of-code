d73b7494dd49cf9ab26850e38a6bdf8a5d3ebc21
Chalarangelo <chalarangelo@gmail.com>
2021-04-21 22:23:22 +0300

    Include manifest and icons in Meta

--- src/components/organisms/meta/index.jsx
+++ src/components/organisms/meta/index.jsx
@@ -183,6 +183,67 @@ const Meta = ({
         type='application/opensearchdescription+xml'
         title='Snippet search'
       />
+      <link
+        rel='icon'
+        href={`/assets/icons/favicon-32x32.png?v=${settings.manifestCacheKey}`}
+        type='image/png'
+      />
+      ,
+      <link
+        rel='manifest'
+        href='/manifest.webmanifest'
+        crossOrigin='anonymous'
+      />
+      ,
+      <meta name='theme-color' content='#1e253d' />,
+      <link
+        rel='apple-touch-icon'
+        sizes='48x48'
+        href={`/assets/icons/icon-48x48.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='72x72'
+        href={`/assets/icons/icon-72x72.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='96x96'
+        href={`/assets/icons/icon-96x96.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='144x144'
+        href={`/assets/icons/icon-144x144.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='192x192'
+        href={`/assets/icons/icon-192x192.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='256x256'
+        href={`/assets/icons/icon-256x256.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='384x384'
+        href={`/assets/icons/icon-384x384.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
+      <link
+        rel='apple-touch-icon'
+        sizes='512x512'
+        href={`/assets/icons/icon-512x512.png?v=${settings.manifestCacheKey}`}
+      />
+      ,
       {canonical ? (
         <link rel='canonical' href={`${settings.websiteUrl}${canonical}`} />
       ) : null}
