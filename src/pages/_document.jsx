a5c0738682a4a6a006365201aa5427c5d7b3211b
Chalarangelo <chalarangelo@gmail.com>
2021-04-21 22:25:45 +0300

    Add custom _document

---
+++ src/pages/_document.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
