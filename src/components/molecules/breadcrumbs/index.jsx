f7d1d3840ecf6e0c2d58e272eda67562af4f9799
Chalarangelo <chalarangelo@gmail.com>
2021-06-10 00:06:14 +0300

    Simplify breadcrumb code and styles

--- src/components/molecules/breadcrumbs/index.jsx
+++ src/components/molecules/breadcrumbs/index.jsx
@@ -16,15 +16,21 @@ const breadcrumbPropTypes = {
 const Breadcrumbs = ({ breadcrumbs }) => (
   <nav aria-label='breadcrumbs' className='breadcrumbs'>
     <ol className='flex m-0'>
-      {breadcrumbs.map(({ name, url }, i) => (
-        <li key={`breadcrumb-${i}`} className='breadcrumb-item inline-block'>
-          <Link href={url}>
-            <a aria-current={i === breadcrumbs.length - 1 ? 'page' : null}>
-              {name}
-            </a>
-          </Link>
-        </li>
-      ))}
+      {breadcrumbs.map(({ name, url }, i) => {
+        const isCurrentPage = i === breadcrumbs.length - 1;
+        return (
+          <li
+            key={`breadcrumb-${i}`}
+            className={`breadcrumb-item inline-block ${
+              isCurrentPage ? 'txt-050' : 'txt-150'
+            }`}
+          >
+            <Link href={url}>
+              <a aria-current={isCurrentPage ? 'page' : null}>{name}</a>
+            </Link>
+          </li>
+        );
+      })}
     </ol>
   </nav>
 );
