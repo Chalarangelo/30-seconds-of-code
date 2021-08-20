9c6be8791872e30157a6d2de5b059a5d14c33154
Chalarangelo <chalarangelo@gmail.com>
2021-08-20 18:41:15 +0300

    Tidy up Logger utility block
    Remove format method.
    Replace this with Logger.
    Clean up code.

--- src/blocks/utilities/logger/index.js
+++ src/blocks/utilities/logger/index.js
@@ -21,14 +21,6 @@ export class Logger {
   static outputStream = process.stdout;
 
   /**
-   * Format a string with the given chalk formatter.
-   * @param {string} msg - The message to be formatted.
-   * @param {array} format - The name of the chalk function(s) to use.
-   */
-  static format = (msg, ...format) =>
-    format.reduce((m, f) => (chalk[f] ? chalk[f](m) : m), msg);
-
-  /**
    * Logs a message to the output stream.
    * @param {string} msg - The message to be logged.
    * @param {*} options - Options for formatting the message.
@@ -41,7 +33,7 @@ export class Logger {
    */
   static log = (msg, options = {}) => {
     const opts =
-      options && typeof options === 'object'
+      typeof options === 'object'
         ? { ...defaultLogOpts, ...options }
         : typeof options === 'string'
         ? { ...defaultLogOpts, type: options }
@@ -50,44 +42,39 @@ export class Logger {
 
     let message = breakLine ? `${msg}\n` : `${msg}`;
     if (procName) message = `[${bold(procName)}] ${message}`;
-    if (validTypes.includes(type)) message = `${prefixes[type]}  ${message}`;
+    if (type && validTypes.includes(type))
+      message = `${prefixes[type]}  ${message}`;
 
-    this.outputStream.write(message);
+    Logger.outputStream.write(message);
   };
 
   /**
    * Returns this with the log's `process` option preset to the given procName.
    * @param {string} procName - The name of the current process.
    */
-  static bind = procName => {
-    const boundLog = _procName => (msg, options) => {
-      if (typeof options === 'string')
-        this.log(msg, { type: options, process: _procName });
-      else this.log(msg, { ...options, process: _procName });
-    };
-
-    const _boundLog = boundLog(procName);
-    return _boundLog;
+  static bind = procName => (msg, options) => {
+    if (typeof options === 'string')
+      Logger.log(msg, { type: options, process: procName });
+    else Logger.log(msg, { ...options, process: procName });
   };
 
   /**
    * Logs an empty line.
    */
   static breakLine = () => {
-    this.log('', 'info');
+    Logger.log('', 'info');
   };
 
   /**
    * Logs information about the current process.
    */
   static logProcessInfo = () => {
-    const information = [
+    [
       `Operating system:  ${process.platform} (node: ${process.version})`,
       `Process info:      ${process.title} (pid: ${process.pid})`,
       `Working directory: ${process.cwd()}`,
       `Executable info:   ${process.execPath} {${process.execArgv}}`,
       `Command-line args: ${process.argv.slice(2)}`,
-    ];
-    information.forEach(i => this.log(i, 'info'));
+    ].forEach(i => Logger.log(i, 'info'));
   };
 }
