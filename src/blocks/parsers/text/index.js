2688fa01b79c59b55b96a8f17e835c347b3674de
Chalarangelo <chalarangelo@gmail.com>
2021-08-21 15:59:03 +0300

    Remove dead code from TextParser

--- src/blocks/parsers/text/index.js
+++ src/blocks/parsers/text/index.js
@@ -1,6 +1,5 @@
 import fs from 'fs-extra';
 import util from 'util';
-import { exec } from 'child_process';
 import frontmatter from 'front-matter';
 import { FileParser } from 'blocks/parsers/file';
 
@@ -34,53 +33,6 @@ export class TextParser {
         };
       })
       .catch(err => err);
-  };
-
-  static fromPathOld = (filePath, { withMetadata = false } = {}) => {
-    const [, dirPath, fileName] = filePath.match(/(.*)\/([^/]*)$/);
-    const promises = [
-      new Promise((res, rej) =>
-        readFile(filePath, 'utf8')
-          .then(content => res(frontmatter(content)))
-          .catch(err => rej(err))
-      ),
-    ];
-
-    if (withMetadata) {
-      promises.push(
-        new Promise(rsl =>
-          exec(
-            `cd ${dirPath}; git log --pretty=format:%at -- ${fileName}`,
-            (error, stdout) => {
-              const dates = stdout.toString().split('\n');
-              rsl([dates.slice(-1), dates[0]]);
-            }
-          )
-        )
-      );
-    }
-
-    return new Promise((resolve, reject) => {
-      Promise.all(promises)
-        .then(values => {
-          const data = {
-            body: values[0].body,
-            ...values[0].attributes,
-            fileName,
-          };
-
-          if (withMetadata) {
-            resolve({
-              ...data,
-              firstSeen: new Date(+`${values[1][0]}000`),
-              lastUpdated: new Date(+`${values[1][1]}000`),
-            });
-          } else {
-            resolve(data);
-          }
-        })
-        .catch(err => reject(err));
-    });
   };
 
   static fromDir = async (dirPath, { withMetadata = false } = {}) => {
