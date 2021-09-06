0ebddea32f5991c9f65aead98a15f3aa40e7fe38
Chalarangelo <chalarangelo@gmail.com>
2021-09-06 16:08:15 +0300

    Replace React with Preact only in production

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
+};
