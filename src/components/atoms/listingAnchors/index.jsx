f00838bd39d8e12f6e3991f21d26e04ef290b946
Chalarangelo <chalarangelo@gmail.com>
2021-09-15 21:47:59 +0300

    Use refs to scroll the listing anchors

--- src/components/atoms/listingAnchors/index.jsx
+++ src/components/atoms/listingAnchors/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import PropTypes from 'typedefs/proptypes';
 import Link from 'next/link';
 
@@ -16,27 +16,33 @@ const propTypes = {
  * Used in Listing pages and Home page.
  */
 const ListingAnchors = ({ items = [] }) => {
+  const containerRef = useRef();
+  const selectedRef = useRef();
   // Scroll the selected tag into view, so that users always know where they are
   useEffect(() => {
-    // Wrap in try-catch to be able to test in Jest/Enzyme
-    try {
-      const listItem = document.querySelector('.listing-anchors .selected')
-        .parentNode;
-      const container = listItem.parentNode;
-      const listItemLeft = listItem.offsetLeft + listItem.offsetWidth;
-      if (container.scrollLeft + container.clientWidth < listItemLeft)
-        container.scrollTo(listItemLeft, 0);
-    } catch {
-      return;
+    if (!containerRef.current || !selectedRef.current) return;
+    const selectedItemLeft =
+      selectedRef.current.offsetLeft + selectedRef.current.offsetWidth;
+    if (
+      selectedItemLeft >
+      containerRef.current.offsetWidth + containerRef.current.scrollLeft
+    ) {
+      containerRef.current.scrollTo(selectedItemLeft, 0);
     }
   }, []);
 
   return (
-    <ul className='list-section listing-anchors flex pb-1 mt-1 mx-3.5 mb-6'>
+    <ul
+      className='list-section listing-anchors flex pb-1 mt-1 mx-3.5 mb-6'
+      ref={containerRef}
+    >
       {items.map(item => (
         <li className='flex-none my-1 mx-0' key={item.url}>
           <Link href={item.url}>
-            <a className={`btn action-btn ${item.selected ? 'selected' : ''}`}>
+            <a
+              className={`btn action-btn ${item.selected ? 'selected' : ''}`}
+              ref={item.selected ? selectedRef : undefined}
+            >
               {item.name}
             </a>
           </Link>
