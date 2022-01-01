a6b69b9897062334715879820c1c163a2e060f12
Chalarangelo <chalarangelo@gmail.com>
2022-01-01 13:19:08 +0200

    Refactor Next.js loading logic

--- src/pages/collections.jsx
+++ src/pages/collections.jsx
@@ -1,9 +1,17 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 import ListingPage from 'components/templates/listingPage';
 
 export async function getStaticProps() {
-  return await import('../next/utils').then(({ getStaticPageProps }) =>
-    getStaticPageProps('collections')
+  const pagePath = path.join(
+    process.cwd(),
+    '.content',
+    'pages',
+    'collections.json'
   );
+  const pageData = await fs.readFile(pagePath, 'utf8').then(JSON.parse);
+  return { props: pageData.context };
 }
 
 export default ListingPage;
