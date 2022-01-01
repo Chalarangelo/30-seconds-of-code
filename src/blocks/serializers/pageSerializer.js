a1c872aa482a555c3084325be00e9c61e92b03cc
Chalarangelo <chalarangelo@gmail.com>
2022-01-01 12:49:06 +0200

    Add PageSerializer

---
+++ src/blocks/serializers/pageSerializer.js
@@ -0,0 +1,11 @@
+export const pageSerializer = {
+  name: 'PageSerializer',
+  attributes: [
+    'template',
+    'relRoute',
+    'fullRoute',
+    'priority',
+    'isUnlisted',
+    'context',
+  ],
+};
