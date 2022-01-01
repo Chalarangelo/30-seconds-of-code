0d1d29fbb65e38830944c02b58a8432cd2305061
Chalarangelo <chalarangelo@gmail.com>
2022-01-01 13:18:45 +0200

    Update literals

--- src/lang/en/listing/index.js
+++ src/lang/en/listing/index.js
@@ -1,5 +1,18 @@
-import { Tag } from 'blocks/utilities/tag';
 import settings from 'settings/global';
+import tagSettings from 'settings/tags';
+import { capitalize } from 'utils';
+
+const { specialTagsDictionary } = tagSettings;
+const specialTags = Object.keys(specialTagsDictionary);
+// TODO: Ideally, the tag values received here should be formatted already, so
+// we can remove this utility function.
+const formatTag = tag => {
+  if (!tag.length) return '';
+  return !specialTags.includes(tag.toLowerCase())
+    ? capitalize(tag)
+    : specialTagsDictionary[tag.toLowerCase()];
+};
+
 /* istanbul ignore next */
 const literals = {
   featuredCollections: 'Featured Collections',
@@ -8,13 +21,13 @@ const literals = {
   topSnippets: 'Top Snippets',
   snippetList: 'Snippet List',
   blog: 'Articles',
-  tag: t => `${Tag.format(t)}`,
+  tag: t => `${formatTag(t)}`,
   shortCodelang: l => `${l}`,
-  shortCodelangTag: (l, t) => `${l} ${Tag.format(t)}`,
-  shortBlogTag: t => `${Tag.format(t)}`,
+  shortCodelangTag: (l, t) => `${l} ${formatTag(t)}`,
+  shortBlogTag: t => `${formatTag(t)}`,
   codelang: l => `${l} Snippets`,
-  codelangTag: (l, t) => `${l} ${Tag.format(t)} Snippets`,
-  blogTag: t => `${Tag.format(t)} Articles`,
+  codelangTag: (l, t) => `${l} ${formatTag(t)} Snippets`,
+  blogTag: t => `${formatTag(t)} Articles`,
   snippetCount: c => `${c} snippets`,
   pageDescription: (t, p) => {
     switch (t) {
@@ -22,12 +35,12 @@ const literals = {
         return `Browse ${p.snippetCount} ${p.listingLanguage} code snippets for all your development needs on ${settings.websiteName}.`;
       case 'tag':
         return p.listingLanguage
-          ? `Browse ${p.snippetCount} ${p.listingLanguage} ${Tag.format(
+          ? `Browse ${p.snippetCount} ${p.listingLanguage} ${formatTag(
               p.listingTag
             )} code snippets for all your development needs on ${
               settings.websiteName
             }.`
-          : `Browse ${p.snippetCount} ${Tag.format(
+          : `Browse ${p.snippetCount} ${formatTag(
               p.listingTag
             )} articles for all your development needs on ${
               settings.websiteName
