0f144b915e3586197ba72cd75d3651d391ae0d90
Chalarangelo <chalarangelo@gmail.com>
2022-01-01 12:48:20 +0200

    Introduce Snippet model

---
+++ src/blocks/models/snippet.js
@@ -0,0 +1,242 @@
+import path from 'path';
+import { convertToSeoSlug, uniqueElements, capitalize } from 'utils';
+import { Ranker } from 'blocks/utilities/ranker';
+import { Recommender } from 'blocks/utilities/recommender';
+import tokenizeSnippet from 'utils/search';
+import clientLiterals from 'lang/en/client/common';
+import expertiseLevels from 'settings/expertise';
+import tagSettings from 'settings/tags';
+
+const { specialTagsDictionary } = tagSettings;
+const specialTags = Object.keys(specialTagsDictionary);
+
+const formatTag = tag => {
+  if (!tag.length) return '';
+  return !specialTags.includes(tag.toLowerCase())
+    ? capitalize(tag)
+    : specialTagsDictionary[tag.toLowerCase()];
+};
+
+export const snippet = {
+  name: 'Snippet',
+  fields: [
+    { name: 'fileName', type: 'stringRequired' },
+    {
+      name: 'title',
+      type: 'stringRequired',
+    },
+    {
+      name: 'tags',
+      type: 'stringArray',
+      validators: {
+        minLength: 1,
+        uniqueValues: true,
+      },
+    },
+    { name: 'firstSeen', type: 'dateRequired' },
+    { name: 'lastUpdated', type: 'dateRequired' },
+    { name: 'listed', type: 'booleanRequired' },
+    {
+      name: 'type',
+      type: 'stringRequired',
+      validators: {
+        isBlogTypeOrSnippet: value => {
+          return value.startsWith('blog') || value === 'snippet';
+        },
+      },
+    },
+    {
+      name: 'text',
+      type: 'object',
+      validators: {
+        containsAllTexts: value => {
+          return ['full', 'short'].every(key => value[key]);
+        },
+      },
+    },
+    {
+      name: 'html',
+      type: 'object',
+      validators: {
+        hasDescriptionKeys: value =>
+          ['fullDescription', 'description'].every(key => value[key]),
+      },
+    },
+    { name: 'code', type: 'object' },
+    { name: 'cover', type: 'string' },
+    { name: 'seoDescription', type: 'stringRequired' },
+  ],
+  validators: {
+    updatedAfterSeen: snippet => snippet.lastUpdated >= snippet.firstSeen,
+    blogHasCover: snippet => (snippet.isBlog ? Boolean(snippet.cover) : true),
+    blogHasAuthor: snippet =>
+      snippet.isBlog ? snippet.authors && snippet.authors.length : true,
+  },
+  properties: {
+    cardTemplate: snippet => snippet.repository.cardTemplate,
+    primaryTag: snippet => snippet.tags[0],
+    truePrimaryTag: snippet => {
+      if (!snippet.isBlog) return snippet.primaryTag;
+      const language = snippet.language;
+      if (!language) return snippet.primaryTag;
+      return snippet.tags.filter(t => t !== language.id)[0];
+    },
+    formattedPrimaryTag: snippet => formatTag(snippet.primaryTag),
+    formattedTags: snippet => {
+      return {
+        primary: formatTag(snippet.primaryTag),
+        all: snippet.tags
+          .filter(tag => !expertiseLevels.includes(tag))
+          .map(formatTag),
+      };
+    },
+    hasOtherLanguages: snippet => {
+      return (
+        snippet.repository.otherLanguages &&
+        snippet.repository.otherLanguages.size > 0
+      );
+    },
+    formattedLanguages: snippet => {
+      if (snippet.isBlog) return {};
+      const lang = {
+        short: snippet.language.short,
+        long: snippet.language.name,
+      };
+      if (snippet.hasOtherLanguages) {
+        lang.otherLanguages = snippet.repository.otherLanguages.flatMap(
+          lang => ({
+            short: lang.short,
+            long: lang.name,
+          })
+        );
+      }
+      return lang;
+    },
+    isBlog: snippet => snippet.type.startsWith('blog'),
+    isCSS: snippet => snippet.repository.isCSS,
+    isReact: snippet => snippet.repository.isReact,
+    isReactHook: snippet =>
+      snippet.repository.isReact && snippet.primaryTag === 'hooks',
+    slug: snippet => `/${snippet.id}`,
+    titleSlug: snippet => convertToSeoSlug(snippet.title),
+    url: snippet => `${snippet.repository.repoUrlPrefix}/${snippet.fileName}`,
+    vscodeUrl: snippet =>
+      `vscode://file/${path.resolve(
+        snippet.repository.vscodeUrlPrefix,
+        snippet.fileName
+      )}`,
+    actionType: snippet => {
+      if (snippet.isBlog) return undefined;
+      if (snippet.isCSS) return 'cssCodepen';
+      if (snippet.isReact) return 'codepen';
+      return 'copy';
+    },
+    expertise: snippet => {
+      if (snippet.isBlog) return 'article';
+      const expertiseTag = snippet.tags.find(tag =>
+        expertiseLevels.includes(tag)
+      );
+      return expertiseTag ? expertiseTag : expertiseLevels[1];
+    },
+    isScheduled: snippet => snippet.firstSeen > new Date(),
+    isPublished: snippet => !snippet.isScheduled,
+    isListed: snippet =>
+      snippet.repository.featured && snippet.listed && !snippet.isScheduled,
+    ranking: snippet => Ranker.rankSnippet(snippet),
+    searchTokensArray: snippet => {
+      const tokenizableElements = snippet.isBlog
+        ? [
+            ...snippet.tags.filter(tag => !expertiseLevels.includes(tag)),
+            ...tokenizeSnippet(`${snippet.text.short} ${snippet.title}`),
+          ]
+        : [
+            ...snippet.title.split(' '),
+            snippet.repository.language.short,
+            snippet.repository.language.long,
+            ...snippet.tags.filter(tag => !expertiseLevels.includes(tag)),
+            ...tokenizeSnippet(snippet.text.short),
+          ];
+      return uniqueElements(tokenizableElements.map(v => v.toLowerCase()));
+    },
+    searchTokens: snippet => snippet.searchTokensArray.join(' '),
+    breadcrumbs: snippet => {
+      const slugParts = snippet.slug.split('/').filter(Boolean).slice(0, -2);
+      const crumbs = [
+        {
+          url: '/',
+          name: clientLiterals.home,
+        },
+        {
+          url: `/${slugParts[0]}/p/1`,
+          name: snippet.isBlog
+            ? formatTag('article')
+            : snippet.repository.language.long,
+        },
+      ];
+
+      if (snippet.isBlog)
+        crumbs.push({
+          url: `/${slugParts[0]}/t/${snippet.primaryTag.toLowerCase()}/p/1`,
+          name: formatTag(snippet.primaryTag),
+        });
+
+      crumbs.push({
+        url: snippet.slug,
+        name: snippet.title,
+      });
+
+      return crumbs;
+    },
+    hasCollection: snippet => snippet.collections && snippet.collections.length,
+    recommendedCollection: snippet =>
+      snippet.hasCollection ? snippet.collections.first : null,
+  },
+  lazyProperties: {
+    icon: ({ models: { Language } }) => snippet => {
+      let mapped;
+      if (snippet.isBlog) {
+        const lang = Language.records.find(l => snippet.tags.includes(l.id));
+        if (lang) {
+          const tag = snippet.tags.find(t => lang.tagIcons[t]);
+          mapped = tag ? tag : lang.icon;
+        }
+        return mapped ? mapped : snippet.repository.icon;
+      }
+      return snippet.language.tagIcons[snippet.primaryTag]
+        ? snippet.language.tagIcons[snippet.primaryTag]
+        : snippet.repository.icon;
+    },
+    language: ({ models: { Language } }) => snippet => {
+      if (!snippet.isBlog) return snippet.repository.language;
+      for (let tag of snippet.tags) {
+        const lang = Language.records.get(tag);
+        if (lang) return lang;
+      }
+      return null;
+    },
+    recommendedSnippets: ({ models: { Snippet } }) => snippet => {
+      const recommendedSnippetIds = Recommender.recommendSnippets(
+        snippet,
+        Snippet.records
+      );
+      return Snippet.records.only(...recommendedSnippetIds);
+    },
+  },
+  cacheProperties: [
+    'ranking',
+    'searchTokensArray',
+    'searchTokens',
+    'language',
+    'icon',
+    'primaryTag',
+    'truePrimaryTag',
+  ],
+  scopes: {
+    snippets: snippet => snippet.type === 'snippet',
+    blogs: snippet => snippet.type.startsWith('blog'),
+    listed: snippet => snippet.isListed,
+    unlisted: snippet => !snippet.isListed,
+    scheduled: snippet => snippet.isScheduled,
+    published: snippet => snippet.isPublished,
+  },
+};
