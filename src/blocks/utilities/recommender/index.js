32a01f216fe3a96698d45553bb3f6c89874a7a0a
Chalarangelo <chalarangelo@gmail.com>
2022-01-01 13:16:52 +0200

    Update recommender

--- src/blocks/utilities/recommender/index.js
+++ src/blocks/utilities/recommender/index.js
@@ -1,6 +1,3 @@
-import { Tag } from 'blocks/utilities/tag';
-import { Snippet } from 'blocks/entities/snippet';
-import { ArgsError } from 'blocks/utilities/error';
 import recommenderSettings from 'settings/recommendationEngine';
 
 // Get data from configuration
@@ -17,83 +14,98 @@ const totalScoreLimit =
  * Utility for creating snippet recommendations.
  */
 export class Recommender {
-  static recommendSnippets = (
-    snippet,
-    snippets = [...Snippet.instances.values()]
-  ) => {
-    if (!(snippet instanceof Snippet)) {
-      throw new ArgsError(
-        "Invalid arguments. 'snippet' must be an instance of 'Snippet'."
-      );
-    }
+  static recommendSnippets = (snippet, snippets) => {
+    const language = snippet.language;
+    const primaryTag = snippet.truePrimaryTag;
+    const searchTokens = snippet.searchTokensArray;
 
-    const isBlog = snippet.config.isBlog;
+    const recommendationRankings = new Map();
+    let minRankings = [];
 
-    // Determine relevant attributes for the snippet
-    const language = (isBlog
-      ? Tag.determineLanguage(snippet.tags.all).long
-      : snippet.config.language.long
-    ).toLowerCase();
-    const primaryTag = (isBlog
-      ? Tag.stripLanguage(snippet.tags.all)[0]
-      : snippet.tags.primary || ''
-    ).toLowerCase();
-    const searchTokens = snippet.searchTokens.split(' ');
+    snippets.forEach(s => {
+      // Filter out any nodes with the same id (this very snippet)
+      if (
+        s.id !== snippet.id &&
+        s.titleSlug !== snippet.titleSlug &&
+        (!snippet.isListed || s.isListed)
+      ) {
+        // Determine score for language:
+        //  * Same language, as language = 100% of language score
+        //  * Same language, but as a tag = 25% of language score
+        //  * Not same language = 0% of language score
+        const languageScore =
+          (s.language === language ? 1 : s.tags.includes(language) ? 0.25 : 0) *
+          languageScoreLimit;
+        // Determine primary tag score:
+        //  * Same primary tag = 100% of tag score
+        //  * Contains primary tag, but not primary = 50% of tag score
+        //  * Doesn't contain tag = 0% of language score
+        const primaryTagScore =
+          (s.primaryTag === primaryTag
+            ? 1
+            : s.tags.includes(primaryTag)
+            ? 0.5
+            : 0) * primaryTagScoreLimit;
+        // Determine search token score
+        const searchTokenScore =
+          searchTokens.reduce(
+            (a, t) =>
+              (a = s.searchTokens.includes(t)
+                ? a + 1 / searchTokens.length
+                : a),
+            0
+          ) * searchTokenScoreLimit;
 
-    return snippets
-      .map(s => {
-        s.recommendationRanking = 0;
-        // Filter out any nodes with the same id (this very snippet)
-        if (
-          s.id !== snippet.id &&
-          s.titleSlug !== snippet.titleSlug &&
-          (!snippet.isListed || s.isListed)
-        ) {
-          // Determine score for language:
-          //  * Same language, as language = 100% of language score
-          //  * Same language, but as a tag = 25% of language score
-          //  * Not same language = 0% of language score
-          const languageScore =
-            (s.config.language && s.config.language.long === language
-              ? 1
-              : s.tags.all.find(t => t.toLowerCase() === language)
-              ? 0.25
-              : 0) * languageScoreLimit;
-          // Determine primary tag score:
-          //  * Same primary tag = 100% of tag score
-          //  * Contains primary tag, but not primary = 50% of tag score
-          //  * Doesn't contain tag = 0% of language score
-          const primaryTagScore =
-            (s.tags.primary.toLowerCase() === primaryTag
-              ? 1
-              : s.tags.all.find(t => t.toLowerCase() === primaryTag)
-              ? 0.5
-              : 0) * primaryTagScoreLimit;
-          // Determine search token score
-          const searchTokenScore =
-            searchTokens.reduce(
-              (a, t) =>
-                (a =
-                  s.searchTokens.indexOf(t) !== -1
-                    ? a + 1 / searchTokens.length
-                    : a),
-              0
-            ) * searchTokenScoreLimit;
+        // Divide by the limit to get a value between 0 and 1
+        const recommendationRanking = (
+          (languageScore + primaryTagScore + searchTokenScore) /
+          totalScoreLimit
+        ).toFixed(2);
 
-          // Divide by the limit to get a value between 0 and 1
-          s.recommendationRanking = (
-            (languageScore + primaryTagScore + searchTokenScore) /
-            totalScoreLimit
-          ).toFixed(2);
+        // Performance optimization to minimize the number of times we have to
+        // sort afterwards. As soon as the minimum amount of snippets has been
+        // considered, we can start trimming off any snippets below the lowest
+        // snippet's recommendation ranking.
+        if (recommendationRanking > 0) {
+          if (minRankings.length < recommendationCount) {
+            // First 3 snippets are always added
+            minRankings.push(recommendationRanking);
+            minRankings.sort((a, b) => a - b);
+          } else {
+            // If the new ranking is lower than the lowest ranking, ignore it
+            if (recommendationRanking < minRankings[0]) {
+              return;
+            } else {
+              // Otherwise, replace the lowest ranking with the new ranking
+              minRankings.push(recommendationRanking);
+              minRankings.sort((a, b) => a - b);
+              minRankings.shift();
+            }
+          }
+          recommendationRankings.set(s.id, [
+            recommendationRanking,
+            s.ranking,
+            languageScore,
+            primaryTagScore,
+            searchTokenScore,
+          ]);
         }
-        return s;
-      })
-      .sort((a, b) => {
-        // Rank by recommendationRanking as long as possible, otherwise by ranking
-        if (a.recommendationRanking === b.recommendationRanking)
-          return b.ranking - a.ranking;
-        return +b.recommendationRanking - +a.recommendationRanking;
-      })
-      .slice(0, recommendationCount);
+      }
+    });
+
+    return [...recommendationRankings.entries()]
+      .sort(
+        (
+          [, [aRecommendationRanking, aRanking]],
+          [, [bRecommendationRanking, bRanking]]
+        ) => {
+          // Rank by recommendationRanking as long as possible, otherwise by ranking
+          if (aRecommendationRanking === bRecommendationRanking)
+            return bRanking - aRanking;
+          return +bRecommendationRanking - +aRecommendationRanking;
+        }
+      )
+      .slice(0, recommendationCount)
+      .map(([id]) => id);
   };
 }
--- src/blocks/utilities/recommender/index.test.js
+++ src/blocks/utilities/recommender/index.test.js
@@ -1,5 +1,4 @@
 import { Recommender } from '.';
-import { ArgsError } from 'blocks/utilities/error';
 import SnippetFactory from 'test/fixtures/factories/blockSnippet';
 
 describe('Recommender', () => {
@@ -9,10 +8,6 @@ describe('Recommender', () => {
   });
 
   describe('recommendSnippets', () => {
-    it('throws if snippet is not of the correct type', () => {
-      expect(() => Recommender.recommendSnippets({})).toThrow(ArgsError);
-    });
-
     it('returns an array of snippets with the correct length', () => {
       snippets.forEach(snippet => {
         const recommendations = Recommender.recommendSnippets(snippet);
