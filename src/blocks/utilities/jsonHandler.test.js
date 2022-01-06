d6b33776f7b8567a4b1d107140b5c36f5bc6b809
Chalarangelo <chalarangelo@gmail.com>
2022-01-06 16:58:21 +0200

    Restore utility tests

---
+++ src/blocks/utilities/jsonHandler.test.js
@@ -0,0 +1,76 @@
+import { JSONHandler } from './jsonHandler';
+
+jest.mock('glob', () => ({
+  sync: jest.fn(() => ['test/item/index.json']),
+}));
+
+jest.mock('path', () => ({
+  resolve: jest.fn(x => x),
+  dirname: jest.fn(),
+}));
+
+jest.mock('fs-extra', () => ({
+  ensureDirSync: jest.fn(),
+}));
+
+jest.mock('fs/promises', () => ({
+  writeFile: jest.fn((path, data) => Promise.resolve([path, data])),
+}));
+
+jest.mock(
+  'test/item/index.json',
+  () => ({
+    a: 1,
+  }),
+  { virtual: true }
+);
+
+describe('JSONHandler', () => {
+  describe('toFile', () => {
+    it('passes the correct arguments', () => {
+      return JSONHandler.toFile('my/path', { a: 1 }).then(data => {
+        expect(data).toEqual(['my/path', '{\n  "a": 1\n}']);
+      });
+    });
+  });
+
+  describe('fromGlob', () => {
+    it('throws if the pattern is invalid', () => {
+      expect(() => JSONHandler.fromGlob('test/item')).toThrow();
+    });
+
+    it('throws if the options are invalid', () => {
+      expect(() =>
+        JSONHandler.fromGlob('test/item/*.json', {
+          withNames: true,
+          reduced: true,
+        })
+      ).toThrow();
+    });
+
+    it('returns the correct data with default arguments', () => {
+      expect(JSONHandler.fromGlob('test/item/*.json')).toEqual([{ a: 1 }]);
+    });
+
+    it('returns the correct data with names', () => {
+      expect(
+        JSONHandler.fromGlob('test/item/*.json', { withNames: true })
+      ).toEqual([['test/item/index.json', { a: 1 }]]);
+    });
+
+    it('returns the correct data with reducer', () => {
+      expect(
+        JSONHandler.fromGlob('test/item/*.json', {
+          reduced: true,
+          initialValue: {},
+        })
+      ).toEqual({ a: 1 });
+    });
+  });
+
+  describe('fromFile', () => {
+    it('returns the correct data', () => {
+      expect(JSONHandler.fromFile('test/item/index.json')).toEqual({ a: 1 });
+    });
+  });
+});
---
+++ src/blocks/utilities/logger.test.js
@@ -0,0 +1,129 @@
+import { Writable } from 'stream';
+import { Logger } from './logger';
+
+describe('Logger', () => {
+  const writeFn = jest.fn();
+  const originalStream = Logger.outputStream;
+
+  beforeAll(() => {
+    jest.spyOn(process.stdout, 'write').mockImplementation(writeFn);
+  });
+
+  afterEach(() => {
+    writeFn.mockClear();
+  });
+
+  describe('outputStream', () => {
+    it('should be a Stream.Writable', () => {
+      expect(originalStream instanceof Writable).toBe(true);
+    });
+  });
+
+  describe('static methods', () => {
+    describe('log', () => {
+      it('calls the write method of the outputStream', () => {
+        Logger.log('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+    });
+
+    describe('debug', () => {
+      it('calls the write method of the outputStream', () => {
+        Logger.debug('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+    });
+
+    describe('logProcessInfo', () => {
+      it('logs the appropriate number of messages', () => {
+        Logger.logProcessInfo();
+        expect(writeFn.mock.calls.length).toBe(6);
+      });
+    });
+  });
+
+  describe('instance methods', () => {
+    const logger = new Logger('test');
+
+    describe('log', () => {
+      it('calls the write method of the outputStream', () => {
+        logger.log('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+
+      it('prints the correct status code, process name and message', () => {
+        logger.log('Hello');
+        const output = writeFn.mock.calls[0][0];
+        expect(output).toContain('Hello');
+        expect(output).toContain('info');
+        expect(output).toContain('test');
+      });
+    });
+
+    describe('info', () => {
+      it('calls the write method of the outputStream', () => {
+        logger.info('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+
+      it('prints the correct status code, process name and message', () => {
+        logger.info('Hello');
+        const output = writeFn.mock.calls[0][0];
+        expect(output).toContain('Hello');
+        expect(output).toContain('info');
+        expect(output).toContain('test');
+      });
+    });
+
+    describe('success', () => {
+      it('calls the write method of the outputStream', () => {
+        logger.success('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+
+      it('prints the correct status code, process name and message', () => {
+        logger.success('Hello');
+        const output = writeFn.mock.calls[0][0];
+        expect(output).toContain('Hello');
+        expect(output).toContain('done');
+        expect(output).toContain('test');
+      });
+
+      it('prints the time taken', () => {
+        logger.success('Hello');
+        const output = writeFn.mock.calls[0][0];
+        expect(output).toMatch(/\(\d+ms\)/);
+      });
+    });
+
+    describe('error', () => {
+      it('calls the write method of the outputStream', () => {
+        logger.error('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+
+      it('prints the correct status code, process name and message', () => {
+        logger.error('Hello');
+        const output = writeFn.mock.calls[0][0];
+        expect(output).toContain('Hello');
+        expect(output).toContain('err');
+        expect(output).toContain('test');
+      });
+    });
+
+    describe('warn', () => {
+      it('calls the write method of the outputStream', () => {
+        logger.warn('Hello');
+        expect(writeFn.mock.calls.length).toBe(1);
+      });
+
+      it('prints the correct status code, process name and message', () => {
+        logger.warn('Hello');
+        const output = writeFn.mock.calls[0][0];
+        expect(output).toContain('Hello');
+        expect(output).toContain('warn');
+        expect(output).toContain('test');
+      });
+    });
+  });
+});
