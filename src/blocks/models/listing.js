882e71e61691b6f1c1a00e65c784e58ba26c0015
Chalarangelo <chalarangelo@gmail.com>
2022-01-07 13:53:47 +0200

    Ensure collections don't contain unpublished content

--- src/blocks/models/listing.js
+++ src/blocks/models/listing.js
@@ -140,7 +140,7 @@ export const listing = {
         });
       }
       // Catch all, also catches 'custom' for collection types
-      return listing.snippets;
+      return listing.snippets.published;
     },
   },
   lazyProperties: {
