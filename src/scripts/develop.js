6d11cb61006256e7a314bd487862dfa19c528ea9
Chalarangelo <chalarangelo@gmail.com>
2022-01-19 12:24:57 +0200

    Fix develop script issues

--- src/scripts/develop.js
+++ src/scripts/develop.js
@@ -1,20 +1,18 @@
 import { Application } from 'blocks/application';
-import { Logger } from 'blocks/utilities/logger';
-import { IconWriter } from 'blocks/writers/iconWriter';
-import { AssetWriter } from 'blocks/writers/assetWriter';
-import { PageWriter } from 'blocks/writers/pageWriter';
-import { SearchIndexWriter } from 'blocks/writers/searchIndexWriter';
 
 export const build = async () => {
-  Logger.log('Build process is starting up...\n');
-  Logger.logProcessInfo();
-
-  await Promise.all([Application.extractAndInitialize(), IconWriter.write()]);
+  Application.Logger.log('Build process is starting up...\n');
+  Application.Logger.logProcessInfo();
 
   await Promise.all([
-    SearchIndexWriter.write(),
-    AssetWriter.write(),
-    PageWriter.write(),
+    Application.extractAndInitialize(),
+    Application.IconWriter.write(),
+  ]);
+
+  await Promise.all([
+    Application.SearchIndexWriter.write(),
+    Application.AssetWriter.write(),
+    Application.PageWriter.write(),
   ]);
 };
 
