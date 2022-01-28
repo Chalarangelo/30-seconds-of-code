14cbf69177f1bba34d51cc095e737daf6861679f
Chalarangelo <chalarangelo@gmail.com>
2022-01-28 16:53:28 +0200

    Introduce Schemer utility
    Handles structured data generation.

---
+++ src/blocks/utilities/schemer.js
@@ -0,0 +1,86 @@
+import settings from 'settings/global';
+
+const { websiteUrl, orgName } = settings;
+const orgLogoSrc = '/assets/30s-icon.png';
+
+/**
+ * Utility for generating structured data for pages.
+ */
+export class Schemer {
+  static logo = {
+    '@type': 'ImageObject',
+    url: `${websiteUrl}${orgLogoSrc}`,
+  };
+
+  static organization = {
+    '@type': 'Organization',
+    name: orgName,
+    logo: Schemer.logo,
+  };
+
+  static generateSnippetData = ({
+    title,
+    slug,
+    description,
+    cover,
+    firstSeen,
+    lastUpdated,
+    author,
+  }) => {
+    const url = `${websiteUrl}${slug}`;
+    const coverSrc = cover ? cover : orgLogoSrc;
+    const _author = author
+      ? {
+          '@type': 'Person',
+          name: author.name,
+          url: author.profile,
+        }
+      : Schemer.organization;
+
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'TechArticle',
+      name: title,
+      url,
+      mainEntityOfPage: { '@type': 'WebPage', '@id': url },
+      headline: title,
+      description,
+      image: `${websiteUrl}${coverSrc}`,
+      datePublished: firstSeen,
+      dateModified: lastUpdated,
+      author: _author,
+      publisher: Schemer.organization,
+    };
+  };
+
+  static generateListingData = ({ title, slug, items }) => {
+    const url = `${websiteUrl}${slug}`;
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'ItemList',
+      name: title,
+      url,
+      mainEntityOfPage: { '@type': 'WebPage', '@id': url },
+      numberOfItems: items.length,
+      itemListElement: items.map((li, i) => ({
+        '@type': 'ListItem',
+        position: i + 1,
+        url: `${websiteUrl}${li.url}`,
+        name: li.title,
+      })),
+    };
+  };
+
+  static generateHomeData = () => {
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'WebSite',
+      url: websiteUrl,
+      potentialAction: {
+        '@type': 'SearchAction',
+        target: `${websiteUrl}/search?keyphrase={keyphrase}`,
+        'query-input': 'required name=keyphrase',
+      },
+    };
+  };
+}
