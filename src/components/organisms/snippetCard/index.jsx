a044c1017b739593f22856682b8a4371572fe37e
Chalarangelo <chalarangelo@gmail.com>
2022-01-31 14:48:53 +0200

    Merge standard and CSS snippet cards

--- src/components/organisms/snippetCard/index.jsx
+++ src/components/organisms/snippetCard/index.jsx
@@ -1,9 +1,7 @@
 import StandardSnippetCard from './standardSnippetCard';
-import CssSnippetCard from './cssSnippetCard';
 import BlogSnippetCard from './blogSnippetCard';
 
 export default {
   StandardSnippetCard,
-  CssSnippetCard,
   BlogSnippetCard,
 };
--- src/components/organisms/snippetCard/standardSnippetCard/index.jsx
+++ src/components/organisms/snippetCard/standardSnippetCard/index.jsx
@@ -1,11 +1,30 @@
+import { useEffect } from 'react';
 import Card, { CardIcon, CardTitle, CardSubtitle } from 'components/atoms/card';
 import Actions from 'components/molecules/actions';
+import literals from 'lang/en/client/common';
 
 /**
  * Standard snippet card.
  * Used for non-article snippets.
  */
 const SnippetCard = ({ snippet }) => {
+  useEffect(() => {
+    if (!snippet.code || !snippet.code.js) return;
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.innerHTML = `function snippet_preview_js(){${snippet.code.js}};`;
+    document.body.appendChild(s);
+    try {
+      window['snippet_preview_js']();
+    } catch (e) {
+      /* istanbul ignore next */
+      console.warn(
+        'There is an issue with JavaScript execution on the snippet preview of this page.'
+      );
+    }
+  }, []);
+
   return (
     <Card className='snippet-card'>
       <div className='card-meta grid'>
@@ -23,6 +42,20 @@ const SnippetCard = ({ snippet }) => {
         className='card-description'
         dangerouslySetInnerHTML={{ __html: snippet.fullDescription }}
       />
+      {snippet.code && snippet.code.scopedCss ? (
+        <div className='card-preview-content'>
+          <h5 className='snippet-preview-title txt-150 fs-md'>
+            {literals.preview}
+          </h5>
+          <div
+            className='snippet-preview m-0 br-lg'
+            data-scope={snippet.id.slice(snippet.id.lastIndexOf('/') + 1)}
+          >
+            <style>{snippet.code.scopedCss}</style>
+            <div dangerouslySetInnerHTML={{ __html: snippet.code.html }} />
+          </div>
+        </div>
+      ) : null}
       <div className='card-source-content mt-5 mx-0 mb-0'>
         {snippet.codeBlocks.map(({ language, htmlContent }) => (
           <pre
