72132c567f347bfdd6e8759d048e053047e9f411
Chalarangelo <chalarangelo@gmail.com>
2022-02-02 17:53:19 +0200

    Manually generate web manifest
    Move manifest generation to its own script.
    Commit generated manifest to git.

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -4,10 +4,7 @@ export const build = async () => {
   Application.Logger.log('Build process is starting up...\n');
   Application.Logger.logProcessInfo();
 
-  await Promise.all([
-    Application.extractAndInitialize(),
-    Application.ManifestWriter.write(),
-  ]);
+  await Application.extractAndInitialize();
 
   await Promise.all([
     Application.AssetWriter.write(),
