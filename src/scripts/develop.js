83042d68b0daf31d0c2068326647f5a19e68dd56
Chalarangelo <chalarangelo@gmail.com>
2022-02-02 17:50:12 +0200

    Manually generate icon font
    Move icon font generation to its own script.

--- src/scripts/develop.js
+++ src/scripts/develop.js
@@ -4,10 +4,7 @@ export const build = async () => {
   Application.Logger.log('Build process is starting up...\n');
   Application.Logger.logProcessInfo();
 
-  await Promise.all([
-    Application.extractAndInitialize(),
-    Application.IconWriter.write(),
-  ]);
+  await Promise.all([Application.extractAndInitialize()]);
 
   await Promise.all([
     Application.SearchIndexWriter.write(),
