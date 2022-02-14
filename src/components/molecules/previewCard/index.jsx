1bed2a9e04fbd4495b2b6d8d0aa6fc88e66e645d
Chalarangelo <chalarangelo@gmail.com>
2022-02-14 10:56:31 +0200

    Use utilities for card-data

--- src/components/molecules/previewCard/index.jsx
+++ src/components/molecules/previewCard/index.jsx
@@ -10,7 +10,7 @@ import { CardIcon, CardTitle, CardSubtitle } from 'components/atoms/card';
 const PreviewCard = ({ contentItem }) => (
   <li className='card srfc-01dp txt-100 list-card grid'>
     <CardIcon icon={contentItem.icon} expertise={contentItem.expertise} />
-    <div className='card-data'>
+    <div className='card-data mx-2 my-0'>
       <CardTitle isSecondary>
         <Link href={contentItem.url}>
           <a className='inherit'>{contentItem.title}</a>
