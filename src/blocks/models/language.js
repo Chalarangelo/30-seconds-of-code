5ffd4de459828935e71d161848c2dd478c44961c
Chalarangelo <chalarangelo@gmail.com>
2022-03-04 09:44:59 +0200

    Add utility properties to Language model
    Allow access to repository, slug prefix and tags.

--- src/blocks/models/language.js
+++ src/blocks/models/language.js
@@ -6,6 +6,18 @@ export const language = {
     { name: 'name', type: 'stringRequired' },
     { name: 'icon', type: 'string' },
   ],
+  properties: {
+    mainRepository: language =>
+      language.repositories && language.repositories.length
+        ? language.repositories.first
+        : null,
+    slugPrefix: language =>
+      language.mainRepository ? `/${language.mainRepository.slug}` : null,
+    tagShortIds: language =>
+      language.mainRepository
+        ? language.mainRepository.tags.flatPluck('shortId')
+        : [],
+  },
   lazyMethods: {
     getTagIcon: ({ models: { Tag } }) => (language, tag) => {
       if (!language.repositories || !language.repositories.length)
@@ -17,6 +29,7 @@ export const language = {
       return tagRec.icon;
     },
   },
+  cacheProperties: ['mainRepository', 'slugPrefix', 'tagShortIds'],
   scopes: {
     // Hacky way to exclude the HTML language from the list
     full: language => language.icon,
