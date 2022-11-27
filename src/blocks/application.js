fcc4f323fbdb284059ff6a5946b86d4afe2959e8
Angelos Chalaris <chalarangelo@gmail.com>
2022-11-27 12:18:01 +0200

    Allow collection listings to have a parent
    Resolves #377
    Collections can now be configured via a `parent`
    field in their JSON config to belong to a parent.

--- src/blocks/application.js
+++ src/blocks/application.js
@@ -420,16 +420,6 @@ export class Application {
         lastUpdated: new Date(lastUpdated),
       });
     });
-    // Populate collections and link to snippets
-    collections.forEach(collection => {
-      const { snippetIds, typeMatcher, ...rest } = collection;
-      const collectionRec = Collection.createRecord(rest);
-      if (snippetIds && snippetIds.length) collectionRec.snippets = snippetIds;
-      if (typeMatcher)
-        collectionRec.snippets = Snippet.records
-          .where(snippet => snippet.type === typeMatcher)
-          .flatPluck('id');
-    });
     // Populate listings and create relationships
     Repository.records.forEach(repo => {
       const type = repo.isBlog ? 'blog' : 'language';
@@ -454,7 +444,26 @@ export class Application {
           featuredIndex: featuredListings.indexOf(tagId),
           parent: repoListingId,
         });
-        return tagId;
+      });
+    });
+    // Populate collections, collection listings and link to snippets and parent listings
+    collections.forEach(collection => {
+      const { snippetIds, typeMatcher, parent, ...rest } = collection;
+      const collectionRec = Collection.createRecord(rest);
+      if (snippetIds && snippetIds.length) collectionRec.snippets = snippetIds;
+      if (typeMatcher)
+        collectionRec.snippets = Snippet.records
+          .where(snippet => snippet.type === typeMatcher)
+          .flatPluck('id');
+      const slugPrefix = `/${collection.slug}`;
+      const listingId = `collection${slugPrefix}`;
+      Listing.createRecord({
+        id: listingId,
+        relatedRecordId: collection.id,
+        type: 'collection',
+        slugPrefix,
+        featuredIndex: featuredListings.indexOf(listingId),
+        parent,
       });
     });
     // Populate the main listing
@@ -472,18 +481,6 @@ export class Application {
       slugPrefix: '/collections',
       featuredIndex: -1,
       ...collectionListingConfig,
-    });
-    // Populate listings for custom collections
-    Collection.records.forEach(collection => {
-      const slugPrefix = `/${collection.slug}`;
-      const listingId = `collection${slugPrefix}`;
-      Listing.createRecord({
-        id: listingId,
-        relatedRecordId: collection.id,
-        type: 'collection',
-        slugPrefix,
-        featuredIndex: featuredListings.indexOf(listingId),
-      });
     });
     // Populate snipet pages
     Snippet.records.forEach(snippet => {
