0d92ae0e0204a17850e1ca6bebbb961ee1a9e8b8
Angelos Chalaris <chalarangelo@gmail.com>
2022-12-24 21:52:03 +0200

    Serialize listing cover images

--- src/blocks/serializers/listingContextSerializer.js
+++ src/blocks/serializers/listingContextSerializer.js
@@ -5,13 +5,20 @@ export const listingContextSerializer = {
   methods: {
     splashUrl: listing =>
       listing.splash
-        ? `/${pathSettings.staticAssetPath}/${listing.splash}`
-        : `/${pathSettings.staticAssetPath}/laptop-view.jpg`,
+        ? `/${pathSettings.staticAssetPath}/splash/${listing.splash}`
+        : `/${pathSettings.staticAssetPath}/splash/laptop-view.png`,
+    coverUrl: listing =>
+      listing.splash
+        ? `/${
+            pathSettings.staticAssetPath
+          }/splash_cover/${listing.splash.replace('.png', 'jpg')}`
+        : `/${pathSettings.staticAssetPath}/splash_cover/laptop-view.jpg`,
   },
   attributes: [
     ['name', 'listingName'],
     ['description', 'listingDescription'],
     ['splashUrl', 'listingImage'],
+    ['coverUrl', 'listingCover'],
     ['sublinks', 'listingSublinks'],
     ['seoDescription', 'pageDescription'],
   ],
