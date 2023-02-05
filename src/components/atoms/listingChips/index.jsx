66c44f3a29ddec067b486b4d78338d1593caa16e
Angelos Chalaris <chalarangelo@gmail.com>
2023-02-05 13:24:52 +0200

    Simplify render of ListingChips

--- src/components/atoms/listingChips/index.jsx
+++ src/components/atoms/listingChips/index.jsx
@@ -1,19 +1,17 @@
 import Link from 'next/link';
 
-const ListingChips = ({ items = [] }) => {
-  return (
-    <ul className='list-section listing-chips flex pb-1 my-2 mx-3.5 g-c2'>
-      {items.map(item => (
-        <li className='flex-none fs-sm md:fs-md' key={item.url}>
-          <Link href={item.url}>
-            <a className={`btn action-btn ${item.selected ? 'selected' : ''}`}>
-              {item.title}
-            </a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const ListingChips = ({ items = [] }) => (
+  <ul className='list-section listing-chips flex pb-1 my-2 mx-3.5 g-c2'>
+    {items.map(item => (
+      <li className='flex-none fs-sm md:fs-md' key={item.url}>
+        <Link href={item.url}>
+          <a className={`btn action-btn ${item.selected ? 'selected' : ''}`}>
+            {item.title}
+          </a>
+        </Link>
+      </li>
+    ))}
+  </ul>
+);
 
 export default ListingChips;
