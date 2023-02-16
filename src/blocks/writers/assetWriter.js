919c43dffe37d2dd38563efc4349e7dec0604a1f
Angelos Chalaris <chalarangelo@gmail.com>
2023-02-16 21:57:36 +0200

    Process image assets from new paths
    Use the new directory structure of the config repository to parse images
    from the new paths. Cover images are processed a little differently now
    and preview images are also generated to ensure maximum performance.

--- src/blocks/writers/assetWriter.js
+++ src/blocks/writers/assetWriter.js
@@ -9,15 +9,14 @@ const {
   publicPath,
   staticAssetPath,
   rawAssetPath: inPath,
-  rawContentPath: contentPath,
   assetPath: outPath,
 } = Application.settings.paths;
 const inContentPath = 'content/configs/assets';
 
 // Image asset constants
 const supportedExtensions = ['jpeg', 'jpg', 'png', 'webp', 'tif', 'tiff'];
-const maxWidth = 846;
-const outputQuality = 80;
+const coverImageDimensions = { width: 798, height: 399 };
+const coverPreviewDimensions = { width: 144, height: 144 };
 // Icon asset constants
 const dimensions = [32, 180, 192, 512];
 const iconOutName = 'icon';
@@ -31,71 +30,33 @@ export class AssetWriter {
    */
   static write = async () => {
     const logger = new Logger('AssetWriter.write');
-    const repos = Application.dataset.getModel('Repository').records.withImages;
-    logger.log('Processing assets from config...');
 
     logger.log(
-      `Processing static assets from ${path.resolve(inPath)} to ${path.resolve(
+      `Processing assets from ${path.resolve(inPath)} to ${path.resolve(
         outPath
       )}`
     );
-    fs.ensureDirSync(outPath);
+
+    // As all directories are known ahead of time, just ensure they exist right away
+    [
+      outPath,
+      path.join(outPath, 'splash'),
+      path.join(outPath, 'illustrations'),
+      path.join(outPath, 'cover'),
+      path.join(outPath, 'preview'),
+    ].forEach(dir => fs.ensureDirSync(dir));
+
+    // Copy completely static assets
     await fs.copy(inPath, outPath);
-    await fs.copy(inContentPath, outPath);
-    const staticAssets = glob
-      .sync(`${inContentPath}/**/*.@(${supportedExtensions.join('|')})`)
-      .map(file => path.resolve(file));
-    // FIXME: Only images inside directories under the 30_content assets will work
-    // TODO: Make this more generic, temporary fix for listing splash and cover
-    fs.ensureDirSync(path.join(outPath, 'splash'));
-    fs.ensureDirSync(path.join(outPath, 'splash_cover'));
-    await Promise.all(
-      staticAssets.map(asset =>
-        AssetWriter.processImageAsset(asset, outPath, true)
-      )
-    );
+
+    // Process splash and illustrations
+    logger.log('Processing static assets...');
+    await AssetWriter.processStaticAssets();
     logger.success('Processing static assets complete');
 
-    logger.log(`Processing image assets from configuration files`);
-    for (const [, repo] of repos) {
-      fs.ensureDirSync(path.join(outPath, repo.imagesName));
-      let assets = glob
-        .sync(
-          `${contentPath}/sources/${repo.dirName}/${
-            repo.imagesPath
-          }/*.@(${supportedExtensions.join('|')})`
-        )
-        .map(file => path.resolve(file));
-      if (process.env.NODE_ENV !== 'production') {
-        const originalLength = assets.length;
-        logger.log(`Found ${assets.length} images for ${repo.dirName}.`);
-        const existingAssets = glob
-          .sync(
-            `${outPath}/${repo.imagesName}/*.@(${supportedExtensions.join(
-              '|'
-            )})`
-          )
-          .map(file => file.split('/').pop());
-        logger.log(
-          `Found ${existingAssets.length} images already generated for ${repo.dirName}.`
-        );
-        assets = assets.filter(asset => {
-          const assetName = asset.split('/').pop();
-          return !existingAssets.includes(assetName);
-        });
-        logger.log(
-          `Filtered out ${
-            originalLength - assets.length
-          } images that were already generated for ${repo.dirName}.`
-        );
-      }
-      await Promise.all(
-        assets.map(asset =>
-          AssetWriter.processImageAsset(asset, `${outPath}/${repo.imagesName}`)
-        )
-      );
-    }
-    logger.success(`Processing image assets from configuration files complete`);
+    logger.log(`Processing cover images...`);
+    await AssetWriter.processCoverAssets();
+    logger.success(`Processing cover images complete`);
 
     logger.log('Processing icons...');
     await AssetWriter.processIcons();
@@ -115,35 +76,119 @@ export class AssetWriter {
   };
 
   /**
-   * Processes the given image asset, converting it to the correct size and quality.
-   * @param {string} asset - The filename of the given asset.
-   * @param {string} imageDirName - The output directory.
-   * @param {boolean} includeSubdir - Whether or not to include the last subdirectory in the output path.
-   * @returns {Promise} A promise that resolves when the file has finished writing to disk.
+   * Processes all cover images. Briefly, the steps taken are:
+   * - Find all cover images that match the supported extensions.
+   * - Filter out images that have already been processed (dev only).
+   * - Resize the cover images to the correct dimensions.
+   * - Resize the cover images to the correct dimensions for the preview.
+   * - Write the cover images as their original format and webp.
+   * - Write the cover preview images as their original format and webp.
+   * @returns {Promise} A promise that resolves when all cover images have been processed.
    */
-  static processImageAsset = (asset, outDir, includeSubdir = false) =>
-    new Promise((resolve, reject) => {
-      const fileName = asset.slice(asset.lastIndexOf('/'));
-      const subDir = asset.split('/').slice(-2, -1)[0];
-      const outPath = includeSubdir && subDir ? `${outDir}/${subDir}` : outDir;
+  static processCoverAssets = () => {
+    const logger = new Logger('AssetWriter.processCoverAssets');
 
-      const img = sharp(asset);
-      return img.metadata().then(metadata => {
-        const resizeWidth = Math.min(maxWidth, metadata.width);
-        const name = fileName.slice(0, fileName.lastIndexOf('.'));
-        const format = metadata.format;
-        const resized = img.resize({ width: resizeWidth });
-        const quality = metadata.format === 'png' ? 90 : outputQuality;
-        return Promise.all([
-          resized
-            .toFormat(format, { quality })
-            .toFile(`${outPath}/${fileName}`),
-          resized.webp({ quality }).toFile(`${outPath}/${name}.webp`),
-        ])
-          .then(() => resolve())
-          .catch(() => reject());
+    let coverAssets = glob
+      .sync(`${inContentPath}/cover/*.@(${supportedExtensions.join('|')})`)
+      .map(fileName => ({
+        filePath: path.resolve(fileName),
+        fileName: fileName.slice(
+          fileName.lastIndexOf('/'),
+          fileName.lastIndexOf('.')
+        ),
+      }));
+
+    if (process.env.NODE_ENV !== 'production') {
+      const originalLength = coverAssets.length;
+      logger.log(`Found ${coverAssets.length} cover images.`);
+
+      const existingAssets = glob
+        .sync(`${outPath}/cover/*.@(${supportedExtensions.join('|')})`)
+        .map(file => file.split('/').pop());
+
+      logger.log(
+        `Found ${existingAssets.length} cover images already generated.`
+      );
+
+      coverAssets = coverAssets.filter(({ filePath }) => {
+        const assetName = filePath.split('/').pop();
+        return !existingAssets.includes(assetName);
       });
-    });
+
+      logger.log(
+        `Filtered out ${
+          originalLength - coverAssets.length
+        } cover images that were already generated.`
+      );
+    }
+
+    return Promise.all(
+      coverAssets.map(
+        ({ filePath, fileName }) =>
+          new Promise((resolve, reject) => {
+            const cover = sharp(filePath).resize(coverImageDimensions);
+            const preview = sharp(filePath).resize(coverPreviewDimensions);
+            return Promise.all([
+              cover
+                .jpeg({ quality: 80 })
+                .toFile(`${outPath}/cover/${fileName}.jpg`),
+              cover
+                .webp({ quality: 85 })
+                .toFile(`${outPath}/cover/${fileName}.webp`),
+              preview
+                .jpeg({ quality: 85 })
+                .toFile(`${outPath}/preview/${fileName}.jpg`),
+              preview
+                .webp({ quality: 90 })
+                .toFile(`${outPath}/preview/${fileName}.webp`),
+            ])
+              .then(() => resolve())
+              .catch(() => reject());
+          })
+      )
+    );
+  };
+
+  static processStaticAssets = () => {
+    const staticAssets = ['splash', 'illustrations'].reduce(
+      (allAssets, assetType) => {
+        const assets = glob
+          .sync(
+            `${inContentPath}/${assetType}/*.@(${supportedExtensions.join(
+              '|'
+            )})`
+          )
+          .map(fileName => ({
+            filePath: path.resolve(fileName),
+            fullFileName: fileName.slice(fileName.lastIndexOf('/')),
+            fileName: fileName.slice(
+              fileName.lastIndexOf('/'),
+              fileName.lastIndexOf('.')
+            ),
+            directory: assetType,
+          }));
+        return allAssets.concat(assets);
+      },
+      []
+    );
+
+    return Promise.all(
+      staticAssets.map(
+        ({ filePath, fileName, fullFileName, directory }) =>
+          new Promise((resolve, reject) => {
+            const image = sharp(filePath);
+            return Promise.all([
+              fs.copy(filePath, `${outPath}/${directory}/${fullFileName}`),
+              image
+                .webp({ quality: 90 })
+                .toFile(`${outPath}/${directory}/${fileName}.webp`),
+            ])
+              .then(() => resolve())
+              .catch(() => reject());
+          })
+      )
+    );
+  };
 
   /**
    * Takes a PNG image and produces all the icon assets necessary for the website.
