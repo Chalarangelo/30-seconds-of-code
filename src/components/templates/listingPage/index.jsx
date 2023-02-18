01bb631ebe2a2f3b5fb89e666141eb6811bd7f17
Angelos Chalaris <chalarangelo@gmail.com>
2023-02-18 22:08:49 +0200

    Prefer structured data names over original names.
    This can be revisited in the future and drop the fallbacks.

--- src/components/templates/listingPage/index.jsx
+++ src/components/templates/listingPage/index.jsx
@@ -24,7 +24,7 @@ const ListingPage = ({
 }) => (
   <>
     <Meta
-      title={listingName}
+      title={structuredData ? structuredData.name : listingName}
       description={pageDescription}
       logoSrc={listingCover ? listingCover : undefined}
       structuredData={structuredData}
