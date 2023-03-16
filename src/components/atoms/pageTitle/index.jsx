93fc77807a6c62fd99b6fe7a25bf8db3b742b42d
Angelos Chalaris <chalarangelo@gmail.com>
2023-03-16 23:38:41 +0200

    Migrate atoms to Astro

--- src/components/atoms/pageTitle/index.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * Page title component. Renders a simple `<h2>` element with a base class
- * and passes children to the element.
- */
-const PageTitle = ({ className = '', children }) => (
-  <h2 className={`page-title txt-200 fs-x2 f-alt ${className}`}>{children}</h2>
-);
-
-export default PageTitle;
