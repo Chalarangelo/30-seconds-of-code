a34f1217f5c58971f7bf2839b474cbe67321566a
Angelos Chalaris <chalarangelo@gmail.com>
2023-03-16 23:43:08 +0200

    Update search data for Omnisearch

--- src/blocks/serializers/listingPreviewSerializer.js
+++ src/blocks/serializers/listingPreviewSerializer.js
@@ -4,24 +4,32 @@ import pathSettings from 'settings/paths';
 export const listingPreviewSerializer = {
   name: 'ListingPreviewSerializer',
   methods: {
-    description: (listing, { withDescription } = {}) =>
-      withDescription
+    description: (listing, { withDescription, withSearch } = {}) => {
+      if (withSearch) return undefined;
+      return withDescription
         ? listing.shortDescription
             .replace('<p>', '')
             .replace('</p>', '')
             .replace(/<a.*?>(.*?)<\/a>/g, '$1')
-        : undefined,
+        : undefined;
+    },
     tags: () => literals.snippetCollection,
     searchTokens: (listing, { withSearch } = {}) =>
       withSearch ? listing.searchTokens : undefined,
     searchResultTag: (listing, { withSearch } = {}) =>
-      withSearch ? literals.snippetCollectionShort : undefined,
-    splashUrl: listing =>
-      listing.splash
+      withSearch
+        ? literals.snippetCount(listing.listedSnippets.length)
+        : undefined,
+    splashUrl: (listing, { withSearch } = {}) => {
+      if (withSearch) return undefined;
+      return listing.splash
         ? `/${pathSettings.staticAssetPath}/splash/${listing.splash}`
-        : `/${pathSettings.staticAssetPath}/splash/laptop-view.png`,
-    snippetCount: listing =>
-      literals.snippetCount(listing.listedSnippets.length),
+        : `/${pathSettings.staticAssetPath}/splash/laptop-view.png`;
+    },
+    snippetCount: (listing, { withSearch } = {}) => {
+      if (withSearch) return undefined;
+      literals.snippetCount(listing.listedSnippets.length);
+    },
     url: listing => `${listing.slugPrefix}/p/1`,
     type: () => 'collection',
   },
