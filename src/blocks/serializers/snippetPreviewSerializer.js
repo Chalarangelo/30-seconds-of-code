f519b084ca1d7d01ffec4e8a23fd7758186e1639
Angelos Chalaris <chalarangelo@gmail.com>
2023-03-16 23:43:08 +0200

    Update search data for Omnisearch

--- src/blocks/serializers/snippetPreviewSerializer.js
+++ src/blocks/serializers/snippetPreviewSerializer.js
@@ -3,30 +3,32 @@ import pathSettings from 'settings/paths';
 export const snippetPreviewSerializer = {
   name: 'SnippetPreviewSerializer',
   methods: {
-    description: snippet =>
-      snippet.descriptionHtml
+    description: (snippet, { withSearch } = {}) => {
+      if (withSearch) return undefined;
+      return snippet.descriptionHtml
         .trim()
         .replace('<p>', '')
         .replace('</p>', '')
-        .replace(/<a.*?>(.*?)<\/a>/g, '$1'),
+        .replace(/<a.*?>(.*?)<\/a>/g, '$1');
+    },
     searchTokens: (snippet, { withSearch } = {}) =>
       withSearch ? snippet.searchTokens : undefined,
     searchResultTag: (snippet, { withSearch } = {}) =>
       withSearch ? snippet.formattedMiniPreviewTag : undefined,
-    previewTitle: (snippet, { withSearch }) => {
-      if (!withSearch) return undefined;
-      return snippet.shortTitle;
-    },
-    previewUrl: snippet =>
-      snippet.cover
+    previewUrl: (snippet, { withSearch } = {}) => {
+      if (withSearch) return undefined;
+      return snippet.cover
         ? `/${pathSettings.staticAssetPath}/preview/${snippet.cover}.jpg`
-        : undefined,
-    dateFormatted: snippet =>
-      snippet.lastUpdated.toLocaleDateString('en-US', {
+        : undefined;
+    },
+    dateFormatted: (snippet, { withSearch } = {}) => {
+      if (withSearch) return undefined;
+      return snippet.lastUpdated.toLocaleDateString('en-US', {
         day: 'numeric',
         month: 'short',
         year: 'numeric',
-      }),
+      });
+    },
     type: () => 'snippet',
   },
   attributes: [
