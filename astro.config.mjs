8274d291ac95f0ca09de74697b3b952c9b284404
Angelos Chalaris <chalarangelo@gmail.com>
2023-03-18 11:49:27 +0200

    Add astro-compress plugin
    Compress HTML, CSS, JS & SVG.
    Minimal logging.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,19 @@ import { defineConfig } from 'astro/config';
 import settings from './src/settings/global.json';
 
 // https://astro.build/config
+import compress from 'astro-compress';
+
+// https://astro.build/config
 export default defineConfig({
   site: settings.websiteUrl,
+  integrations: [
+    compress({
+      img: false,
+      html: true,
+      js: true,
+      svg: true,
+      css: true,
+      logger: 1,
+    }),
+  ],
 });
