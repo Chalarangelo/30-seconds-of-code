6665ad3107e33818424d539d3ac0387fdc01fb39
Angelos Chalaris <chalarangelo@gmail.com>
2023-04-10 21:54:46 +0300

    Create prefabs for page titles

---
+++ src/prefabs/index.js
@@ -0,0 +1,8 @@
+const pageTitleBase = 'txt-200 fs-x2 f-alt f-strong mx-3.5 lh-normal';
+
+export default {
+  pageTitle: `${pageTitleBase} my-0`,
+  staticPageTitle: `${pageTitleBase} mt-4 mb-0`,
+  recommendationsTitle: `${pageTitleBase} mt-8 mb-0 pt-4`,
+  listingTitle: `${pageTitleBase} my-0 f-center md:f-left`,
+};
