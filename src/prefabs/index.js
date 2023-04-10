98a08747512e82c449fb1e5fdc6dc85ec2e6b3a4
Angelos Chalaris <chalarangelo@gmail.com>
2023-04-10 21:58:14 +0300

    Replace CardSubtitle with prefab

--- src/prefabs/index.js
+++ src/prefabs/index.js
@@ -1,8 +1,11 @@
 const pageTitleBase = 'txt-200 fs-x2 f-alt f-strong mx-3.5 lh-normal';
 
 export default {
+  // Titles
   pageTitle: `${pageTitleBase} my-0`,
   staticPageTitle: `${pageTitleBase} mt-4 mb-0`,
   recommendationsTitle: `${pageTitleBase} mt-8 mb-0 pt-4`,
   listingTitle: `${pageTitleBase} my-0 f-center md:f-left`,
+  // Cards
+  cardSubtitle: 'inline-block txt-050 fs-xs m-0',
 };
