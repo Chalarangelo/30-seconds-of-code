109c65ed3e5a71d997c2bbc2b08ef6e668832834
Angelos Chalaris <chalarangelo@gmail.com>
2023-04-14 10:54:54 +0300

    Replace Card with prefab

--- src/prefabs/index.js
+++ src/prefabs/index.js
@@ -9,9 +9,13 @@ const pageTitles = {
 };
 
 // Cards
+const cardBase = 'card mt-7 mx-1 mb-4 md:mx-3.5 srfc-01db txt-100 br-lg';
 const cardTitleBase = 'card-title txt-200 fs-lg md:fs-xl f-alt f-ellipsis';
 
 const cards = {
+  snippetCard: `${cardBase} snippet-card g-c2 px-4 py-6 md:px-6 md:py-8`,
+  previewCard: `${cardBase} list-card grid a-center py-5 px-4 md:p-6 relative no-overflow`,
+  simpleCard: `${cardBase} px-4 py-6 md:px-6 md:py-8`,
   cardTitle: `${cardTitleBase} mt-0 mx-0 mb-1`,
   secondaryCardTitle: `${cardTitleBase} f-clamp m-0`,
   cardSubtitle: 'inline-block txt-050 fs-xs m-0',
