8f2893c77d2789b2322ce2786e0d51de3b61651c
Angelos Chalaris <chalarangelo@gmail.com>
2023-06-04 15:51:29 +0300

    Replace astro-compress with compressHTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,11 @@
 import { defineConfig } from 'astro/config';
 import settings from './src/settings/global.json';
 
-import compress from 'astro-compress';
-import critters from 'astro-critters';
-
 // https://astro.build/config
 export default defineConfig({
   site: settings.websiteUrl,
   experimental: {
     inlineStylesheets: 'always',
   },
-  integrations: [
-    compress({
-      img: false,
-      html: true,
-      js: true,
-      svg: true,
-      css: true,
-      logger: 1,
-    }),
-  ],
+  compressHTML: true,
 });
