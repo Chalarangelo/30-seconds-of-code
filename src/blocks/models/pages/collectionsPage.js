88b5ae62f488abcd1ff9c5f56ae1b3590fb94c54
Angelos Chalaris <chalarangelo@gmail.com>
2023-06-11 12:16:23 +0300

    Use cached previews for collections pages

--- src/blocks/models/pages/collectionsPage.js
+++ src/blocks/models/pages/collectionsPage.js
@@ -18,72 +18,63 @@ export const collectionPage = {
       return { lang, listing };
     },
     baseSlug: page => page.slug.replace(/\/p\/\d+$/, ''),
-  },
-  lazyProperties: {
-    props:
-      ({ serializers: { PreviewSerializer } }) =>
-      page => {
-        const context = {};
+    props: page => {
+      const context = {};
 
-        context.slug = page.slug;
-        context.pageDescription = page.shortDescription;
+      context.slug = page.slug;
+      context.pageDescription = page.shortDescription;
 
-        context.collection = {
-          name: page.name,
-          description: page.description,
-          cover: `/${pathSettings.staticAssetPath}/splash/${page.splash}`,
-          sublinks: [],
-        };
+      context.collection = {
+        name: page.name,
+        description: page.description,
+        cover: `/${pathSettings.staticAssetPath}/splash/${page.splash}`,
+        sublinks: [],
+      };
 
-        const pageNumber = page.pageNumber;
-        const totalPages = page.pageCount;
-        const baseUrl = page.baseSlug;
-        let buttons =
-          totalPages === 2
-            ? [1, 2]
-            : [
-                1,
-                Math.min(Math.max(pageNumber, 2), totalPages - 1),
-                totalPages,
-              ];
-        context.paginator =
-          totalPages > 1
-            ? {
-                previous:
-                  pageNumber > 1
-                    ? {
-                        url: `${baseUrl}/p/${pageNumber - 1}`,
-                        label: 'Previous',
-                      }
-                    : null,
-                pages: buttons.map(buttonNumber => ({
-                  label: buttonNumber,
-                  url: `${baseUrl}/p/${buttonNumber}`,
-                  current: buttonNumber === pageNumber,
-                })),
-                next:
-                  pageNumber < totalPages
-                    ? {
-                        url: `${baseUrl}/p/${pageNumber + 1}`,
-                        label: 'Next',
-                      }
-                    : null,
-              }
-            : null;
+      const pageNumber = page.pageNumber;
+      const totalPages = page.pageCount;
+      const baseUrl = page.baseSlug;
+      let buttons =
+        totalPages === 2
+          ? [1, 2]
+          : [1, Math.min(Math.max(pageNumber, 2), totalPages - 1), totalPages];
+      context.paginator =
+        totalPages > 1
+          ? {
+              previous:
+                pageNumber > 1
+                  ? {
+                      url: `${baseUrl}/p/${pageNumber - 1}`,
+                      label: 'Previous',
+                    }
+                  : null,
+              pages: buttons.map(buttonNumber => ({
+                label: buttonNumber,
+                url: `${baseUrl}/p/${buttonNumber}`,
+                current: buttonNumber === pageNumber,
+              })),
+              next:
+                pageNumber < totalPages
+                  ? {
+                      url: `${baseUrl}/p/${pageNumber + 1}`,
+                      label: 'Next',
+                    }
+                  : null,
+            }
+          : null;
 
-        context.collectionItems = PreviewSerializer.serializeArray(
-          page.collections.toArray(),
-          { type: 'collection' }
-        );
+      context.collectionItems = page.collections.flatMap(
+        collection => collection.preview
+      );
 
-        context.structuredData = Schemer.generateListingData({
-          title: page.name,
-          slug: page.slug,
-          items: context.collectionItems,
-        });
+      context.structuredData = Schemer.generateListingData({
+        title: page.name,
+        slug: page.slug,
+        items: context.collectionItems,
+      });
 
-        return context;
-      },
+      return context;
+    },
   },
   cacheProperties: ['baseSlug'],
 };
