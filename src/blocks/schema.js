3a7a404e810e05606f0f70fc82d9b73448bfa3ca
Angelos Chalaris <chalarangelo@gmail.com>
2023-10-01 12:28:39 +0300

    Add imports and exports to schema

--- src/blocks/schema.js
+++ src/blocks/schema.js
@@ -1,5 +1,37 @@
-export const schema = {
-  name: 'WebData',
+import { author } from '#blocks/models/author';
+import { collection } from '#blocks/models/collection';
+import { language } from '#blocks/models/language';
+import { snippet } from '#blocks/models/snippet';
+
+import { collectionPage } from '#blocks/models/pages/collectionPage';
+import { collectionsPage } from '#blocks/models/pages/collectionsPage';
+import { homePage } from '#blocks/models/pages/homePage';
+import { snippetPage } from '#blocks/models/pages/snippetPage';
+
+import { pageSerializer } from '#blocks/serializers/pageSerializer';
+import { previewSerializer } from '#blocks/serializers/previewSerializer';
+import { searchResultSerializer } from '#blocks/serializers/searchResultSerializer';
+import { snippetContextSerializer } from '#blocks/serializers/snippetContextSerializer';
+
+export default {
+  models: [
+    // Main models
+    author,
+    collection,
+    language,
+    snippet,
+    // Page models
+    collectionPage,
+    collectionsPage,
+    homePage,
+    snippetPage,
+  ],
+  serializers: [
+    pageSerializer,
+    previewSerializer,
+    searchResultSerializer,
+    snippetContextSerializer,
+  ],
   relationships: [
     // Main models
     {
