f34e2a385ecc0bdde549ace8fcc7db3de1387a71
Angelos Chalaris <chalarangelo@gmail.com>
2023-10-01 12:39:29 +0300

    Add settings exports

---
+++ src/settings/settings.js
@@ -0,0 +1,15 @@
+import global from './global.js';
+import paths from './paths.js';
+import searchEngine from './searchEngine.js';
+import jsxSnippetPresets from './jsxSnippetPresets.js';
+import presentation from './presentation.js';
+import tags from './tags.js';
+
+export default {
+  global,
+  paths,
+  searchEngine,
+  jsxSnippetPresets,
+  presentation,
+  tags,
+};
