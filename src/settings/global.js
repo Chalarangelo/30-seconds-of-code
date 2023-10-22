93c9b97634e4064a154d2436112b04af5224c14e
Angelos Chalaris <chalarangelo@gmail.com>
2023-10-22 23:09:19 +0300

    Remove Adsense and Analytics settings

--- src/settings/global.js
+++ src/settings/global.js
@@ -8,22 +8,5 @@ export default {
   websiteUrl: 'https://www.30secondsofcode.org',
   githubRepoUrl: 'https://github.com/30-seconds/30-seconds-of-code',
   licenseUrl: 'https://creativecommons.org/licenses/by/4.0/',
-  googleSiteVerification: 'YX9mF-TxoHZGJ9SZ8XwvWgGR_KTcbH1uHul4iDklyr0',
-  googleAnalytics: {
-    id: 'G-75CH3J0T7L',
-    config: {
-      anonymize_ip: true,
-      cookie_expires: 0,
-      send_page_view: true,
-    },
-    consent: {
-      ad_storage: 'denied',
-      ads_data_redaction: 'true',
-      analytics_storage: 'denied',
-    },
-    consentGranted: {
-      analytics_storage: 'granted',
-    },
-  },
   manifestCacheKey: '305c210420234555',
 };
