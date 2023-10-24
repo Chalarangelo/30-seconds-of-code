0d18a408087db6a60a63be7842984d4a0122b365
Angelos Chalaris <chalarangelo@gmail.com>
2023-10-24 23:00:00 +0300

    Change references to new repo URL

--- src/settings/global.js
+++ src/settings/global.js
@@ -6,7 +6,7 @@ export default {
   ownerName: 'Angelos Chalaris',
   ownerUrl: 'https://github.com/Chalarangelo',
   websiteUrl: 'https://www.30secondsofcode.org',
-  githubRepoUrl: 'https://github.com/30-seconds/30-seconds-of-code',
+  githubRepoUrl: 'https://github.com/Chalarangelo/30-seconds-of-code',
   licenseUrl: 'https://creativecommons.org/licenses/by/4.0/',
   manifestCacheKey: '305c210420234555',
 };
