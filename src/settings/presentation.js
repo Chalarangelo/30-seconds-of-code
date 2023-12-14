8d75f66962c79a3b3b10b732b4448394632874db
Angelos Chalaris <chalarangelo@gmail.com>
2023-12-14 23:07:33 +0200

    Change number of cards per page

--- src/settings/presentation.js
+++ src/settings/presentation.js
@@ -1,7 +1,7 @@
 export default {
-  cardsPerPage: 15,
-  newSnippetCards: 5,
-  topSnippetCards: 5,
+  cardsPerPage: 24,
+  newSnippetCards: 6,
+  topSnippetCards: 6,
   topCollectionChips: 8,
   homePageSplashImage: '/assets/splash/work-sunrise.png',
 };
