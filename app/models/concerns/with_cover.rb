3d3fec627fbe806c965a7c733cc78710733d3f3d
Angelos Chalaris <chalarangelo@gmail.com>
2024-06-21 19:19:27 +0300

    Create with_cover concern

---
+++ app/models/concerns/with_cover.rb
@@ -0,0 +1,13 @@
+module WithCover
+  extend ActiveSupport::Concern
+
+  included do
+    delegate :cover_url, :cover_srcset, to: :cover_presenter
+
+    private
+
+    def cover_presenter
+      @cover_presenter ||= CoverPresenter.new(self)
+    end
+  end
+end
