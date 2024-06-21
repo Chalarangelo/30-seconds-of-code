7925f39a14a9b37a04f8f6fe2cd483fbc1fa2642
Angelos Chalaris <chalarangelo@gmail.com>
2024-06-21 19:30:34 +0300

    Update models

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,13 @@
 class Collection < ApplicationRecord
-  # Change primary key to `cid` to make `find` available
-  self.primary_key = :cid
+  include CommonApi
+
+  MAIN_COLLECTION_CID = 'snippets'.freeze
+  MORE_COLLECTIONS_SUBLINK = {
+    title: 'More',
+    url: '/collections/p/1',
+    icon: 'arrow-right',
+    selected: false
+  }.freeze
 
   # https://guides.rubyonrails.org/v5.0/association_basics.html
   has_one :parent,
@@ -14,7 +21,123 @@ class Collection < ApplicationRecord
     primary_key: 'cid',
     inverse_of: :parent
 
-  has_and_belongs_to_many :snippets,
-    association_foreign_key: 'snippet_cid',
+  has_many :collection_snippets,
     foreign_key: 'collection_cid'
+
+  has_many :snippets,
+    through: :collection_snippets
+
+  scope :with_parent, -> { where.not(parent_cid: nil) }
+  scope :primary, -> { where(top_level: true) }
+  scope :secondary, -> { with_parent }
+  scope :listed, -> { where(featured: true) }
+  scope :featured, -> { listed.order('featured_index asc') }
+
+  def self.main
+    find(MAIN_COLLECTION_CID)
+  end
+
+  def has_parent?
+    parent.present?
+  end
+
+  def is_main?
+    cid == MAIN_COLLECTION_CID
+  end
+
+  def is_primary?
+    top_level?
+  end
+
+  def is_secondary?
+    has_parent?
+  end
+
+  def root_url
+    has_parent? ? parent.slug : slug
+  end
+
+  def siblings
+    has_parent? ? parent.children : []
+  end
+
+  def siblings_except_self
+    siblings - [self]
+  end
+
+  def is_searchable
+    featured?
+  end
+
+  def search_tokens_array
+    _tokens.split(';')
+  end
+
+  def first_page_slug
+    "#{slug}/p/1"
+  end
+
+  def page_count
+    # TODO:  cardsPerPage setting
+    # TODO: Extract the listed snippets count from below
+    (snippets.count / 24.0).ceil
+  end
+
+  def listed_snippets
+    collection_snippets.
+      published.
+      listed.
+      by_position.
+      joins(:snippet).
+      # merge(Snippet.published).
+      # where.not(collection_snippets: { position: -1 }).
+      # order('collection_snippets.position asc').
+      preload(:snippet).
+      map(&:snippet)
+  end
+
+  def formatted_snippet_count
+    count = collection_snippets.published.listed.count
+    "#{count} snippets"
+  end
+
+  def formatted_description
+    description.strip_html_paragraphs_and_links
+  end
+
+  # TODO: Extract into a presenter ideally
+  def sublinks
+    if is_main?
+      return Collection.
+              primary.
+              ranked.
+              map(&:to_sublink).
+              flatten.
+              append(MORE_COLLECTIONS_SUBLINK)
+    end
+
+    return [] if !is_primary? && !has_parent?
+    return [] if is_primary? && children.empty?
+
+    (has_parent? ? siblings : children).map do |link|
+      link.to_sublink(cid)
+    end.prepend({
+      title: 'All',
+      url: "#{root_url}/p/1",
+      selected: is_primary?
+    })
+  end
+
+  def to_sublink(collection_cid = nil)
+    {
+      title: mini_name,
+      url: first_page_slug,
+      selected: collection_cid == cid
+    }
+  end
+
+  # TODO: A little fiddly
+  def matches_tag(tag)
+    cid.end_with?("/#{tag}")
+  end
 end
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,30 +1,170 @@
 class Snippet < ApplicationRecord
-  # Change primary key to `cid` to make `find` available
-  self.primary_key = :cid
+  # TODO: Might want to index the ranking column
+  include CommonApi
+
+  ARTICLE_MINI_PREVIEW_TAG = 'Article'
+  GITHUB_URL_PREFIX = 'https://github.com/Chalarangelo/30-seconds-of-code/blob/master/content/snippets'
 
   # Relationships
   has_one :language, primary_key: 'language_cid', foreign_key: 'cid'
-  has_and_belongs_to_many :collections,
-    association_foreign_key: 'collection_cid',
+
+  has_many :collection_snippets,
+    class_name: 'CollectionSnippet',
     foreign_key: 'snippet_cid'
+
+  has_many :collections,
+    through: :collection_snippets
 
   # Prevent certain attributes from getting printed in the console
   self.filter_attributes += [
     :description_html,
     :full_description_html,
-    :full_text
+    :full_text,
+    :table_of_contents_html
   ]
 
   # TODO: Rails 7.2 will introduce this, check later
   # self.attributes_for_inspect = [:id, :cid]
 
-  scope :all_by_new, -> { order('date_modified desc') }
+  scope :by_new, -> { order('date_modified desc') }
 
-  def self.[](cid)
-    where(cid: cid).first
+  scope :unlisted, -> { where(listed: false) }
+  scope :listed, -> { where(listed: true) }
+
+  # We explicitly name the model here to avoid conflicts with the
+  # `CollectionSnippet` model having a column of the same name.
+  scope :published, -> do
+     where(snippets: { date_modified: ..Date.today })
+  end
+  scope :scheduled, -> do
+    where.not(snippets: { date_modified: ..Date.today })
   end
 
+  scope :with_language, ->(language) { where(language_cid: language) }
+  scope :with_tag, ->(tag) do
+    where(
+      '_tags like ? or _tags like ? or _tags like ? or _tags like ?',
+      "#{tag};%",
+      "%;#{tag};%",
+      "%;#{tag}",
+      "#{tag}"
+    )
+  end
+
+  # memo :tags, :seo_description, :seo_title, :primary_tag, :formatted_primary_tag,
+  #       :formatted_mini_preview_tag, :formatted_tags, :formatted_preview_tags,
+  #       :formatted_description, :slug, :is_scheduled?, :is_published?, :is_listed?,
+  #       :date_formatted, :date_machine_formatted, :search_tokens_array,
+  #       :search_tokens, :has_collection? #, :preview
+
+
   def tags
-    @tags ||= _tags.split(';')
+    _tags.split(';')
+  end
+
+  def has_language?
+    language.present?
+  end
+
+  def seo_title
+    reutrn title unless has_language?
+
+    title_language =
+      if language_cid == 'javascript' && primary_tag === 'node'
+        formatted_primary_tag
+      else
+        language.name
+      end
+
+    title.include?(title_language) ? title : "#{title_language} - #{title}"
+  end
+
+  def primary_tag
+    tags.first
+  end
+
+  def formatted_primary_tag
+    TagFormatter.format(primary_tag)
+  end
+
+  # Used for snippet previews in search autocomplete
+  def formatted_mini_preview_tag
+    language.name if has_language?
+
+    ARTICLE_MINI_PREVIEW_TAG
+  end
+
+  def formatted_tags
+    tags.map { |tag| TagFormatter.format(tag) }
+    tags.prepend(language.name) if has_language?
+    tags.join(', ')
+  end
+
+  def formatted_preview_tags
+    language.name if has_language?
+
+    formatted_primary_tag
+  end
+
+  def formatted_description
+    description_html.strip_html_paragraphs_and_links
+  end
+
+  def github_url
+    "#{GITHUB_URL_PREFIX}/#{file_name}"
+  end
+
+  def is_scheduled?
+    date_modified > Date.today
+  end
+
+  def is_published?
+    !is_scheduled?
+  end
+
+  def is_listed?
+    listed && is_published?
+  end
+
+  def date_formatted
+    date_modified.strftime('%B %d, %Y')
+  end
+
+  def date_machine_formatted
+    date_modified.strftime('%Y-%m-%d')
+  end
+
+  def search_tokens_array
+    tokenizable_elements = [
+      file_name.slice(0..-4),
+      *tags,
+      *_tokens.split(';'),
+      *title.normalized_tokens,
+      language&.short&.downcase,
+      language&.long&.downcase,
+    ].compact.uniq
+  end
+
+  def has_collection?
+    collections.present?
+  end
+
+  def breadcrumbs
+    breadcrumbs_presenter.breadcrumbs
+  end
+
+  def recommended_collection
+    breadcrumbs_presenter.recommended_collection
+  end
+
+  # recommended_snippets
+
+  # preview
+
+  private
+
+  # TODO: This will most likely cause N+1, needs a preload on a higher level.
+  def breadcrumbs_presenter
+    @breadcrumbs_presenter ||= BreadcrumbPresenter.new(self)
   end
 end
