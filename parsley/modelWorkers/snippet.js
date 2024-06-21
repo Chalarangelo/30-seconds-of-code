0d54bfae598dd5170d04c7ed37ea37954581106e
Angelos Chalaris <chalarangelo@gmail.com>
2024-06-21 19:13:17 +0300

    Optimize snippet parsing

--- parsley/modelWorkers/snippet.js
+++ parsley/modelWorkers/snippet.js
@@ -1,4 +1,4 @@
-import { snippetPrefix, mdCodeFence } from '../config.js';
+import { snippetPrefix } from '../config.js';
 import { FileHandler } from '../fileHandler.js';
 import { MarkdownParser } from '../markdownParser/markdownParser.js';
 import { stripMarkdownFormat } from '#utils';
@@ -9,79 +9,61 @@ import { TocReader } from '../tocReader.js';
 export const extractSnippetData = async (snippetGlob, languageData) => {
   const snipppetData = await FileHandler.read(snippetGlob);
 
-  return snipppetData.map(snippet => {
-    const {
-      filePath,
-      fileName,
-      title,
-      shortTitle = title,
-      tags: rawTags,
-      type = 'snippet',
-      language: languageKey,
-      excerpt,
-      cover,
-      dateModified,
-      body,
-      unlisted,
-    } = snippet;
+  return await Promise.all(
+    snipppetData.map(async snippet => {
+      const {
+        filePath,
+        fileName,
+        title,
+        shortTitle = title,
+        tags,
+        language: languageKey,
+        body: fullText,
+        // TODO: Add an excerpt everywhere and get rid of this.
+        excerpt = fullText.replace(/\r\n/g, '\n').split('\n\n')[0],
+        cover,
+        dateModified,
+        unlisted,
+      } = snippet;
 
-    const language = languageData.get(languageKey) || undefined;
-    const id = filePath.replace(snippetPrefix, '').slice(0, -3);
-    const tags = rawTags.join(';').toLowerCase();
+      const language = languageData.get(languageKey);
+      const id = filePath.replace(snippetPrefix, '').slice(0, -3);
+      const shortText = excerpt.trim();
 
-    const bodyText = body
-      .slice(0, body.indexOf(mdCodeFence))
-      .replace(/\r\n/g, '\n');
-    const shortText =
-      excerpt?.trim() || bodyText.slice(0, bodyText.indexOf('\n\n'));
+      const [descriptionHtml, fullDescriptionHtml] = await Promise.all([
+        MarkdownParser.parse(shortText, language?.short),
+        MarkdownParser.parse(fullText, language?.short),
+      ]);
 
-    const fullText = body;
-
-    const { descriptionHtml, fullDescriptionHtml } =
-      MarkdownParser.parseSegments(
-        {
-          fullDescription: fullText,
-          description: shortText,
-        },
-        language?.short
+      const tokens = tokenize(stripMarkdownFormat(`${shortText} ${title}`));
+      const ranking = Ranker.rankIndexableContent(
+        [title, ...tags, language?.long, fullText, shortText]
+          .filter(Boolean)
+          .join(' ')
+          .toLowerCase()
       );
+      const tableOfContentsHtml = TocReader.readToC(fullDescriptionHtml) || '';
 
-    const tokens = tokenize(stripMarkdownFormat(`${shortText} ${title}`)).join(
-      ';'
-    );
-    const indexableContent = [
-      title,
-      ...rawTags,
-      language?.long,
-      fullText,
-      shortText,
-    ]
-      .filter(Boolean)
-      .join(' ')
-      .toLowerCase();
-    const ranking = Ranker.rankIndexableContent(indexableContent);
-    const tableOfContentsHtml = TocReader.readToC(fullDescriptionHtml) || '';
-
-    return {
-      id,
-      fileName,
-      title,
-      tags,
-      shortTitle,
-      dateModified,
-      listed: unlisted === true ? false : true,
-      ctype: type,
-      shortText,
-      fullText,
-      descriptionHtml,
-      fullDescriptionHtml,
-      tableOfContentsHtml,
-      cover,
-      languageKey,
-      tokens,
-      ranking,
-    };
-  });
+      return {
+        id,
+        fileName,
+        title,
+        tags: tags.join(';').toLowerCase(),
+        shortTitle,
+        dateModified,
+        listed: unlisted === true ? false : true,
+        shortText,
+        fullText,
+        descriptionHtml,
+        fullDescriptionHtml,
+        tableOfContentsHtml,
+        cover,
+        languageKey,
+        tokens: tokens.join(';'),
+        ranking,
+      };
+    })
+  );
 };
 
 export const exportSnippetData = snippetData => {
