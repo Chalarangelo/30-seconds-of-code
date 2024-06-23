cc6584acf75b94b4caa93a9755e1e8b019a31cef
Angelos Chalaris <chalarangelo@gmail.com>
2024-06-23 18:48:28 +0300

    Create settings

---
+++ config/settings/defaults.rb
@@ -0,0 +1,17 @@
+Orbit::settings website: {
+  name: '30 seconds of code',
+  short_name: '30s',
+  url: 'https://www.30secondsofcode.org',
+  description: 'Short code snippets for all your development needs'
+}
+
+Orbit::settings owner: {
+  name: 'Angelos Chalaris',
+  url: 'https://github.com/Chalarangelo'
+}
+
+Orbit::settings repository_url: 'https://github.com/Chalarangelo/30-seconds-of-code'
+
+Orbit::settings license_url: 'https://creativecommons.org/licenses/by/4.0/'
+
+Orbit::settings manifest_cache_key: '30swp20231218115417'
