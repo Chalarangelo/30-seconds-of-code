6105941fdc641e944c3ab88eca5583eab0c0b9e9
Angelos Chalaris <chalarangelo@gmail.com>
2024-06-23 18:48:28 +0300

    Create settings

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,15 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module Orbit
+  def self.settings(opts = nil)
+    @settings ||= {}
+
+    return @settings if opts.nil?
+
+    @settings.merge!(opts)
+    @settings
+  end
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
