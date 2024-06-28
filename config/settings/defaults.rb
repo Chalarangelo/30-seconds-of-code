7ba827494a5a2984c91ce961c4c1011994459eec
Angelos Chalaris <chalarangelo@gmail.com>
2024-06-28 19:25:41 +0300

    Add presentation settings

--- config/settings/defaults.rb
+++ config/settings/defaults.rb
@@ -17,3 +17,9 @@ Orbit::settings license_url: 'https://creativecommons.org/licenses/by/4.0/'
 Orbit::settings manifest_cache_key: '30swp20231218115417'
 
 Orbit::settings cards_per_page: 24.0
+Orbit::settings collection_cards_per_page: 12.0
+Orbit::settings new_snippet_cards: 6.0
+Orbit::settings top_snippet_cards: 6.0
+Orbit::settings top_collection_chips: 8.0
+
+Orbit::settings home_cover: 'work-sunrise'
