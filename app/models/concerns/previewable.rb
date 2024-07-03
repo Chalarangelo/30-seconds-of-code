734a39362a3fd214e3b0c6c768ec2af06f1aff70
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-03 22:38:38 +0300

    Add previewable concern

---
+++ app/models/concerns/previewable.rb
@@ -0,0 +1,27 @@
+module Previewable
+  extend ActiveSupport::Concern
+
+  included do
+    # Define a class method to store previews.
+    self.instance_variable_set(:@previews, {})
+
+    # Define a class method to prepare previews.
+    def self.prepare_previews
+      self.instance_variable_set(:@previews, all.map do |record|
+        [record.cid, record.serialize_as(:preview)]
+      end.to_h)
+    end
+
+    # Define a class method to get a preview by cid.
+    def self.get_preview(cid)
+      self.instance_variable_get(:@previews)[cid]
+    end
+
+    # Define an instance method to get a preview.
+    def preview
+      return @preview if defined?(@preview)
+
+      @preview ||= self.class.get_preview(cid) || serialize_as(:preview)
+    end
+  end
+end
