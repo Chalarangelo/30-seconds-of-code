7ed1c28dd0df3de94f4a98d235869bd6554f5e7e
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-10 21:18:55 +0300

    Add RSS feed generation

---
+++ app/controllers/feed_controller.rb
@@ -0,0 +1,16 @@
+class FeedController < ApplicationController
+  attr_accessor :nodes
+
+  def index
+    @nodes = nodes
+  end
+
+  def generate_feed
+    feed = render_to_string 'feed/index', layout: false, locals: { nodes: nodes }
+    File.write('public/feed.xml', feed)
+  end
+
+  def nodes
+    Snippet.listed.by_new.first(50)
+  end
+end
