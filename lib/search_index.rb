fe024eb0a4c0a3268c3dc53319074d484f7383d7
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-10 21:24:19 +0300

    Tidy up search and tags

--- lib/search_index.rb
+++ lib/search_index.rb
@@ -1,9 +1,15 @@
 class SearchIndex
   def self.generate
-    snippets = Snippet.listed.preload(:language, :collections)
-    # FIXME: There's a N+1 here!
-    collections = Collection.listed
+    snippets =
+      Snippet.listed.preload(:language, :collections)
+    collections =
+      Collection.listed.preload(:collection_snippets, :parent, :children, snippets: [:language])
 
     search_index = SearchResultSerializer.serialize_array(snippets + collections)
+
+    File.write(
+      'public/search-data.json',
+      JSON.pretty_generate(search_index)
+    )
   end
 end
