031f77cd11e4e02d5dccf588fe948b811b1d61b0
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-10 21:19:06 +0300

    Add sitemap generation

---
+++ app/controllers/sitemap_controller.rb
@@ -0,0 +1,25 @@
+class SitemapController < ApplicationController
+  HOME_PAGE_URL = "#{Orbit::settings[:website][:url]}/".freeze
+  STATIC_PAGE_URLS = [
+    "#{Orbit::settings[:website][:url]}/about",
+    "#{Orbit::settings[:website][:url]}/faw",
+  ].freeze
+
+  attr_accessor :urls
+
+  def index
+    @urls = urls
+  end
+
+  def generate_sitemap
+    sitemap = render_to_string 'sitemap/index', layout: false, locals: { urls: urls }
+    File.write('public/sitemap.xml', sitemap)
+  end
+
+  def urls
+    [HOME_PAGE_URL] +
+      Snippet.published.map(&:full_url) +
+      Collection.all.map(&:all_page_full_urls).flatten +
+      STATIC_PAGE_URLS
+  end
+end
