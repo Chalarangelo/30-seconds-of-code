69e033d91c118a8934da966fbbd34638538e04ad
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-10 21:27:51 +0300

    Extract settings for paths

--- lib/json_importer.rb
+++ lib/json_importer.rb
@@ -1,9 +1,7 @@
 class JsonImporter
-  CONTENT_JSON_PATH = './.content/content_new.json'
-
   # bin/parsley; rake db:migrate:reset; rake import:content
   def self.import
-    data = JSON.load_file(CONTENT_JSON_PATH).deep_symbolize_keys
+    data = JSON.load_file(Orbit::settings[:content_import_path]).deep_symbolize_keys
     data.each do |key, value|
       model = key.to_s.singularize.camelize.constantize
       model.upsert_all(value)
