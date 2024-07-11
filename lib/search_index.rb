0c8a254b6841fa9674768fc7d55cf7160c20dddf
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-11 23:01:30 +0300

    Sort search index

--- lib/search_index.rb
+++ lib/search_index.rb
@@ -1,11 +1,21 @@
 class SearchIndex
   def self.generate
     snippets =
-      Snippet.listed.preload(:language, :collections)
+      Snippet.
+        listed.
+        preload(:language, :collections).
+        sort_by(&:cid)
     collections =
-      Collection.listed.preload(:collection_snippets, :parent, :children, snippets: [:language])
+      Collection.
+        listed.
+        preload(:collection_snippets, :parent, :children, snippets: [:language]).
+        sort_by(&:cid)
 
-    search_index = SearchResultSerializer.serialize_array(snippets + collections)
+    search_index =
+      { search_index: SearchResultSerializer.serialize_array(snippets + collections) }.
+        transform_keys! do |key|
+          key.to_s.camelize(:lower)
+        end
 
     File.write(
       'public/search-data.json',
