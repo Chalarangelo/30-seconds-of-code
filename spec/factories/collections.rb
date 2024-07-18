380cca22c71cfa95f0bf38f9495cf2b1bc02c0a1
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-18 21:56:47 +0300

    Update collections factory

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -8,11 +8,19 @@ FactoryBot.define do
     short_description { 'My collection short description.' }
     listed { true }
     featured_index { 1 }
-    top_level { true }
+    top_level { false }
     parent_cid { nil }
     cover { 'cover' }
     _tokens { 'my;collection;short;description' }
     ranking { 0.65 }
+
+    trait :main do
+      cid { 'snippets' }
+    end
+
+    trait :collections do
+      cid { 'collections' }
+    end
 
     trait :listed do
       listed { true }
@@ -30,11 +38,16 @@ FactoryBot.define do
       featured_index { nil }
     end
 
-    trait :top_level do
+    trait :primary do
       top_level { true }
     end
 
-    trait :not_top_level do
+    trait :secondary do
+      before(:create) do |collection|
+        create(:collection, cid: 'parent-collection')
+      end
+
+      parent_cid { 'parent-collection' }
       top_level { false }
     end
 
