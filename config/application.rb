f2860ee49a6c942040c71a10b0b77917f7f83cb2
Angelos Chalaris <chalarangelo@gmail.com>
2024-07-25 18:50:18 +0300

    Fix extensions loading

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Orbit
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w(assets tasks core_extensions))
 
     # Configuration for the application, engines, and railties goes here.
     #
