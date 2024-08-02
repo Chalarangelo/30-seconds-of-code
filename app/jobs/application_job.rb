d23117d9815bb4a434efa3730fd523a63ebfee38
Angelos Chalaris <chalarangelo@gmail.com>
2024-08-02 15:53:26 +0300

    Remove jobs

--- app/jobs/application_job.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
-end
