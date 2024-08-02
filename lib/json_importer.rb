ce67c9f7c2595000ca278b45757665daed78de0a
Angelos Chalaris <chalarangelo@gmail.com>
2024-08-02 15:55:28 +0300

    Port libs to JS

--- lib/json_importer.rb
+++
@@ -1,9 +0,0 @@
-class JsonImporter
-  def self.import
-    data = JSON.load_file(Orbit::settings[:content_import_path]).deep_symbolize_keys
-    data.each do |key, value|
-      model = key.to_s.singularize.camelize.constantize
-      model.upsert_all(value)
-    end
-  end
-end
