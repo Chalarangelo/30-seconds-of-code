72f3a07fef09baad5d16fcc27bc9befbb78909bc
Angelos Chalaris <chalarangelo@gmail.com>
2024-08-05 16:06:00 +0300

    Add MD support for collections' `description` field

--- src/lib/contentUtils/modelWorkers/collection.js
+++ src/lib/contentUtils/modelWorkers/collection.js
@@ -1,62 +1,70 @@
 /* eslint-disable no-unused-vars */
 import FileHandler from '#src/lib/contentUtils/fileHandler.js';
+import MarkdownParser from '#src/lib/contentUtils/markdownParser/markdownParser.js';
 import tokenize from '#src/lib/search/search.js';
 import Ranker from '#src/lib/contentUtils/ranker.js';
+import StringUtils from '#src/lib/stringUtils.js';
 
 export const extractCollectionData = async (collectionGlob, hub) => {
   const { featuredListings } = hub;
   const collections = await FileHandler.read(collectionGlob);
 
-  return [...collections, hub].map(config => {
-    const {
-      snippetIds = [],
-      slug: id,
-      title,
-      shortTitle = title,
-      miniTitle = shortTitle,
-      description,
-      shortDescription,
-      topLevel = false,
-      allowUnlisted = false,
-      splash,
-      listed = false,
-      parent = null,
-      languageMatcher,
-      tagMatcher,
-    } = config;
+  return await Promise.all(
+    [...collections, hub].map(async config => {
+      const {
+        snippetIds = [],
+        slug: id,
+        title,
+        shortTitle = title,
+        miniTitle = shortTitle,
+        description,
+        shortDescription,
+        topLevel = false,
+        allowUnlisted = false,
+        splash,
+        listed = false,
+        parent = null,
+        languageMatcher,
+        tagMatcher,
+      } = config;
 
-    const tokens = tokenize(`${shortDescription || ''} ${title}`).join(';');
-    const indexableContent = [title, description, shortDescription]
-      .filter(Boolean)
-      .join(' ')
-      .toLowerCase();
-    const ranking = Ranker.rankIndexableContent(indexableContent);
-    const featuredIndex = featuredListings.includes(id)
-      ? featuredListings.indexOf(id)
-      : null;
+      const descriptionHtml = await MarkdownParser.parse(description);
+      const tokens = tokenize(
+        StringUtils.stripMarkdown(`${shortDescription} ${title}`)
+      ).join(';');
+      const indexableContent = [title, description, shortDescription]
+        .filter(Boolean)
+        .join(' ')
+        .toLowerCase();
+      const ranking = Ranker.rankIndexableContent(indexableContent);
+      const featuredIndex = featuredListings.includes(id)
+        ? featuredListings.indexOf(id)
+        : null;
 
-    return {
-      id,
-      title,
-      shortTitle,
-      miniTitle,
-      listed,
-      featuredIndex,
-      splash,
-      description,
-      shortDescription,
-      topLevel,
-      allowUnlisted,
-      parent,
-      tokens,
-      matchers: {
-        language: languageMatcher,
-        tag: tagMatcher,
-      },
-      snippetIds,
-      ranking,
-    };
-  });
+      return {
+        id,
+        title,
+        shortTitle,
+        miniTitle,
+        listed,
+        featuredIndex,
+        splash,
+        description,
+        descriptionHtml,
+        shortDescription,
+        topLevel,
+        allowUnlisted,
+        parent,
+        tokens,
+        matchers: {
+          language: languageMatcher,
+          tag: tagMatcher,
+        },
+        snippetIds,
+        ranking,
+      };
+    })
+  );
 };
 
 export const exportCollectionData = collectionData => {
@@ -71,7 +79,7 @@ export const exportCollectionData = collectionData => {
       listed: collection.listed,
       featuredIndex: collection.featuredIndex,
       cover: collection.splash,
-      content: collection.description,
+      content: collection.descriptionHtml,
       description: collection.shortDescription,
       topLevel: collection.topLevel,
       parentId: collection.parent,
