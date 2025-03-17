b8d99f9bb2d0399b0794a6bb523b273b48d08e06
Angelos Chalaris <chalarangelo@gmail.com>
2025-03-17 22:35:44 +0200

    Make DocumentIndex just a storage solution

--- src/lib/search/documentIndex.js
+++ src/lib/search/documentIndex.js
@@ -1,13 +1,10 @@
-import { tokenize } from '#src/lib/search/server.js';
-
 export default class DocumentIndex {
   constructor(documents) {
     this.documents = new Map();
     this.invertedIndex = new Map();
 
-    if (documents) {
+    if (documents)
       documents.forEach(({ id, content }) => this.addDocument(id, content));
-    }
   }
 
   get terms() {
@@ -15,8 +12,6 @@ export default class DocumentIndex {
   }
 
   addDocument(docId, terms) {
-    // const terms = tokenize(content);
-
     // Store original document
     this.documents.set(docId, { terms, length: terms.length });
 
@@ -32,49 +27,5 @@ export default class DocumentIndex {
     });
 
     return docId;
-  }
-
-  search(query, limit = null) {
-    let scores = this.searchForTerms(tokenize({ text: query }));
-    if (!scores.size) this.searchForTerms(tokenize({ tokens: query }));
-
-    // Sort documents by score
-    const results = Array.from(scores.entries())
-      .sort((a, b) => b[1] - a[1])
-      .map(([docId, score]) => ({
-        id: docId,
-        score: parseFloat(score.toFixed(5)),
-      }));
-
-    // Limit results if requested
-    if (limit) return results.slice(0, limit);
-    return results;
-  }
-
-  searchForTerms(terms) {
-    const scores = new Map();
-
-    // Calculate scores for each document
-    this.documents.forEach((doc, docId) => {
-      let score = 0;
-
-      terms.forEach(term => {
-        if (this.invertedIndex.has(term)) {
-          const indexEntry = this.invertedIndex.get(term).get(docId);
-          if (indexEntry) {
-            // Calculate TF-IDF score for this term in document
-            const tf = indexEntry / doc.length;
-            const idf = Math.log(
-              this.documents.size / this.invertedIndex.get(term).size
-            );
-            score += tf * idf;
-          }
-        }
-      });
-
-      if (score > 0) scores.set(docId, score);
-    });
-
-    return scores;
   }
 }
