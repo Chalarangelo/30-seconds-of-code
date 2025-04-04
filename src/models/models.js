688137e859e6befd94e837022e510f3216d3f9c5
Angelos Chalaris <chalarangelo@gmail.com>
2025-04-04 17:56:51 +0300

    Expose ContentModel in models

--- src/models/models.js
+++ src/models/models.js
@@ -1,6 +1,13 @@
+import ContentModel from '#src/models/contentModel.js';
 import Collection from '#src/models/collection.js';
 import CollectionSnippet from '#src/models/collectionSnippet.js';
 import Language from '#src/models/language.js';
 import Snippet from '#src/models/snippet.js';
 
-export default { Collection, CollectionSnippet, Language, Snippet };
+export default {
+  ContentModel,
+  Collection,
+  CollectionSnippet,
+  Language,
+  Snippet,
+};
