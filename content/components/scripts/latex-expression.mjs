77c59bf72ca0caaf63b030745fa4cf9fee210042
Angelos Chalaris <chalarangelo@gmail.com>
2025-04-08 18:04:40 +0300

    Add experimental support for LaTex
    Via KaTex, loaded as ESM. More info here:
    https://katex.org/docs/browser#ecmascript-module

---
+++ content/components/scripts/latex-expression.mjs
@@ -0,0 +1,34 @@
+import katex from 'https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.mjs';
+import katexStyles from 'https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.css' with { type: 'css' };
+import styles from '../styles/latex-expression.css' with { type: 'css' };
+
+class LatexExpression extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    try {
+      const tex = this.innerText;
+      const rendered = katex.renderToString(tex, {
+        displayMode: true,
+        output: 'html',
+        throwOnError: true,
+      });
+      const figure = document.createElement('figure');
+      figure.innerHTML = rendered;
+      figure.setAttribute('aria-hidden', 'true');
+      this.innerHTML = '';
+      this.appendChild(figure);
+      this.setAttribute('interactive', 'true');
+    } catch {
+      this.setAttribute('aria-hidden', 'true');
+      this.setAttribute('interactive', 'error');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.adoptedStyleSheets.push(katexStyles, styles);
+  customElements.define('latex-expression', LatexExpression);
+});
